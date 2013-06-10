dcf6ef44f531e354a78289805ccbc7d4f2443891
David Geo Holmes <geometryzen@gmail.com>
2013-06-10 14:56:12 -0700

    Save file.

--- turtle.py
+++ turtle.py
@@ -9,7 +9,7 @@ if (canvas):
 canvas = document.createElement("canvas")
 canvas.id = "canvas"
 canvas.width = 400
-canvas.height = "300"
+canvas.height = 300
 #canvas.setAttribute("id", "canvas")
 container = document.getElementById("canvas-container")
 container.appendChild(canvas)
