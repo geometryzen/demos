348d224af8daae3cfca5240fb257aa12cc9214ec
David Geo Holmes <geometryzen@gmail.com>
2013-06-10 16:01:34 -0700

    Save file.

--- turtle.py
+++ turtle.py
@@ -2,17 +2,21 @@
 from turtle import *
 from browser import *
 
+# Discard the old canvas if it exists.
 canvas = document.getElementById("canvas")
 if (canvas):
     canvas.parentNode.removeChild(canvas)
-    
-canvas = document.createElement("canvas", {"id": "canvas", "width": 400})
-canvas.id = "canvas"
+
+# Create a new canvas, setting the attributes in three ways! 
+canvas = document.createElement("canvas", {"id": "canvas"})
 canvas.width = 400
-canvas.height = 300
-#canvas.setAttribute("id", "canvas")
+canvas.setAttribute("height", 300)
+
+# Append the canvas to the provided container (the content of the widget).
 container = document.getElementById("canvas-container")
 container.appendChild(canvas)
+
+# The turtle code will be happy that it now has a canvas element!
 
 screen = Screen()
 screen.bgcolor("black")
