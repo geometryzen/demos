13f43c1e28995a5c221ebfc5650d83d31ef619a2
David Geo Holmes <geometryzen@gmail.com>
2013-06-10 13:59:51 -0700

    Save file.

--- turtle.py
+++ turtle.py
@@ -1,1 +1,16 @@
 # turtle.py
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("black")
+tess = turtle.Turtle()
+tess.color("yellow")
+tess.shape("turtle")
+
+print(range(5,60,2))
+tess.penup()
+for size in range(5,60,2):
+    tess.stamp()
+    tess.forward(size)
+    tess.right(24)
+
+wn.exitonclick()
