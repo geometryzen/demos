135565c9af3ff5314d35323af11879a94f22a3fa
David Geo Holmes <geometryzen@gmail.com>
2013-06-10 17:00:22 -0700

    Save file.

--- python-range.py
+++ python-range.py
@@ -1,1 +1,10 @@
 # python-range.py
+print range(10)  # => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print range(1)   # => [0]
+
+print range(0)   # => []
+
+print range(-1)  # => []
+
+print range(-10) # => []
