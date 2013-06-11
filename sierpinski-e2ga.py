f1679cbefbdf688d85529da95890713ab2c5bc5d
David Geo Holmes <geometryzen@gmail.com>
2013-06-11 09:09:35 -0700

    Save file.

--- sierpinski-e2ga.py
+++ sierpinski-e2ga.py
@@ -23,7 +23,7 @@ def drawTriangle(points, color, sprite):
     sprite.location = Vector2(points[0][0], points[0][1])
     sprite.down()
     sprite.begin_fill()
-    sprite.goto(points[1][0], points[1][1])
+    sprite.location = Vector2(points[1][0], points[1][1])
     sprite.goto(points[2][0], points[2][1])
     sprite.goto(points[0][0], points[0][1])
     sprite.end_fill()
