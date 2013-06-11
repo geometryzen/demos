c178405a2414c4736ab35a5e14768117e44be52a
David Geo Holmes <geometryzen@gmail.com>
2013-06-10 17:37:22 -0700

    Save file.

--- sierpinski.py
+++ sierpinski.py
@@ -1,5 +1,23 @@
 # sierpinski.py
 import sprite
+from browser import *
+
+# Discard the old canvas if it exists. 
+canvas = document.getElementById("canvas")
+if (canvas):
+    canvas.parentNode.removeChild(canvas)
+
+# Create a new canvas, setting the attributes in three ways! 
+canvas = document.createElement("canvas", {"id": "canvas"})
+canvas.width = 400
+canvas.height = 300
+canvas.setAttribute("height", "300")
+
+# Append the canvas to the provided container (the content of the widget).
+container = document.getElementById("canvas-container")
+container.appendChild(canvas)
+
+# The turtle code will be happy now that it now has a canvas element!
 
 def drawTriangle(points,color,myTurtle):
     myTurtle.fillcolor(color)
