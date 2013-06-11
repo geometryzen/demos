58f84c85781c835c5432d65ad3c475becfda6c5b
David Geo Holmes <geometryzen@gmail.com>
2013-06-10 17:42:07 -0700

    Save file.

--- sierpinski.py
+++ sierpinski.py
@@ -1,5 +1,5 @@
-# sierpinski.py
-import sprite
+# sierpinski.py by Brad Miller
+from sprite import *
 from browser import *
 
 # Discard the old canvas if it exists. 
@@ -19,43 +19,43 @@ container.appendChild(canvas)
 
 # The turtle code will be happy now that it now has a canvas element!
 
-def drawTriangle(points,color,myTurtle):
-    myTurtle.fillcolor(color)
-    myTurtle.up()
-    myTurtle.goto(points[0][0],points[0][1])
-    myTurtle.down()
-    myTurtle.begin_fill()
-    myTurtle.goto(points[1][0],points[1][1])
-    myTurtle.goto(points[2][0],points[2][1])
-    myTurtle.goto(points[0][0],points[0][1])
-    myTurtle.end_fill()
+def drawTriangle(points, color, sprite):
+    sprite.fillcolor(color)
+    sprite.up()
+    sprite.goto(points[0][0],points[0][1])
+    sprite.down()
+    sprite.begin_fill()
+    sprite.goto(points[1][0],points[1][1])
+    sprite.goto(points[2][0],points[2][1])
+    sprite.goto(points[0][0],points[0][1])
+    sprite.end_fill()
 
 def getMid(p1,p2):
     return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2)
 
-def sierpinski(points,degree,myTurtle):
+def sierpinski(points, degree, sprite):
     colormap = ['blue','red','green','white','yellow',
                 'violet','orange']
-    drawTriangle(points,colormap[degree],myTurtle)
+    drawTriangle(points,colormap[degree], sprite)
     if degree > 0:
         sierpinski([points[0],
                         getMid(points[0], points[1]),
                         getMid(points[0], points[2])],
-                   degree-1, myTurtle)
+                   degree-1, sprite)
         sierpinski([points[1],
                         getMid(points[0], points[1]),
                         getMid(points[1], points[2])],
-                   degree-1, myTurtle)
+                   degree-1, sprite)
         sierpinski([points[2],
                         getMid(points[2], points[1]),
                         getMid(points[0], points[2])],
-                   degree-1, myTurtle)
+                   degree-1, sprite)
 
 def main():
-    myTurtle = sprite.Sprite()
-    myWin = sprite.Screen()
-    myPoints = [[-100,-50],[0,100],[100,-50]]
-    sierpinski(myPoints, 3, myTurtle)
-    myWin.exitonclick()
+    sprite = Sprite()
+    screen = Screen()
+    points = [[-100,-50],[0,100],[100,-50]]
+    sierpinski(points, 3, sprite)
+    screen.exitonclick()
 
 main()
