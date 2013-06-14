e6ac2ac182f69dfc6f096b9cd0b42145eaccdc30
David Holmes <david.geo.holmes@gmail.com>
2013-06-13 22:56:58 -0400

    Moved files to examples

--- python-arrays.py
+++
@@ -1,7 +0,0 @@
-# python-arrays.py
-
-# Watch out for the colon on the end of the for line!
-# And the print must be indented.
-for name in ["Alice", "Bob", "Charlie"]:
-    print("Hi, ", name, "!")
-    
