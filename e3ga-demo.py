40d04ee37f3db6d950505d131496285447023d80
David Geo Holmes <geometryzen@gmail.com>
2013-06-17 07:09:39 -0700

    Save file.

--- e3ga-demo.py
+++ e3ga-demo.py
@@ -65,4 +65,11 @@ for a in blades:
     for b in blades:
         showValue(str(a) + " ^ " + str(b), a ^ b)
     print ""
+print ""
+print "Left Contraction Product Table *"
+print "========================="
+for a in blades:
+    for b in blades:
+        showValue(str(a) + " << " + str(b), a << b)
+    print ""
 
