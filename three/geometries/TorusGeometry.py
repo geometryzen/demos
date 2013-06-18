6153294fb5767725cd15150332188e24c06a15e9
David Geo Holmes <geometryzen@gmail.com>
2013-06-18 16:46:31 -0700

    Save file.

--- three/geometries/TorusGeometry.py
+++ three/geometries/TorusGeometry.py
@@ -1,1 +1,82 @@
-#
+# TorusGeometry demonstration.
+from three import *
+# We will control the horizontal. We will control the vertical.
+from browser import *
+from math import pi
+
+# Discard the old canvas if it exists. 
+for canvas in document.getElementsByTagName("canvas"):
+    canvas.parentNode.removeChild(canvas)
+
+scene = Scene()
+
+# Aspect ratio will be reset in onWindowResize
+camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
+camera.position.z = 100
+
+renderer = WebGLRenderer()
+
+container = document.getElementById("canvas-container")
+container.appendChild(renderer.domElement)
+
+# TorusGeometry(...)
+# The arguments and their default values, in order, are...
+radius = 50 # The radius of the sphere
+tube
+radialSegments = 8 # The number of segments in the phi coordinate (min 3)
+tubularSegments = 6 # The number of segments in the theta coordinate (min 2)
+arc = 0
+# All arguments are optional.
+torus = TorusGeometry(radius, tube, radialSegments, tubularSegments, arc)
+
+print repr(torus)
+print "radius:         " + str(torus.radius)
+print "tube:           " + str(torus.tube)
+print "radialSegments: " + str(torus.radialSegments)
+print "tubularSegments:" + str(torus.tubularSegments)
+print "arc:            " + str(torus.arc)
+print torus
+
+mesh = Mesh(torus, MeshNormalMaterial())
+scene.add(mesh)
+
+requestID = None
+progress = None
+progressEnd = 6000
+startTime =  None
+
+def render():
+    mesh.rotation.x = mesh.rotation.x + 0.02
+    mesh.rotation.y = mesh.rotation.y + 0.02
+    mesh.rotation.z = mesh.rotation.z + 0.02
+        
+    renderer.render(scene, camera)
+
+def onWindowResize():
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.size = (window.innerWidth, window.innerHeight)
+    
+def step(timestamp):
+    global requestID, progress, startTime
+    if (startTime):
+        progress = timestamp - startTime
+    else:
+        if (timestamp):
+            startTime = timestamp
+        else:
+            progress = 0
+        
+    if (progress < progressEnd):
+        requestID = window.requestAnimationFrame(step)
+        render()
+    else:
+        window.cancelAnimationFrame(requestID)
+        # container.removeChild(renderer.domElement)
+        # TODO: Remove the "resize" event listener
+
+window.addEventListener("resize", onWindowResize, False)
+
+onWindowResize()
+
+step(None)
