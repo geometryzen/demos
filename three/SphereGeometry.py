2f46ea43e7b31916ffba70d90ecd2a5e3f04b04e
David Geo Holmes <geometryzen@gmail.com>
2013-06-18 14:53:12 -0700

    Save file.

--- three/SphereGeometry.py
+++ three/SphereGeometry.py
@@ -24,7 +24,7 @@ container.appendChild(renderer.domElement)
 radius = 50 # The radius of the sphere
 widthSegments = 8 # The number of segments in the phi coordinate (min 3)
 heightSegments = 6 # The number of segments in the theta coordinate (min 2)
-phiStart = 0.5 * pi
+phiStart = 0
 phiLength = pi
 thetaStart = 0
 thetaLength = pi
