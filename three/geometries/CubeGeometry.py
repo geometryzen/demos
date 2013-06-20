aed8904ccb59e86f60d53c925e2632433eeb0856
David Geo Holmes <geometryzen@gmail.com>
2013-06-20 16:32:26 -0700

    Save file.

--- three/geometries/CubeGeometry.py
+++ three/geometries/CubeGeometry.py
@@ -15,6 +15,7 @@ camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
 camera.position.z = 20
 
 renderer = WebGLRenderer()
+renderer.setClearColor(Color(0x080808), 1.0)
 
 container = document.getElementById("canvas-container")
 container.appendChild(renderer.domElement)
