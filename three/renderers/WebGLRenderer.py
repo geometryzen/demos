f09ecccfd01aa4f89764966ecb3532420aaff893
David Geo Holmes <geometryzen@gmail.com>
2013-06-20 13:46:32 -0700

    Save file.

--- three/renderers/WebGLRenderer.py
+++ three/renderers/WebGLRenderer.py
@@ -30,7 +30,7 @@ print str(renderer)
 container = document.getElementById("canvas-container")
 container.appendChild(renderer.domElement)
 
-sphere = SphereGeometry(50, 16, 12)
+sphere = SphereGeometry(50, 32, 24)
 
 mesh = Mesh(sphere, MeshNormalMaterial())
 scene.add(mesh)
