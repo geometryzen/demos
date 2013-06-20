b765efa42c76d516ec9a76b001a78061d0049572
David Geo Holmes <geometryzen@gmail.com>
2013-06-19 20:25:52 -0700

    Save file.

--- three/math/Color.py
+++ three/math/Color.py
@@ -39,8 +39,8 @@ print "b: " + str(blue.b)
 print blue
 
 colors = [Color("rgb(250,0,0)"),Color("rgb(100%,0%,0%)"), Color("#ff0000"),Color("#f00"),Color("red")]
-for color in colors:
-    print color
+for c in colors:
+    print c
     
 x = Color(color)
 print "x: " + repr(x)
