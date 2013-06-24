edf9abe2a34e8c2f4012bdc8a5257255cae113eb
David Geo Holmes <geometryzen@gmail.com>
2013-06-24 12:39:30 -0700

    Save file.

--- three/math/Vector3.py
+++ three/math/Vector3.py
@@ -1,1 +1,7 @@
-# Vector3.py
+# Vector3 demonstration
+#
+# It is important to realize that Vector3 is mutable.
+# This contrasts to the usual approach of treating mathematical
+# objects as immutable types.
+
+a = Vector3(1,1,1)
