75dc69e691e649f35f3777f0568697880426e098
David Geo Holmes <geometryzen@gmail.com>
2013-06-24 12:44:14 -0700

    Save file.

--- three/math/Vector3.py
+++ three/math/Vector3.py
@@ -1,19 +1,22 @@
 # Vector3 demonstration
 from three import *
-#
-# It is important to realize that Vector3 is mutable.
-# This contrasts to the usual approach of treating mathematical
-# objects as immutable types.
 
 a = Vector3(1,1,1)
+
 print str(a)
 print repr(a)
 print str(type(a))
 print repr(type(a))
+
+#
+# It is important to realize that Vector3 is mutable.
+# This contrasts to the usual approach of treating mathematical
+# objects as immutable types.
 a.x = 2
 a.y = 3
 a.z = 5
 print a
+
 # The string value of the zero vector is just "0"
 print str(Vector3(0,0,0))
 print repr(Vector3(0,0,0))
