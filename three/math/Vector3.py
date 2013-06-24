d9012e70bac9a410c14be51ebf1174e9a00ce065
David Geo Holmes <geometryzen@gmail.com>
2013-06-24 14:04:03 -0700

    Save file.

--- three/math/Vector3.py
+++ three/math/Vector3.py
@@ -38,8 +38,11 @@ print a.getComponent(0)
 print a.getComponent(1)
 print a.getComponent(2)
 
+# Linear Algebra
 # Scalar multiplication.
 i = Vector3(1,0,0)
 j = Vector3(0,1,0)
 print j * 2.0
 print 2.0 * j
+
+print i + j
