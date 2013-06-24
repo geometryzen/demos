2148764b3a9eebabab827e5f926a584f8e2c5c22
David Geo Holmes <geometryzen@gmail.com>
2013-06-24 13:29:36 -0700

    Save file.

--- three/math/Vector3.py
+++ three/math/Vector3.py
@@ -37,3 +37,8 @@ print a
 print a.getComponent(0)
 print a.getComponent(1)
 print a.getComponent(2)
+
+# Multiplication promotes the vectors to Euclidean3 
+i = Vector3(1,0,0)
+j = Vector3(0,1,0)
+print i * j
