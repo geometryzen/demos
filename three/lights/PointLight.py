c3845dcea08d3a5497741930b5fd6f7281637f63
David Geo Holmes <geometryzen@gmail.com>
2013-06-24 11:55:52 -0700

    Save file.

--- three/lights/PointLight.py
+++ three/lights/PointLight.py
@@ -18,9 +18,11 @@ print "rotation:" + str(light.rotation)
 light.color = 0x00FF00
 light.intensity = 0.5
 light.distance = 200
+
 light.position.x = 1
 light.position.y = 2
 light.position.z = 3
+
 light.rotation.x = 4
 light.rotation.y = 5
 light.rotation.z = 6
@@ -29,5 +31,9 @@ print "intensity:" + str(light.intensity)
 print "distance:" + str(light.distance)
 print "position:" + str(light.position)
 print "rotation:" + str(light.rotation)
+
+r = Vector3(2,2,2)
+light.position = r
+print "position:" + str(light.position)
 
 
