f3fe7ca1c542a18c495e323962df788408198b3f
David Geo Holmes <geometryzen@gmail.com>
2013-06-24 06:22:44 -0700

    Save file.

--- python/ComplexNumbers.py
+++ python/ComplexNumbers.py
@@ -1,11 +1,13 @@
 # ComplexNumbers.py
 from cmath import *
 
-z1 = complex(3,5)
+z1 = complex(3, 5)
+z2 = complex(7, 11)
 
 print z1
-
 print str(z1)
 print repr(z1)
+print "----------"
+print z1 + z2
 
 
