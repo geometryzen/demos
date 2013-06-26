3ff46327aa4c52ee3c6b3c4bb0de1c4da47071f2
David Geo Holmes <geometryzen@gmail.com>
2013-06-26 11:39:46 -0700

    Save file.

--- browser/canvas.py
+++ browser/canvas.py
@@ -24,9 +24,7 @@ debugCanvas.height = 512
 debugCanvas.style.position = "absolute"
 debugCanvas.style.top = "0px"
 debugCanvas.style.left = "0px"
-print debugCanvas.style.position
-print debugCanvas.style.top
-print debugCanvas.style.left
+debugContext = debugCanvas.getContext("2d")
 
 # Use Python's dictionary to handle event.keyCode(s).
 def escKey(downFlag):
