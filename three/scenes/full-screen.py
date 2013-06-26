800e29a9c42ae1f199408d9a19a4b2b88a8657ec
David Geo Holmes <geometryzen@gmail.com>
2013-06-25 17:51:50 -0700

    Save file.

--- three/scenes/full-screen.py
+++ three/scenes/full-screen.py
@@ -1,1 +1,60 @@
 # scenes/full-screen.py
+from three import *
+# We will control the horizontal. We will control the vertical.
+from browser import *
+
+container = document.createElement("div")
+document.body.appendChild(container)
+
+scene = Scene()
+
+# Aspect ratio will be reset in onWindowResize
+camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
+camera.position.z = 2
+
+renderer = WebGLRenderer()
+
+view = document.getElementById("view")
+view.parentNode.insertBefore(renderer.domElement, view)
+
+mesh = Mesh(CubeGeometry(1.0, 1.0, 1.0), MeshNormalMaterial())
+scene.add(mesh)
+
+requestID = None
+progress = None
+progressEnd = 20000 # run for 10 seconds
+startTime =  None
+
+def render():
+    mesh.rotation.x = mesh.rotation.x + 0.02
+    mesh.rotation.y = mesh.rotation.y + 0.02
+    mesh.rotation.z = mesh.rotation.z + 0.02
+        
+    renderer.render(scene, camera)
+
+def onWindowResize():
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.size = (window.innerWidth, window.innerHeight)
+    
+def step(timestamp):
+    global requestID, progress, startTime
+    if (startTime):
+        progress = timestamp - startTime
+    else:
+        if (timestamp):
+            startTime = timestamp
+        else:
+            progress = 0
+        
+    if (progress < progressEnd):
+        requestID = window.requestAnimationFrame(step)
+        render()
+    else:
+        window.cancelAnimationFrame(requestID)
+        view.parentNode.removeChild(renderer.domElement)
+
+window.addEventListener("resize", onWindowResize, False)
+onWindowResize()
+
+step(None)
