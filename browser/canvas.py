d7553e2a9f593b050b5c6d5b2d9dd066c86cc027
David Geo Holmes <geometryzen@gmail.com>
2013-06-26 14:30:01 -0700

    Save file.

--- browser/canvas.py
+++ browser/canvas.py
@@ -25,6 +25,8 @@ graph.height = 400
 graph.style.position = "absolute"
 graph.style.top = "0px"
 graph.style.left = "0px"
+graph.style.width = "700px"
+graph.stype.height = "250px"
 context = graph.getContext("2d")
 context.setTransform(1, 0, 0, 1, 200, 200)
 context.strokeStyle = "#808080"
