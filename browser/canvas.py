ca4cc4e8d87a7e89b4d72d4f9cc9a1c897451a03
David Geo Holmes <geometryzen@gmail.com>
2013-06-26 14:18:19 -0700

    Save file.

--- browser/canvas.py
+++ browser/canvas.py
@@ -74,7 +74,7 @@ def onWindowResize():
     else:
         camera.aspect = window.innerWidth / window.innerHeight
         camera.updateProjectionMatrix()
-        renderer.setSize(window.innerWidth, window.innerHeight)
+        renderer.setSize(window.innerWidth, window.innerHeight, False)
     
 def discardExistingCanvas():
     for canvas in document.getElementsByTagName("canvas"):
