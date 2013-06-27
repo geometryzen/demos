0fdd57a7d43793fddaad99b47cd3cf2721c1867c
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 11:54:45 -0700

    Save file.

--- three/materials/MeshLambertMaterial.py
+++ three/materials/MeshLambertMaterial.py
@@ -21,7 +21,7 @@ pointLight.position.set(20, 20, 20)
 scene.add(pointLight)
 
 directionalLight = DirectionalLight(0xFFFFFF)
-directionalLight.position.set(2, 20, 20).normalize()
+directionalLight.position.set(20, 20, 20).normalize()
 scene.add(directionalLight)
 
 renderer = WebGLRenderer()
