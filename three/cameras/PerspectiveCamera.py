d5f19eb7dbe1d0b9781eccfe753e220e8168f3c8
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 12:52:33 -0700

    Save file.

--- three/cameras/PerspectiveCamera.py
+++ three/cameras/PerspectiveCamera.py
@@ -1,71 +1,155 @@
-# PerspectiveCamera demonstration.
+# Under Construction!
+# HTML5 (2D) Canvas coexisting with a WebGL canvas.
 from three import *
+# We will control the horizontal. We will control the vertical.
 from browser import *
 
-# Discard the old canvas if it exists. 
-for canvas in document.getElementsByTagName("canvas"):
-    canvas.parentNode.removeChild(canvas)
+# Convenince variable controlling whether we embed in the existing canvas container
+# or create a nearly full screen canvas
+useLargeCanvas = True
 
+# Variables to track the intentions of the user.
+moveForward = False
+moveBackward = False
+moveLeft = False
+moveRight = False
+
+# Global variables
+camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
+renderer = WebGLRenderer({"antialias": True})
 scene = Scene()
+view = document.getElementById("view")
+graph = document.createElement("canvas")
+graph.style.position = "absolute"
+graph.style.top = "0px"
+graph.style.left = "0px"
+context = graph.getContext("2d")
 
-camera  = PerspectiveCamera(45, 1.0, 0.1, 10000)
-camera.position.set(10, 10, 10)
-camera.lookAt(scene.position)
+# Use Python's dictionary to handle event.keyCode(s).
+def escKey(downFlag):
+    terminate()
 
-ambientLight = AmbientLight(0x111111)
-scene.add(ambientLight)
+def leftArrowKey(downFlag):
+    global moveLeft
+    moveLeft = downFlag
 
-pointLight = PointLight(0xFFFFFF)
-pointLight.position.set(20, 20, 20)
-scene.add(pointLight)
+def upArrowKey(downFlag):
+    global moveForward
+    moveForward = downFlag
+    
+def rightArrowKey(downFlag):
+    global moveRight
+    moveRight = downFlag
 
-directionalLight = DirectionalLight(0xFFFFFF)
-directionalLight.position.set(0, 1, 0)
-scene.add(directionalLight)
+def downArrowKey(downFlag):
+    global moveBackward
+    moveBackward = downFlag
 
-renderer = WebGLRenderer()
-renderer.autoClear   = True
-renderer.gammaInput  = True
-renderer.gammaOutput = True
-renderer.setClearColor(Color(0x080808), 1.0)
+keyHandlers = {
+ 27: escKey,
+ 37: leftArrowKey,
+ 38: upArrowKey,
+ 39: rightArrowKey,
+ 40: downArrowKey
+}
+    
+    
+def onDocumentKeyDown(event):
+    event.preventDefault()
+    keyHandlers[event.keyCode](True)
 
-container = document.getElementById("canvas-container")
-container.appendChild(renderer.domElement)
-
-material = MeshLambertMaterial({"color":0x0000FF})
-material.name = "bluecube"
-
-print "repr(material)    => " + repr(material)
-print "id:                  " + str(material.id)
-print "name:                " + material.name
-print "color:               " + str(material.color)
-print "needsUpdate:         " + str(material.needsUpdate)
-print "opacity:             " + str(material.opacity)
-print "overdraw:            " + str(material.overdraw)
-print "transparent:         " + str(material.transparent)
-print "visible:             " + str(material.visible)
-print "str(material)     => " + str(material)
-
-mesh = Mesh(CubeGeometry(5, 5, 5), material)
-
-scene.add(mesh)
-
-requestID = None
-progress = None
-progressEnd = 6000
-startTime =  None
-
-def render():
-    mesh.rotation.x = mesh.rotation.x + 0.02
-    mesh.rotation.y = mesh.rotation.y + 0.02
-    mesh.rotation.z = mesh.rotation.z + 0.02
-        
-    renderer.render(scene, camera)
+def onDocumentKeyUp(event):
+    event.preventDefault()
+    keyHandlers[event.keyCode](False)
 
 def onWindowResize():
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.size = (window.innerWidth, window.innerHeight)
+    if (useLargeCanvas):
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.size = (window.innerWidth, window.innerHeight)
+        graph.width = window.innerWidth
+        graph.height = window.innerHeight
+        graph.style.width = str(window.innerWidth) + "px"
+        graph.style.height = str(window.innerHeight) + "px"
+    else:
+        container = document.getElementById("canvas-container")
+        camera.aspect = container.clientWidth / container.clientHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(container.clientWidth, container.clientHeight)
+        graph.width = container.clientWidth
+        graph.height = container.clientHeight
+        graph.style.width = str(container.clientWidth) + "px"
+        graph.style.height = str(container.clientHeight) + "px"
+    
+def discardExistingCanvas():
+    for cs in document.getElementsByTagName("canvas"):
+        cs.parentNode.removeChild(cs)
+        
+requestID = None
+progress = None
+progressEnd = 60000
+startTime =  None
+
+def init():
+    discardExistingCanvas()
+    if (useLargeCanvas):
+        container = document.createElement("div")
+        document.body.appendChild(container)
+        view.parentNode.insertBefore(renderer.domElement, view)
+        view.parentNode.insertBefore(graph, view)
+    else:
+        container = document.getElementById("canvas-container")
+        container.appendChild(graph)
+        container.appendChild(renderer.domElement)
+
+    camera.position.z = 2
+
+    mesh = Mesh(CubeGeometry(1.0, 1.0, 1.0), MeshNormalMaterial())
+    scene.add(mesh)
+    
+    document.addEventListener("keydown", onDocumentKeyDown, False)
+    document.addEventListener("keyup", onDocumentKeyUp, False)
+
+    window.addEventListener("resize", onWindowResize, False)
+    onWindowResize()
+
+def render():
+    if (moveForward):
+        camera.position.z -= 0.02
+    if (moveBackward):
+        camera.position.z += 0.02
+    if (moveLeft):
+        camera.position.x -= 0.02
+    if (moveRight):
+        camera.position.x += 0.02
+        
+    context.setTransform(1, 0, 0, 1, 200, 200)
+    context.fillStyle = "#FF66CC"
+    context.strokeStyle = "#808080" 
+
+    context.clearRect(-200, -200, 400, 400)
+
+    context.fillRect(100, 100, 50, 50)
+    context.strokeRect(75, 75, 50, 50)
+
+    context.beginPath()
+    context.moveTo(0,30)
+    context.lineTo(0,100)
+    # center
+    context.moveTo(-10, 0)
+    context.lineTo(10, 0)
+    context.moveTo(0, -10)
+    context.lineTo(0, 10)
+    context.fill()
+
+    context.rect(10, 10, 50, 50)
+
+    context.strokeText("Hello, Canvas", 60, 60)
+
+    context.closePath()
+    context.stroke()
+      
+    renderer.render(scene, camera)
     
 def animate(timestamp):
     global requestID, progress, startTime
@@ -81,11 +165,19 @@ def animate(timestamp):
         requestID = window.requestAnimationFrame(animate)
         render()
     else:
-        window.cancelAnimationFrame(requestID)
-        # container.removeChild(renderer.domElement)
+        terminate()
+        
+def terminate():
+    window.cancelAnimationFrame(requestID)
+    if (useLargeCanvas):
+        view.parentNode.removeChild(renderer.domElement)
+        view.parentNode.removeChild(graph)
+    else:
+        discardExistingCanvas()
+    document.removeEventListener("keydown", onDocumentKeyDown, False)
+    document.removeEventListener("keyup", onDocumentKeyUp, False)
 
-window.addEventListener("resize", onWindowResize, False)
-
-onWindowResize()
-
+print "ESC to terminate, Arrow keys to move Left, Right, Forward, Backward."
+print "This program will self-destruct in 60 seconds."
+init()
 animate(None)
