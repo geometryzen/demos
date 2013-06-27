b09dcccd91f826b5d08ec5e9fb45182b93375aea
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 12:45:50 -0700

    Save file.

--- three/cameras/PerspectiveCamera.py
+++ three/cameras/PerspectiveCamera.py
@@ -1,1 +1,91 @@
-# cameras/PerspectiveCamera.py
+# PerspectiveCamera demonstration.
+from three import *
+from browser import *
+
+# Discard the old canvas if it exists. 
+for canvas in document.getElementsByTagName("canvas"):
+    canvas.parentNode.removeChild(canvas)
+
+scene = Scene()
+
+camera  = PerspectiveCamera(45, 1.0, 0.1, 10000)
+camera.position.set(10, 10, 10)
+camera.lookAt(scene.position)
+
+ambientLight = AmbientLight(0x111111)
+scene.add(ambientLight)
+
+pointLight = PointLight(0xFFFFFF)
+pointLight.position.set(20, 20, 20)
+scene.add(pointLight)
+
+directionalLight = DirectionalLight(0xFFFFFF)
+directionalLight.position.set(0, 1, 0)
+scene.add(directionalLight)
+
+renderer = WebGLRenderer()
+renderer.autoClear   = True
+renderer.gammaInput  = True
+renderer.gammaOutput = True
+renderer.setClearColor(Color(0x080808), 1.0)
+
+container = document.getElementById("canvas-container")
+container.appendChild(renderer.domElement)
+
+material = MeshLambertMaterial({"color":0x0000FF})
+material.name = "bluecube"
+
+print "repr(material)    => " + repr(material)
+print "id:                  " + str(material.id)
+print "name:                " + material.name
+print "color:               " + str(material.color)
+print "needsUpdate:         " + str(material.needsUpdate)
+print "opacity:             " + str(material.opacity)
+print "overdraw:            " + str(material.overdraw)
+print "transparent:         " + str(material.transparent)
+print "visible:             " + str(material.visible)
+print "str(material)     => " + str(material)
+
+mesh = Mesh(CubeGeometry(5, 5, 5), material)
+
+scene.add(mesh)
+
+requestID = None
+progress = None
+progressEnd = 6000
+startTime =  None
+
+def render():
+    mesh.rotation.x = mesh.rotation.x + 0.02
+    mesh.rotation.y = mesh.rotation.y + 0.02
+    mesh.rotation.z = mesh.rotation.z + 0.02
+        
+    renderer.render(scene, camera)
+
+def onWindowResize():
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.size = (window.innerWidth, window.innerHeight)
+    
+def step(timestamp):
+    global requestID, progress, startTime
+    if (startTime):
+        progress = timestamp - startTime
+    else:
+        if (timestamp):
+            startTime = timestamp
+        else:
+            progress = 0
+        
+    if (progress < progressEnd):
+        requestID = window.requestAnimationFrame(step)
+        render()
+    else:
+        window.cancelAnimationFrame(requestID)
+        # container.removeChild(renderer.domElement)
+
+window.addEventListener("resize", onWindowResize, False)
+
+onWindowResize()
+
+step(None)
