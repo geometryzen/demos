30347ce1e2edd530f2668950cafa2f806073d86d
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 09:15:24 -0700

    Save file.

--- three/materials/MeshBasicMaterial.py
+++ three/materials/MeshBasicMaterial.py
@@ -23,7 +23,7 @@ renderer.setClearColor(Color(0x080808), 1.0)
 container = document.getElementById("canvas-container")
 container.appendChild(renderer.domElement)
 
-material = MeshBasicMaterial({"color":0x00FF00, "wireframe":False})
+material = MeshBasicMaterial({"color":0x00FF00, "wireframe":True})
 material.wireframeLinewidth = 2
 material.name = "greenie"
 
