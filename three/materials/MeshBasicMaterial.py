5bd6244d8c98a6e276b12aa54fbf81ba8fca61e8
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 09:24:51 -0700

    Save file.

--- three/materials/MeshBasicMaterial.py
+++ three/materials/MeshBasicMaterial.py
@@ -24,7 +24,7 @@ container = document.getElementById("canvas-container")
 container.appendChild(renderer.domElement)
 
 material = MeshBasicMaterial({"color":0x00FF00, "wireframe":True})
-material.wireframeLinewidth = 1
+material.wireframeLinewidth = 4
 material.name = "greenie"
 
 print "repr(material)    => " + repr(material)
