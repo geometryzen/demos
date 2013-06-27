43804ef4a300d2367fe2d248327319af02bb317a
David Geo Holmes <geometryzen@gmail.com>
2013-06-26 21:22:49 -0700

    Save file.

--- browser/canvas-ice-cream.py
+++ browser/canvas-ice-cream.py
@@ -17,13 +17,8 @@ moveRight = False
 # Global variables
 view = document.getElementById("view")
 canvas = document.createElement(CANVAS_TAGNAME)
-#canvas.style.position = "absolute"
-#canvas.style.top = "0px"
-#canvas.style.left = "0px"
 canvas.width = 400 * window.devicePixelRatio
 canvas.height = 400 * window.devicePixelRatio
-#canvas.style.width = str(canvas.width) + "px"
-#canvas.style.height = str(canvas.height) + "px"
 context = canvas.getContext("2d")
 
 # Use Python's dictionary to handle event.keyCode(s).
@@ -104,28 +99,12 @@ def init():
     onWindowResize()
 
 def render():
-    context.setTransform(1, 0, 0, 1, 200, 200)
-    context.fillStyle = "#FF66CC"
-    context.strokeStyle = "#808080" 
-
-    context.clearRect(-200, -200, 400, 400)
-
-    context.fillRect(100, 100, 50, 50)
-    context.strokeRect(75, 75, 50, 50)
-
     context.beginPath()
-    context.moveTo(0,30)
-    context.lineTo(0,100)
-    # center
-    context.moveTo(-10, 0)
-    context.lineTo(10, 0)
-    context.moveTo(0, -10)
-    context.lineTo(0, 10)
+    context.moveTo(150, 100)
+    context.lineTo(200, 225)
+    context.lineTo(250, 100)
+    context.fillStyle = "#D3BEA5"
     context.fill()
-
-    context.rect(10, 10, 50, 50)
-
-    context.strokeText("Hello, Canvas", 60, 60)
 
     context.closePath()
     context.stroke()
