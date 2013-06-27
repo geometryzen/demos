e57664d07beb483636baaa49d06c50c35c7fcced
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 11:01:40 -0700

    Save file.

--- three/materials/MeshLambertMaterial.py
+++ three/materials/MeshLambertMaterial.py
@@ -9,8 +9,8 @@ for canvas in document.getElementsByTagName("canvas"):
 
 scene = Scene()
 
-camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 100
+camera  = PerspectiveCamera(35, 1.0, 0.1, 10000)
+camera.position.set(-15, 10, 10)
 
 light = PointLight(0xFFFFFF)
 light.position.set(20, 20, 20)
