3e410056bdb17ae5a2dd785b2c2b16107edd31a0
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 12:55:03 -0700

    Save file.

--- three/cameras/PerspectiveCamera.py
+++ three/cameras/PerspectiveCamera.py
@@ -1,11 +1,7 @@
 # Under Construction!
-# HTML5 (2D) Canvas coexisting with a WebGL canvas.
 from three import *
-# We will control the horizontal. We will control the vertical.
 from browser import *
 
-# Convenince variable controlling whether we embed in the existing canvas container
-# or create a nearly full screen canvas
 useLargeCanvas = True
 
 # Variables to track the intentions of the user.
@@ -19,38 +15,13 @@ camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
 renderer = WebGLRenderer({"antialias": True})
 scene = Scene()
 view = document.getElementById("view")
-graph = document.createElement("canvas")
-graph.style.position = "absolute"
-graph.style.top = "0px"
-graph.style.left = "0px"
-context = graph.getContext("2d")
 
 # Use Python's dictionary to handle event.keyCode(s).
 def escKey(downFlag):
     terminate()
 
-def leftArrowKey(downFlag):
-    global moveLeft
-    moveLeft = downFlag
-
-def upArrowKey(downFlag):
-    global moveForward
-    moveForward = downFlag
-    
-def rightArrowKey(downFlag):
-    global moveRight
-    moveRight = downFlag
-
-def downArrowKey(downFlag):
-    global moveBackward
-    moveBackward = downFlag
-
 keyHandlers = {
- 27: escKey,
- 37: leftArrowKey,
- 38: upArrowKey,
- 39: rightArrowKey,
- 40: downArrowKey
+ 27: escKey
 }
     
     
@@ -67,19 +38,11 @@ def onWindowResize():
         camera.aspect = window.innerWidth / window.innerHeight
         camera.updateProjectionMatrix()
         renderer.size = (window.innerWidth, window.innerHeight)
-        graph.width = window.innerWidth
-        graph.height = window.innerHeight
-        graph.style.width = str(window.innerWidth) + "px"
-        graph.style.height = str(window.innerHeight) + "px"
     else:
         container = document.getElementById("canvas-container")
         camera.aspect = container.clientWidth / container.clientHeight
         camera.updateProjectionMatrix()
         renderer.setSize(container.clientWidth, container.clientHeight)
-        graph.width = container.clientWidth
-        graph.height = container.clientHeight
-        graph.style.width = str(container.clientWidth) + "px"
-        graph.style.height = str(container.clientHeight) + "px"
     
 def discardExistingCanvas():
     for cs in document.getElementsByTagName("canvas"):
@@ -96,10 +59,8 @@ def init():
         container = document.createElement("div")
         document.body.appendChild(container)
         view.parentNode.insertBefore(renderer.domElement, view)
-        view.parentNode.insertBefore(graph, view)
     else:
         container = document.getElementById("canvas-container")
-        container.appendChild(graph)
         container.appendChild(renderer.domElement)
 
     camera.position.z = 2
@@ -114,41 +75,6 @@ def init():
     onWindowResize()
 
 def render():
-    if (moveForward):
-        camera.position.z -= 0.02
-    if (moveBackward):
-        camera.position.z += 0.02
-    if (moveLeft):
-        camera.position.x -= 0.02
-    if (moveRight):
-        camera.position.x += 0.02
-        
-    context.setTransform(1, 0, 0, 1, 200, 200)
-    context.fillStyle = "#FF66CC"
-    context.strokeStyle = "#808080" 
-
-    context.clearRect(-200, -200, 400, 400)
-
-    context.fillRect(100, 100, 50, 50)
-    context.strokeRect(75, 75, 50, 50)
-
-    context.beginPath()
-    context.moveTo(0,30)
-    context.lineTo(0,100)
-    # center
-    context.moveTo(-10, 0)
-    context.lineTo(10, 0)
-    context.moveTo(0, -10)
-    context.lineTo(0, 10)
-    context.fill()
-
-    context.rect(10, 10, 50, 50)
-
-    context.strokeText("Hello, Canvas", 60, 60)
-
-    context.closePath()
-    context.stroke()
-      
     renderer.render(scene, camera)
     
 def animate(timestamp):
@@ -177,7 +103,5 @@ def terminate():
     document.removeEventListener("keydown", onDocumentKeyDown, False)
     document.removeEventListener("keyup", onDocumentKeyUp, False)
 
-print "ESC to terminate, Arrow keys to move Left, Right, Forward, Backward."
-print "This program will self-destruct in 60 seconds."
 init()
 animate(None)
