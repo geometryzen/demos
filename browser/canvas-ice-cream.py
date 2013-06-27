93d057ddcdee33c12b11c7c45896946747662057
David Geo Holmes <geometryzen@gmail.com>
2013-06-26 20:34:02 -0700

    Save file.

--- browser/canvas-ice-cream.py
+++ browser/canvas-ice-cream.py
@@ -104,7 +104,31 @@ def init():
     onWindowResize()
 
 def render():
-    x = 1
+    context.setTransform(1, 0, 0, 1, 200, 200)
+    context.fillStyle = "#FF66CC"
+    context.strokeStyle = "#808080" 
+
+    context.clearRect(-200, -200, 400, 400)
+
+    context.fillRect(100, 100, 50, 50)
+    context.strokeRect(75, 75, 50, 50)
+
+    context.beginPath()
+    context.moveTo(0,30)
+    context.lineTo(0,100)
+    # center
+    context.moveTo(-10, 0)
+    context.lineTo(10, 0)
+    context.moveTo(0, -10)
+    context.lineTo(0, 10)
+    context.fill()
+
+    context.rect(10, 10, 50, 50)
+
+    context.strokeText("Hello, Canvas", 60, 60)
+
+    context.closePath()
+    context.stroke()
 
 def animate(timestamp):
     global requestID, progress, startTime
