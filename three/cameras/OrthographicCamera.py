94c26871c990df740bc81f392cce331c226796a4
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 21:21:41 -0700

    Save file.

--- three/cameras/OrthographicCamera.py
+++ three/cameras/OrthographicCamera.py
@@ -88,7 +88,6 @@ def init():
     for i in range(0, 100):
         cube = Mesh(geometry, material)
         cube.scale.y = floor(random() * 2 + 1)
-        print cube.scale
         cube.position.x = floor((random() * 1000 - 500) / 50) * 50 + 25
         cube.position.y = (cube.scale.y * 50) / 2
         cube.position.z = floor((random() * 1000 - 500) / 50) * 50 + 25
