3ca8bc01e399708c7f98e3e7cd54f2772f3e0689
David Geo Holmes <geometryzen@gmail.com>
2013-06-27 22:39:49 -0700

    Save file.

--- three/materials/MeshLambertMaterial.py
+++ three/materials/MeshLambertMaterial.py
@@ -15,11 +15,12 @@ ambientLight = AmbientLight(0x111111)
 scene.add(ambientLight)
 
 pointLight = PointLight(0xFFFFFF)
+print pointLight.position
 pointLight.position.set(20, 20, 20)
+print pointLight.position
 scene.add(pointLight)
 
 directionalLight = DirectionalLight(0xFFFFFF)
-print directionalLight.position
 directionalLight.position.set(0, 1, 0)
 scene.add(directionalLight)
 
