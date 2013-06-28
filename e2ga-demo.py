b5fe30f593f8c5b8ccd0ecf41ef10cb19330fe38
David Geo Holmes <geometryzen@gmail.com>
2013-06-28 12:36:50 -0700

    Save file.

--- e2ga-demo.py
+++ e2ga-demo.py
@@ -27,7 +27,7 @@ i    = explain(Vector(1, 0))
 j    = explain(Vector(0, 1))
 I    = explain(Pseudoscalar(1))
 
-blades = [one, two, i, j, I]
+blades = [zero, one, two, i, j, I]
 
 print ""
 print "Sum"
