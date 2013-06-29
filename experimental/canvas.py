511a59a4504c30ab6c3c7ebb34331aab3ff02650
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 12:08:30 -0700

    Save file.

--- experimental/canvas.py
+++ experimental/canvas.py
@@ -14,11 +14,9 @@ view = document.getElementById("view")
 graph = document.createElement("canvas")
 graph.height = 400
 graph.width = 400
-graph.style.position = "absolute"
-graph.style.top = "0px"
-graph.style.left = "0px"
-graph.style.height = "400px"
-graph.style.width = "400px"
+#graph.style.position = "absolute"
+#graph.style.top = "0px"
+#graph.style.left = "0px"
 
 position = Vector3(0,0,0)
 
