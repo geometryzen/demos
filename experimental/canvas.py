eaaadb9390233ef66daaffdfa5024cf2dbce06ff
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 12:20:48 -0700

    Save file.

--- experimental/canvas.py
+++ experimental/canvas.py
@@ -81,6 +81,7 @@ def init():
     discardCanvases()
     if useLargeCanvas:
         # It would be more flexible to use document.body.firstChild here
+        
         document.body.insertBefore(graph, document.getElementsByTagName("header")[0])
     else:
         container = document.getElementById("canvas-container")
