4b879a23b7c10a72f4cef825e1bf733046ded931
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 08:30:53 -0700

    Save file.

--- browser/README.py
+++ browser/README.py
@@ -1,1 +1,8 @@
-# README.py
+# The 'browser' Python module supports interaction with the current window and document objects of
+# the web browser. This enables virtually unlimited control over the browser. While most programs
+# will place the WebGL output in the adjacent Canvas, you are free to to take over the browser.
+#
+# We will control the horizontal. We will control the vertical!
+#
+# The 'browser' Python module also provides a gateway to other HTML5 APIs such as the 2D Canvas API.
+
