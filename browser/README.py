497209ce2abb65d6762ece66b9effc4229f15a1c
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 13:30:15 -0700

    Save file.

--- browser/README.py
+++ browser/README.py
@@ -1,7 +1,9 @@
-# The 'browser' Python module supports interaction with the current window and document objects of
-# the web browser. This enables virtually unlimited control over the browser. While most programs
-# will place the graphics output in the adjacent Canvas, you are free to to take over the browser.
+'''
+The 'browser' Geometry Zen Python module supports interaction with the current window
+# and document objects of the web browser. This enables virtually unlimited control over the browser.
+# While most programs will place the graphics output in the adjacent Canvas, you are free to to take over the browser.
 #
+'''
 print "We will control the horizontal. We will control the vertical!"
 #
 # The 'browser' Python module also provides a gateway to other HTML5 APIs such as the 2D Canvas API.
