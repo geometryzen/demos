4d0b3724b06829da0d899fc851987e95ac95c68e
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 09:09:03 -0700

    Save file.

--- physics/beginning.py
+++ physics/beginning.py
@@ -91,7 +91,7 @@ startTime =  None
 def init():
     print "Hello!"
     print "This program is an exploration of ways to improve the user experience."        
-    print "ESC to terminate, Arrow keys to move the 3D cube Left, Right, Forward, Backward."
+    print "Press ESC to terminate, Arrow keys to move the 3D cube Left, Right, Forward, Backward."
     print "This program will 'self-destruct' in "+str(progressEnd/1000)+" seconds!"
     print "Try setting the useLargeCanvas variable to True. Then scroll down to see what is going on."
     discardExistingCanvas()
