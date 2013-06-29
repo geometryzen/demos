980e9c7754184b208c2b1780fdafe84d589eca8d
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 08:49:58 -0700

    Save file.

--- sprite/README.py
+++ sprite/README.py
@@ -1,1 +1,7 @@
-# README.py
+This folder contains examples of 2D programming using the 'sprite' module.
+
+The 'sprite' module is a tweaked version of the 'turtle' programming model.
+
+I don't recommend using 'sprite' it except for very new programmers learning Python.
+
+Even then, it may be better to use the HTML5 Canvas API through the 'browser' module.
