dd556fcd4ab28ff08b77d7717733fae669082144
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 15:42:10 -0700

    Save file.

--- physics/particle.py
+++ physics/particle.py
@@ -14,7 +14,7 @@ camera  = PerspectiveCamera(45, 1.0, 0.1, 1000)
 camera.position.z = 4
 renderer = WebGLRenderer({"antialias": True})
 scene = Scene()
-particle = Mesh(SphereGeometry(1.0), MeshNormalMaterial())
+particle = Mesh(SphereGeometry(1.0, 16, 12), MeshNormalMaterial())
 scene.add(particle)
 
 graph = document.createElement("canvas")
