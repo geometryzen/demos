11aab5b94a87dce18720e2cf2621e9c1fe96a3b8
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 15:51:16 -0700

    Save file.

--- physics/particle.py
+++ physics/particle.py
@@ -11,7 +11,7 @@ moveLeft = False
 moveRight = False
 
 camera  = PerspectiveCamera(45, 1.0, 0.1, 1000)
-camera.position.z = 100
+camera.position.z = 50
 renderer = WebGLRenderer({"antialias": True})
 renderer.setClearColor(0xFFFFFF, 1.0)
 scene = Scene()
