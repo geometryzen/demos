ad8d5ef94e0351d6a87ae53403259198eb288bc4
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 09:17:51 -0700

    Save file.

--- browser/canvas.py
+++ browser/canvas.py
@@ -72,8 +72,8 @@ def onWindowResize():
         camera.aspect = container.clientWidth / container.clientHeight
         camera.updateProjectionMatrix()
         renderer.setSize(container.clientWidth, container.clientHeight)
-        graph.width = container.clientWidth
-        graph.height = container.clientHeight
+        #graph.width = container.clientWidth
+        #graph.height = container.clientHeight
         #graph.style.width = str(container.clientWidth) + "px"
         #graph.style.height = str(container.clientHeight) + "px"
     
