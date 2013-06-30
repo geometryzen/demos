7e3168c24bc7d98b2d9d6f49be4295a7efc012f0
David Geo Holmes <geometryzen@gmail.com>
2013-06-30 10:57:28 -0700

    Save file.

--- experimental/ArrowGeometry.py
+++ experimental/ArrowGeometry.py
@@ -28,7 +28,7 @@ c = Vector3(radiusShaft, 0, lengthShaft)
 d = Vector3(radiusShaft, 0, 0)
 e = Vector3(0, 0, 0)
 #points = [a, b, c, d, e, a]
-points = [a, b, d, a]
+points = [a, b, e, a]
 arrow = LatheGeometry(points)
 
 print repr(arrow)
