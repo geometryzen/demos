0735356da3ffd24bcf396b3dcd8264e1c39c38bb
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 19:25:06 -0700

    Save file.

--- experimental/performance.py
+++ experimental/performance.py
@@ -13,7 +13,7 @@ moveRight = False
 
 camera  = PerspectiveCamera(45, 1.0, 0.1, 1000)
 camera.position.z = 20
-renderer = WebGLRenderer({"antialias": True})
+renderer = WebGLRenderer()
 renderer.setClearColor(0xFFFFFF, 1.0)
 scene = Scene()
 particle = Mesh(SphereGeometry(1.0, 32, 24), MeshLambertMaterial({"color":0x0000FF}))
