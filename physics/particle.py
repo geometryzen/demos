4536cf56ca25c968235e8e5a38c1d47f17d62f12
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 22:14:22 -0700

    Save file.

--- physics/particle.py
+++ physics/particle.py
@@ -120,7 +120,8 @@ def render(n, t, dt):
     global x, v
     # TODO: Implement Multivector division by at least scalars and vectors.    
     a = F(x, v, t) * (1/m)
-    v += a * dt
+    # TODO: Why doesn't += work here?
+    v = v + a * dt
     x = x + v * dt
     particle.position = x
     
