f132e8ea5e2e4f069a7d065a2cdacc8eea843102
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 17:15:31 -0700

    Save file.

--- physics/particle.py
+++ physics/particle.py
@@ -134,7 +134,7 @@ def render(n, t, dt):
     if (moveRight):
         camera.position.x += 0.2
     
-    particle.position = particle.position + velocity * dt
+    particle.position.add(velocity * dt)
     
     renderer.render(scene, camera)
     
