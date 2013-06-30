891c35ca980a589d8146342e8015cd5f83f0a00b
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 22:55:33 -0700

    Save file.

--- physics/particle.py
+++ physics/particle.py
@@ -32,6 +32,8 @@ i = Vector3(1,0,0)
 j = Vector3(0,1,0)
 k = Vector3(0,0,1)
 ij = i ^ j
+jk = MultiVector3(0, 0, 0, 0, 0, 1, 0, 0)
+ki = Bivector3(0, 0, 1)
 I = Pseudoscalar3(1)
 
 # The user-defined force field, F, may depend upon the particle position, velocity and time.
