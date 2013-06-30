aaec1279ec44b03352671f451e55bd36e0ea08ad
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 21:45:10 -0700

    Save file.

--- physics/particle.py
+++ physics/particle.py
@@ -9,7 +9,7 @@ camera.position.z = 1000
 renderer = WebGLRenderer()
 renderer.setClearColor(0x080808, 1.0)
 scene = Scene()
-particle = Mesh(SphereGeometry(50, 32, 24), MeshLambertMaterial({"color":0x0000FF}))
+particle = Mesh(SphereGeometry(50, 32, 24), MeshLambertMaterial({"color":0xFF0000}))
 scene.add(particle)
 
 ambientLight = AmbientLight(0x222222)
