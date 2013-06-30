861a2bde44aca4b0493268cf69cc9d9b4135d1d3
David Geo Holmes <geometryzen@gmail.com>
2013-06-29 23:21:42 -0700

    Save file.

--- eight/geometries/TorusGeometry.py
+++ eight/geometries/TorusGeometry.py
@@ -36,7 +36,7 @@ print "tubularSegments:" + str(torus.tubularSegments)
 print "arc:            " + str(torus.arc)
 print torus
 
-mesh = Mesh(torus, MeshBasicMaterial({"color":0xFFFF00,"wireframe":True, "wireframeLinewidth":3}))
+mesh = Mesh(torus, MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3}))
 scene.add(mesh)
 
 requestID = None
