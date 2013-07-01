42dd4a33f600eef1ff165079188645cb02aee983
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 07:06:12 -0700

    Save file.

--- eight/mesh/Mesh.py
+++ eight/mesh/Mesh.py
@@ -50,7 +50,7 @@ scene.add(redMesh)
 scene.add(bluMesh)
 scene.add(grnMesh)
 
-bluMesh.setGeometry(CubeGeometry(1))
+#bluMesh.setGeometry(CubeGeometry(1))
 
 ambientLight = AmbientLight(0x222222)
 scene.add(ambientLight)
