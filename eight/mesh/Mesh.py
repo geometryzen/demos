01b0c63a559309bb89080f0eb818e03f902a5172
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 06:25:13 -0700

    Save file.

--- eight/mesh/Mesh.py
+++ eight/mesh/Mesh.py
@@ -1,1 +1,100 @@
-# mesh/Mesh.py
+# Under Construction
+# Mesh demonstration.
+from eight import *
+from browser import *
+from math import pi
+
+for canvas in document.getElementsByTagName("canvas"):
+    canvas.parentNode.removeChild(canvas)
+
+scene = Scene()
+
+camera  = PerspectiveCamera(45, 1.0, 0.1, 1000)
+camera.position.z = 5
+
+renderer = WebGLRenderer()
+renderer.autoClear = True
+renderer.gammaInput = True
+renderer.gammaOutput = True
+renderer.setClearColor(Color(0x080808), 1.0)
+
+container = document.getElementById("canvas-container")
+container.appendChild(renderer.domElement)
+
+length = 1
+radiusShaft = length * 0.05
+radiusCone = radiusShaft * 2
+lengthCone = radiusCone * 2
+lengthShaft = length - lengthCone
+a = Vector3(0, 0, length)
+b = Vector3(radiusCone, 0, lengthShaft)
+c = Vector3(radiusShaft, 0, lengthShaft)
+d = Vector3(radiusShaft, 0, 0)
+e = Vector3(0, 0, 0)
+points = [a, b, c, d, e]
+
+redLamb = MeshLambertMaterial({"color":0xFF0000})
+
+redWire = MeshBasicMaterial({"color":0xFF0000, "wireframe":True, "wireframeLinewidth":3})
+bluWire = MeshBasicMaterial({"color":0x0000FF, "wireframe":True, "wireframeLinewidth":3})
+grnWire = MeshBasicMaterial({"color":0x00FF00, "wireframe":True, "wireframeLinewidth":3})
+segments = 6
+redArrow = Mesh(LatheGeometry(points, segments), redWire)
+bluArrow = Mesh(LatheGeometry(points, segments), bluWire)
+grnArrow = Mesh(LatheGeometry(points, segments), grnWire)
+
+scene.add(redArrow)
+scene.add(bluArrow)
+scene.add(grnArrow)
+
+ambientLight = AmbientLight(0x222222)
+scene.add(ambientLight)
+
+pointLight = PointLight(0xFFFFFF)
+pointLight.position.set(20, 20, 20)
+scene.add(pointLight)
+
+directionalLight = DirectionalLight(0xFFFFFF)
+directionalLight.position.set(0, 1, 0)
+scene.add(directionalLight)
+
+requestID = None
+progress = None
+progressEnd = 6000
+startTime =  None
+movement = Vector3(0.02, 0.02, 0.02)
+
+def render():
+    grnArrow.position.set(1,1,1);
+    grnArrow.scale.set(1,1,1)
+    grnArrow.rotation.add(movement)
+        
+    renderer.render(scene, camera)
+
+def onWindowResize():
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.size = (window.innerWidth, window.innerHeight)
+    
+def step(timestamp):
+    global requestID, progress, startTime
+    if (startTime):
+        progress = timestamp - startTime
+    else:
+        if (timestamp):
+            startTime = timestamp
+        else:
+            progress = 0
+        
+    if (progress < progressEnd):
+        requestID = window.requestAnimationFrame(step)
+        render()
+    else:
+        window.cancelAnimationFrame(requestID)
+        # container.removeChild(renderer.domElement)
+
+window.addEventListener("resize", onWindowResize, False)
+
+onWindowResize()
+
+step(None)
