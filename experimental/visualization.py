2fdf98689692df0ae7435a95ad825787afc3278f
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 10:20:11 -0700

    Save file.

--- experimental/visualization.py
+++ experimental/visualization.py
@@ -83,11 +83,8 @@ progressEnd = 60000
 startTime =  None
 
 def init():
-    print "Hello!"
-    print "This program is a demonstration of mixing the HTML5 2d and WebGL Canvases."        
-    print "Press ESC to terminate, Arrow keys to move the 3D cube Left, Right, Forward, Backward."
+    print "Press ESC to terminate."
     print "This program will 'self-terminate' in "+str(progressEnd/1000)+" seconds!"
-    print "Try setting the useLargeCanvas variable to True. Then scroll down to see what is going on."
     discardCanvases()
     if useLargeCanvas:
         document.body.insertBefore(graph, document.body.firstChild)
@@ -168,7 +165,7 @@ def terminate():
     discardCanvases()
     document.removeEventListener("keydown", onDocumentKeyDown, False)
     document.removeEventListener("keyup", onDocumentKeyUp, False)
-    print "Goodbye."
+    print "Done."
 
 init()
 animate(None)
