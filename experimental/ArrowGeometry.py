6e7a1169f7a3521b7fe50ba60e46844edd43c6b0
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 12:07:07 -0700

    Save file.

--- experimental/ArrowGeometry.py
+++ experimental/ArrowGeometry.py
@@ -12,7 +12,7 @@ scene = Scene()
 camera  = PerspectiveCamera(45, 1.0, 0.1, 1000)
 print camera.up
 camera.up.set(0,0,1)
-camera.position.set(3,3,3)
+camera.position.set(2,2,2)
 camera.lookAt(scene.position)
 
 renderer = WebGLRenderer()
