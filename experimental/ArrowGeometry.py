c1fe470da682e9d4f3650f973b930e8f00f7f273
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 16:26:26 -0700

    Save file.

--- experimental/ArrowGeometry.py
+++ experimental/ArrowGeometry.py
@@ -29,7 +29,7 @@ container.appendChild(renderer.domElement)
 
 length = 1
 radiusShaft = length * 0.01
-radiusCone = length * 0.1
+radiusCone = length * 0.08
 lengthCone = length * 0.2
 lengthShaft = length - lengthCone
 a = Vector3(0, 0, length)
