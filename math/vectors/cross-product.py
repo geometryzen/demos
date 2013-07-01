7e0b8de6b1cd70d1e2a9ce96577782772a9d79ca
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 13:30:37 -0700

    Save file.

--- math/vectors/cross-product.py
+++ math/vectors/cross-product.py
@@ -6,12 +6,16 @@ from eight import *
 a = Vector3(1, 3, 5)
 b = Vector3(7, 11, 13)
 
-print "a=" + str(a)
-print "b=" + str(b)
+print "a => " + str(a)
+print "b => " + str(b)
 
 # Make a copy of a first because the cross method mutates its target.
 c = a.clone().cross(b)
-print "a=" + str(a)
-print "b=" + str(b)
-print "c=" + str(c)
+print "a => " + str(a)
+print "b => " + str(b)
+print "c => " + str(c)
+
+# We can now verify the three defining properties of the cross product in 3D.
+# 1) c is orthogonal to both b and c
+print "c.dot(a) => " + str(c.dot(a))
 
