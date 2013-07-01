44efcbc3f6b8a3fbbd0778ca7bad1be2c826c10d
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 10:30:52 -0700

    Save file.

--- experimental/visualization.py
+++ experimental/visualization.py
@@ -2,57 +2,82 @@
 from eight import *
 from browser import *
 
-useLargeCanvas = False
-
-moveForward = False
-moveBackward = False
-moveLeft = False
-moveRight = False
-
+# TODO: This could be a particular coordinate system, not just a graphics scene?
 scene = Scene()
 
-camera = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.up.set(0,0,1)
-camera.position.set(10,10,10)
-camera.lookAt(scene.position)
+particle = Mesh(SphereGeometry(50, 32, 24), MeshLambertMaterial({"color":0x0000FF}))
+scene.add(particle)
 
-renderer = WebGLRenderer({"antialias": True})
-renderer.setClearColor(0x080808, 1)
+# Initialize the system configuration.
+x = Vector3(-500,0,0)
+v = Vector3(75,75,0)
+m = 1
+g = Vector3(0, -9.81, 0)
 
+# Charge, Electric Field, and Magnetic Field as an bivector (no "dishonest" axial vectors, Thank You).
+e = Scalar3(1)
+E = Vector3()
+B = Bivector3(0,0,0)
+
+# Some more examples of constructing reference multivectors.
+one = MultiVector3(1, 0, 0, 0, 0, 0, 0, 0)
+i = Vector3(1,0,0)
+j = Vector3(0,1,0)
+k = Vector3(0,0,1)
+ij = i ^ j
+jk = MultiVector3(0, 0, 0, 0, 0, 1, 0, 0)
+ki = Bivector3(0, 0, 1)
+I = Pseudoscalar3(1)
+
+# The user-defined force field, F, may depend upon the particle position, velocity and time.
+def F(x,v,t):
+    return m * g
+    # TODO: Something like this for Electrodynamics (with vectors and bivectors).
+    # No "dishonest" vectors here; Feynman would be proud!
+    # return e * (E + (v << B))
+    
+def integrate(n, t, dt):
+    global x, v
+    # TODO: Implement Multivector division by at least scalars and vectors.    
+    a = F(x, v, t) * (1/m)
+    # TODO: Why doesn't += work here?
+    v = v + a * dt
+    x = x + v * dt
+    # TODO: Implement a bounce with the Geometric reflection formula (-nvn).
+    # TODO: Should we have Rigid Bodies with state/kinematic variables?
+    # TODO: What about intrinsic properties such as mass or inertia tensor?
+    particle.position = x
+
+useLargeCanvas = False
+
+camera  = PerspectiveCamera(45, 1.0, 0.1, 10000)
+camera.position.z = 1000
+renderer = WebGLRenderer()
+renderer.setClearColor(0xFFFFFF, 1.0)
+
+ambientLight = AmbientLight(0x222222)
+scene.add(ambientLight)
+
+pointLight = PointLight(0xFFFFFF)
+pointLight.position.set(20, 20, 20)
+scene.add(pointLight)
+
+directionalLight = DirectionalLight(0xFFFFFF)
+directionalLight.position.set(0, 1, 0)
+scene.add(directionalLight)
+
+# We're not actually using the HTML Canvas (2d), but it is here just in case we need it.
 graph = document.createElement("canvas")
-graph.height = 400
-graph.width = 400
 graph.style.position = "absolute"
 graph.style.top = "0px"
 graph.style.left = "0px"
-
 context = graph.getContext("2d")
 
 def escKey(downFlag):
     terminate()
 
-def leftArrowKey(downFlag):
-    global moveLeft
-    moveLeft = downFlag
-
-def upArrowKey(downFlag):
-    global moveForward
-    moveForward = downFlag
-    
-def rightArrowKey(downFlag):
-    global moveRight
-    moveRight = downFlag
-
-def downArrowKey(downFlag):
-    global moveBackward
-    moveBackward = downFlag
-
 keyHandlers = {
- 27: escKey,
- 37: leftArrowKey,
- 38: upArrowKey,
- 39: rightArrowKey,
- 40: downArrowKey
+ 27: escKey
 }
     
 def onDocumentKeyDown(event):
@@ -67,9 +92,11 @@ def onWindowResize():
     if (useLargeCanvas):
         camera.aspect = window.innerWidth / window.innerHeight
         camera.updateProjectionMatrix()
-        renderer.setSize(window.innerWidth, window.innerHeight)
+        renderer.size = (window.innerWidth, window.innerHeight)
         graph.width = window.innerWidth
         graph.height = window.innerHeight
+        graph.style.width = str(window.innerWidth) + "px"
+        graph.style.height = str(window.innerHeight) + "px"
     else:
         container = document.getElementById("canvas-container")
         camera.aspect = container.clientWidth / container.clientHeight
@@ -77,60 +104,66 @@ def onWindowResize():
         renderer.setSize(container.clientWidth, container.clientHeight)
         graph.width = container.clientWidth
         graph.height = container.clientHeight
+        graph.style.width = str(container.clientWidth) + "px"
+        graph.style.height = str(container.clientHeight) + "px"
     
 def discardCanvases():
     for cs in document.getElementsByTagName("canvas"):
         cs.parentNode.removeChild(cs)
         
 requestID = None
-progress = None
-progressEnd = 60000
+frameIndex = 0
+DURATION_SECONDS = 15
 startTime =  None
+frameTime = None
+endTime = None
 
-def init():
+def run():       
     print "Press ESC to terminate."
-    print "This program will 'self-terminate' in "+str(progressEnd/1000)+" seconds!"
+    print "This program will 'self-terminate' in "+str(DURATION_SECONDS)+" seconds!"
     discardCanvases()
-    if useLargeCanvas:
-        document.body.insertBefore(graph, document.body.firstChild)
+    if (useLargeCanvas):
         document.body.insertBefore(renderer.domElement, document.body.firstChild)
+        document.body.insertBefore(graph, document.body.firstChild)
     else:
         container = document.getElementById("canvas-container")
         container.appendChild(graph)
         container.appendChild(renderer.domElement)
-    
+
     document.addEventListener("keydown", onDocumentKeyDown, False)
     document.addEventListener("keyup", onDocumentKeyUp, False)
 
     window.addEventListener("resize", onWindowResize, False)
     onWindowResize()
+    window.requestAnimationFrame(frameZero)
 
-def render():
-
+def frameZero(timestamp):
+    global requestID, startTime, frameTime, endTime
+    startTime = timestamp
+    frameTime = startTime
+    endTime = startTime + DURATION_SECONDS * 1000
+    requestID = window.requestAnimationFrame(animate)
+    integrate(frameIndex, (frameTime - startTime)/1000, 0.0)
     renderer.render(scene, camera)
     
 def animate(timestamp):
-    global requestID, progress, startTime
-    if startTime:
-        progress = timestamp - startTime
-    else:
-        if timestamp:
-            startTime = timestamp
-        else:
-            progress = 0
-        
-    if progress < progressEnd:
+    global requestID, frameIndex, frameTime
+    frameIndex += 1
+    interval = timestamp - frameTime 
+    frameTime = timestamp   
+    if frameTime < endTime:
         requestID = window.requestAnimationFrame(animate)
-        render()
+        integrate(frameIndex, (frameTime - startTime)/1000, interval/1000)
+        renderer.render(scene, camera)
     else:
         terminate()
         
 def terminate():
     window.cancelAnimationFrame(requestID)
-    discardCanvases()
     document.removeEventListener("keydown", onDocumentKeyDown, False)
     document.removeEventListener("keyup", onDocumentKeyUp, False)
+    time = (frameTime-startTime)/1000
+    count = frameIndex+1
     print "Done."
 
-init()
-animate(None)
+run()
