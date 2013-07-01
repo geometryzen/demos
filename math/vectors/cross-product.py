52a28197e176ec9ef9455b3d0f0442d9eacea33e
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 13:22:26 -0700

    Save file.

--- math/vectors/cross-product.py
+++ math/vectors/cross-product.py
@@ -1,11 +1,10 @@
 # This program examines the vector cross-product from a Geometric Algebra perspective
 # and provides an alternative geometric formulation of the Lorentz force law.
 from eight import *
-from math import random
 
-# Start with two random vectors a and b:
-a = Vector3(random(), random(), random())
-b = Vector3(random(), random(), random())
+# Start with two example vectors a and b:
+a = Vector3(1, 3, 5)
+b = Vector3(7, 11, 13)
 
 print "a=" + str(a)
 print "b=" + str(b)
