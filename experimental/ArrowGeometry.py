a0191b5c1ee1d1db110b221823b47c94c9e505de
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 16:52:56 -0700

    Save file.

--- experimental/ArrowGeometry.py
+++ experimental/ArrowGeometry.py
@@ -44,7 +44,7 @@ grnWire = MeshLambertMaterial({"color":0x00FF00})
 bluWire = MeshBasicMaterial({"color":0x0000FF})
 segments = 24
 
-redGeom = ArrowGeometry(1,5,0.1)
+redGeom = ArrowGeometry(1,5,0.08)
 grnGeom = LatheGeometry(points, segments)
 bluGeom = LatheGeometry(points, segments)
 
