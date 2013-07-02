4e0b9a8923b0d73d9122235269288661fbf14770
David Geo Holmes <geometryzen@gmail.com>
2013-07-01 18:12:36 -0700

    Save file.

--- experimental/ArrowGeometry.py
+++ experimental/ArrowGeometry.py
@@ -36,7 +36,7 @@ grnGeom = ArrowGeometry()
 bluGeom = ArrowGeometry()
 
 redMesh = Mesh(redGeom, redWire)
-#redMesh.position.set(0,1,0)
+redMesh.position.set(0,1,0)
 #redMesh.rotation.set(0,pi/2,0)
 grnMesh = Mesh(bluGeom, grnWire)
 #grnMesh.rotation.set(-pi/2,0,0)
