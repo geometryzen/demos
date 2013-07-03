223d4e8ae3bc2a07a9013cb210732f4f7a77ab2a
David Geo Holmes <geometryzen@gmail.com>
2013-07-03 04:19:54 -0700

    Save file.

--- blade/e2ga-in-place.py
+++ blade/e2ga-in-place.py
@@ -1,2 +1,27 @@
 # e2ga-in-place.py
 from blade import *
+
+# Convenience functions for creating Blades.
+def Scalar2(w):
+    return Euclidean2(w, 0, 0, 0)
+
+def Vector2(x, y):
+    return Euclidean2(0, x, y, 0)
+
+def Pseudoscalar2(s):
+    return Euclidean2(0, 0, 0, s)
+
+def explain(m):
+    print str(m) + " is " + repr(m)
+    return m
+
+def showValue(name, x):
+    print name + " => " + str(x)
+    return x
+
+i = Vector2(1,0)
+j = Vector2(0,0)
+x = Vector2()
+explain(i)
+explain(j)
+explain(x)
