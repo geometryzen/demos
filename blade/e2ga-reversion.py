4e91e958585e78b655934eecba9a31ea0248d32b
David Geo Holmes <geometryzen@gmail.com>
2013-07-03 13:57:57 -0700

    Save file.

--- blade/e2ga-reversion.py
+++ blade/e2ga-reversion.py
@@ -1,1 +1,32 @@
 # e2ga-reversion.py
+# e2ga-in-place.py
+from blade import *
+
+# Convenience functions for creating Blades.
+def Scalar2(w):
+    return Euclidean2(w, 0, 0, 0)
+
+def Vector2(x, y):
+    return Euclidean2(0, x, y, 0)
+
+def Pseudoscalar2(s):
+    return Euclidean2(0, 0, 0, s)
+
+def explain(m):
+    print str(m) + " is " + repr(m)
+    return m
+
+def showValue(name, x):
+    print name + " => " + str(x)
+    return x
+
+i = Vector2(1,0)
+j = Vector2(0,1)
+x = Vector2(0,0)
+I = Pseudoscalar2(1)
+explain(i)
+explain(j)
+explain(x)
+
+x = i * j
+showValue("x", x)
