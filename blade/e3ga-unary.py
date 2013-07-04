acf15fbfbeff1e9c45dbd18b900a276ecc391cf3
David Geo Holmes <geometryzen@gmail.com>
2013-07-03 20:27:06 -0700

    Save file.

--- blade/e3ga-unary.py
+++ blade/e3ga-unary.py
@@ -1,1 +1,24 @@
 # e3ga-unary.py
+from eight import *
+
+def explain(m):
+    print str(m) + " is " + repr(m)
+    return m
+
+def showValue(name, m):
+    print name + " => " + str(m)
+    return m
+
+zero  = explain(MultiVector3(0, 0, 0, 0, 0, 0, 0, 0))
+one   = explain(Scalar3(1))
+two   = explain(Scalar3(2))
+three = explain(3)
+i     = explain(Vector3(1, 0, 0))
+j     = explain(Vector3(0, 1, 0))
+k     = explain(Vector3(0, 0, 1))
+ij    = explain(Bivector3(1, 0, 0))
+jk    = explain(Bivector3(0, 1, 0))
+ki    = explain(Bivector3(0, 0, 1))
+I     = explain(Pseudoscalar3(1))
+
+showValue("+zero", +zero)
