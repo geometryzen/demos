5a87267cff9384248c0b1231d4dbf959f4fbd0ee
David Geo Holmes <geometryzen@gmail.com>
2013-07-05 12:47:55 -0700

    Save file.

--- blade/e2ga-division.py
+++ blade/e2ga-division.py
@@ -1,1 +1,29 @@
 # e2ga-division.py
+from e2ga import *
+from math import random, floor
+
+def explain(m):
+    print str(m) + " is " + repr(m)
+    return m
+
+def showValue(name, m):
+    print name + " => " + str(m)
+    return m
+
+def ri():
+    return floor(100*random())
+
+# A general multivector (in 3D at least) has a path to inversion.
+a = Euclidean2(ri(),ri(),ri(),ri())
+# A spinor in 3D is more direct.
+#a = Euclidean3(ri(),0,0,0,ri(),ri(),ri(),0)
+showValue("a",a)
+
+b = ~a
+showValue("~a",b)
+
+c = a * b
+showValue("a * ~a", repr(c))
+
+d = c * c
+showValue("d", d)
