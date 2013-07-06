4f826619c236e13f0d9859c1c9f92696768c6ef4
David Geo Holmes <geometryzen@gmail.com>
2013-07-06 09:44:49 -0700

    Save file.

--- blade/e3ga-division.py
+++ blade/e3ga-division.py
@@ -17,6 +17,7 @@ def gradeInvolution(A):
 def inverse(M):
     r = ~M
     m = M * r
+    showValue("m", m)
     s = r * gradeInvolution(m)
     k = (M * s).w
     return Euclidean3(s.w/k, s.x/k, s.y/k, s.z/k, s.xy/k, s.yz/k, s.zx/k, s.xyz/k)
