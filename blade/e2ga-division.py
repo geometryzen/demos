1aebb06d0e49a043cb6baf4df7a944b0274b49d9
David Geo Holmes <geometryzen@gmail.com>
2013-07-06 08:50:39 -0700

    Save file.

--- blade/e2ga-division.py
+++ blade/e2ga-division.py
@@ -1,10 +1,6 @@
 from e2ga import *
 from math import random, floor
 
-def explain(m):
-    print str(m) + " is " + repr(m)
-    return m
-
 def showValue(name, m):
     print name + " => " + str(m)
     return m
@@ -12,13 +8,13 @@ def showValue(name, m):
 def ri():
     return floor(100*random())
 
-def cc(A):
+def cliffordConjugate(A):
     return Euclidean2(A.w, -A.x, -A.y, -A.xy)
 
 def inv(M):
     r = ~M
     m = M * r
-    s = r * cc(m)
+    s = r * cliffoedConjugate(m)
     k = (M * s).w
     return Euclidean2(s.w/k, s.x/k, s.y/k, s.xy/k)
 
