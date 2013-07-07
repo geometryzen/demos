3e525600a61dd6025cf95402f00bd9befa370838
David Geo Holmes <geometryzen@gmail.com>
2013-07-07 05:17:47 -0700

    Save file.

--- physics/projectile.py
+++ physics/projectile.py
@@ -41,8 +41,8 @@ def F(x,v,t):
     
 def integrate(n, t, dt):
     global x, v
-    # TODO: Implement Multivector division by at least scalars and vectors.    
-    a = F(x, v, t) * (1/m)
+
+    a = F(x, v, t) / m
     v += a * dt
     x += v * dt
     particle.position = x
