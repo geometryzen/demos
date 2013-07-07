2b11b4f5fa0bfb04b335991b4f6a80ddcbd4b0c6
David Geo Holmes <geometryzen@gmail.com>
2013-07-07 14:08:49 -0700

    Save file.

--- eight/geometries/CircleGeometry.py
+++ eight/geometries/CircleGeometry.py
@@ -1,1 +1,79 @@
 # CircleGeometry.py
+from eight import *
+from browser import *
+from math import pi
+
+for canvas in document.getElementsByTagName("canvas"):
+    canvas.parentNode.removeChild(canvas)
+
+scene = Scene()
+
+camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
+camera.position.z = 100
+
+renderer = WebGLRenderer()
+renderer.setClearColor(Color(0x080808), 1.0)
+
+container = document.getElementById("canvas-container")
+container.appendChild(renderer.domElement)
+
+radius = 50
+widthSegments = 32
+heightSegments = 24
+phiStart = 0
+phiLength = 2 * pi
+thetaStart = 0
+thetaLength = pi
+sphere = SphereGeometry(radius, widthSegments, heightSegments, phiStart, phiLength, thetaStart, thetaLength)
+
+print repr(sphere)
+print "radius:         " + str(sphere.radius)
+print "widthSegments:  " + str(sphere.widthSegments)
+print "heightSegments: " + str(sphere.heightSegments)
+print "phiStart:       " + str(sphere.phiStart)
+print "phiLength:      " + str(sphere.phiLength)
+print "thetaStart:     " + str(sphere.thetaStart)
+print "thetaLength:    " + str(sphere.thetaLength)
+print sphere
+
+mesh = Mesh(sphere, MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3}))
+scene.add(mesh)
+
+requestID = None
+progress = None
+progressEnd = 6000
+startTime =  None
+movement = Vector3(0.02, 0.02, 0.02)
+
+def render():
+    mesh.rotation.add(movement)
+        
+    renderer.render(scene, camera)
+
+def onWindowResize():
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.size = (window.innerWidth, window.innerHeight)
+    
+def step(timestamp):
+    global requestID, progress, startTime
+    if (startTime):
+        progress = timestamp - startTime
+    else:
+        if (timestamp):
+            startTime = timestamp
+        else:
+            progress = 0
+        
+    if (progress < progressEnd):
+        requestID = window.requestAnimationFrame(step)
+        render()
+    else:
+        window.cancelAnimationFrame(requestID)
+        # container.removeChild(renderer.domElement)
+
+window.addEventListener("resize", onWindowResize, False)
+
+onWindowResize()
+
+step(None)
