fd54fd51c0d25d03f60945b2ed2d59bb21e5a357
David Geo Holmes <geometryzen@gmail.com>
2013-07-07 18:57:56 -0700

    Save file.

--- eight/math/Quaternion.py
+++ eight/math/Quaternion.py
@@ -5,6 +5,7 @@ quaternion = Quaternion(1,2,3,4)
 print quaternion
 print str(quaternion)
 print repr(quaternion)
+print "quaternion.x => " + str(quaternion.x)
 
 print Quaternion()
 print repr(Quaternion())
