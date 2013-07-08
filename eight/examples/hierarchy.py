f9dac64d13e2e03987ef08df4a8dc0d20947abec
David Geo Holmes <geometryzen@gmail.com>
2013-07-07 18:01:22 -0700

    Save file.

--- eight/examples/hierarchy.py
+++ eight/examples/hierarchy.py
@@ -1,6 +1,7 @@
 from eight import *
 from browser import *
-from math import pi, random
+from math import pi, random, sin
+from time import clock
 
 useLargeCanvas = True
 
@@ -86,6 +87,11 @@ def render():
     camera.position.x += (mouseX - camera.position.x) * 0.05
     camera.position.y += (mouseX - camera.position.y) * 0.05
     camera.lookAt(scene.position)
+    
+    currentSeconds = clock()
+    group.rotation.x = sin(currentSeconds * 0.7) * 0.5
+    group.rotation.y = sin(currentSeconds * 0.3) * 0.5
+    group.rotation.z = sin(currentSeconds * 0.2) * 0.5
 
 def run():       
     print "Press ESC to terminate."
