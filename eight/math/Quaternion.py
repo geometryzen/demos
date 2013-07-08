010ef0a0ec2cf3cba20891073f864f4ab685a196
David Geo Holmes <geometryzen@gmail.com>
2013-07-07 19:28:35 -0700

    Save file.

--- eight/math/Quaternion.py
+++ eight/math/Quaternion.py
@@ -6,7 +6,6 @@ print "Construction"
 print "----------"
 quaternion = Quaternion(1, 2, 3, 4)
 print repr(quaternion)
-print quaternion
 print str(quaternion)
 print "quaternion.x => " + str(quaternion.x)
 print "quaternion.y => " + str(quaternion.y)
@@ -16,8 +15,8 @@ print "quaternion.w => " + str(quaternion.w)
 print "----------"
 print "Defaults"
 print "----------"
-print Quaternion()
 print repr(Quaternion())
+print str(Quaternion())
 print "x => " + str(Quaternion().x)
 print "y => " + str(Quaternion().y)
 print "z => " + str(Quaternion().z)
