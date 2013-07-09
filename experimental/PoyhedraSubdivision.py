785b10102b0c17055bc0207c4afd2346ab67aa6d
David Geo Holmes <geometryzen@gmail.com>
2013-07-08 19:31:16 -0700

    Save file.

--- experimental/PoyhedraSubdivision.py
+++ experimental/PoyhedraSubdivision.py
@@ -22,5 +22,26 @@ faces = [
     (5, 3, 4),
     (5, 4, 1) ]
 
-print verts
-print faces
+def subdivide(verts, faces):
+    """Subdivide each triangle into four triangles, pushing verts to the unit sphere"""
+    triangles = len(faces)
+    for faceIndex in xrange(triangles):
+    
+        # Create three new verts at the midpoints of each edge:
+        face = faces[faceIndex]
+        a,b,c = (Vector3(*verts[vertIndex]) for vertIndex in face)
+        verts.append((a + b).normalized()[:])
+        verts.append((b + c).normalized()[:])
+        verts.append((a + c).normalized()[:])
+
+        # Split the current triangle into four smaller triangles:
+        i = len(verts) - 3
+        j, k = i+1, i+2
+        faces.append((i, j, k))
+        faces.append((face[0], i, k))
+        faces.append((i, face[1], j))
+        faces[faceIndex] = (k, j, face[2])
+
+    return verts, faces
+
+print subdivide(verts, faces)
