68de6825eaeebc028cfca3cd8b84c412f6a863fb
David Geo Holmes <geometryzen@gmail.com>
2013-07-08 19:43:25 -0700

    Save file.

--- python/SplatOperator.py
+++ python/SplatOperator.py
@@ -2,4 +2,7 @@
 from eight import *
 
 print repr(Vector3(1,2,3))
+
+# The Python splat operator takes a list as input and expands it into
+# positional arguments in a function call.
 print repr(Vector3(*[4,5,6]))
