87a154ad04fa9ec0595b69917ef7b4c5c523b93a
David Geo Holmes <geometryzen@gmail.com>
2013-07-08 19:28:46 -0700

    Save file.

--- experimental/PoyhedraSubdivision.py
+++ experimental/PoyhedraSubdivision.py
@@ -1,1 +1,23 @@
 # PoyhedraSubdivision.py
+from math import *
+	 
+"""Construct an eight-sided polyhedron"""
+f = sqrt(2.0) / 2.0
+
+verts = [    
+    ( 0, -1,  0),
+	(-f,  0,  f),
+	( f,  0,  f),
+	( f,  0, -f),
+	(-f,  0, -f),
+	( 0,  1,  0) ]
+
+faces = [
+    (0, 2, 1),
+    (0, 3, 2),
+    (0, 4, 3),
+    (0, 1, 4),
+    (5, 1, 2),
+    (5, 2, 3),
+    (5, 3, 4),
+    (5, 4, 1) ]
