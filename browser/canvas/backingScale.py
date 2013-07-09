08b7d3eb474acb18e4f34364689f7822a9b7d9c8
David Geo Holmes <geometryzen@gmail.com>
2013-07-09 12:10:05 -0700

    Save file.

--- browser/canvas/backingScale.py
+++ browser/canvas/backingScale.py
@@ -101,7 +101,7 @@ def render():
     context.rotate(pi/4) # Not seeing this do much?
     context.setTransform(1, 0, 0, 1, 0, 0)
     context.fillStyle = "rgba(255, 102, 207, 1.0)" # "#FF66CC"
-    context.strokeStyle = "#808080"
+    context.strokeStyle = "black" # "#808080"
     context.lineWidth = 2 * pixelRatio
 
     context.clearRect(-200, -200, 400, 400)
