33c61ca12fb94bbf460dfaa872351060c0af8d88
David Geo Holmes <geometryzen@gmail.com>
2013-07-08 19:51:08 -0700

    Save file.

--- experimental/PoyhedraSubdivision.py
+++ experimental/PoyhedraSubdivision.py
@@ -30,7 +30,6 @@ def subdivide(verts, faces):
     for faceIndex in range(triangles):
         # Create three new verts at the midpoints of each edge:
         face = faces[faceIndex]
-        for vertIndex in face:
 #        a,b,c = (Vector3(*verts[vertIndex]) for vertIndex in face)
 #        verts.append((a + b).normalized()[:])
 #        verts.append((b + c).normalized()[:])
