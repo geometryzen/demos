8022f60bc78610bf8f3e02ab46dc684aa0075016
David Geo Holmes <geometryzen@gmail.com>
2013-07-08 17:53:53 -0700

    Save file.

--- experimental/visualization.py
+++ experimental/visualization.py
@@ -48,6 +48,10 @@ redMesh.lookAt(Vector3(1,0,0))
 grnMesh.lookAt(Vector3(0,1,0))
 grnMesh.visible = True
 yloMesh.useQuaternion = True
+a = k
+b = i
+R = (1 + b * a)/(1 + b << a)
+print R
 yloMesh.quaternion.set(0,0,0,1)
 
 print "position   => " + str(bluMesh.position)
