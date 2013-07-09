68d3b21640c416a74986bb37f8cb705de802ba19
David Geo Holmes <geometryzen@gmail.com>
2013-07-08 17:22:31 -0700

    Save file.

--- experimental/visualization.py
+++ experimental/visualization.py
@@ -47,6 +47,7 @@ yloMesh = Mesh(yloGeom, yloWire)
 redMesh.lookAt(Vector3(1,0,0))
 grnMesh.lookAt(Vector3(0,1,0))
 grnMesh.visible = True
+yloMesh.useQuaternion = True
 print yloMesh.useQuaternion
 
 print "position   => " + str(bluMesh.position)
