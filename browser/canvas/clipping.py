5d2e04a737fa44a3fec7bd478c461cb69a196abf
David Geo Holmes <geometryzen@gmail.com>
2013-07-09 13:09:00 -0700

    Save file.

--- browser/canvas/clipping.py
+++ browser/canvas/clipping.py
@@ -98,7 +98,7 @@ def init():
 def drawMask():
     context.beginPath()
     context,moveTo(canvas.width/2, 0)
-    canvas.lineTo(canvas.width, canvas.height)
+    context.lineTo(canvas.width, canvas.height)
     context.font = "24pt Helvetica"
     context.fillText("Triangular Clipping Mask", 20, 180)
 
