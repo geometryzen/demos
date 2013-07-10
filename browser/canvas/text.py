6313b5451c794cd342df5c2671126325fff5b35e
David Geo Holmes <geometryzen@gmail.com>
2013-07-10 06:42:42 -0700

    Save file.

--- browser/canvas/text.py
+++ browser/canvas/text.py
@@ -100,7 +100,7 @@ def render():
     context.fillStyle = "blue"
     context.strokeStyle = "blue"
     context.font = "80pt Helvetica"
-    context.fillText("Geometry Zen", 100, 100)
+    context.fillText("Geometry Zen", 10, 100)
 
     
 def animate(timestamp):
