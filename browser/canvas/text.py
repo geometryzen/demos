3318a14ca3572848a14eacb8c9d3620df7ad4e85
David Geo Holmes <geometryzen@gmail.com>
2013-07-10 06:41:22 -0700

    Save file.

--- browser/canvas/text.py
+++ browser/canvas/text.py
@@ -99,7 +99,7 @@ def init():
 def render():
     context.fillStyle = "blue"
     context.strokeStyle = "blue"
-    context.font = "40pt Arial"
+    context.font = "80pt Arial"
     context.strokeText("Hello", 60, 60)
 
     
