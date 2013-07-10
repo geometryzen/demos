1a1fcd2091926a7ae755de3ddc6181d6baa0db4a
David Geo Holmes <geometryzen@gmail.com>
2013-07-10 06:26:20 -0700

    Save file.

--- browser/canvas/text.py
+++ browser/canvas/text.py
@@ -26,12 +26,6 @@ pixelRatio = backingScale(context)
 canvas.height = 400 * pixelRatio
 canvas.width = 400 * pixelRatio
 
-#context.textBaseline = "top"
-#context.textAlign = "right"
-context.fillStyle = "blue"
-context.font = "30px Arial"
-context.fillText("Hello", 10, 250)
-
 def escKey(downFlag):
     terminate()
 
