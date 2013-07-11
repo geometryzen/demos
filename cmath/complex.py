1e8b7b8a31dbe64b3e6fd4149af400b581f586a9
David Geo Holmes <geometryzen@gmail.com>
2013-07-11 09:57:26 -0700

    Save file.

--- cmath/complex.py
+++ cmath/complex.py
@@ -1,1 +1,47 @@
 # cmath/complex.py
+from cmath import *
+
+print "----------"
+print "construction"
+print "----------"
+z1 = complex(11.0, 7.0)
+print "----------"
+print "properties"
+print "----------"
+print z1.real
+print z1.imag
+print "----------"
+print "representations"
+print "----------"
+print z1
+print str(z1)
+print repr(z1)
+print "----------"
+print "type"
+print "----------"
+print type(z1)
+print str(type(z1))
+print repr(type(z1))
+
+z2 = complex(5, 3)
+one = complex(1, 0)
+two = 2
+i = complex(0, 1)
+zs = [one, two, i, z1, z2]
+print "----------"
+print "Addition"
+print "----------"
+for a in zs:
+  for b in zs:
+    print star(a)""
+#print z1 + z2
+#print z1 - z2
+#print z1 * z2
+#print z1 / z2
+#print "----------"
+#print z1 == z1
+#print z1 != z1
+#print z1 == z2
+#print z1 != z2
+#print "----------"
+#print phase(complex(-1.0, 0.0))
