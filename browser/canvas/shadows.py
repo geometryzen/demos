2b7d709a4e3ee0a7ca7fc0d2dd20aad38d2d63fe
David Geo Holmes <geometryzen@gmail.com>
2013-07-11 06:47:52 -0700

    Save file.

--- browser/canvas/shadows.py
+++ browser/canvas/shadows.py
@@ -1,1 +1,104 @@
-# shadows.py
+from browser import *
+
+useLargeCanvas = False
+
+def backingScale(context):
+    if window.devicePixelRatio and context.webkitBackingStorePixelRatio:
+        return window.devicePixelRatio
+    else:
+        return 1
+
+canvas = document.createElement("canvas")
+
+context = canvas.getContext("2d")
+
+pixelRatio = backingScale(context)
+# Multiply width and height of canvas by the backing scale.
+# Drawing instructions that refer to points in the coordinate space
+# must also be multiplied by the backing scale.
+canvas.height = 400 * pixelRatio
+canvas.width = 400 * pixelRatio
+
+def escKey(downFlag):
+    terminate()
+
+keyHandlers = {
+ 27: escKey
+}
+    
+def onDocumentKeyDown(event):
+    event.preventDefault()
+    keyHandlers[event.keyCode](True)
+
+def onDocumentKeyUp(event):
+    event.preventDefault()
+    keyHandlers[event.keyCode](False)
+
+def onWindowResize():
+    if (useLargeCanvas):
+        canvas.width = window.innerWidth * pixelRatio
+        canvas.height = window.innerHeight * pixelRatio
+    else:
+        container = document.getElementById("canvas-container")
+        canvas.width = container.clientWidth * pixelRatio
+        canvas.height = container.clientHeight * pixelRatio
+    
+def discardCanvases():
+    for cs in document.getElementsByTagName("canvas"):
+        cs.parentNode.removeChild(cs)
+        
+requestID = None
+progress = None
+progressEnd = 10000
+startTime =  None
+
+def init():
+    print "Press ESC to terminate."
+    print "This program will 'self-terminate' in "+str(progressEnd/1000)+" seconds!"
+    discardCanvases()
+    if useLargeCanvas:
+        document.body.insertBefore(canvas, document.body.firstChild)
+    else:
+        container = document.getElementById("canvas-container")
+        container.appendChild(canvas)
+    
+    document.addEventListener("keydown", onDocumentKeyDown, False)
+    document.addEventListener("keyup", onDocumentKeyUp, False)
+
+    window.addEventListener("resize", onWindowResize, False)
+    onWindowResize()
+
+def render():
+    context.font = "48pt Helvetica"
+    context.fillStyle = "blue"
+    context.fillText("Geometry Zen", 0, 100)
+    context.setTransform(1, 0, 0, -1, 0, 0)
+    context.fillStyle = "red"
+    context.fillText("Geometry Zen", 0, -100)
+
+    
+def animate(timestamp):
+    global requestID, progress, startTime
+    if startTime:
+        progress = timestamp - startTime
+    else:
+        if timestamp:
+            startTime = timestamp
+        else:
+            progress = 0
+        
+    if progress < progressEnd:
+        requestID = window.requestAnimationFrame(animate)
+        render()
+    else:
+        terminate()
+        
+def terminate():
+    window.cancelAnimationFrame(requestID)
+    discardCanvases()
+    document.removeEventListener("keydown", onDocumentKeyDown, False)
+    document.removeEventListener("keyup", onDocumentKeyUp, False)
+    print "Done."
+
+init()
+animate(None)
