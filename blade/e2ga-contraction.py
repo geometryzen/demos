ec431803fd2f0ec18afd8ae19e2287859866bb3f
David Geo Holmes <geometryzen@gmail.com>
2013-07-24 19:37:48 -0700

    Save file.

--- blade/e2ga-contraction.py
+++ blade/e2ga-contraction.py
@@ -49,4 +49,4 @@ for A in blades:
     for B in blades:
         M = A << B
         N = ~((~A) >> (~B)) 
-        assertEqual(M, N, str(A) + " << " + str(B) + " => "+ str(M) + ", S/B " + +str(A) + " >> " + str(B) + str(N))
+        assertEqual(M, N, str(A) + " << " + str(B) + " => "+ str(M) + ", S/B " + str(A) + " >> " + str(B) + str(N))
