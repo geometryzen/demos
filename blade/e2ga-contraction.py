7bcee32ebb0294249d230cb3b2207f909fc15b55
David Geo Holmes <geometryzen@gmail.com>
2013-07-24 19:05:23 -0700

    Save file.

--- blade/e2ga-contraction.py
+++ blade/e2ga-contraction.py
@@ -28,39 +28,6 @@ j    = explain(Vector(0, 1))
 I    = explain(Pseudoscalar(1))
 
 blades = [zero, one, two, i, j, I]
-
-print ""
-print "Sum"
-for a in blades:
-    print ""
-    for b in blades:
-        showValue(str(a) + " + " + str(b), repr(a + b))
-print ""
-print "Difference"
-for a in blades:
-    print ""
-    for b in blades:
-        showValue(str(a) + " - " + str(b), repr(a - b))
-print ""
-print "Geometric Product"
-for a in blades:
-    print ""
-    for b in blades:
-        showValue(str(a) + " * " + str(b), repr(a * b))
-print ""
-print "Exterior Product"
-for a in blades:
-    print ""
-    for b in blades:
-        showValue(str(a) + " ^ " + str(b), repr(a ^ b))
-print ""
-print "Left Contraction"
-for a in blades:
-    print ""
-    for b in blades:
-        showValue(str(a) + " << " + str(b), repr(a << b))
-print ""
-print "Right Contraction"
 for a in blades:
     print ""
     for b in blades:
