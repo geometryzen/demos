2c7c20c714a63e68c6dd7458f6132e1b302753e9
David Geo Holmes <geometryzen@gmail.com>
2013-07-25 17:04:29 -0700

    Save file.

--- blade/e3ga-contraction.py
+++ blade/e3ga-contraction.py
@@ -1,1 +1,53 @@
-# e3ga-contraction.py
+from e2ga import *
+import math
+
+def isCloseTo(expected, actual, precision):
+    return math.fabs(expected - actual) < (math.pow(10, -precision) / 2)
+
+def assertEqual(expect, actual, message):
+    if expect != actual:
+        print {"expect":expect,"actual":actual,"message":message}
+
+def assertCloseTo(expect, actual, message):
+    if expect != actual:
+        print {"expect":expect,"actual":actual,"message":message}
+
+def Scalar(w):
+    return Euclidean2(w, 0, 0, 0)
+
+def Vector(x, y):
+    return Euclidean2(0, x, y, 0)
+
+def Pseudoscalar(s):
+    return Euclidean2(0, 0, 0, s)
+
+def explain(m):
+    print str(m) + " is " + repr(m)
+    return m
+
+def showValue(name, x):
+    print name + " => " + str(x)
+    return x
+
+z = explain(Euclidean2(0,0,0,0))
+u = explain(Scalar(1))
+d = explain(Scalar(2))
+i   = explain(Vector(1, 0))
+j   = explain(Vector(0, 1))
+I   = explain(Pseudoscalar(1))
+
+assertEqual(+z, ~z, str(z))
+assertEqual(+u, ~u, str(u))
+assertEqual(+i, ~i, str(i))
+assertEqual(+j, ~j, str(j))
+assertEqual(-I, ~I, str(I))
+
+blades = [z, u, d, i, j, I]
+for A in blades:
+    print ""
+    for B in blades:
+        M = B >> A
+        Ms = str(B) + ">>" + str(A) + "=>"+ str(M)
+        N = ~((~A) << (~B))
+        Ns = "~(~" + str(A) + "<<~" + str(B) + ")=>" + str(N)
+        assertEqual(M, N, Ms  + " " + Ns)
