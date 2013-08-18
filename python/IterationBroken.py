7d15bcee0564166d3afdc9dded090d44eab3bb41
David Geo Holmes <geometryzen@gmail.com>
2013-08-18 07:21:50 -0700

    Save file.

--- python/IterationBroken.py
+++ python/IterationBroken.py
@@ -1,1 +1,12 @@
 # IterationBroken.py
+class MyTest:
+    def __init__(self,s):
+        self.w = list(s)
+
+    def __iter__(self):
+        return self.w.__iter__()
+
+x = MyTest("foo")
+
+for i in x:
+    print i
