154a644148ad9e105b9689454cdaec929d25d345
David Geo Holmes <geometryzen@gmail.com>
2013-08-23 07:35:20 -0700

    Delete item.

--- README.py
+++
@@ -1,7 +0,0 @@
-# README.py
-print "Geometry Zen Computational Models"
-
-# It might be interesting to make the README more interactive by letting it be a Python program?
-#
-# FYI: The rendering of markdown (*.md) documents will look a bit wierd in Geometry Zen until we 
-# recognize them as markdown and not Python programs.
