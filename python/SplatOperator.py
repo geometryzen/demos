fe9fc10e763c341fefda561cbab006c122e6cbed
David Geo Holmes <geometryzen@gmail.com>
2013-08-29 10:26:51 -0700

    Save file.

--- python/SplatOperator.py
+++ python/SplatOperator.py
@@ -1,7 +1,7 @@
 # The Python splat operator takes a list as input and expands it into
 # positional arguments in a function call.
-from eight import *
+from e3ga import *
 
-print repr(Vector3(1,2,3))    # Vector3(1, 2, 3)
+print repr(VectorE3(1,2,3))    # Vector3(1, 2, 3)
 
-print repr(Vector3(*[4,5,6])) # Vector3(4, 5, 6)
+print repr(VectorE3(*[4,5,6])) # Vector3(4, 5, 6)
