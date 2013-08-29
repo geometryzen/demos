0cd5374cd26887c8c30f54431d63674e3d144e78
David Geo Holmes <geometryzen@gmail.com>
2013-08-29 13:30:10 -0700

    Save file.

--- units/units-of-measure.py
+++ units/units-of-measure.py
@@ -1,11 +1,11 @@
 from e3ga import *
 from units import kilogram, meter, second, newton, joule, watt
  
-m = Scalar3(10) * kilogram
+m = ScalarE3(10) * kilogram
  
 print "m => " + str(m)
  
-g = 9.81 * Vector3(0, 0, -1) * newton / kilogram
+g = 9.81 * VectorE3(0, 0, -1) * newton / kilogram
 print "g => " + str(g)
  
 F = m * g
