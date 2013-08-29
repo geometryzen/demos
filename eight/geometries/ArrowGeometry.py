5fd8737464fc3c22f6b6b5a04a5999b3531928b3
David Geo Holmes <geometryzen@gmail.com>
2013-08-28 20:33:04 -0700

    Save file.

--- eight/geometries/ArrowGeometry.py
+++ eight/geometries/ArrowGeometry.py
@@ -55,7 +55,7 @@ def render():
         
     renderer.render(scene, camera)
 
-def onWindowResize():
+def onWindowResize(event):
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.size = (window.innerWidth, window.innerHeight)
