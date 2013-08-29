e640db2c616be6a5ad8adf3e6e2703d355756e3f
David Geo Holmes <geometryzen@gmail.com>
2013-08-29 12:46:14 -0700

    Save file.

--- three/materials/MeshLambertMaterial.py
+++ three/materials/MeshLambertMaterial.py
@@ -41,9 +41,6 @@ print "color: " + str(material.color)
 print "needsUpdate: " + str(material.needsUpdate)
 print "opacity: " + str(material.opacity)
 print "overdraw: " + str(material.overdraw)
-material.skinning = True
-material.skinning = False
-print "skinning: " + str(material.skinning)
 print "transparent: " + str(material.transparent)
 print "visible: " + str(material.visible)
 print "str(material) => " + str(material)
