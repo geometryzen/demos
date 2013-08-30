97bdf7c0551e5be3c06792555e45646ca2e710f1
David Geo Holmes <geometryzen@gmail.com>
2013-08-30 13:27:37 -0700

    Save file.

--- units/fundamental-units.py
+++ units/fundamental-units.py
@@ -8,6 +8,11 @@ h = 6.6e-34 * joule * second
 
 print h
 
-G = 6.7e-11 * newton * meter ** 2 / kilogram ** 2
+G = 6.7e-11 * newton * (meter ** 2) / kilogram ** 2
 
 print G
+
+length = (h * G / c ** 3) ** 0.5
+
+print length
+
