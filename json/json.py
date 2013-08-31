e9f5978cb035538b8900dae74a6e3d9b0042a02f
David Geo Holmes <geometryzen@gmail.com>
2013-08-31 10:56:23 -0700

    Save file.

--- json/json.py
+++ json/json.py
@@ -9,7 +9,7 @@ x = json.parse('{"name":"David", "height":1.8542, "favorite":6, "male":true, "re
 
 print json.stringify(x)
 #print json.stringify(x, None)
-#print json.stringify(x, None, 5)
+print json.stringify(x, None, 5)
 print json.stringify(x, replacer, 5)
 
 # This gives a TypeError 'function' does not support indexing
