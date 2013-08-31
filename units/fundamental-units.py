879e252c5fa7e0f41763174fd3859b0432627102
David Geo Holmes <geometryzen@gmail.com>
2013-08-31 14:54:04 -0700

    Save file.

--- units/fundamental-units.py
+++ units/fundamental-units.py
@@ -4,6 +4,7 @@ from units import *
 c = 3e+8 * meter / second ** 2
 
 print c
+print "c: " + repr(c)
 
 h = 6.6e-34 * joule * second
 
@@ -13,6 +14,8 @@ print "h: " + repr(h)
 G = 6.7e-11 * newton * (meter ** 2) / (kilogram ** 2)
 
 print G
+print "G: " + repr(G)
+
 print repr((h * G / c ** 3))
 planckL = (h * G / c ** 3) ** 0.5
 
