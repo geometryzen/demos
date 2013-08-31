85835e2aa22e7ef25a1b3d72f773e2f4179871ea
David Geo Holmes <geometryzen@gmail.com>
2013-08-31 10:16:14 -0700

    Save file.

--- json/json.py
+++ json/json.py
@@ -2,18 +2,9 @@ import json
 
 x = json.parse('{"name":"David", "height":1.8542, "favorite":6, "male":true, "religion":null}')
 
-print x
-print type(x)
-print x['name']
-print repr(x["name"])
-print x['height']
-print type(x['height'])
-print x['favorite']
-print type(x['favorite'])
-
+print json.stringify(x)
+print json.stringify(x, None)
 print json.stringify(x, None, 2)
 
 # This gives a TypeError 'function' does not support indexing
 #x = json.parse['["David"]']
-
-print x
