813d0e4d53257809b1f857ac698bb21d72a01b5b
David Geo Holmes <geometryzen@gmail.com>
2013-08-31 15:28:58 -0700

    Save file.

--- units/measures.py
+++ units/measures.py
@@ -1,5 +1,6 @@
 from units import *
 
 print "meter => " + str(meter)
+# Units don't really add using traditional arithmetic. Why?
 print "meter + meter => " + str(meter + meter)
 print "meter * meter => " + str(meter * meter)
