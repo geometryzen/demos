a6170bc62ee06f30c224afeb46149abd12c29d02
David Geo Holmes <geometryzen@gmail.com>
2013-08-31 12:44:06 -0700

    Save file.

--- json/json.py
+++ json/json.py
@@ -1,9 +1,9 @@
 import json
 
-#def givesNone():
-#    return
+def givesNone():
+    return
 
-#print "givesNone() => " + str(givesNone())
+print "givesNone() => " + str(givesNone())
 
 # Note: It's not possible to return 'undefined' and drop an attribute.
 def replacer(key, value):
@@ -16,14 +16,14 @@ def replacer(key, value):
     else:
         return None
 
-#x = json.parse('{"name":"David", "height":1.8542, "favorite":6, "male":true, "religion":null}')
+x = json.parse('{"name":"David", "height":1.8542, "favorite":6, "male":true, "religion":null}')
 
-#print type(replacer)
+print type(replacer)
 
-#print json.stringify(x)
-#print json.stringify(x, None)
-#print json.stringify(x, None, 5)
-#print json.stringify(x, replacer, 5)
+print json.stringify(x)
+print json.stringify(x, None)
+print json.stringify(x, None, 5)
+print json.stringify(x, replacer, 5)
 
 # This gives a TypeError 'function' does not support indexing
 #x = json.parse['["David"]']
