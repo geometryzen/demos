75bb80b28d82b3e01dcaa547fba091756c343da4
David Geo Holmes <geometryzen@gmail.com>
2013-08-31 16:50:52 -0700

    Save file.

--- units/measures.py
+++ units/measures.py
@@ -1,7 +1,7 @@
 from units import *
 
 print "meter => " + str(meter)
-print "meter * meter => "  + str(meter * meter)
-print "meter + meter => "  + str(meter + meter)
+print "meter * meter  => " + str(meter * meter)
+print "meter + meter  => " + str(meter + meter)
 print "meter * second => " + str(meter * second)
-print "meter + meter => "  + str(meter + meter)
+print "meter + second => " + str(meter + meter)
