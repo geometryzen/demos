90a0cfe03fa382e350d3cf2f2482262ee995e029
David Geo Holmes <geometryzen@gmail.com>
2013-09-01 13:20:56 -0700

    Save file.

--- visual/prototype.py
+++ visual/prototype.py
@@ -27,7 +27,7 @@ renderer.setClearColor(Color(0x080808), 1.0)
 container = document.getElementById("canvas-container")
 container.appendChild(renderer.domElement)
 
-cube = box(length=2)
+cube = box(length=2, height=2, width=2)
 
 scene.add(cube)
 
