d82227ed6a4af0ae0d81aeb390940878e70aebd3
David Geo Holmes <geometryzen@gmail.com>
2013-09-01 15:51:22 -0700

    Save file.

--- visual/prototype.py
+++ visual/prototype.py
@@ -8,7 +8,7 @@ from browser import *
 for canvas in document.getElementsByTagName("canvas"):
     canvas.parentNode.removeChild(canvas)
 
-scene = Scene()
+scene = world()
 
 camera = PerspectiveCamera(45, 1.0, 0.1, 10000)
 camera.position.set(2, 2, 2)
