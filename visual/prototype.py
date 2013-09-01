8b1ba6f639fcd5e4b17402542ea932629e018957
David Geo Holmes <geometryzen@gmail.com>
2013-09-01 15:11:45 -0700

    Save file.

--- visual/prototype.py
+++ visual/prototype.py
@@ -27,7 +27,7 @@ renderer.setClearColor(Color(0x080808), 1.0)
 container = document.getElementById("canvas-container")
 container.appendChild(renderer.domElement)
 
-shape = cylinder()
+shape = cuboid()
 
 scene.add(shape)
 
