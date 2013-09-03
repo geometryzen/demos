da96c1c0ac8d7e1488bbb76887eb935096b6c117
David Geo Holmes <geometryzen@gmail.com>
2013-09-02 19:51:36 -0700

    Save file.

--- three/lights/PointLight.py
+++ three/lights/PointLight.py
@@ -3,9 +3,6 @@
 from three import *
 from browser import *
 
-for canvas in document.getElementsByTagName("canvas"):
-    canvas.parentNode.removeChild(canvas)
-
 scene = Scene()
 
 camera = PerspectiveCamera(45, 1.0, 0.1, 10000)
@@ -32,41 +29,32 @@ mesh = Mesh(CubeGeometry(5, 5, 5), material)
 
 scene.add(mesh)
 
-requestID = None
-progress = None
-progressEnd = 6000
-startTime = None
 movement = Vector3(0.02, 0.02, 0.02)
 
-def render():
+def discardCanvases():
+    for canvas in document.getElementsByTagName("canvas"):
+        canvas.parentNode.removeChild(canvas)
+
+def setUp():
+    # Do nothing
+    
+def tick(elapsed):
     mesh.rotation += movement
-        
     renderer.render(scene, camera)
+    
+def terminate(elapsed):
+    return elapsed > 6000
+
+def tearDown():
+    discardCanvases()
 
 def onWindowResize(event):
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.size = (window.innerWidth, window.innerHeight)
-    
-def animate(timestamp):
-    global requestID, progress, startTime
-    if (startTime):
-        progress = timestamp - startTime
-    else:
-        if (timestamp):
-            startTime = timestamp
-        else:
-            progress = 0
-        
-    if (progress < progressEnd):
-        requestID = window.requestAnimationFrame(animate)
-        render()
-    else:
-        window.cancelAnimationFrame(requestID)
-        # container.removeChild(renderer.domElement)
 
 window.addEventListener("resize", onWindowResize, False)
 
 onWindowResize(None)
 
-animate(None)
+WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
