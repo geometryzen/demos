0370b21a1f93af19df09edd578149e2fac80c887
David Geo Holmes <geometryzen@gmail.com>
2013-09-02 20:37:31 -0700

    Save file.

--- visual/prototype.py
+++ visual/prototype.py
@@ -1,11 +1,3 @@
-# This is a work in progress. It may not work for you yet!
-# The idea is to create a simple 3D visualization library.
-# The library will be similar to Visual Python.
-#
-# Sep 1, 2013 world, cube, cylinder, sphere
-# Sep 2, 2013 WindowAnimationRunner
-# TODO: Managing the size for the renderer and aspect for the camera.
-# TODO: Shape color and name.
 from three import *
 from browser import *
 
@@ -24,35 +16,23 @@ container = None
 
 movement = Vector3(0.02, 0.02, 0.02)
 
-def removeElementsByTagName(tagName):
-    for element in document.getElementsByTagName(tagName):
+def discardCanvasElements():
+    for element in document.getElementsByTagName("canvas"):
         element.parentNode.removeChild(element)
 
 def tick(elapsed):
-    """
-    Called by the animation runner for each animation frame.
-    """
     shape.rotation += movement
-        
     renderer.render(scene, camera)
     
 def terminate(elapsed):
-    """
-    Called by the animation runner to determine if the animation should terminate.
-    """
     return elapsed > 10000
 
 def setUp():
-    """
-    Called by the animation runner before the animation has started.
-    """
     global renderer
 
-    removeElementsByTagName("canvas")
+    discardCanvasElements()
 
     renderer = WebGLRenderer()
-    #renderer.gammaInput = True
-    #renderer.gammaOutput = True
     renderer.setClearColor(Color(0x080808), 1.0)
 
     container = document.getElementById("canvas-container")
@@ -65,9 +45,6 @@ def setUp():
 
 
 def tearDown():
-    """
-    Called by the animation runner after the animation has finished.
-    """
     removeElementsByTagName("canvas")
 
 WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
