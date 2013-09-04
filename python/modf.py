f461821263669c428052ac7091b1c5f50a594ed8
David Geo Holmes <geometryzen@gmail.com>
2013-09-04 14:21:38 -0700

    Save file.

--- python/modf.py
+++ python/modf.py
@@ -1,1 +1,6 @@
-# modf.py
+import math
+
+print "math.modf(100.12) : ", math.modf(100.12)
+print "math.modf(100.72) : ", math.modf(100.72)
+print "math.modf(119L) : ", math.modf(119L)
+print "math.modf(math.pi) : ", math.modf(math.pi)
