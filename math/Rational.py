a83f056041dadb96c216e6b47b5a020b2a26281f
David Geo Holmes <geometryzen@gmail.com>
2013-09-04 09:52:44 -0700

    Save file.

--- math/Rational.py
+++ math/Rational.py
@@ -1,5 +1,6 @@
 from units import *
 
+# If the denominator is defined, Rational requires exactly two arguments.
 try:
     x = Rational(1,2,3)
 except AssertionError as e:
@@ -7,6 +8,7 @@ except AssertionError as e:
 else:
     print "Expecting a Rational constructed with three arguments to raise an error."
 
+Rational(1.1, 1)
 a = Rational(1,3)
 print a
 b = Rational(5,2)
