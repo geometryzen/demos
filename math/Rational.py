33231dc0ab36d937885aa9095469e16e862ad6a6
David Geo Holmes <geometryzen@gmail.com>
2013-09-04 11:43:00 -0700

    Save file.

--- math/Rational.py
+++ math/Rational.py
@@ -7,32 +7,32 @@ def assertEqual(actual, expect):
         print {"actual": actual, "expect": expect}
 
 try:
-    x = Rational(1,2,3)
+    x = Fraction(1,2,3)
 except AssertionError as e:
     assertEqual(str(e),"AssertionError: Rational() takes exactly 2 arguments (3 given) on line 10")
 else:
     print "Expecting error"
 
 try:
-    Rational(1.1, 2)
+    Fraction(1.1, 2)
 except TypeError as e:
     assertEqual(str(e),"TypeError: numerator must be a int on line 17")
 else:
     print "Expecting error"
 
 try:
-    Rational(1, 2.2)
+    Fraction(1, 2.2)
 except TypeError as e:
     assertEqual(str(e),"TypeError: denominator must be a int on line 24")
 else:
     print "Expecting error"
 
-a = Rational(2,3)
+a = Fraction(2,3)
 assertEqual(str(a),"2/3")
 assertEqual(repr(a),"Rational(2,3)")
 assertEqual(a.numer, 2)
 print a
-b = Rational(5,2)
+b = Fraction(5,2)
 print b
 print a + b
 print a - b
