fbff3a86448421c9e3c9f7c156dd38b66dfb0a0b
David Geo Holmes <geometryzen@gmail.com>
2013-09-04 12:50:29 -0700

    Save file.

--- math/Fraction.py
+++ math/Fraction.py
@@ -48,7 +48,7 @@ assertEqual(Fraction(123).denom, 1)
 assertEqual(Fraction().numer, 0)
 assertEqual(Fraction().denom, 1)
 
-assertEquals(a + b, Fraction(19, 6))
+assertEqual(a + b, Fraction(19, 6))
 print b
 print a + b
 print a - b
