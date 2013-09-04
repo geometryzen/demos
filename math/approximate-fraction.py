17bd2d537e1c6dd72f4665a78c1f7aa9eb62263c
David Geo Holmes <geometryzen@gmail.com>
2013-09-04 14:35:53 -0700

    Save file.

--- math/approximate-fraction.py
+++ math/approximate-fraction.py
@@ -35,6 +35,6 @@ def approximate_fraction(x, e):
     from x by no more than e."""
     return simplest_fraction_in_interval(x - e, x + e)
 
-assertEqual(approximate_fraction(6.75, 0.01), Fraction(27,5))
+assertEqual(approximate_fraction(6.75, 0.01), Fraction(27,4))
 print approximate_fraction(pi, 0.00001) # 355/113
 print approximate_fraction((1 + sqrt(5)) / 2, 0.00001) #377/233
