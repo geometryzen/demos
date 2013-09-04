520a6c220bf01de9bd86cc22d643877cd0ffa36b
David Geo Holmes <geometryzen@gmail.com>
2013-09-04 09:50:27 -0700

    Save file.

--- math/Rational.py
+++ math/Rational.py
@@ -3,9 +3,7 @@ from units import *
 try:
     x = Rational(1,2,3)
 except AssertionError as e:
-    print repr(e)
     print e
-    #print "I/O error({0}): {1}".format(e.errno, e.strerror)
 else:
     print "Expecting a Rational constructed with three arguments to raise an error."
 
