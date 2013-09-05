6233a264af895c3de7bf391c7b7ece18ae1ad2f1
David Geo Holmes <geometryzen@gmail.com>
2013-09-04 19:58:12 -0700

    Save file.

--- visual/prototype.py
+++ visual/prototype.py
@@ -5,7 +5,7 @@ scene = world()
 
 shape = cylinder()
 shape.name = "rod"
-shape.matrixAutoUpdate = False
+shape.matrixAutoUpdate = True
 scene.add(shape)
 
 camera = PerspectiveCamera(45, 1.0, 0.1, 10000)
