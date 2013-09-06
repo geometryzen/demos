4f10d096adaf2b390af3ba7fe4f31bd48a5cca8a
David Geo Holmes <geometryzen@gmail.com>
2013-09-05 17:19:27 -0700

    Save file.

--- three/geometries/ArrowGeometry.py
+++ three/geometries/ArrowGeometry.py
@@ -1,7 +1,7 @@
 # ArrowGeometry is not a standard Geometry in the Three.js library.
 # It is created from the LatheGeometry using a set of points that create the cone and shaft
 # of an arrow.
-from three import Scene, PerspectiveCamera, ArrowGeometry, MeshNormalMaterial
+from three import Scene, PerspectiveCamera, ArrowGeometry, MeshNormalMaterial, Mesh, AmbientLight
 from browser import document, window, WindowAnimationRunner
 
 scene = Scene()
