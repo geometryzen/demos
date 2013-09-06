70608cf3a7169f6f79b23e72aa4b086905f5dee1
David Geo Holmes <geometryzen@gmail.com>
2013-09-06 12:21:36 -0700

    Save file.

--- visual/bleeding-edge.py
+++ visual/bleeding-edge.py
@@ -1,5 +1,5 @@
 # TODO: We don't want world and shapes to come from three anymore.
-from visual import World, world, cylinder, PerspectiveCamera, WebGLRenderer, Color, Vector3
+from visual import EuclideanGeometry3, world, cylinder, PerspectiveCamera, WebGLRenderer, Color, Vector3
 from browser import document, window, WindowAnimationRunner
 
 world = World()
