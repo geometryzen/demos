54a171cd79738cd1804bf0c1c3d5c0a5cb7387e6
David Geo Holmes <geometryzen@gmail.com>
2013-09-06 10:57:31 -0700

    Save file.

--- units/fundamental-units.py
+++ units/fundamental-units.py
@@ -15,6 +15,7 @@ print "Universal gravitational constant, G=" + str(G)
 print ""
 lp = (hbar * G / c ** 3) ** 0.5
 print "Planck length: " + str(lp)
+print "Planck length: " + str(lp/cm) + " cm"
 
 mp = (hbar * c / G) ** 0.5
 print "Planck mass:   " + str(mp)
