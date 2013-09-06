1407f0369d3b8da17416eb83ecd59a69ee11ff89
David Geo Holmes <geometryzen@gmail.com>
2013-09-05 23:07:21 -0700

    Save file.

--- visual/bleeding-edge.py
+++ visual/bleeding-edge.py
@@ -1,1 +1,46 @@
-# bleeding-edge.py
+# TODO: We don't want world and shapes to come from three anymore.
+from three import world, cylinder, PerspectiveCamera, WebGLRenderer, Color, Vector3
+from browser import document, window, WindowAnimationRunner
+
+scene = world()
+
+shape = cylinder()
+scene.add(shape)
+
+camera = PerspectiveCamera(45, 1.0, 0.1, 10000)
+camera.position.set(8, 8, 8)
+camera.lookAt(scene.position)
+
+renderer = None
+container = None
+
+movement = Vector3(0.02, 0.02, 0.02)
+
+def discardCanvasElements():
+    for element in document.getElementsByTagName("canvas"):
+        element.parentNode.removeChild(element)
+
+def tick(elapsed):
+    shape.rotation += movement
+    renderer.render(scene, camera)
+    
+def terminate(elapsed):
+    return elapsed > 6000
+
+def setUp():
+    global renderer
+
+    discardCanvasElements()
+
+    renderer = WebGLRenderer()
+    renderer.setClearColor(Color(0x080808), 1.0)
+
+    document.body.insertBefore(renderer.domElement, document.body.firstChild)
+    renderer.size = (window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+def tearDown():
+    discardCanvasElements()
+
+WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
