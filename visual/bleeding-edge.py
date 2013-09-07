2a97024bbaf70303b3bdde7b4a7f5924f3984981
David Geo Holmes <geometryzen@gmail.com>
2013-09-07 09:05:07 -0700

    Save file.

--- visual/bleeding-edge.py
+++ visual/bleeding-edge.py
@@ -8,7 +8,7 @@ def onWindowResize(event):
     space.viewSize(window.innerWidth, window.innerHeight)
 
 def setUp():
-    document.removeElementsByTagName("canvas")
+    document.removeElementsByTagName(123)#"canvas")
     document.body.insertBefore(space.renderer.domElement, document.body.firstChild)
     window.addEventListener("resize", onWindowResize, False)
     onWindowResize(None)
