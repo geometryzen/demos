5095adb7992c4921e343007797928a32918a5eb2
David Geo Holmes <geometryzen@gmail.com>
2013-09-07 05:50:43 -0700

    Save file.

--- browser/document/innerHTML.py
+++ browser/document/innerHTML.py
@@ -2,5 +2,5 @@ from browser import document
 
 pre = document.getElementById('a5f435e0-c92e-11e2-8b8b-0800200c9a66')
 pre.innerHTML = '''
-<h1> Skulpt can also access DOM! </h1>
+<h1>Hello!</h1>
 ''' 
