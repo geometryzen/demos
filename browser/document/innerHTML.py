b699e9fbf6e06aeed6a815c0e0cca7d631ebe804
David Geo Holmes <geometryzen@gmail.com>
2013-09-07 07:09:02 -0700

    Save file.

--- browser/document/innerHTML.py
+++ browser/document/innerHTML.py
@@ -2,7 +2,5 @@ from browser import document
 
 pre = document.getElementById('a5f435e0-c92e-11e2-8b8b-0800200c9a66')
 print repr(pre)
-pre.innerHTML = '''
-<h1>Hello!</h1>
-'''
+pre.innerHTML = "<h1>Hello!</h1>"
 print "Done!"
