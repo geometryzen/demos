6cadcfcb179d28b59110994880a1cfc3a7b98824
David Geo Holmes <geometryzen@gmail.com>
2013-09-06 19:02:26 -0700

    Save file.

--- visual/bleeding-edge.py
+++ visual/bleeding-edge.py
@@ -8,17 +8,17 @@ def discardCanvasElements():
     for element in document.getElementsByTagName("canvas"):
         element.parentNode.removeChild(element)
 
-def tick(elapsed):
-    space.render()
-    
-def terminate(elapsed):
-    return elapsed > 6000
-
 def setUp():
     discardCanvasElements()
 
     document.body.insertBefore(space.renderer.domElement, document.body.firstChild)
     space.viewSize(window.innerWidth, window.innerHeight)
+
+def tick(elapsed):
+    space.render()
+    
+def terminate(elapsed):
+    return elapsed > 6000
 
 def tearDown():
     discardCanvasElements()
