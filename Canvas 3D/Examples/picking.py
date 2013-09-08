033812f1701cdd73e7aa29f021327701dcba4e69
David Geo Holmes <geometryzen@gmail.com>
2013-09-08 12:47:41 -0700

    Save file.

--- Canvas 3D/Examples/picking.py
+++ Canvas 3D/Examples/picking.py
@@ -1,6 +1,15 @@
 from three import *
 
+projector = Projector();
+
 def onDocumentMouseDown(event):
+    # Convert window coordinates to normalized device coordinates.
     mouseVector = Vector3(2 * (event.clientX / canvasWidth) - 1, 1 - 2 * (event.clientY - canvasHeight)
-    projector = Projector();
     raycaster = projector.pickingRay(mouseVector.clone(), camera)
+    intersects = raycaster.intersectObject(objects)
+    if (intersects.length > 0):
+        intersects[0].object.material.color.setRGB(random(), random(), random())
+        
+        sphere = Mesh(sphereGeom, sphereMaterial)
+        sphere.position = intersects[0].point
+        scene.add(sphere)
