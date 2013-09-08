65d0effec8651ecd60b82bf683a7981a812d8198
David Geo Holmes <geometryzen@gmail.com>
2013-09-08 12:43:05 -0700

    Save file.

--- Canvas 3D/Examples/picking.py
+++ Canvas 3D/Examples/picking.py
@@ -1,1 +1,6 @@
-# Examples/picking.py
+from three import *
+
+def onDocumentMouseDown(event):
+    mouseVector = Vector3(2 * (event.clientX / canvasWidth) - 1, 1 - 2 * (event.clientY - canvasHeight)
+    projector = Projector();
+    raycaster = projector.pickingRay(mouseVector.clone(), camera)
