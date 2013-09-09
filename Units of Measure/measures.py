5b8392f1c1f5223fbc3d8fd95219a8e5487d8e92
David Geo Holmes <geometryzen@gmail.com>
2013-09-09 13:07:31 -0700

    Save file.

--- Units of Measure/measures.py
+++ Units of Measure/measures.py
@@ -1,6 +1,12 @@
 from e3ga import *
 from units import *
 
+def assertEqual(actual, expect):
+    if expect == actual:
+        pass
+    else:
+        print {"actual": actual, "expect": expect}
+
 velocity = VectorE3(1,0,0) * meter / second ** 2
 mass = ScalarE3(10) * kilogram
 print repr(kilogram)
@@ -20,5 +26,5 @@ print "meter * second => " + str(meter * second)
 # The following operation raises
 try:
     print "meter + second => " + str(meter + second)
-except: e
+except e:
     print e
