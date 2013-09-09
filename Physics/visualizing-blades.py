f4c9f6d819c262e2eaabf8102374b22ee318973f
David Geo Holmes <geometryzen@gmail.com>
2013-09-09 13:25:33 -0700

    Save file.

--- Physics/visualizing-blades.py
+++ Physics/visualizing-blades.py
@@ -1,1 +1,30 @@
-# visualizing-blades.py
+'''
+This example represents efforts to provide convenient abstractions
+of the Three.js WebGL Computer Graphics API for use with Physics simulations.
+'''
+from geometry import *
+from browser import *
+
+space = CartesianSpace()
+space.add(cylinder())
+
+def onWindowResize(event):
+    space.viewSize(window.innerWidth, window.innerHeight)
+
+def tick(elapsed):
+    space.render()
+    
+def terminate(elapsed):
+    return elapsed > 6000
+
+def setUp():
+    document.removeElementsByTagName("canvas")
+    document.body.insertBefore(space.renderer.domElement, document.body.firstChild)
+    window.addEventListener("resize", onWindowResize, False)
+    onWindowResize(None)
+
+def tearDown():
+    window.removeEventListener("resize", onWindowResize, False)
+    document.removeElementsByTagName("canvas")
+
+WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
