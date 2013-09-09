14cfad6cd717c10b90bdfe13c50cd4e382dd861d
David Geo Holmes <geometryzen@gmail.com>
2013-09-09 16:26:54 -0700

    Save file.

--- Physics/visualizing-blades.py
+++ Physics/visualizing-blades.py
@@ -8,7 +8,7 @@ from browser import *
 from math import pi
 
 space = CartesianSpace()
-shape = cylinder()
+shape = cube()
 # The Graphics Library uses Vector3, but Geometric Algebra uses Euclidean3.
 # We'd like to have a way of not experiencing this mismatch.
 print space.camera.up
