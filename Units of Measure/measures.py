ea93e2776962d704f07286bf2b5490c025e821c7
David Geo Holmes <geometryzen@gmail.com>
2013-09-09 13:23:36 -0700

    Save file.

--- Units of Measure/measures.py
+++ Units of Measure/measures.py
@@ -9,8 +9,6 @@ def assertEqual(actual, expect):
 
 velocity = VectorE3(1,0,0) * meter / second ** 2
 mass = ScalarE3(10) * kilogram
-print repr(kilogram)
-mass = kilogram * ScalarE3(10)
 
 print velocity
 
