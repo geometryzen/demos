945d12907231cb51d7efad8621d8fb06559d2d54
David Geo Holmes <geometryzen@gmail.com>
2013-09-09 14:27:12 -0700

    Save file.

--- Physics/visualizing-blades.py
+++ Physics/visualizing-blades.py
@@ -7,13 +7,13 @@ from e3ga import *
 from browser import *
 
 origin = VectorE3(0, 0, 0)
-i = VectorE3(2,0,0)
+i = VectorE3(1,0,0)
 space = CartesianSpace()
 shape = cylinder()
 # The Graphics Library uses Vector3, but Geometric Algebra uses Euclidean3.
 # We'd like to have a way of not experiencing this mismatch.
 shape.position.set(origin.x, origin.y, origin.z)
-shape.translateOnAxis(Vector3(1,0,0),1)
+shape.translateOnAxis(Vector3(1,1,1),1)
 space.add(shape)
 
 def onWindowResize(event):
