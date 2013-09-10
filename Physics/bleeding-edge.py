5b8caeab49eefba60b3b5c5ffd6b29b2f450b243
David Geo Holmes <geometryzen@gmail.com>
2013-09-09 21:20:11 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -3,9 +3,24 @@ This example represents efforts to provide convenient abstractions
 of the Three.js WebGL Computer Graphics API for use with Physics simulations.
 '''
 from geometry import *
+from e3ga import *
 from browser import *
+from math import pi
 
 space = CartesianSpace()
+s1 = cylinder()
+s1.translateX(-2.5)
+s1.translateY(-2.5)
+s1.rotateX(pi/2)
+space.add(s1)
+s2 = cube()
+s2.translateX(2.5)
+s2.translateY(2.5)
+space.add(s2)
+s3 = sphere()
+s3.translateX(-2.5)
+s3.translateY(+2.5)
+space.add(s3)
 
 def onWindowResize(event):
     space.viewSize(window.innerWidth, window.innerHeight)
@@ -14,7 +29,7 @@ def tick(elapsed):
     space.render()
     
 def terminate(elapsed):
-    return elapsed > 6000
+    return elapsed > 5000
 
 def setUp():
     document.removeElementsByTagName("canvas")
