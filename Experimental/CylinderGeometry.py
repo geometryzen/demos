7e795e02e347f5228cbd7ade0ef8123733400f0e
David Geo Holmes <geometryzen@gmail.com>
2013-09-10 14:42:44 -0700

    Save file.

--- Experimental/CylinderGeometry.py
+++ Experimental/CylinderGeometry.py
@@ -28,9 +28,9 @@ def setUp():
     radiusSegments = 32
     heightSegments = 1
     openEnded = False
-    mesh = CylinderBuilder().radiusTop(radiusTop).radiusBottom(radiusBottom).height(height).build()
-    cylinder = mesh.geometry
-#    cylinder = CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded)
+#    mesh = CylinderBuilder().radiusTop(radiusTop).radiusBottom(radiusBottom).height(height).build()
+#    cylinder = mesh.geometry
+    cylinder = CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded)
 
     print repr(cylinder)
     print "radiusTop:      " + str(cylinder.radiusTop)
@@ -41,7 +41,7 @@ def setUp():
     print "openEnded:      " + str(cylinder.openEnded)
     print cylinder
 
-#    mesh = Mesh(cylinder, MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3}))
+    mesh = Mesh(cylinder, MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3}))
     scene.add(mesh)
 
     window.addEventListener("resize", onWindowResize, False)
