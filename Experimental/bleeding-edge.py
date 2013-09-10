56f7449b7fea83bbe39c2d70c7ebb6ef447be814
David Geo Holmes <geometryzen@gmail.com>
2013-09-10 13:12:12 -0700

    Save file.

--- Experimental/bleeding-edge.py
+++ Experimental/bleeding-edge.py
@@ -1,26 +1,45 @@
+'''
+This example represents efforts to provide convenient abstractions
+of the Three.js WebGL Computer Graphics API for use with Physics simulations.
+'''
 from geometry import *
+from e3ga import *
 from browser import *
+from math import pi
 
 space = CartesianSpace()
-space.add(cylinder())
+
+space.add(CylinderBuilder().color(0x00FF00).volume(1).build().translateX(-2.5).translateY(-2.5))
+space.add(CubeBuilder().color(0x0000FF).volume(1).build().translateX(2.5).translateY(2.5))
+space.add(SphereBuilder().color(0xFF0000).volume(1).build().translateX(+2.5).translateY(-2.5))
+space.add(ConeBuilder().color(0xFFFF00).volume(1).build().translateX(-2.5).translateY(+2.5))
+
+timeout = 600000
+
+def onDocumentKeyDown(event):
+    global timeout
+    if event.keyCode == 27:
+        timeout = 0
 
 def onWindowResize(event):
     space.viewSize(window.innerWidth, window.innerHeight)
-
-def setUp():
-    document.removeElementsByTagName("canvas")
-    document.body.insertBefore(space.renderer.domElement, document.body.firstChild)
-    window.addEventListener("resize", onWindowResize, False)
-    onWindowResize(None)
 
 def tick(elapsed):
     space.render()
     
 def terminate(elapsed):
-    return elapsed > 6000
+    return elapsed > timeout
+
+def setUp():
+    document.removeElementsByTagName("canvas")
+    document.body.insertBefore(space.renderer.domElement, document.body.firstChild)
+    document.addEventListener("keydown", onDocumentKeyDown, False)
+    window.addEventListener("resize", onWindowResize, False)
+    onWindowResize(None)
 
 def tearDown():
     window.removeEventListener("resize", onWindowResize, False)
+    document.removeEventListener("keydown", onDocumentKeyDown, False)
     document.removeElementsByTagName("canvas")
 
 WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
