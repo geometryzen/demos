8c9c38b4d3114fadd02435d67b57144ff0f5e672
David Geo Holmes <geometryzen@gmail.com>
2013-09-10 13:19:16 -0700

    Save file.

--- Experimental/CylinderGeometry.py
+++ Experimental/CylinderGeometry.py
@@ -1,6 +1,6 @@
 # The purpose of this experiment is to see if the details of a general example can be reproduced using convenience functions.
 from three import *
-from browser import *
+from browser import document
 from math import pi
 
 # Discard the old canvas if it exists. 
