b886b0cb2f903af37a523ef04d94e1b9d9958623
David Geo Holmes <geometryzen@gmail.com>
2013-09-10 06:10:12 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -15,7 +15,7 @@ space.add(sphere().translateX(+2.5).translateY(-2.5))
 
 space.add(ConeBuilder().build().translateX(-2.5).translateY(2.5).translateZ(0.5).rotateX(pi/2))
 
-timeout = 30000
+timeout = 60000
 
 def onDocumentKeyDown(event):
     global timeout
