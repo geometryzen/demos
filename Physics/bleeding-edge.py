3f7b2019119c79dfb43eab4c4adbb38d8b27e262
David Geo Holmes <geometryzen@gmail.com>
2013-09-09 21:28:37 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -9,11 +9,7 @@ from math import pi
 
 space = CartesianSpace()
 
-s1 = cylinder()
-s1.translateX(-2.5)
-s1.translateY(-2.5)
-s1.translateZ(0.5)
-s1.rotateX(pi/2)
+s1 = cylinder().translateX(-2.5).translateY(-2.5).translateZ(0.5).rotateX(pi/2)
 space.add(s1)
 
 s2 = cube()
