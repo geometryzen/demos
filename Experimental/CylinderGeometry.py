c9ba37dc11e960d3c27e6fd991b8f107eeb33fbb
David Geo Holmes <geometryzen@gmail.com>
2013-09-10 13:26:27 -0700

    Save file.

--- Experimental/CylinderGeometry.py
+++ Experimental/CylinderGeometry.py
@@ -3,49 +3,41 @@ from three import *
 from browser import document, window, WindowAnimationRunner
 from math import pi
 
-# Discard the old canvas if it exists. 
-for canvas in document.getElementsByTagName("canvas"):
-    canvas.parentNode.removeChild(canvas)
-
-renderer = WebGLRenderer({"antialias": True})
-renderer.setClearColor(Color(0x080808), 1.0)
-
-container = document.getElementById("canvas-container")
-container.appendChild(renderer.domElement)
-
-scene = Scene()
-
-camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 100
-
-radiusTop = 20
-radiusBottom = 20
-height = 100
-radiusSegments = 32
-heightSegments = 1
-openEnded = False
-cylinder = CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded)
-
-print repr(cylinder)
-print "radiusTop:      " + str(cylinder.radiusTop)
-print "radiusBottom:   " + str(cylinder.radiusBottom)
-print "height:         " + str(cylinder.height)
-print "radiusSegments: " + str(cylinder.radiusSegments)
-print "heightSegments: " + str(cylinder.heightSegments)
-print "openEnded:      " + str(cylinder.openEnded)
-print cylinder
-
-mesh = Mesh(cylinder, MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3}))
-scene.add(mesh)
-
-requestID = None
-progress = None
 progressEnd = 10000
-startTime =  None
 movement = Vector3(0.02, 0.02, 0.02)
 
 def setUp():
     document.removeElementsByTagName('canvas')
+    renderer = WebGLRenderer({"antialias": True})
+    renderer.setClearColor(Color(0x080808), 1.0)
+
+    container = document.getElementById("canvas-container")
+    container.appendChild(renderer.domElement)
+
+    scene = Scene()
+
+    camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
+    camera.position.z = 100
+
+    radiusTop = 20
+    radiusBottom = 20
+    height = 100
+    radiusSegments = 32
+    heightSegments = 1
+    openEnded = False
+    cylinder = CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded)
+
+    print repr(cylinder)
+    print "radiusTop:      " + str(cylinder.radiusTop)
+    print "radiusBottom:   " + str(cylinder.radiusBottom)
+    print "height:         " + str(cylinder.height)
+    print "radiusSegments: " + str(cylinder.radiusSegments)
+    print "heightSegments: " + str(cylinder.heightSegments)
+    print "openEnded:      " + str(cylinder.openEnded)
+    print cylinder
+
+    mesh = Mesh(cylinder, MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3}))
+    scene.add(mesh)
 
 def tick(elapsed):
     mesh.rotation += movement
