fe2aac1b9352d74b13b0cdfdfc44220b3f9f9efb
David Geo Holmes <geometryzen@gmail.com>
2013-09-10 12:56:06 -0700

    Save file.

--- Physics/visualizing-blades.py
+++ Physics/visualizing-blades.py
@@ -8,12 +8,18 @@ from browser import *
 from math import pi
 
 space = CartesianSpace()
-s1 = cylinder().translateX(-2.5).translateY(-2.5).rotateX(pi/2)
-space.add(s1)
-s2 = cube().translateX(2.5).translateY(2.5)
-space.add(s2)
-s3 = sphere().translateX(-2.5).translateY(+2.5)
-space.add(s3)
+
+space.add(CylinderBuilder().color(0x00FF00).volume(1).build().translateX(-2.5).translateY(-2.5))
+space.add(CubeBuilder().color(0x0000FF).volume(1).build().translateX(2.5).translateY(2.5))
+space.add(SphereBuilder().color(0xFF0000).volume(1).build().translateX(+2.5).translateY(-2.5))
+space.add(ConeBuilder().color(0xFFFF00).volume(1).build().translateX(-2.5).translateY(+2.5))
+
+timeout = 600000
+
+def onDocumentKeyDown(event):
+    global timeout
+    if event.keyCode == 27:
+        timeout = 0
 
 def onWindowResize(event):
     space.viewSize(window.innerWidth, window.innerHeight)
@@ -22,16 +28,18 @@ def tick(elapsed):
     space.render()
     
 def terminate(elapsed):
-    return elapsed > 10000
+    return elapsed > timeout
 
 def setUp():
     document.removeElementsByTagName("canvas")
     document.body.insertBefore(space.renderer.domElement, document.body.firstChild)
+    document.addEventListener("keydown", onDocumentKeyDown, False)
     window.addEventListener("resize", onWindowResize, False)
     onWindowResize(None)
 
 def tearDown():
     window.removeEventListener("resize", onWindowResize, False)
+    document.removeEventListener("keydown", onDocumentKeyDown, False)
     document.removeElementsByTagName("canvas")
 
 WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
