1f1036741ec48d82f598defe9ca977537a27f752
David Geo Holmes <geometryzen@gmail.com>
2013-09-10 19:15:06 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -11,6 +11,7 @@ space = CartesianSpace()
 
 space.add(CylinderBuilder().color(0x00FF00).volume(1).build().translateX(-2.5).translateY(-2.5))
 ball = CubeBuilder().color(0x0000FF).volume(1).build().translateX(2.5).translateY(2.5)
+ball.name = "x"
 space.add(ball)
 space.add(SphereBuilder().color(0xFF0000).volume(1).build().translateX(+2.5).translateY(-2.5))
 space.add(ConeBuilder().color(0xFFFF00).volume(1).build().translateX(-2.5).translateY(+2.5))
