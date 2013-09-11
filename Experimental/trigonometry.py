e0f0e81a27ef069e3a80f7bebbe4ac229cde605b
David Geo Holmes <geometryzen@gmail.com>
2013-09-11 14:36:21 -0700

    Save file.

--- Experimental/trigonometry.py
+++ Experimental/trigonometry.py
@@ -1,11 +1,12 @@
 from e2ga import *
 
+def showValue(name, m):
+    print name + " => " + str(m)
+    return m
+
 north = VectorE2(0, 1)
 east = VectorE2(1, 0)
 south = -north
 west = -east
 
-print north
-print east
-print south
-print west
+showValue("north", north)
