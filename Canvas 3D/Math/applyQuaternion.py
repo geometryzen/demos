221f2db4bccbac2ace205afa6d721e5227d2ea3d
David Geo Holmes <geometryzen@gmail.com>
2013-09-11 11:14:53 -0700

    Save file.

--- Canvas 3D/Math/applyQuaternion.py
+++ Canvas 3D/Math/applyQuaternion.py
@@ -1,5 +1,9 @@
 from three import *
 
+def showValue(name, m):
+    print name + " => " + str(m)
+    return m
+
 e1 = Vector3(1, 0, 0)
 e2 = Vector3(0, 1, 0)
 e3 = Vector3(0, 0, 1)
@@ -17,10 +21,5 @@ qs = [q1, q2, q3]
 
 for e in es:
     for q in qs:
-        showValue(str(a) + " + " + str(b), a + b)
+        showValue(str(e) + " applyQuaternion " + str(q), e.applyQuaternion(q))
     print ""
-
-print "i => " + str(e1.applyQuaternion(q1))
-print e2.applyQuaternion(q1)
-
-print e1.applyQuaternion(q2)
