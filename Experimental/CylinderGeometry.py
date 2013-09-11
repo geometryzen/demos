a3eaf92d90e2353cd7107dfe54f9a952e9c05597
David Geo Holmes <geometryzen@gmail.com>
2013-09-10 20:43:07 -0700

    Save file.

--- Experimental/CylinderGeometry.py
+++ Experimental/CylinderGeometry.py
@@ -19,7 +19,7 @@ def setUp():
     container.appendChild(space.renderer.domElement)
 
     material = MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3})
-    mesh = CylinderBuilder().radius(0.5).height(1).material(material).build()
+    mesh = CylinderBuilder().radius(1).height(2).material(material).build()
 
     space.add(mesh)
 
