2047b6c6c8c88220dff1e9cd9f357eed737afb0d
David Geo Holmes <geometryzen@gmail.com>
2013-09-11 11:10:39 -0700

    Save file.

--- Canvas 3D/Math/applyQuaternion.py
+++ Canvas 3D/Math/applyQuaternion.py
@@ -6,8 +6,10 @@ e3 = Vector3(0, 0, 1)
 
 q1 = Quaternion(1, 0, 0, 1).normalize()
 q2 = Quaternion(0, 1, 0, 1).normalize()
+q3 = Quaternion(0, 0, 1, 1).normalize()
 
 print q1
 print q2
+print q3
 
 print e1.applyQuaternion(q1)
