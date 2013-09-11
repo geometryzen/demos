e8e164b9ce2af01711eaa4b86df8625bfe597a7d
David Geo Holmes <geometryzen@gmail.com>
2013-09-11 14:51:08 -0700

    Save file.

--- Experimental/trigonometry.py
+++ Experimental/trigonometry.py
@@ -18,7 +18,11 @@ showValue("west", west)
 def toRadians(deg):
     return deg * pi / 180
 
-def towards(a, b, angle):
+# TODO: We want to return a vector that represents the semantics
+def towards(a, b, theta):
+'''
+Returns a vector which is the result of rotating a towards b by and angle theta.
+'''
     denom = 2 * (1 + (b << a)).w
     print repr(denom)
     R = (1 + a * b) / sqrt(denom)
