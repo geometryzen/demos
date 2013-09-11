e7e940a2e65bc07b1990aabf79413c046287602c
David Geo Holmes <geometryzen@gmail.com>
2013-09-11 11:23:25 -0700

    Save file.

--- Canvas 3D/Math/applyQuaternion.py
+++ Canvas 3D/Math/applyQuaternion.py
@@ -1,3 +1,5 @@
+# The Quaternion approach to rotation of vectors.
+# 
 from three import *
 
 def showValue(name, m):
@@ -8,17 +10,19 @@ e1 = Vector3(1, 0, 0)
 e2 = Vector3(0, 1, 0)
 e3 = Vector3(0, 0, 1)
 
+q0 = Quaternion(0, 0, 0, 1).normalize()
 q1 = Quaternion(1, 0, 0, 1).normalize()
 q2 = Quaternion(0, 1, 0, 1).normalize()
 q3 = Quaternion(0, 0, 1, 1).normalize()
 
+print q0
 print q1
 print q2
 print q3
 print ""
 
 es = [e1, e2, e3]
-qs = [q1, q2, q3]
+qs = [q0, q1, q2, q3]
 
 for e in es:
     for q in qs:
