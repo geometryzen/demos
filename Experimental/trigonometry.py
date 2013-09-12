62f4e8fe852a80bd950c96680246a9ad0f9e7db0
David Geo Holmes <geometryzen@gmail.com>
2013-09-11 22:27:26 -0700

    Save file.

--- Experimental/trigonometry.py
+++ Experimental/trigonometry.py
@@ -97,8 +97,8 @@ def towards(a, b, theta):
 
 d1 = 100 * east
 d2 = 300 * south
-d3 = towards(west, south, toRadians(30))
-d4 = towards(west, north, toRadians(60))
+d3 = 150 * towards(west, south, toRadians(30))
+d4 = 200 * towards(west, north, toRadians(60))
 
 showValue("d1", d1)
 showValue("d2", d2)
