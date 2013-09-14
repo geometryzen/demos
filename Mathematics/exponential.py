b7468ee28a6b619fb7c60662d629962bdb14e56f
David Geo Holmes <geometryzen@gmail.com>
2013-09-14 08:35:19 -0700

    Save file.

--- Mathematics/exponential.py
+++ Mathematics/exponential.py
@@ -3,8 +3,9 @@ from math import exp, pi
 
 i = VectorE3(1,0,0)
 j = VectorE3(0,1,0)
+theta = pi / 4
 I = i * j
 
 # TODO: exp(B * theta) => cos(theta) + B * sin(theta)
-print repr(exp(I * pi  / 4))
+print repr(exp(I * theta))
 
