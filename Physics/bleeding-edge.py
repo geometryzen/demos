1219a7fc21eef2d090e6fbb563b4b2e738b5a29a
David Geo Holmes <geometryzen@gmail.com>
2013-09-14 12:05:47 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -10,7 +10,7 @@ from units import *
 
 space = CartesianSpace()
 
-shape = SphereBuilder().radus(0.5).build().translateX(-2.5).translateY(+2.5)
+shape = SphereBuilder().radius(0.5).build().translateX(-2.5).translateY(+2.5)
 space.add(shape)
 
 i = VectorE3(1, 0, 0)
