9d6a4c102bae1059949fe74eae0b5afd47d4d50d
David Geo Holmes <geometryzen@gmail.com>
2013-09-14 09:02:30 -0700

    Save file.

--- Mathematics/exponential.py
+++ Mathematics/exponential.py
@@ -1,11 +1,12 @@
 from e3ga import *
 from math import exp, pi
+from units import *
 
 i = VectorE3(1,0,0)
 j = VectorE3(0,1,0)
 theta = pi / 3
-I = i * j
+B = i * j
 
 # TODO: exp(B * theta) => cos(theta) + B * sin(theta)
-print repr(exp(I * theta))
+print repr(exp(B * theta))
 
