a16ab255a320c8364dac52acde8c6d8848de89ec
David Geo Holmes <geometryzen@gmail.com>
2013-09-14 11:05:48 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -38,7 +38,7 @@ def tick(elapsed):
     S = exp(angle) # ~R not yet supported
     r = R * (5 * i * meter) * S
     position = r.quantity
-    print position
+    print angle
     shape.position.set(position.x, position.y, position.z)
     space.render()
     
