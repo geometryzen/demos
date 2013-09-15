510b9065a18a9c824775d803baafb6910a5e02e2
David Geo Holmes <geometryzen@gmail.com>
2013-09-15 15:24:58 -0700

    Save file.

--- Mathematics/exponential.py
+++ Mathematics/exponential.py
@@ -15,10 +15,8 @@ print "exp(theta)  => " + str(exp(theta))
 print "exp(+theta) => " + str(exp(+theta))
 print "exp(-theta) => " + str(exp(-theta))
 
-R = exp(theta * -1)
-# TODO: Measure does not support reversion.
-S = exp(theta * +1) # ~R
-r = R * (5 * i * meter) * S
+R = exp(-theta/2)
+r = R * (5 * i * meter) * ~R
 
 print r
 print r.quantity
