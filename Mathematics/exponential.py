c37ea5095d6a37dc7b4121ae36fdde9b0944e32a
David Geo Holmes <geometryzen@gmail.com>
2013-09-15 15:23:24 -0700

    Save file.

--- Mathematics/exponential.py
+++ Mathematics/exponential.py
@@ -7,7 +7,7 @@ j = VectorE3(0,1,0)
 theta = (pi / 3) * i * j
 omega = theta * (1 / second)
 
-t = 1 * second
+t = ScalarE3(1) * second
 print repr(omega)
 theta = omega * t
 print "omega       => " + str(omega)
