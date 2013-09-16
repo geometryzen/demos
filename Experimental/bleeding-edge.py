8459a7824abc8d48a07a179844224b6dcc560a8d
David Geo Holmes <geometryzen@gmail.com>
2013-09-16 14:18:54 -0700

    Save file.

--- Experimental/bleeding-edge.py
+++ Experimental/bleeding-edge.py
@@ -5,14 +5,14 @@ from browser import *
 space = CartesianSpace()
 timeOut = 3000
 
+e3 = ArrowBuilder().length(1).color(0x0000FF).build()
+
 workbench = Workbench(space.renderer, space.camera)
 
 def setUp():
     workbench.setUp()
 
-    mesh = ArrowBuilder().length(1).color(0x0000FF).build()
-
-    space.add(mesh)
+    space.add(e3)
 
 def tick(elapsed):
     space.render()
