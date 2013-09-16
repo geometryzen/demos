112eeb53e33e61017239c026a919841d776e13c8
David Geo Holmes <geometryzen@gmail.com>
2013-09-16 14:24:02 -0700

    Save file.

--- Experimental/bleeding-edge.py
+++ Experimental/bleeding-edge.py
@@ -7,7 +7,7 @@ space = CartesianSpace()
 timeOut = 3000
 
 e1 = ArrowBuilder().color(0xFF0000).build()
-e1.quaternion.setAxisAngle(Vector3(0,1,0), pi/2)
+e1.quaternion.setFromAxisAngle(Vector3(0,1,0), pi/2)
 e3 = ArrowBuilder().color(0x0000FF).build()
 
 workbench = Workbench(space.renderer, space.camera)
