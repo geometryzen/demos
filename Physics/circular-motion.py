52c0ecbe521afcf89262a6ea19b584526034e1d7
David Geo Holmes <geometryzen@gmail.com>
2013-09-16 11:26:47 -0700

    Save file.

--- Physics/circular-motion.py
+++ Physics/circular-motion.py
@@ -34,13 +34,11 @@ def tick(elapsed):
     t = ScalarE3(elapsed) * milli * second
     angle = omega * t / 2
     R = exp(-angle)
-    r = R * (4 * i * meter) * ~R
-    position = r.quantity
-    rotor = R.quantity
+    position = R * (4 * i * meter) * ~R
+    shape.position.set(position.quantity.x, position.quantity.y, position.quantity.z)
     # To convert a Euclidean3 rotor to a Quaternion, use the 'dual' parts with a sign change.
     # The quaternion property of the mesh is what we would call the attitude - a spinor.
-    shape.position.set(position.x, position.y, position.z)
-    shape.quaternion.set(-R.quantity.yz, -rotor.zx, -rotor.xy, rotor.w)
+    shape.quaternion.set(-R.quantity.yz, -R.quantity.zx, -R.quantity.xy, R.quantity.w)
     space.render()
     
 def terminate(elapsed):
