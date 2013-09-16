03f79fc1025ff7bbcdd2ff2ea52e5e8b089b1644
David Geo Holmes <geometryzen@gmail.com>
2013-09-16 11:32:27 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -16,11 +16,10 @@ space.add(shape)
 i = VectorE3(1, 0, 0)
 j = VectorE3(0, 1, 0)
 k = VectorE3(0, 0, 1)
-B = i * j
 
 # The geometric angular velocity measure (quantity with unit-of-measure).
 # The angular velocity describes a motion of one revolution every 12 seconds in the x-y plane, counterclockwise.
-omega = 2 * pi * B / (12 * second)
+omega = 2 * pi * i * j / (12 * second)
 
 timeout = 12 * kilo# * milli * second
 
@@ -36,13 +35,11 @@ def tick(elapsed):
     t = ScalarE3(elapsed) * milli * second
     angle = omega * t / 2
     R = exp(-angle)
-    r = R * (4 * i * meter) * ~R
-    position = r.quantity
-    rotor = R.quantity
+    position = (R * (4 * i * meter) * ~R).quantity
+    shape.position.set(position.x, position.y, position.z)
     # To convert a Euclidean3 rotor to a Quaternion, use the 'dual' parts with a sign change.
     # The quaternion property of the mesh is what we would call the attitude - a spinor.
-    shape.position.set(position.x, position.y, position.z)
-    shape.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
+    shape.quaternion.set(-R.quantity.yz, -R.quantity.zx, -R.quantity.xy, R.quantity.w)
     space.render()
     
 def terminate(elapsed):
