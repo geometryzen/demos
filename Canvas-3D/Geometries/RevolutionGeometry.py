0dda7fe8fb0aa2dc901ec20e69c1c7b09e7c8251
David Geo Holmes <geometryzen@gmail.com>
2013-09-16 19:03:49 -0700

    Save file.

--- Canvas-3D/Geometries/RevolutionGeometry.py
+++ Canvas-3D/Geometries/RevolutionGeometry.py
@@ -1,1 +1,57 @@
-# RevolutionGeometry.py
+# RevolutionGeometry is not a standard Geometry in the Three.js library.
+# It is an experimental enhancement to LatheGeometry for closed surfaces of revolution.
+from three import Scene, PerspectiveCamera, ArrowGeometry, MeshNormalMaterial, Mesh, AmbientLight, PointLight, DirectionalLight, Vector3, WebGLRenderer, Color
+from browser import document, window, Workbench, WindowAnimationRunner
+
+scene = Scene()
+
+camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
+camera.position.z = 1.3
+
+renderer = WebGLRenderer()
+renderer.autoClear = True
+renderer.gammaInput = True
+renderer.gammaOutput = True
+renderer.setClearColor(Color(0x080808), 1.0)
+
+# All arguments are optional and the defaults, in order, are as follows.
+length = 1
+segments = 10
+radiusShaft = 0.01
+radiusCone = 0.08
+lengthCone = 0.2
+arrow = RevolutionGeometry(length, segments)
+
+material = MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 3})
+mesh = Mesh(arrow,material)
+scene.add(mesh)
+
+ambientLight = AmbientLight(0x222222)
+scene.add(ambientLight)
+
+pointLight = PointLight(0xFFFFFF)
+pointLight.position.set(20, 20, 20)
+scene.add(pointLight)
+
+directionalLight = DirectionalLight(0xFFFFFF)
+directionalLight.position.set(0, 1, 0)
+scene.add(directionalLight)
+
+movement = Vector3(0.02, 0.02, 0.02)
+
+workbench = Workbench(renderer, camera)
+
+def tick(elapsed):
+    mesh.rotation += movement
+    renderer.render(scene, camera)
+    
+def terminate(elapsed):
+    return elapsed > 12000
+
+def setUp():
+    workbench.setUp()
+
+def tearDown():
+    workbench.tearDown()
+
+WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
