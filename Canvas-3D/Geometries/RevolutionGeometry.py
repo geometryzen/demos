93f5ac0555bae55e9b39463d82f1a56783d4cbc5
David Geo Holmes <geometryzen@gmail.com>
2013-09-16 20:31:28 -0700

    Save file.

--- Canvas-3D/Geometries/RevolutionGeometry.py
+++ Canvas-3D/Geometries/RevolutionGeometry.py
@@ -18,8 +18,9 @@ b = Vector3(radiusCone, 0, lengthShaft)
 c = Vector3(radiusShaft, 0, lengthShaft)
 d = Vector3(radiusShaft, 0, 0)
 e = Vector3(0, 0, 0)
-points = [a, b, c, d, e]
-arrow = RevolutionGeometry(points, 6, 0, 2 * pi)
+#points = [a, b, c, d, e]
+points = [b]
+arrow = RevolutionGeometry(points, 3, 0, pi)
 
 material = MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":2})
 mesh = Mesh(arrow, material)
