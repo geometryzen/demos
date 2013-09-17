200fe4d72dce5d4d988d0c4cb257c060c90dd7b5
David Geo Holmes <geometryzen@gmail.com>
2013-09-16 19:13:15 -0700

    Save file.

--- Canvas-3D/Geometries/RevolutionGeometry.py
+++ Canvas-3D/Geometries/RevolutionGeometry.py
@@ -1,18 +1,10 @@
 # RevolutionGeometry is not a standard Geometry in the Three.js library.
 # It is an experimental enhancement to LatheGeometry for closed surfaces of revolution.
+from geometry import *
 from three import *
 from browser import *
 
-scene = Scene()
-
-camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 1.3
-
-renderer = WebGLRenderer()
-renderer.autoClear = True
-renderer.gammaInput = True
-renderer.gammaOutput = True
-renderer.setClearColor(Color(0x080808), 1.0)
+space = CartesianSpace()
 
 # All arguments are optional and the defaults, in order, are as follows.
 radiusCone = 0.08
@@ -32,24 +24,13 @@ material = MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3})
 mesh = Mesh(arrow, material)
 scene.add(mesh)
 
-ambientLight = AmbientLight(0x222222)
-scene.add(ambientLight)
-
-pointLight = PointLight(0xFFFFFF)
-pointLight.position.set(20, 20, 20)
-scene.add(pointLight)
-
-directionalLight = DirectionalLight(0xFFFFFF)
-directionalLight.position.set(0, 1, 0)
-scene.add(directionalLight)
-
 movement = Vector3(0.02, 0.02, 0.02)
 
 workbench = Workbench(renderer, camera)
 
 def tick(elapsed):
     mesh.rotation += movement
-    renderer.render(scene, camera)
+    space.render()
     
 def terminate(elapsed):
     return elapsed > 12000
