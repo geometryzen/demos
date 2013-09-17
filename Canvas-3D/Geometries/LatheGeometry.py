1d01cdd35772bc3cf8f6e1d4757f0e80565642fa
David Geo Holmes <geometryzen@gmail.com>
2013-09-17 09:12:11 -0700

    Save file.

--- Canvas-3D/Geometries/LatheGeometry.py
+++ Canvas-3D/Geometries/LatheGeometry.py
@@ -1,10 +1,5 @@
-# Note: There is currently a bug in the Three.JS LatheGeometry that causes segments to be omitted.
-# Use RevolutionGeometry as a temporary workaround.
 from three import *
 from browser import *
-
-for canvas in document.getElementsByTagName("canvas"):
-    canvas.parentNode.removeChild(canvas)
 
 scene = Scene()
 
@@ -16,9 +11,6 @@ renderer.autoClear = True
 renderer.gammaInput = True
 renderer.gammaOutput = True
 renderer.setClearColor(Color(0x080808), 1.0)
-
-container = document.getElementById("canvas-container")
-container.appendChild(renderer.domElement)
 
 radiusCone = 20
 radiusShaft = 10
@@ -32,56 +24,25 @@ e = Vector3(0, 0, 0)
 points = [a, b, c, d, e]
 arrow = LatheGeometry(points, 25)
 
-material = MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3})
+material = MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 3})
 mesh = Mesh(arrow, material)
 scene.add(mesh)
 
-ambientLight = AmbientLight(0x222222)
-scene.add(ambientLight)
-
-pointLight = PointLight(0xFFFFFF)
-pointLight.position.set(20, 20, 20)
-scene.add(pointLight)
-
-directionalLight = DirectionalLight(0xFFFFFF)
-directionalLight.position.set(0, 1, 0)
-scene.add(directionalLight)
-
-requestID = None
-progress = None
-progressEnd = 10000
-startTime =  None
 movement = Vector3(0.02, 0.02, 0.02)
 
-def render():
+workbench = Workbench(renderer, camera)
+
+def setUp();
+    workbench.setUp()
+
+def tick(elapsed):
     mesh.rotation += movement
-        
     renderer.render(scene, camera)
-
-def onWindowResize(event):
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.size = (window.innerWidth, window.innerHeight)
     
-def step(timestamp):
-    global requestID, progress, startTime
-    if (startTime):
-        progress = timestamp - startTime
-    else:
-        if (timestamp):
-            startTime = timestamp
-        else:
-            progress = 0
-        
-    if (progress < progressEnd):
-        requestID = window.requestAnimationFrame(step)
-        render()
-    else:
-        window.cancelAnimationFrame(requestID)
-        # container.removeChild(renderer.domElement)
+def terminate(elapsed):
+    return elapsed > 10000
 
-window.addEventListener("resize", onWindowResize, False)
+def tearDown():
+    workbench.tearDown()
 
-onWindowResize(None)
-
-step(None)
+WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
