680cff1904753a70c6204aeeaa16c0e19bf71c39
David Geo Holmes <geometryzen@gmail.com>
2013-09-16 19:18:19 -0700

    Save file.

--- Canvas-3D/Geometries/RevolutionGeometry.py
+++ Canvas-3D/Geometries/RevolutionGeometry.py
@@ -20,7 +20,7 @@ e = Vector3(0, 0, 0)
 points = [a, b, c, d, e]
 arrow = RevolutionGeometry(points, 12)
 
-material = MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":1})
+material = MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":2})
 mesh = Mesh(arrow, material)
 space.add(mesh)
 
