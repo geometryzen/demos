a68a3040fedf67e3aeed67567402b8d2177bd541
David Geo Holmes <geometryzen@gmail.com>
2013-09-18 14:35:00 -0700

    Save file.

--- Units of Measure/extreme.py
+++ Units of Measure/extreme.py
@@ -1,1 +1,19 @@
 # Work In Progress
+from e3ga import *
+from units import *
+ 
+m = ScalarE3(10) * kilogram
+ 
+print "m => " + str(m)
+ 
+g = 9.81 * VectorE3(0, 0, -1) * newton / kilogram
+print "g => " + str(g)
+ 
+F = m * g
+ 
+print "F => " + str(F)
+ 
+a = F / m
+ 
+print "a => " + repr(a)
+print "a => " + str(a)
