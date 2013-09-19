4838791c7e0d36e345fca1cb9e47b09b0df8f761
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 06:51:21 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -2,3 +2,5 @@
 # It can be found in the e3ga module.
 # It is also part of the three module where it unifies the Vector3 and Quaternion numbers.
 from three import *
+
+mass = Euclidean3(1,0,0,0,0,0,0,0,0,0,0)
