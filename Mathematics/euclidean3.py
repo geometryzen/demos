a82022c74398c0dac090efcb40e2a40792a108fc
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 09:43:34 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -162,7 +162,7 @@ assertEqual("k", str(e3))
 assertEqual("2*i+3*j+5*k", str(VectorE3(2, 3, 5)))
 
 # The magnitude and length.
-print len(VectorE3(2, 3, 5))
+print len(VectorE3(3, 4, 12))
 
 
 print "I hope you enjoyed the tour of Euclidean3!"
