cf8418a60606266cc08d3d1749a47db232132450
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 07:02:18 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -1,6 +1,9 @@
 # Euclidean3 is the multivector used to represent the Geometric Algebra of 3D Euclidean space.
 # It can be found in the e3ga module.
 # It is also part of the three module where it unifies the Vector3 and Quaternion numbers.
+# Euclidean3 was designed to be used in conjunction with a graphics library and so was made mutable.
+# However, mutability is generally not desirable and so a mutable latch is provided to prevent mutation.
+# It should be obvious from the context whether a quantity is expected to be constant.
 from three import *
 
 # The following functions are provided for testing purposes.
