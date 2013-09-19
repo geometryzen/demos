ab9e37e757ff43dea3749439145ca12261a52cfb
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 10:25:33 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -214,8 +214,8 @@ assertFalse(e12 * e12 == -1)
 
 # Bivectors, like e12, that square to -1 are the generators of rotations.
 # A rotor can be constructed from normalized bivector using the exponential function.
-R = exp(-e12 * pi / 2)
-assertEqual(R.w, cos(pi / 2))
+R = exp(-e12 * pi / 4)
+assertEqual(R.w, cos(pi / 4))
 assertEqual(R.x, 0)
 assertEqual(R.y, 0)
 assertEqual(R.z, 0)
