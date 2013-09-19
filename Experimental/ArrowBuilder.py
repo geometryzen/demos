89695fb529359b4a42f889f2ca7b2c8accd524a3
David Geo Holmes <geometryzen@gmail.com>
2013-09-18 19:39:30 -0700

    Save file.

--- Experimental/ArrowBuilder.py
+++ Experimental/ArrowBuilder.py
@@ -11,7 +11,6 @@ space = CartesianSpace()
 
 # Specify an attitude as the rotor that rotates e3(k) onto e1(i)
 rotor = exp(-BivectorE3(0,0,1)*pi/4)
-print rotor.quaternion
 shape = ArrowBuilder().color(0xFFFF00).attitude(rotor).build()
 space.add(shape)
 
