5dd3527153375b2727c5f9527e7833a923baf09f
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 11:25:50 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -304,6 +304,7 @@ assertEqual(I * I, -one)
 
 # There's also the left- and right-contraction which are represented by the Python bitshift operators << and >>.
 assertEqual(e1 << e12, e2)
+assertEqual(e2 << e12, e1)
 
 
 print "I hope you enjoyed the tour of Euclidean3!"
