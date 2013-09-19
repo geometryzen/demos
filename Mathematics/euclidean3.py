deef7a2aa72cb072fed524948115523655e06969
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 06:53:11 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -1,6 +1,8 @@
-# Euclidean3 is the multivector used to represent 3D Euclidean space.
+# Euclidean3 is the multivector used to represent the Geometric Algebra of 3D Euclidean space.
 # It can be found in the e3ga module.
 # It is also part of the three module where it unifies the Vector3 and Quaternion numbers.
 from three import *
 
+# Euclidean3 forms a Linear Space of 8=2*2*2= dimensions.
 mass = Euclidean3(1,0,0,0,0,0,0,0)
+print 2**3
