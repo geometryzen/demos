898e6b6e3ac3ef64d23f403742b666bf1c316bc9
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 11:43:15 -0700

    Save file.

--- Mathematics/exponential.py
+++ Mathematics/exponential.py
@@ -3,27 +3,20 @@ from math import exp, pi
 from units import *
 
 i = VectorE3(1,0,0)
-print "i           => " + repr(i)
 j = VectorE3(0,1,0)
-print "j           => " + repr(j)
 theta = (2 * pi) * i * j
-print "theta       => " + repr(theta)
 omega = theta * (1 / second)
-print "omega       => " + repr(omega)
 print "omega       => " + str(omega)
 
 t = ScalarE3(1) * second
-print "t           => " + repr(t)
 print "t           => " + str(t)
 theta = omega * t
-print "theta       => " + repr(theta)
 print "theta       => " + str(theta)
 print "exp(theta)  => " + str(exp(theta))
 print "exp(+theta) => " + str(exp(+theta))
 print "exp(-theta) => " + str(exp(-theta))
 
 R = exp(-theta/2)
-print "R           => " + repr(R)
 print "R           => " + str(R)
 r = R * (1 * i * meter) * ~R
 
