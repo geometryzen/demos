21d3a1c3c9475e0075d76ecfb0c97a4fda1a356b
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 10:02:19 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -179,5 +179,9 @@ assertEqual(3,  e1 % VectorE3(3, 4, 12))
 assertEqual(4,  e2 % VectorE3(3, 4, 12))
 assertEqual(12, e3 % VectorE3(3, 4, 12))
 
+# Things get a bit more interesting when we extend the vectors using the outer product.
+e12 = e1 ^ e2
+print e12
+
 
 print "I hope you enjoyed the tour of Euclidean3!"
