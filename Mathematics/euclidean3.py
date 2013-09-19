a02714c73a110d4c6c3eeecd19c6d1e63a2fe823
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 11:20:51 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -291,9 +291,11 @@ assertEqual(I * one, one * I)
 assertEqual(I * e1, e1 * I)
 assertEqual(I * e12, e12 * I)
 # It can be use to compute the Hodge dual.
+assertEqual(one * I, I)
 assertEqual(e1 * I, e23)
 assertEqual(e2 * I, e31)
 assertEqual(e3 * I, e12)
+assertEqual(I * I, -one)
 
 
 print "I hope you enjoyed the tour of Euclidean3!"
