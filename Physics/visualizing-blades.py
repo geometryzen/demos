d6cdd2af6cd5f1b827e2aa3d5055cc71e3d31eae
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 11:51:23 -0700

    Save file.

--- Physics/visualizing-blades.py
+++ Physics/visualizing-blades.py
@@ -13,7 +13,7 @@ space.add(CubeBuilder().color(0x0000FF).volume(1).build().translateX(2.5).transl
 space.add(SphereBuilder().color(0xFF0000).volume(1).build().translateX(+2.5).translateY(-2.5))
 space.add(ConeBuilder().color(0xFFFF00).volume(1).build().translateX(-2.5).translateY(+2.5))
 
-timeout = 600000
+timeout = 6000
 
 def onDocumentKeyDown(event):
     global timeout
