6350d29263dd393306ca18b5aeab07a762aa0d06
David Geo Holmes <geometryzen@gmail.com>
2013-09-19 07:04:56 -0700

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -26,3 +26,5 @@ def showValue(name, m):
 # Most of the time, you will want to construct scalars, vectors or rotors.
 mass = Euclidean3(1, 0, 0, 0, 0, 0, 0, 0)
 assertEqual(mass.x, 0)
+assertEqual(mass.y, 0)
+assertEqual(mass.z, 0)
