a185b4d023aeecb9d3ccf7795896ac0dc29483f3
David Geo Holmes <geometryzen@gmail.com>
2013-09-20 12:55:33 -0700

    Save file.

--- Experimental/bleeding-edge.py
+++ Experimental/bleeding-edge.py
@@ -4,7 +4,7 @@ from browser import *
 from math import pi
 
 space = CartesianSpace()
-timeOut = 5000
+timeOut = 5
 
 e1 = ArrowBuilder().color(0xFF0000).axis(Vector3(1,0,0)).build()
 e2 = ArrowBuilder().color(0x00FF00).axis(Vector3(0,1,0)).build()
@@ -39,14 +39,14 @@ def setUp():
 
     document.addEventListener("keydown", onDocumentKeyDown, False)
 
-def tick(elapsed):
+def tick(t):
     space.render()
     
-def terminate(elapsed):
-    return elapsed > timeOut
+def terminate(t):
+    return t > timeOut
 
 def tearDown():
     document.removeEventListener("keydown", onDocumentKeyDown, False)
     workbench.tearDown()
 
-WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
