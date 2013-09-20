d0ca99b09b88fa2bf0ebf5e0fc985182a9309927
David Geo Holmes <geometryzen@gmail.com>
2013-09-20 12:45:15 -0700

    Save file.

--- Canvas-3D/Geometries/OctahedronGeometry.py
+++ Canvas-3D/Geometries/OctahedronGeometry.py
@@ -29,14 +29,14 @@ workbench = Workbench(renderer, camera)
 def setUp():
     workbench.setUp()
 
-def tick(elapsed):
+def tick(t):
     mesh.rotation += movement    
     renderer.render(scene, camera)
     
-def terminate(elapsed):
-    return elapsed > 6000
+def terminate(t):
+    return t > 6
 
 def tearDown():
     workbench.tearDown()
 
-WindowAnimationRunner(window, tick, terminate, setUp, tearDown).start()
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
