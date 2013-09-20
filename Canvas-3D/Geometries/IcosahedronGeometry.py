4d16193d0aca94c90376ec1b1739054e1c9d0ba5
David Geo Holmes <geometryzen@gmail.com>
2013-09-20 09:03:29 -0700

    Save file.

--- Canvas-3D/Geometries/IcosahedronGeometry.py
+++ Canvas-3D/Geometries/IcosahedronGeometry.py
@@ -1,13 +1,8 @@
+from geometry import *
 from three import *
 from browser import *
 
-scene = Scene()
-
-camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 2
-
-renderer = WebGLRenderer()
-renderer.setClearColor(Color(0x080808), 1.0)
+space = CartesianSpace()
 
 radius = 1
 detail = 0 # Must be an integer: 0,1,2,...
@@ -20,20 +15,20 @@ print "detail:         " + str(icosa.detail)
 print icosa
 
 mesh = Mesh(icosa, MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 3}))
-scene.add(mesh)
+space.add(mesh)
 
 progressEnd = 10000
 
 movement = VectorE3(0.02, 0.02, 0.02)
 
-workbench = Workbench(renderer, camera)
+workbench = Workbench(space.renderer, space.camera)
 
 def setUp():
     workbench.setUp();
 
 def tick(elapsed):
     mesh.rotation += movement
-    renderer.render(scene, camera)
+    space.render()
     
 def terminate(elapsed):
     return elapsed > progressEnd
