2ee517f0b386a43ab875b0c6893158773a7d6923
David Geo Holmes <geometryzen@gmail.com>
2013-09-20 12:49:09 -0700

    Save file.

--- Physics/circular-motion.py
+++ Physics/circular-motion.py
@@ -6,6 +6,8 @@ from e3ga import *
 from browser import *
 from math import pi, exp
 
+timeOut = 12
+
 space = CartesianSpace()
 
 shape = ConeBuilder().color(0xFFFF00).build()#.scale(1).axis(Vector3(1,0,0)).build()
@@ -16,10 +18,8 @@ j = VectorE3(0, 1, 0)
 k = VectorE3(0, 0, 1)
 
 # The geometric angular velocity quantity.
-# The angular velocity describes a motion of one revolution every 12 seconds in the x-y plane, counterclockwise.
-omega = 2 * pi * i * j / 6
-
-timeOut = 6
+# The angular velocity describes a motion of one revolution every timeOut seconds in the x-y plane, counterclockwise.
+omega = 2 * pi * i * j / timeOut
 
 workbench = Workbench(space.renderer, space.camera)
 
