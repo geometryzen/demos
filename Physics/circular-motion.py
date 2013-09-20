d217c00b8abad04ef373ac68f490e4d3a3f0aa95
David Geo Holmes <geometryzen@gmail.com>
2013-09-20 12:14:15 -0700

    Save file.

--- Physics/circular-motion.py
+++ Physics/circular-motion.py
@@ -8,7 +8,6 @@ from math import pi, exp
 
 space = CartesianSpace()
 
-# TODO: This will be simplified by consolidating Euclidean3 and Vector3, Quaternion.
 shape = ConeBuilder().color(0xFFFF00).build()#.scale(1).axis(Vector3(1,0,0)).build()
 space.add(shape)
 
@@ -16,7 +15,7 @@ i = VectorE3(1, 0, 0)
 j = VectorE3(0, 1, 0)
 k = VectorE3(0, 0, 1)
 
-# The geometric angular velocity measure (quantity with unit-of-measure).
+# The geometric angular velocity quantity.
 # The angular velocity describes a motion of one revolution every 12 seconds in the x-y plane, counterclockwise.
 omega = 2 * pi * i * j / 12
 
@@ -39,8 +38,8 @@ def tick(t):
     shape.attitude = R
     space.render()
     
-def terminate(elapsed):
-    return elapsed > timeOut
+def terminate(t):
+    return t > timeOut
 
 def tearDown():
     document.removeEventListener("keydown", onDocumentKeyDown, False)
