6021945b33a4a660598d5b63901bcb12b9323414
David Geo Holmes <geometryzen@gmail.com>
2013-09-21 12:19:27 -0700

    Save file.

--- Experimental/how-to-demo.py
+++ Experimental/how-to-demo.py
@@ -39,6 +39,15 @@ def setUp():
     ball.x = 200
     ball.y = 100
 
+    tween = Tween.get(ball, {"loop": False})
+    tween.to({"x":ball.x,"y":canvas.height - 55, "rotation":-360}, 1500, Ease.bounceOut)
+    tween.wait(1000)
+    tween.to({"x":canvas.width - 55, "rotation":360}, 2500, Ease.bounceOut)
+    tween.wait(1000)
+    tween.to({"scaleX":2, "scaleY":2, "x":canvas.width-110, "y":canvas.height-110}, 2500, Ease.bounceOut)
+    tween.wait(1000)
+    tween.to({"scaleX":0.5, "scaleY":0.5, "x":30, "y":canvas.height-30, "rotation":-360}, 2500, Ease.bounceOut)
+
     stage.addChild(ball)
     Ticker.addEventListener("tick", stage)
 
