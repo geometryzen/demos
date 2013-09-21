e254f172a3de372621d81674381e235aa9758634
David Geo Holmes <geometryzen@gmail.com>
2013-09-21 13:16:11 -0700

    Save file.

--- Easel/localToGlobal.py
+++ Easel/localToGlobal.py
@@ -1,1 +1,41 @@
-# Easel/localToGlobal.py
+from easel import *
+from browser import *
+from workbench import *
+
+canvas = document.createElement("canvas")
+
+stage = Stage(canvas)
+
+target = stage.addChild(Shape())
+target.graphics.beginFill("blue").drawCircle(0, 0, 50).endFill()
+target.graphics.beginFill("white").drawCircle(0, 0, 30).endFill()
+target.graphics.beginFill("red").drawCircle(0, 0, 10).endFill()
+target.x = 100
+target.y = 180
+
+arm = stage.addChild(Shape())
+arm.graphics.beginFill("black").drawRect(-2, -2, 100, 4).endFill()
+arm.graphics.beginFill("black").drawCircle(100, 0, 8).endFill()
+arm.x = 180
+arm.y = 100
+
+workbench2D = Workbench2D(canvas)
+
+def setUp():
+    workbench2D.setUp()
+
+def tick(t):
+    arm.rotation += 5
+    target.alpha = 0.2
+    point = arm.localToLocal(100, 0, target)
+    if (target.hitTest(point.x, point.y)):
+        target.alpha = 1
+    stage.update()
+    
+def terminate(t):
+    return t > 6
+
+def tearDown():
+    workbench2D.tearDown()
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
