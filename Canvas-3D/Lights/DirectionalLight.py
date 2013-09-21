f3a1981c92dfa73fbb43ff7b47096c909507eb02
David Geo Holmes <geometryzen@gmail.com>
2013-09-21 07:41:05 -0700

    Save file.

--- Canvas-3D/Lights/DirectionalLight.py
+++ Canvas-3D/Lights/DirectionalLight.py
@@ -1,9 +1,6 @@
 from three import *
 from browser import *
 
-for canvas in document.getElementsByTagName("canvas"):
-    canvas.parentNode.removeChild(canvas)
-
 scene = Scene()
 
 camera = PerspectiveCamera(45, 1.0, 0.1, 10000)
@@ -20,9 +17,6 @@ renderer.gammaInput = True
 renderer.gammaOutput = True
 renderer.setClearColor(Color(0x080808), 1.0)
 
-container = document.getElementById("canvas-container")
-container.appendChild(renderer.domElement)
-
 material = MeshLambertMaterial({"color":0x0000FF})
 material.name = "bluecube"
 
@@ -30,16 +24,12 @@ mesh = Mesh(CubeGeometry(5, 5, 5), material)
 
 scene.add(mesh)
 
-requestID = None
-progress = None
-progressEnd = 6000
-startTime = None
 movement = VectorE3(0.02, 0.02, 0.02)
 
 workbench = Workbench(renderer, camera)
 
 def setUp():
-    pass
+    workbench.setUp()
 
 def tick(t):
     mesh.rotation += movement
@@ -49,34 +39,6 @@ def terminate(t):
     return t > 6
 
 def tearDown():
-    pass
-
-def onWindowResize(event):
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.size = (window.innerWidth, window.innerHeight)
+    workbench.tearDown()
     
-def animate(timestamp):
-    global requestID, progress, startTime
-    if (startTime):
-        progress = timestamp - startTime
-    else:
-        if (timestamp):
-            startTime = timestamp
-        else:
-            progress = 0
-        
-    if (progress < progressEnd):
-        requestID = window.requestAnimationFrame(animate)
-        render()
-    else:
-        window.cancelAnimationFrame(requestID)
-        # container.removeChild(renderer.domElement)
-
-window.addEventListener("resize", onWindowResize, False)
-
-onWindowResize(None)
-
-animate(None)
-
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
