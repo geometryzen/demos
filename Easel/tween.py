1e3125293c1ee3b5da4d77106f6e181eeeabc72f
David Geo Holmes <geometryzen@gmail.com>
2013-09-22 11:04:08 -0700

    Save file.

--- Easel/tween.py
+++ Easel/tween.py
@@ -1,1 +1,34 @@
-# tween.py
+from easel import *
+from browser import *
+
+def discardCanvases():
+    for cs in document.getElementsByTagName("canvas"):
+        cs.parentNode.removeChild(cs)
+        
+def onComplete(args):
+    print "The tween is complete"
+    print args
+    print "There could be a bug in the TweenJS library?"
+        
+discardCanvases()
+canvas = document.createElement("canvas")
+canvas.width = 960
+canvas.height = 400
+container = document.getElementById("canvas-container")
+container.appendChild(canvas)
+
+stage = Stage(canvas)
+
+Ticker.addEventListener("tick", stage)
+
+movieClip = MovieClip(None, 0, False, {"start": 0, "middle": 40})
+stage.addChild(movieClip)
+
+target = Shape(Graphics().beginFill("#999999").drawCircle(100, 100, 100))
+
+timeline = movieClip.timeline
+
+target.alpha = 0
+timeline.addTween(Tween.get(target).to({"alpha": 1}, 100).onComplete(onComplete, [11,22,33]))
+
+movieClip.gotoAndPlay("middle")
