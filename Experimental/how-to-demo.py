e34c2d14ebb4e11af7633538f75a33d9b93e1a87
David Geo Holmes <geometryzen@gmail.com>
2013-09-22 15:26:31 -0700

    Save file.

--- Experimental/how-to-demo.py
+++ Experimental/how-to-demo.py
@@ -26,7 +26,7 @@ output.x = window.innerWidth / 2
 output.y = window.innerHeight / 2
 space2D.addChild(output)
 
-quantity = VectorE3(0,0,1)
+quantity = VectorE3(0, 0, 1)
 
 def setUp():
     workbench2D.setUp()
