758cc1397fc0540e9e3c4a3bbf1f4adecdd9aabe
David Geo Holmes <geometryzen@gmail.com>
2013-09-22 15:03:58 -0700

    Save file.

--- Experimental/how-to-demo.py
+++ Experimental/how-to-demo.py
@@ -14,36 +14,20 @@ from geometry import *
 space = CartesianSpace()
 arrow = ArrowBuilder().build()
 space.add(arrow)
+canvas3D = space.renderer.domElement
+workbench3D = Workbench3D(space.renderer.domElement, space.renderer, space.camera)
    
 canvas2D = document.createElement("canvas")
 canvas2D.style.position = "absolute"
 canvas2D.style.top = "0px"
 canvas2D.style.left = "0px"
 workbench2D = Workbench2D(canvas2D)
-
-canvas3D = space.renderer.domElement
-workbench3D = Workbench3D(space.renderer.domElement, space.renderer, space.camera)
 stage = Stage(canvas2D)
 stage.autoClear = True
 
 def setUp():
     workbench2D.setUp()
     workbench3D.setUp()
-
-    ball = Shape()
-    ball.graphics.setStrokeStyle(5, "round", "round")
-    ball.graphics.beginStroke("#000000")
-    ball.graphics.beginFill("#FF0000").drawCircle(0,0,50).endFill()
-    ball.graphics.endStroke()
-    ball.graphics.setStrokeStyle(1, "round", "round")
-    ball.graphics.beginStroke("#000000")
-    ball.graphics.moveTo(0,0)
-    ball.graphics.lineTo(0,50)
-    ball.graphics.endStroke()
-    ball.x = 200
-    ball.y = 100
-
-    stage.addChild(ball)
 
 def tick(t):
     space.render()
