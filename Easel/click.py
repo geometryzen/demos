e0868681b23ae4657602bb6d2c36c6413248edc5
David Geo Holmes <geometryzen@gmail.com>
2013-09-22 10:55:35 -0700

    Save file.

--- Easel/click.py
+++ Easel/click.py
@@ -1,1 +1,31 @@
-# click.py
+from easel import *
+from browser import *
+
+def handleClick(event):
+    window.alert("click => " + str(event))
+        
+canvas = document.createElement("canvas")
+
+stage = Stage(canvas)
+circle = stage.addChild(Shape())
+circle.graphics.beginFill("red").drawCircle(0, 0, 50).endFill()
+circle.x = 100
+circle.y = 100
+
+workbench2D = Workbench2D(canvas)
+
+def setUp():
+    workbench2D.setUp()
+    circle.addEventListener("click", handleClick)
+
+def tick(t):
+    stage.update()
+
+def terminate(t):
+    return t > 5
+
+def tearDown():
+    circle.removeEventListener("click", handleClick)
+    workbench2D.tearDown()
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
