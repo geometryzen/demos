be1e2b01d1d95321f4aff437e16e6fa511fd748a
David Geo Holmes <geometryzen@gmail.com>
2013-09-22 10:59:52 -0700

    Save file.

--- Easel/moving.py
+++ Easel/moving.py
@@ -1,1 +1,28 @@
-# moving.py
+from easel import *
+from browser import *
+
+def discardCanvases():
+    for cs in document.getElementsByTagName("canvas"):
+        cs.parentNode.removeChild(cs)
+        
+discardCanvases()
+canvas = document.createElement("canvas")
+canvas.width = 500
+canvas.height = 200
+container = document.getElementById("canvas-container")
+container.appendChild(canvas)
+
+stage = Stage(canvas)
+circle = Shape()
+circle.graphics.beginFill("red").drawCircle(0, 0, 50)
+circle.x = 100
+circle.y = 100
+stage.addChild(circle)
+
+def handleTick():
+    circle.x += 10
+    if (circle.x > canvas.width):
+        circle.x = 0
+    stage.update()
+
+Ticker.addEventListener("tick", handleTick)
