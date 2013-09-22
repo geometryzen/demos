1a26200ddedb17b64a5c228abb1e0f22bdb7772c
David Geo Holmes <geometryzen@gmail.com>
2013-09-22 11:26:55 -0700

    Save file.

--- Easel/drag.py
+++ Easel/drag.py
@@ -22,39 +22,34 @@ canvas = document.createElement("canvas")
 stage = Stage(canvas)
 stage.mouseMoveOutside = True
 
-circle = Shape()
-circle.graphics.beginFill("#000000").drawCircle(0, 0, 50).endFill()
-circle.name = "circle"
-
-label = Text("drag me", "bold 14px Arial", "#FFFFFF")
-label.textAlign = "center"
-label.y = -7
-
-dragger = Container()
-dragger.x = dragger.y = 100
-dragger.addChild(circle)
-dragger.addChild(label)
-stage.addChild(dragger)
-
-dragger.addEventListener("mousedown", onMouseDown)
-
-stage.update()
-
 workbench2D = Workbench2D(canvas)
 
 def setUp():
     workbench2D.setUp()
-    pass
+    dragger.addEventListener("mousedown", onMouseDown)
+    circle = Shape()
+    circle.graphics.beginFill("#000000").drawCircle(0, 0, 50).endFill()
+    circle.name = "circle"
+
+    label = Text("drag me", "bold 14px Arial", "#FFFFFF")
+    label.textAlign = "center"
+    label.y = -7
+
+    dragger = Container()
+    dragger.x = dragger.y = 100
+    dragger.addChild(circle)
+    dragger.addChild(label)
+    stage.addChild(dragger)
 
 def tick(t):
-    pass
+    stage.update()
 
 def terminate(t):
     return t > 6
 
 
 def tearDown():
+    dragger.removeEventListener("mousedown", onMouseDown)
     workbench2D.tearDown()
-    pass
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
