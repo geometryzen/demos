345838ca8f2b86b29adfad6174e0478ebcfcf1a6
David Geo Holmes <geometryzen@gmail.com>
2013-09-23 07:07:09 -0700

    Save file.

--- Experimental/Probe.py
+++ Experimental/Probe.py
@@ -23,7 +23,7 @@ output.y = window.innerHeight / 2
 space2D.addChild(output)
 
 quantity = Euclidean3(1, 2, 3, 4, 5, 6, 7, 8)
-rotor  = exp(-BivectorE3(0,0,1)*pi/4)
+rotor  = exp(-BivectorE3(0,0,0)*pi/4)
 sphere = SphereBuilder().wireframe(True).color(0xFFFF00).attitude(rotor).build()
 arrow  = ArrowBuilder().wireframe(True).color(0xFFFF00).attitude(rotor).build()
 probe  = ProbeE3(sphere, arrow)
