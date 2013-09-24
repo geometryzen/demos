dad6adc1d28069915e3f7f76a20c62bae44dc4ae
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 12:54:20 -0700

    Save file.

--- Physics/binary-star.py
+++ Physics/binary-star.py
@@ -13,7 +13,7 @@ renderer = WebGLRenderer()
 renderer.setClearColor(Color(0x080808), 1.0)
 
 camera = PerspectiveCamera(50,1,0.1, 1e30)
-camera.position.z = 1e11
+camera.position.z = 1e12
 
 pointLight = PointLight(0xFFFFFF);
 pointLight.position = camera.position
@@ -51,7 +51,7 @@ def tick(t):
     renderer.render(scene, camera)
 
 def terminate(t):
-    return t > 10
+    return t > 30
 
 def tearDown():
     workbench3D.tearDown()
