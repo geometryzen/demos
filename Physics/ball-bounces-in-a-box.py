19ae0036b66ddb815dbd30ceb743d25099d49faa
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 08:20:40 -0700

    Save file.

--- Physics/ball-bounces-in-a-box.py
+++ Physics/ball-bounces-in-a-box.py
@@ -10,8 +10,8 @@ renderer.setClearColor(Color(0x080808), 1.0)
 camera = PerspectiveCamera(75, 1.0, 0.1, 1000)
 camera.position.z = 5
 
-pointLight = PointLight(0xFFFFFF);
-scene.add(pointLight)
+#pointLight = PointLight(0xFFFFFF);
+#scene.add(pointLight)
 
 workbench3D = Workbench(renderer, camera)
 
