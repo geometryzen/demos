e568d9151d31c4123ead9ed2c8a1b780a8ff4ec9
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 15:23:22 -0700

    Save file.

--- Physics/center-of-mass.py
+++ Physics/center-of-mass.py
@@ -15,7 +15,7 @@ workbench3D = Workbench(space3D.renderer, space3D.camera)
 giant = SphereBuilder().color("red").radius(0.4).build()
 giant.position = VectorE3(1, 0, 0)
 giant.mass     = ScalarE3(2)
-giant.momentum = VectorE3(0, -0.5, 1) * giant.mass
+giant.momentum = VectorE3(0.2, -0.5, 0) * giant.mass
 space3D.add(giant)
 
 dwarf = SphereBuilder().color("green").radius(0.2).build()
