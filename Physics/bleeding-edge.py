8a6f4f367accccc3e822628e451a716ad087dcda
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 16:48:42 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -1,20 +1,46 @@
 '''
 This program may not work for you because I am working on it right now!
 '''
+from easel import *
 from three import *
-from geometry import *
 from browser import *
 from workbench import *
-from math import pi, exp
+from geometry import *
+from math import exp, pi
 
-space = CartesianSpace()
-timeOut = 5
+timeOut = 600
 
-e1 = ArrowBuilder().color(0xFF0000).attitude(exp(-BivectorE3(-1, 0, 0)*pi/4)).build()
-e2 = ArrowBuilder().color(0x00FF00).attitude(exp(-BivectorE3( 0, 0, 0)*pi/4)).build()
-e3 = ArrowBuilder().color(0x0000FF).attitude(exp(-BivectorE3( 0, 1, 0)*pi/4)).build()
+space3D = CartesianSpace()
+canvas3D = space3D.renderer.domElement
+workbench3D = Workbench3D(canvas3D, space3D.renderer, space3D.camera)
+   
+canvas2D = document.createElement("canvas")
+canvas2D.style.position = "absolute"
+canvas2D.style.top = "0px"
+canvas2D.style.left = "0px"
+workbench2D = Workbench2D(canvas2D)
+space2D = Stage(canvas2D)
+space2D.autoClear = True
 
-workbench = Workbench(space.renderer, space.camera)
+output = Text("", "20px Helvetica", "white")
+output.x = 100
+output.y = 100
+space2D.addChild(output)
+
+quantity = ScalarE3(1) + VectorE3(2, 4, 0) + BivectorE3(5,0,0) + PseudoscalarE3(8)
+
+# Define the Meshes that you want to represent each grade of the quantity.
+grade0 = SphereBuilder().wireframe(True).color(0x808080).segments(12).build()
+grade1 = ArrowBuilder().color(0x808080).wireframe(True).segments(12).build()
+grade2 = PlaneBuilder().wireframe(True).color(0xFFFFFF).segments(5).build()
+grade3 = CubeBuilder().wireframe(True).color(0xFFFFFF).segments(1).build()
+
+probe  = ProbeE3(grade0, grade1, grade2, grade3)
+
+space3D.add(probe.grade0)
+space3D.add(probe.grade1)
+space3D.add(probe.grade2)
+space3D.add(probe.grade3)
 
 def escKey(event, downFlag):
     event.preventDefault()
@@ -32,25 +58,22 @@ def onDocumentKeyDown(event):
         pass
 
 def setUp():
-    workbench.setUp()
-
-    space.add(e1)
-    space.add(e2)
-    space.add(e3)
-    
-    space.camera.position.set(2,2,2)
-    space.camera.lookAt(VectorE3(0,0,0))
-
+    workbench2D.setUp()
+    workbench3D.setUp()
     document.addEventListener("keydown", onDocumentKeyDown, False)
 
 def tick(t):
-    space.render()
-    
+    probe.quantity = quantity
+    output.text = repr(probe.quantity)
+    space3D.render()
+    space2D.update()
+
 def terminate(t):
     return t > timeOut
 
 def tearDown():
     document.removeEventListener("keydown", onDocumentKeyDown, False)
-    workbench.tearDown()
+    workbench3D.tearDown()
+    workbench2D.tearDown()
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
