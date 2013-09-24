31eadde586cfa73f2cfac48d674da7c951169348
David Geo Holmes <geometryzen@gmail.com>
2013-09-23 17:07:56 -0700

    Save file.

--- Physics/visualizing-blades.py
+++ Physics/visualizing-blades.py
@@ -13,7 +13,7 @@ space.add(CylinderBuilder().color(0x00FF00).volume(1).build().translateX(-2.5).t
 space.add(CubeBuilder().color(0x0000FF).volume(1).build().translateX(2.5).translateY(2.5))
 space.add(SphereBuilder().color(0xFF0000).volume(1).build().translateX(+2.5).translateY(-2.5))
 space.add(ConeBuilder().color(0xFFFF00).volume(1).build().translateX(-2.5).translateY(+2.5))
-space.add(ArrowBuilder().color(0xFFFFFF).segments(12).scale(8).build())
+space.add(ArrowBuilder().color(0xFFFFFF).segments(12).scale(6).build())
 
 timeOut = 10
 
