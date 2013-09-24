ce7a5392c8b797d2ad343ed8195aceea75d0fc3d
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 08:23:17 -0700

    Save file.

--- Physics/ball-bounces-in-a-box.py
+++ Physics/ball-bounces-in-a-box.py
@@ -7,7 +7,7 @@ scene = Scene()
 renderer = WebGLRenderer()
 renderer.setClearColor(Color(0x080808), 1.0)
 
-camera = PerspectiveCamera(75, 1.0, 0.1, 1000)
+camera = PerspectiveCamera()#75, 1.0, 0.1, 1000)
 camera.position.z = 5
 
 #pointLight = PointLight(0xFFFFFF);
