5f09ecb26bb3e814a09becd8e1d27d2fdca82838
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 09:50:49 -0700

    Save file.

--- Physics/ball-bounces-in-a-box.py
+++ Physics/ball-bounces-in-a-box.py
@@ -16,10 +16,17 @@ scene.add(pointLight)
 
 workbench3D = Workbench(renderer, camera)
 
+side = 4.0
+thk = 0.3
+s2 = 2 * side - thk
+s3 = 2 * side + thk
+
 ball = SphereBuilder().color("green").radius(0.8).build()
 ball.mass     = ScalarE3(1.0)
 ball.momentum = VectorE3(-0.15, -0.23, +0.27)
 scene.add(ball)
+
+side = side - thk * 0.5 - ball.size.x / 2
 
 def setUp():
     workbench3D.setUp()
