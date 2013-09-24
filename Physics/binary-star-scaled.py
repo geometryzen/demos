b4aef381480933f2cbfeb8fe4c8266c589a0ea29
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 15:15:00 -0700

    Save file.

--- Physics/binary-star-scaled.py
+++ Physics/binary-star-scaled.py
@@ -24,6 +24,9 @@ dwarf.mass     = ScalarE3(1)
 dwarf.momentum = -giant.momentum
 space3D.add(dwarf)
 
+blob = SphereBuilder().color("blue").radius(0.1).build()
+space3D.add(blob)
+
 dt = 0.02
 
 canvas2D = document.createElement("canvas")
@@ -52,6 +55,8 @@ def tick(t):
     
     for star in [giant, dwarf]:
         star.position = star.position + (star.momentum / star.mass) * dt
+
+    blob.position = (giant.position * giant.mass + dwarf.position * dwarf.mass) / (giant.mass + dwarf.mass)
     
     space3D.render()
     space2D.update()
