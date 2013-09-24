e374e70db058ddd4e517b595abedfb273e1d6e82
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 08:22:46 -0700

    Save file.

--- Physics/ball-bounces-in-a-box.py
+++ Physics/ball-bounces-in-a-box.py
@@ -16,7 +16,7 @@ camera.position.z = 5
 workbench3D = Workbench(renderer, camera)
 
 material = MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 1})
-ball = SphereBuilder().volume(1).material(material).build()
+ball = SphereBuilder().material(material).build()
 scene.add(ball)
 
 def setUp():
