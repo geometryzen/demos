14e02b6f7ca2ac0d8fae4edb1ce9d683e52dd442
David Geo Holmes <geometryzen@gmail.com>
2013-09-23 18:48:06 -0700

    Save file.

--- Experimental/Probe.py
+++ Experimental/Probe.py
@@ -22,9 +22,9 @@ output.x = window.innerWidth / 2
 output.y = window.innerHeight / 2
 space2D.addChild(output)
 
-quantity = VectorE3(3, 0, 0)
+quantity = VectorE3(3, 4, 0)
 sphere = SphereBuilder().color(0xFFFF00).build()
-arrow  = ArrowBuilder().color(0xFFFF00).segments(24).build()
+arrow  = ArrowBuilder().color(0xFFFF00).segments(12).build()
 probe  = ProbeE3(sphere, arrow)
 
 #space3D.add(probe.grade0)
