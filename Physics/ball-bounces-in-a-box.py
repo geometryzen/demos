c3c8dd5cdc95a0d3c1a1b2bcf3dbcf16b58734e1
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 06:52:05 -0700

    Save file.

--- Physics/ball-bounces-in-a-box.py
+++ Physics/ball-bounces-in-a-box.py
@@ -1,1 +1,15 @@
-# ball-bounces-in-a-box.py
+
+
+def setUp():
+    pass
+
+def tick(t):
+    pass
+
+def terminate(t)
+    return t > 5
+
+def tearDown():
+    pass
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
