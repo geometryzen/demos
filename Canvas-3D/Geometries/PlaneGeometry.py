d36dd27344a8613cfc84041223372452114f6c11
David Geo Holmes <geometryzen@gmail.com>
2013-09-24 20:26:34 -0700

    Save file.

--- Canvas-3D/Geometries/PlaneGeometry.py
+++ Canvas-3D/Geometries/PlaneGeometry.py
@@ -2,16 +2,10 @@ from three import *
 from browser import *
 from workbench import *
 
-scene = Scene()
+space = CartesianSpace()
 
-camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 15
-
-renderer = WebGLRenderer()
-renderer.setClearColor(Color(0x080808), 1.0)
-
-width = 16
-height = 16
+width = 4
+height = 4
 widthSegments = 8
 heightSegments = 8
 plane = PlaneGeometry(width, height, widthSegments, heightSegments)
@@ -26,20 +20,13 @@ print plane
 mesh = Mesh(plane, MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3}))
 scene.add(mesh)
 
-requestID = None
-progress = None
-progressEnd = 12000
-startTime =  None
-movement = VectorE3(0.02, 0.02, 0.02)
-
-workbench = Workbench(renderer, camera)
+workbench = Workbench(space.renderer, space.camera)
 
 def setUp():
     workbench.setUp()
 
-def tick(t):
-    mesh.rotation += movement    
-    renderer.render(scene, camera)
+def tick(t):  
+    space.render()
     
 def terminate(t):
     return t > 6
