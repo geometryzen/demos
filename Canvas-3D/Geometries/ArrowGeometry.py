99a1fc2930f6d52b4909c3764bfbc7bca485cce2
David Geo Holmes <geometryzen@gmail.com>
2013-09-25 07:02:20 -0700

    Save file.

--- Canvas-3D/Geometries/ArrowGeometry.py
+++ Canvas-3D/Geometries/ArrowGeometry.py
@@ -5,7 +5,7 @@ from workbench import *
 space = CartesianSpace()
 
 # All arguments are optional and the defaults, in order, are as follows.
-length = 6
+length = 8
 segments = 12
 radiusShaft = 0.01
 radiusCone = 0.08
