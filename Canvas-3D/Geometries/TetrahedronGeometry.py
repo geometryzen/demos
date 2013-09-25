c6b430f271ac38d53a5906c84eef631d502657be
David Geo Holmes <geometryzen@gmail.com>
2013-09-25 06:37:59 -0700

    Save file.

--- Canvas-3D/Geometries/TetrahedronGeometry.py
+++ Canvas-3D/Geometries/TetrahedronGeometry.py
@@ -2,14 +2,7 @@ from three import *
 from browser import *
 from workbench import *
 
-scene = Scene()
-
-# Aspect ratio will be reset in onWindowResize
-camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 2
-
-renderer = WebGLRenderer()
-renderer.setClearColor(Color(0x080808), 1.0)
+space = CartesianSpace()
 
 radius = 1
 detail = 0 # Must be an integer: 0,1,2,...
@@ -21,10 +14,10 @@ print "radius:         " + str(tetra.radius)
 print "detail:         " + str(tetra.detail)
 print tetra
 
-mesh = Mesh(tetra, MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":3}))
+mesh = Mesh(tetra, MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 3}))
 scene.add(mesh)
 
-workbench = Workbench(renderer, camera)
+workbench = Workbench3D(space.renderer.canvas, space.renderer, space.camera)
 
 def setUp():
     workbench.setUp()
