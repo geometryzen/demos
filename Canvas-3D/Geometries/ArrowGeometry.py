60cca65a6259133e73af61cdeb8b1aa940b27420
David Geo Holmes <geometryzen@gmail.com>
2013-09-25 08:09:08 -0700

    Save file.

--- Canvas-3D/Geometries/ArrowGeometry.py
+++ Canvas-3D/Geometries/ArrowGeometry.py
@@ -14,7 +14,7 @@ radiusShaft = 0.01
 radiusCone = 0.08
 lengthCone = 0.2
 material = MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 1})
-arrow = ArrowBuilder().length(length).segments(segments).radiusShaft(radiusShaft).build()
+arrow = ArrowBuilder().scale(length).segments(segments).build()
 arrow.name = 'Foo'
 
 print repr(arrow)
