036262791d82d07b7926aed0e807ee8e6430a949
David Geo Holmes <geometryzen@gmail.com>
2013-09-25 20:35:47 -0700

    Save file.

--- Physics/visualizing-blades.py
+++ Physics/visualizing-blades.py
@@ -1,41 +1,80 @@
 '''
-This example demonstrates coloring shapes and normalizing their volumes to unity.
+Under Construction.
 '''
-from geometry import *
-from e3ga import *
+from easel import *
+from three import *
 from browser import *
 from workbench import *
-from math import pi
+from geometry import *
+from math import exp, pi
 
-space = CartesianSpace()
+timeOut = 600
 
-space.add(CylinderBuilder().color(0x00FF00).volume(1).build().translateX(-2.5).translateY(-2.5))
-space.add(CubeBuilder().color(0x0000FF).volume(1).build().translateX(2.5).translateY(2.5))
-space.add(SphereBuilder().color(0xFF0000).volume(1).build().translateX(+2.5).translateY(-2.5))
-space.add(ConeBuilder().color(0xFFFF00).volume(1).build().translateX(-2.5).translateY(+2.5))
-space.add(ArrowBuilder().color(0xFFFFFF).segments(16).scale(6).build())
+space3D = CartesianSpace()
+canvas3D = space3D.renderer.domElement
+workbench3D = Workbench3D(canvas3D, space3D.renderer, space3D.camera)
+   
+canvas2D = document.createElement("canvas")
+canvas2D.style.position = "absolute"
+canvas2D.style.top = "0px"
+canvas2D.style.left = "0px"
+workbench2D = Workbench2D(canvas2D)
+space2D = Stage(canvas2D)
+space2D.autoClear = True
 
-timeOut = 10
+output = Text("", "20px Helvetica", "white")
+output.x = 100
+output.y = 100
+space2D.addChild(output)
 
-workbench = Workbench(space.renderer, space.camera)
+quantity = ScalarE3(1) + VectorE3(4, 0, 0) + BivectorE3(4,0,0) + PseudoscalarE3(1)
 
-def onDocumentKeyDown(event):
+# Define the Meshes that you want to represent each grade of the quantity.
+wireframe = False
+grade0 = SphereBuilder().wireframe(True).color(0xFFFF00).segments(12).build()
+grade1 = ArrowBuilder().color(0x0000FF).wireframe(wireframe).segments(32).build()
+grade2 = VortexBuilder().wireframe(wireframe).color(0x0000FF).build()
+grade3 = CubeBuilder().wireframe(True).color(0xFFFF00).segments(1).build()
+
+probe  = ProbeE3(grade0, grade1, grade2, grade3)
+
+space3D.add(probe.grade0)
+space3D.add(probe.grade1)
+space3D.add(probe.grade2)
+space3D.add(probe.grade3)
+
+def escKey(event, downFlag):
+    event.preventDefault()
     global timeOut
-    if event.keyCode == 27:
-        timeOut = 0
+    timeOut = 0
+
+keyHandlers = {
+ 27: escKey
+}
+    
+def onDocumentKeyDown(event):
+    try:
+        keyHandlers[event.keyCode](event, True)
+    except:
+        pass
+
+def setUp():
+    workbench2D.setUp()
+    workbench3D.setUp()
+    document.addEventListener("keydown", onDocumentKeyDown, False)
 
 def tick(t):
-    space.render()
-    
+    probe.quantity = quantity
+    output.text = str(probe.quantity)
+    space3D.render()
+    space2D.render()
+
 def terminate(t):
     return t > timeOut
 
-def setUp():
-    workbench.setUp()
-    document.addEventListener("keydown", onDocumentKeyDown, False)
-
 def tearDown():
     document.removeEventListener("keydown", onDocumentKeyDown, False)
-    workbench.tearDown()
+    workbench3D.tearDown()
+    workbench2D.tearDown()
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
