79062b4b63e47bcc4a5d43b026fe9caa3c38da98
David Geo Holmes <geometryzen@gmail.com>
2013-09-27 12:39:11 -0700

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -21,7 +21,7 @@ particle.mass     = ScalarE3(1)
 particle.velocity = VectorE3(0, -1, 0)
 space3D.add(particle)
 
-wire = CylinderBuilder().radius(0.1).height(100).color(0xFF0000).build()
+wire = CylinderBuilder().radius(0.1).height(100).color(0xCCCCCC).build()
 space3D.add(wire)
 
 probeV = ProbeBuilderE3().color(particle.material.color.getHex()).build()
