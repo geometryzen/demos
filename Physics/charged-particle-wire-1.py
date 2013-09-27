36245b3a790020c7f8e2f4c6313a8f4840ff9be3
David Geo Holmes <geometryzen@gmail.com>
2013-09-27 10:19:06 -0700

    Save file.

--- Physics/charged-particle-wire-1.py
+++ Physics/charged-particle-wire-1.py
@@ -1,13 +1,7 @@
-'''
-Under construction
-'''
 from browser import *
-from easel import *
 from three import *
 from workbench import *
 from geometry import *
-from math import pow
-from random import random
 
 timeOut = 60
 
@@ -31,19 +25,6 @@ probeF = ProbeBuilderE3().color(0xFFFF00).build()
 space3D.add(probeF.grade1)
 
 dt = 0.02
-
-canvas2D = document.createElement("canvas")
-canvas2D.style.position = "absolute"
-canvas2D.style.top = "0px"
-canvas2D.style.left = "0px"
-workbench2D = Workbench2D(canvas2D)
-space2D = Stage(canvas2D)
-space2D.autoClear = True
-
-output = Text("", "20px Helvetica", "white")
-output.x = window.innerWidth / 2
-output.y = window.innerHeight / 2
-space2D.addChild(output)
 
 def escKey(event, downFlag):
     event.preventDefault()
@@ -70,7 +51,6 @@ def constantB(position):
     return VectorE3(1, 0, 0)
 
 def setUp():
-    workbench2D.setUp()
     workbench3D.setUp()
     document.addEventListener("keydown", onDocumentKeyDown, False)
 
@@ -105,6 +85,5 @@ def terminate(t):
 def tearDown():
     document.removeEventListener("keydown", onDocumentKeyDown, False)
     workbench3D.tearDown()
-    workbench2D.tearDown()
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
