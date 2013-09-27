07377f20f06e31237d376b46a8777d7445893ae8
David Geo Holmes <geometryzen@gmail.com>
2013-09-27 10:24:41 -0700

    Save file.

--- Physics/charged-particle-wire-1.py
+++ Physics/charged-particle-wire-1.py
@@ -9,7 +9,7 @@ space3D = CartesianSpace()
 
 workbench3D = Workbench(space3D.renderer, space3D.camera)
 
-particle = SphereBuilder().color(0x0000FF).radius(0.1).build()
+particle = SphereBuilder().color(0xCCCCCC).radius(0.1).build()
 particle.position = VectorE3(0, 1, 0)
 particle.mass     = ScalarE3(1)
 particle.velocity = VectorE3(0, -1, 0)
