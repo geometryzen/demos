1b7be601e38ba0e946c458097e47af7189d70669
David Geo Holmes <geometryzen@gmail.com>
2013-09-28 13:04:46 -0700

    Save file.

--- Mathematics/lorentzian.py
+++ Mathematics/lorentzian.py
@@ -107,7 +107,7 @@ assertEqual(C.t, 7)
 X = VectorL4(3, 4, 12, 13)
 assertEqual(X.quadrance(), 0)
 
-A = VectorL4(2, 3, 5, 7)
+A = Lorentzian(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
 B = VectorL4(11,13,17,19)
 C = A * B
 assertEqual(C.w, 0)
