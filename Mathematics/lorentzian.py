a6722681c2f0a81d66d6a40f1dd944d85c353975
David Geo Holmes <geometryzen@gmail.com>
2013-09-28 09:15:47 -0700

    Save file.

--- Mathematics/lorentzian.py
+++ Mathematics/lorentzian.py
@@ -4,7 +4,7 @@ Lorentzian is a Geometric Algebra multivector (quantity) with a metric that is L
 '''
 from lorentz import *
 
-A = VectorL4(2,3,5,7)
+A = VectorL4(2, 3, 5, 7)
 B = VectorL4(11,13,17,19)
 
 print A.x
