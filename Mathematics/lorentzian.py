4479ef9dca49f92caf05a2553be08c586e7430a8
David Geo Holmes <geometryzen@gmail.com>
2013-09-28 10:39:28 -0700

    Save file.

--- Mathematics/lorentzian.py
+++ Mathematics/lorentzian.py
@@ -63,4 +63,11 @@ assertEqual(C.y, 3)
 assertEqual(C.z, 5)
 assertEqual(C.t, 7)
 
+C = 5 + A
+assertEqual(C.w, 5)
+assertEqual(C.x, 2)
+assertEqual(C.y, 3)
+assertEqual(C.z, 5)
+assertEqual(C.t, 7)
+
 print "Done!"
