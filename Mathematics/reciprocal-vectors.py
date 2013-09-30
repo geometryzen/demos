7f6a8abd687f356c861ea56cce848c60344c8427
David Geo Holmes <geometryzen@gmail.com>
2013-09-30 08:39:28 -0700

    Save file.

--- Mathematics/reciprocal-vectors.py
+++ Mathematics/reciprocal-vectors.py
@@ -1,7 +1,11 @@
 '''
-Reciprocal vectors in two dimensions
+Reciprocal vectors in two dimensions.
+
+The purpose is to show that we can construct reciprocal vectors using geometric algebra.
+Note that the cross-product construction in 3 dimensions 
 '''
 from e2ga import *
+from e3ga import *
 
 i = VectorE2(1, 0)
 j = VectorE2(0, 1)
@@ -15,10 +19,15 @@ print r1 << e2
 print r2 << e1
 print r2 << e2
 
-s = e2 - (e2 << e1) * e1 / (e1 * e1)
 
-print s
+i = VectorE3(1, 0)
+j = VectorE3(0, 1)
+e1 = 12 * i
+e2 = 3 * i + 4 * j
+r1 = e2 / (e1 ^ e2)
+r2 = -e1 / (e1 ^ e2)
 
-print s * s
-
-print (e1 * e2) 
+print r1 << e1
+print r1 << e2
+print r2 << e1
+print r2 << e2
