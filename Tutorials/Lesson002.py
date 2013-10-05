26cf9e0678abbad0b0dd0f54fb5419ba4fd71e1f
David Geo Holmes <geometryzen@gmail.com>
2013-10-05 05:54:38 -0700

    Save file.

--- Tutorials/Lesson002.py
+++ Tutorials/Lesson002.py
@@ -5,10 +5,10 @@ from browser import WindowAnimationRunner
 from geometry import CartesianSpace
 from workbench import Workbench
 
-# A CartesianSpace is a pre-made scene containing a renderer, camera and HTML5 canvas element.
+# A CartesianSpace is a pre-made scene containing a renderer, camera, and HTML5 canvas element.
 scene = CartesianSpace()
 
-# The Workbench class knows how to incorporate the scene into the Geometry Zen window document.
+# The Workbench class knows how to incorporate the canvas into the Geometry Zen window document.
 # It also know how to clean up at the end of the animation and how to respond to resize events.
 workbench = Workbench(scene.renderer, scene.camera)
 
