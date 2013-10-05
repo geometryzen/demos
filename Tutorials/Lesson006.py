00570fa0993a0caac82cb13f15d972820959c11c
David Geo Holmes <geometryzen@gmail.com>
2013-10-05 05:22:57 -0700

    Save file.

--- Tutorials/Lesson006.py
+++ Tutorials/Lesson006.py
@@ -12,6 +12,7 @@ scene = Scene()
 
 camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
 camera.position.z = 2
+camera.lookAt(scene.position)
 
 renderer = WebGLRenderer()
 renderer.setClearColor(Color(0x080808), 1.0)
