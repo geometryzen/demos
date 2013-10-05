7f4bfef1352b198474fb17216b2e27b0cb3d7abe
David Geo Holmes <geometryzen@gmail.com>
2013-10-05 07:24:59 -0700

    Save file.

--- Units of Measure/natural-units.py
+++ Units of Measure/natural-units.py
@@ -3,18 +3,18 @@ In natural units, c = 1, hbar = 1
 '''
 from units import kilogram, meter, second, joule, newton, gram, cm
 
-milligram = 0.001 * gram
-mosquitoMassSI = 10 * milligram
-
-# Construct the Plank units for Length, Mass and Time.
 c = 2.998e+8 * meter / second
 print "speed of light, c=" + str(c)
 
 hbar = 1.054e-34 * joule * second
 print "Planck's constant, hbar=" + str(hbar)
 
-energyUnit = hbar / second
-lengthUnit = 1 / c
+eV = 1.6e-19 joule
+milligram = 0.001 * gram
+protonMass = 938.3 * mega * eV
+print "Proton mass " + str(protonMass)
 
-print "mosquito rest energy " + str(mosquitoMassSI * c * c / joule) + " J"
-print "mosquito rest energy (GeV) " + str(mosquitoMassSI * c * c / energyUnit)
+energyUnit = hbar / second
+lengthUnit = c / second
+
+print "proton rest energy " + str(protonMass * c * c / joule) + " J"
