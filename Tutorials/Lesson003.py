dc56ba6aa87db0a97468d2ffb550907efb241f37
David Geo Holmes <geometryzen@gmail.com>
2013-10-04 20:47:09 -0700

    Save file.

--- Tutorials/Lesson003.py
+++ Tutorials/Lesson003.py
@@ -1,1 +1,25 @@
-# Lesson003.py
+from browser import WindowAnimationRunner
+from geometry import CartesianSpace
+from workbench import Workbench
+
+space = CartesianSpace()
+
+sphere = SphereBuilder).build()
+space.add(sphere)
+
+workbench = Workbench(space.renderer, space.camera)
+
+def tick(t):
+    space.render()
+
+def terminate(t):
+    done = t > 4
+    return done
+
+def setUp():
+    workbench.setUp()
+
+def tearDown():
+    workbench.tearDown()
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
