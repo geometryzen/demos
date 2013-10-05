a3a5cd33b88249fb00f9fe85a71a1888ebd256e3
David Geo Holmes <geometryzen@gmail.com>
2013-10-04 20:59:15 -0700

    Save file.

--- Tutorials/Lesson005.py
+++ Tutorials/Lesson005.py
@@ -14,12 +14,14 @@ space.add(sphere)
 
 workbench = Workbench(space.renderer, space.camera)
 
+timeOut = 20
+
 def tick(t):
     sphere.position.set(R * cos(omega*t), R * sin(omega*t), 0)
     space.render()
 
 def terminate(t):
-    done = t > 10
+    done = t > 20
     return done
 
 def setUp():
@@ -29,7 +31,19 @@ def setUp():
 def tearDown():
     workbench.tearDown()
     
+def escKey(event, downFlag):
+    event.preventDefault()
+    global timeOut
+    timeOut = 0
+
+keyHandlers = {
+ 27: escKey
+}
+    
 def onDocumentKeyDown(event):
-    pass
+    try:
+        keyHandlers[event.keyCode](event, True)
+    except:
+        pass
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
