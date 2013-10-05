537c37240c334f8bf320a53b12d62c8cd8f60cd4
David Geo Holmes <geometryzen@gmail.com>
2013-10-04 20:55:25 -0700

    Save file.

--- Tutorials/Lesson005.py
+++ Tutorials/Lesson005.py
@@ -1,1 +1,32 @@
-# Lesson005.py
+from browser import WindowAnimationRunner
+from geometry import CartesianSpace, SphereBuilder
+from math import cos, sin, pi
+from workbench import Workbench
+
+T = 5
+omega = 2 * pi / T
+R = 4
+
+space = CartesianSpace()
+
+sphere = SphereBuilder().color(0x0000FF).build()
+space.add(sphere)
+
+workbench = Workbench(space.renderer, space.camera)
+
+def tick(t):
+    sphere.position.set(R * cos(omega*t), R * sin(omega*t), 0)
+    space.render()
+
+def terminate(t):
+    done = t > 10
+    return done
+
+def setUp():
+    document.addEventListener("keydown", onDocumentKeyDown, False)
+    workbench.setUp()
+
+def tearDown():
+    workbench.tearDown()
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
