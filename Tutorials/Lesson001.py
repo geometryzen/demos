3f5ea161d557f31e28dc78f50609617f59ea995c
David Geo Holmes <geometryzen@gmail.com>
2013-10-04 20:36:18 -0700

    Save file.

--- Tutorials/Lesson001.py
+++ Tutorials/Lesson001.py
@@ -1,3 +1,15 @@
 from browser import WindowAnimationRunner
 
+def tick(t):
+    pass
+
+def terminate(t):
+    return False
+
+def setUp():
+    pass
+
+def tearDown():
+    pass
+
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
