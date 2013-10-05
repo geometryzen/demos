0a0e72cfa668c806c3917d973f66105f5ca2a0b2
David Geo Holmes <geometryzen@gmail.com>
2013-10-04 20:41:25 -0700

    Save file.

--- Tutorials/Lesson002.py
+++ Tutorials/Lesson002.py
@@ -1,1 +1,17 @@
-# Lesson002.py
+from browser import WindowAnimationRunner
+
+def tick(t):
+    print "The time is now " + str(t)
+
+def terminate(t):
+    done = t > 1
+    print "Are we done yet? " + str(done)
+    return done
+
+def setUp():
+    pass
+
+def tearDown():
+    pass
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
