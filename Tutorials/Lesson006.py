a99e33990492cf1c35ff36ad39d8c0ee2baf81f5
David Geo Holmes <geometryzen@gmail.com>
2013-10-05 05:34:45 -0700

    Save file.

--- Tutorials/Lesson006.py
+++ Tutorials/Lesson006.py
@@ -11,14 +11,14 @@ R = 4
 scene = Scene()
 
 camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 5
+camera.position.z = 10
 camera.lookAt(scene.position)
 
 renderer = WebGLRenderer()
 renderer.setClearColor(Color(0x080808), 1.0)
 
 pointLight = PointLight(0xFFFFFF, 1, 100)
-pointLight.position.set(0, 0, 5)
+pointLight.position.set(0, 0, 10)
 scene.add(pointLight)
 
 sphere = SphereBuilder().color(0x0000FF).build()
