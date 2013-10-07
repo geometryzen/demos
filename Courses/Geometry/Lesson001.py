535a33fdbc7036a6c14051005ea91c9dc6f120c0
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 16:40:36 -0700

    Save file.

--- Courses/Geometry/Lesson001.py
+++ Courses/Geometry/Lesson001.py
@@ -1,1 +1,15 @@
-# Courses/Geometry/Lesson001.py
+'''
+The first thing we will do is create a linear space 
+'''
+class Euclidean:
+    def __init__(self):
+        pass
+
+def Vector():
+    return Euclidean()
+
+def Scalar():
+    return Euclidean()
+
+f = Vector()
+    
