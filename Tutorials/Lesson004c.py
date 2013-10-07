7b3cd3ed82f4d8753154a2ffc50b1579d3237f41
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 06:26:12 -0700

    Save file.

--- Tutorials/Lesson004c.py
+++ Tutorials/Lesson004c.py
@@ -1,1 +1,35 @@
-# Lesson004c.py
+'''
+This lesson demonstrates animating an object.
+The sphere performs cirular motion with radius R and periodic time T.
+'''
+from browser import WindowAnimationRunner
+from geometry import CartesianSpace, SphereBuilder
+from math import cos, sin, pi
+from workbench import Workbench
+
+T = 5
+omega = 2 * pi / T
+R = 4
+
+space = CartesianSpace()
+
+sphere = SphereBuilder().color(0x0000FF).build()
+space.add(sphere)
+
+workbench = Workbench(space.renderer, space.camera)
+
+def tick(t):
+    sphere.position.set(R * cos(omega*t), R * sin(omega*t), 0)
+    space.render()
+
+def terminate(t):
+    done = t > 10
+    return done
+
+def setUp():
+    workbench.setUp()
+
+def tearDown():
+    workbench.tearDown()
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
