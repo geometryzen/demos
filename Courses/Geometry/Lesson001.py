4ab294b66079e1dfe518cbb8a0834961194dad7c
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 16:44:45 -0700

    Save file.

--- Courses/Geometry/Lesson001.py
+++ Courses/Geometry/Lesson001.py
@@ -1,5 +1,7 @@
 '''
 The first thing we will do is create a linear space, axiom by axiom.
+Our space will have two kinds of elements we call vectors and scalars.
+With an eye to creating a unified geometric space, the underlying object is a Euclidean.
 '''
 class Euclidean:
     def __init__(self):
