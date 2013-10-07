d9af3aa2e5c04cbabedcac2ffe10a835fccfed90
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 16:53:11 -0700

    Save file.

--- Courses/Geometry/Lesson002.py
+++ Courses/Geometry/Lesson002.py
@@ -1,1 +1,24 @@
-# Lesson002.py
+'''
+
+'''
+class Euclidean:
+    def __init__(self):
+        pass
+    
+    def __repr__(self):
+        return "Euclidean()"
+
+def Vector():
+    return Euclidean()
+
+def Scalar():
+    return Euclidean()
+
+f = Vector()
+g = Vector()
+k = Scalar()
+
+# We want f + g to be in the vector space.
+print "f : " + repr(f)
+print "g : " + repr(g)
+print "f + g : " + repr(f + g)
