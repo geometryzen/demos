2259eb8b67f4f3da0e6598ff9c9d9c40f7c0c687
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 06:42:19 -0700

    Save file.

--- Tutorials/Lesson004b.py
+++ Tutorials/Lesson004b.py
@@ -1,1 +1,66 @@
-# Lesson004b.py
+'''
+This lesson demonstrates animating an object.
+The sphere moves along a square path using geometric instructions.
+'''
+from browser import WindowAnimationRunner
+from geometry import CartesianSpace, SphereBuilder
+from math import cos, sin, pi
+from workbench import Workbench
+
+L = 50
+scale = 3.0/L
+x = -L
+y = -L
+z = 0
+
+moveX = 0
+moveY = 1
+moveZ = 0
+
+space = CartesianSpace()
+
+sphere = SphereBuilder().radius(0.1).color(0xFFFF00).build()
+space.add(sphere)
+
+workbench = Workbench(space.renderer, space.camera)
+
+def tick(t):
+    global x, y, z, moveX, moveY, moveZ
+    x += moveX
+    y += moveY
+    z += moveZ
+    if x > L:
+        x = L
+        moveX = 0
+        moveY = -1
+        moveZ = 0
+    if y > L:
+        y = L
+        moveX = 1
+        moveY = 0
+        moveZ = 0
+    if x < -L:
+        x = -L
+        moveX = 0
+        moveY = 1
+        moveZ = 0
+    if y < -L:
+        y = -L
+        moveX = -1
+        moveY = 0
+        moveZ = 0
+
+    sphere.position.set(x * scale, y * scale, z * scale)
+    space.render()
+
+def terminate(t):
+    done = t > 10
+    return done
+
+def setUp():
+    workbench.setUp()
+
+def tearDown():
+    workbench.tearDown()
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
