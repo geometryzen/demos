81bed932dc1e0b92be97ae779b540ac72ff9c859
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 18:23:47 -0700

    Save file.

--- Courses/Geometry/Lesson001.py
+++ Courses/Geometry/Lesson001.py
@@ -5,36 +5,35 @@ With an eye to creating a unified geometric space, the underlying object is a Eu
 We demonstrate that the objects f, g, and k are elements in the space.
 '''
 class Euclidean:
-    def __init__(self, w, x, y, z):
+    def __init__(self, w, x, y):
         self.w = w
         self.x = x
         self.y = y
-        self.z = z
     
     def __str__(self):
         parts = []
         if self.w != 0:
             parts.append(str(self.w))
-        if self.x != 0 or self.y != 0 or self.z != 0:
-            parts.append("+".join([str(self.x)+"*e1", str(self.y)+"*e2", str(self.z)+"*e3"]))
+        if self.x != 0 or self.y != 0:
+            parts.append("+".join([str(self.x)+"*e1", str(self.y)+"*e2"]))
         return "+".join(parts)
     
     def __repr__(self):
         parts = []
         if self.w != 0:
             parts.append("Scalar(" + str(self.w) + ")")
-        if self.x != 0 or self.y != 0 or self.z != 0:
-            parts.append("Vector(" + ", ".join([str(self.x), str(self.y), str(self.z)]) + ")")
+        if self.x != 0 or self.y != 0:
+            parts.append("Vector(" + ", ".join([str(self.x), str(self.y)]) + ")")
         return "+".join(parts)
 
-def Vector(x, y, z):
-    return Euclidean(0, x, y, z)
+def Vector(x, y):
+    return Euclidean(0, x, y)
 
 def Scalar(w):
-    return Euclidean(w, 0, 0, 0)
+    return Euclidean(w, 0, 0)
 
-f = Vector(1, 2, 3)
-g = Vector(5, 7, 11)
+f = Vector(1, 2)
+g = Vector(5, 7)
 k = Scalar(4)
 
 print "f => " + str(f)
