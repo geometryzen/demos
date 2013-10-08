75f8ab7afc3d9a8b72790a95d447d26dc5efd55f
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 17:04:16 -0700

    Save file.

--- Courses/Geometry/Lesson004.py
+++ Courses/Geometry/Lesson004.py
@@ -5,6 +5,7 @@ There is also a rule for scalar mutiplication.
 If f is in V and k is a scalar, then kf is in V.
 For al f,g,h in V and c,k scalars, the set also satisfies the following rules:
 1. (f + g) + h = f + (g + h)
+2. f + g = g + f
 '''
 class Euclidean:
     def __init__(self):
@@ -38,3 +39,4 @@ print "g : " + repr(g)
 print "f + g : " + repr(f + g)
 print "k * f : " + repr(k * f)
 print (f + g) + h == f + (g + h)
+pring f + g == g + f
