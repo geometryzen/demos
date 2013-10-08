a2991d16fedd28b7d87fd0fc7d022786b7d23dbe
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 19:53:15 -0700

    Save file.

--- Courses/Geometry/Lesson001b.py
+++ Courses/Geometry/Lesson001b.py
@@ -1,1 +1,41 @@
 # Lesson001b.py
+class Euclidean:
+    def __init__(self, w, x, y):
+        if isinstance(w, float):
+            self.w = w
+        else:
+            raise AssertionError("w must be a float")
+        if isinstance(x, float):
+            self.x = x
+        else:
+            raise AssertionError("x must be a float")
+        if isinstance(y, float):
+            self.y = y
+        else:
+            raise AssertionError("y must be a float")
+    
+    def __str__(self):
+        parts = []
+        if self.w != 0.0:
+            parts.append(str(self.w))
+        if self.x != 0.0 or self.y != 0.0:
+            parts.append(" + ".join([str(self.x)+" * i", str(self.y)+" * j"]))
+        return "+".join(parts)
+    
+    def __repr__(self):
+        parts = []
+        if self.w != 0.0:
+            parts.append("Scalar(" + str(self.w) + ")")
+        if self.x != 0.0 or self.y != 0.0:
+            parts.append("Vector(" + ", ".join([str(self.x), str(self.y)]) + ")")
+        return "+".join(parts)
+
+def Vector(x, y):
+    return Euclidean(0.0, x, y)
+
+def Scalar(w):
+    return Euclidean(w, 0.0, 0.0)
+
+f = Vector(1.0, 2.0)
+g = Vector(5.0, 7.0)
+k = Scalar(4.0)
