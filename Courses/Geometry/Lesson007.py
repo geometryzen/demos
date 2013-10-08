cc98b98579307459f1e3faf3f4d934c801068a1d
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 19:33:50 -0700

    Save file.

--- Courses/Geometry/Lesson007.py
+++ Courses/Geometry/Lesson007.py
@@ -91,5 +91,5 @@ print "f => " + str(f)
 print "g => " + str(g)
 print "k => " + str(k)
 
-print "k * (f + g) => " + str(k * (f + g))
+print "k * (f + g)   => " + str(k * (f + g))
 print "k * f + k * g => " + str(k * f + k * g)
