eb7ea0073f60875b2306534ec46809b1786c04e8
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 19:15:59 -0700

    Save file.

--- Courses/Geometry/Lesson005.py
+++ Courses/Geometry/Lesson005.py
@@ -10,54 +10,66 @@ For al f,g,h in V and c,k scalars, the set also satisfies the following rules:
 This n is unique and denoted by 0.
 '''
 class Euclidean:
-    def __init__(self, w, x, y, z):
-        self.w = w
-        self.x = x
-        self.y = y
-        self.z = z
-        
+    def __init__(self, w, x, y):
+        if isinstance(w, float):
+            self.w = w
+        else:
+            raise AssertionError("w must be a float")
+        if isinstance(x, float):
+            self.x = x
+        else:
+            raise AssertionError("x must be a float")
+        if isinstance(y, float):
+            self.y = y
+        else:
+            raise AssertionError("y must be a float")
+            
     def __add__(self, other):
-        return Euclidean(self.w + other.w, self.x + other.x, self.y + other.y, self.z + other.z)
+        if not isinstance(other, Euclidean):
+            raise AssertionError("other must be a Euclidean")
+        return Vector(self.x + other.x, self.y + other.y)
 
     def __mul__(self, other):
-        return Euclidean(0, self.w * other.x, self.w * other.y, self.w * other.z)
-    
+        if not isinstance(other, Euclidean):
+            raise AssertionError("other must be a Euclidean")
+        return Vector(self.w * other.x, self.w * other.y)
+
+    def __rmul__(self, other):
+        if not isinstance(other, float):
+            raise AssertionError("other must be a Euclidean or float")
+        return Vector(other * self.x, other * self.y)
+
     def __eq__(self, other):
-        return self.w == other.w and self.x == other.x and self.y == other.y and self.z == other.z
+        if not isinstance(other, Euclidean):
+            return False
+        return self.w == other.w and self.x == other.x and self.y == other.y
     
     def __str__(self):
         parts = []
-        if self.w != 0:
+        if self.w != 0.0:
             parts.append(str(self.w))
-        if self.x != 0 or self.y != 0 or self.z != 0:
-            parts.append("+".join([str(self.x)+"*e1", str(self.y)+"*e2", str(self.z)+"*e3"]))
-        if len(parts) > 0:
-            return "+".join(parts)
-        else:
-            return "0"
+        if self.x != 0.0 or self.y != 0.0:
+            parts.append(" + ".join([str(self.x)+" * i", str(self.y)+" * j"]))
+        return "+".join(parts)
     
     def __repr__(self):
         parts = []
-        if self.w != 0:
+        if self.w != 0.0:
             parts.append("Scalar(" + str(self.w) + ")")
-        if self.x != 0 or self.y != 0 or self.z != 0:
-            parts.append("Vector(" + ", ".join([str(self.x), str(self.y), str(self.z)]) + ")")
-        if len(parts) > 0:
-            return "+".join(parts)
-        else:
-            return "0"
+        if self.x != 0.0 or self.y != 0.0:
+            parts.append("Vector(" + ", ".join([str(self.x), str(self.y)]) + ")")
+        return "+".join(parts)
 
-def Vector(x, y, z):
-    return Euclidean(0, x, y, z)
+def Vector(x, y):
+    return Euclidean(0.0, x, y)
 
 def Scalar(w):
-    return Euclidean(w, 0, 0, 0)
+    return Euclidean(w, 0.0, 0.0)
 
-f = Vector(1, 2, 3)
-g = Vector(3, 5, 7)
-h = Vector(11, 13, 17)
-k = Scalar(4)
-n = Vector(0, 0, 0)
+f = Vector(1.0, 2.0)
+g = Vector(3.0, 5.0)
+k = Scalar(4.0)
+h = Vector(7.0, 11.0)
 
 print "f => " + str(f)
 print "g => " + str(g)
