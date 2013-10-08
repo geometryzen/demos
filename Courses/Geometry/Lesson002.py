5c3c8b9a1be825a91a18e991ab43908b6d2cb09a
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 17:42:11 -0700

    Save file.

--- Courses/Geometry/Lesson002.py
+++ Courses/Geometry/Lesson002.py
@@ -3,24 +3,43 @@ A linear space, V, is a set endowed with a rule for addition.
 If f and g are in V, then so is f + g.
 '''
 class Euclidean:
-    def __init__(self):
-        pass
+    def __init__(self, w, x, y):
+        self.w = w
+        self.x = x
+        self.y = y
     
-    def __add__(self, other):
-        return Euclidean()
+    def __str__(self):
+        parts = []
+        if self.w != 0:
+            parts.append(str(self.w))
+        if self.x != 0 or self.y != 0:
+            parts.append("[" + str(self.x) + ", " + str(self.y) + "]")
+        return "+".join(parts)
     
     def __repr__(self):
-        return "Euclidean()"
+        parts = []
+        if self.w != 0:
+            parts.append("Scalar(" + str(self.w) + ")")
+        if self.x != 0 or self.y != 0:
+            parts.append("Vector(" + str(self.x) + ", " + str(self.y) + ")")
+        return "+".join(parts)
 
-def Vector():
-    return Euclidean()
+def Vector(x, y):
+    return Euclidean(0, x, y)
 
-def Scalar():
-    return Euclidean()
+def Scalar(w):
+    return Euclidean(w, 0, 0)
 
-f = Vector()
-g = Vector()
-k = Scalar()
+f = Vector(1, 2)
+g = Vector(3, 5)
+k = Scalar(4)
+
+print "f => " + str(f)
+print "g => " + str(g)
+print "k => " + str(k)
+print "repr(f) => " + repr(f)
+print "repr(g) => " + repr(g)
+print "repr(k) => " + repr(k)
 
 # We want f + g to be in the vector space (a Euclidean vector).
 # Python allows us to do this using __add__.
