fabe5ef2fc0e400ab998676a66c9c7f28063fe3c
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 18:46:28 -0700

    Save file.

--- Courses/Geometry/Lesson002.py
+++ Courses/Geometry/Lesson002.py
@@ -3,41 +3,45 @@ A linear space, V, is a set endowed with a rule for addition.
 If f and g are in V, then so is f + g.
 '''
 class Euclidean:
-    def __init__(self, w, x, y, z):
-        self.w = w
-        self.x = x
-        self.y = y
-        self.z = z
-        
-    def __add__(self, other):
-        return Euclidean(self.w + other.w, self.x + other.x, self.y + other.y, self.z + other.z)
+    def __init__(self, w, x, y):
+        if isinstance(w, float):
+            self.w = w
+        else:
+            raise AssertionError("w must be a float")
+        if isinstance(x, float):
+            self.x = x
+        else:
+            raise AssertionError("x must be a float")
+        if isinstance(y, float):
+            self.y = y
+        else:
+            raise AssertionError("y must be a float")
     
     def __str__(self):
         parts = []
-        if self.w != 0:
+        if self.w != 0.0:
             parts.append(str(self.w))
-        if self.x != 0 or self.y != 0 or self.z != 0:
-            parts.append("+".join([str(self.x)+"*e1", str(self.y)+"*e2", str(self.z)+"*e3"]))
+        if self.x != 0.0 or self.y != 0.0:
+            parts.append("+".join([str(self.x)+"i", str(self.y)+"j"]))
         return "+".join(parts)
     
     def __repr__(self):
         parts = []
-        if self.w != 0:
+        if self.w != 0.0:
             parts.append("Scalar(" + str(self.w) + ")")
-        if self.x != 0 or self.y != 0 or self.z != 0:
-            parts.append("Vector(" + ", ".join([str(self.x), str(self.y), str(self.z)]) + ")")
+        if self.x != 0.0 or self.y != 0.0:
+            parts.append("Vector(" + ", ".join([str(self.x), str(self.y)]) + ")")
         return "+".join(parts)
 
-def Vector(x, y, z):
-    return Euclidean(0, x, y, z)
+def Vector(x, y):
+    return Euclidean(0.0, x, y)
 
 def Scalar(w):
-    return Euclidean(w, 0, 0, 0)
+    return Euclidean(w, 0.0, 0.0)
 
-f = Vector(1, 2, 3)
-g = Vector(3, 5, 7)
-k = Scalar(4)
-
+f = Vector(1.0, 2.0)
+g = Vector(5.0, 7.0)
+k = Scalar(4.0)
 print "f => " + str(f)
 print "g => " + str(g)
 print "k => " + str(k)
