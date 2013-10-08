1a30685b91658cc7bdc020259b7368527f340b6d
David Geo Holmes <geometryzen@gmail.com>
2013-10-08 14:11:32 -0700

    Save file.

--- Courses/Geometry/Lesson001b.py
+++ Courses/Geometry/Lesson001b.py
@@ -46,8 +46,8 @@ def Vector(x, y):
 def Scalar(w):
     return Euclidean(w, 0.0, 0.0)
 
-f = Vector(-1.0, -2.0)
-g = Vector(4.0, 3.0)
+f = Vector(1.0, 2.0)
+g = Vector(-4.0, -3.0)
 k = Scalar(4.0)
 
 scene = CartesianSpace()
