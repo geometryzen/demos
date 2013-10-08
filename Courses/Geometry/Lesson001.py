ac653d05afab2eabc57d3599e469e9a4f0cbcf47
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 17:20:25 -0700

    Save file.

--- Courses/Geometry/Lesson001.py
+++ Courses/Geometry/Lesson001.py
@@ -26,6 +26,6 @@ k = Scalar(4)
 
 # It's not possible to distinguish scalars from vectors yet.
 # At least we can see that they are all the same type.
-print "f : " + str(type(f))
-print "g : " + str(type(g))
-print "k : " + str(type(k))
+print "f : " + repr(f)
+print "g : " + repr(g)
+print "k : " + repr(k)
