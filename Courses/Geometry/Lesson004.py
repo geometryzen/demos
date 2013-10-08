96f7bca40e1a3d2c3dd15d3369874983df72c372
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 17:01:46 -0700

    Save file.

--- Courses/Geometry/Lesson004.py
+++ Courses/Geometry/Lesson004.py
@@ -1,1 +1,39 @@
-# Lesson004.py
+'''
+A linear space, V, is a set endowed with a rule for addition.
+If f and g are in V, then so is f + g.
+There is also a rule for scalar mutiplication.
+If f is in V and k is a scalar, then kf is in V.
+For al f,g,h in V and c,k scalars, the set also satisfies the following rules:
+1. (f + g) + h = f + (g + h)
+'''
+class Euclidean:
+    def __init__(self):
+        pass
+    
+    def __add__(self, other):
+        return Euclidean()
+
+    def __mul__(self, other):
+        return Euclidean()
+    
+    def __repr__(self):
+        return "Euclidean()"
+
+def Vector():
+    return Euclidean()
+
+def Scalar():
+    return Euclidean()
+
+f = Vector()
+g = Vector()
+h = Vector()
+k = Scalar()
+
+# We want k * f to be in the vector space (a Euclidean vector).
+# Python allows us to do this using __add__.
+print "f : " + repr(f)
+print "g : " + repr(g)
+print "f + g : " + repr(f + g)
+print "k * f : " + repr(k * f)
+pring 
