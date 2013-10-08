5dc4c44273eb421c804e2ba46640ee13277067b3
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 19:32:36 -0700

    Save file.

--- Courses/Geometry/Lesson007.py
+++ Courses/Geometry/Lesson007.py
@@ -86,11 +86,9 @@ def Scalar(w):
 f = Vector(1.0, 2.0)
 g = Vector(3.0, 5.0)
 k = Scalar(4.0)
-h = Vector(7.0, 11.0)
-n = Vector(0.0, 0.0)
 
 print "f => " + str(f)
 print "g => " + str(g)
-print "n => " + str(n)
+print "k => " + str(k)
 
 print "k * (f + g) => " + str(k * (f + g))
