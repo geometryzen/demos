169ba8ff521b870ca09e81eacf42aaa127cc53ca
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 17:38:26 -0700

    Save file.

--- Courses/Geometry/Lesson001.py
+++ Courses/Geometry/Lesson001.py
@@ -10,8 +10,13 @@ class Euclidean:
         self.x = x
         self.y = y
     
-#    def __str__(self):
-#        return "[]"
+    def __str__(self):
+        parts = []
+        if self.w != 0:
+            parts.append("Scalar(" + str(self.w) + ")")
+        if self.x != 0 or self.y != 0:
+            parts.append("Vector(" + str(self.x) + ", " + str(self.y) + ")")
+        return "+".join(parts)
     
     def __repr__(self):
         parts = []
