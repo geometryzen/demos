b5a7186caf886489e696db4ad37ccd3f1192febc
David Geo Holmes <geometryzen@gmail.com>
2013-10-07 18:03:34 -0700

    Save file.

--- Courses/Geometry/Lesson005.py
+++ Courses/Geometry/Lesson005.py
@@ -10,41 +10,53 @@ For al f,g,h in V and c,k scalars, the set also satisfies the following rules:
 This n is unique and denoted by 0.
 4. For each f in
 '''
-class Euclidean:
-    def __init__(self):
-        pass
-    
-    def __add__(self, other):
-        return Euclidean()
-
-    def __mul__(self, other):
-        return Euclidean()
+ * other.x, self.w * other.y, self.w * other.z)
     
     def __eq__(self, other):
-        return True
+        return self.w == other.w and self.x == other.x and self.y == other.y and self.z == other.z
+    
+    def __str__(self):
+        parts = []
+        if self.w != 0:
+            parts.append(str(self.w))
+        if self.x != 0 or self.y != 0 or self.z != 0:
+            parts.append("+".join([str(self.x)+"*e1", str(self.y)+"*e2", str(self.z)+"*e3"]))
+        return "+".join(parts)
     
     def __repr__(self):
-        return "Euclidean()"
+        parts = []
+        if self.w != 0:
+            parts.append("Scalar(" + str(self.w) + ")")
+        if self.x != 0 or self.y != 0:
+            parts.append("Vector(" + ", ".join([str(self.x), str(self.y), str(self.z)]) + ")")
+        return "+".join(parts)
 
-def Vector():
-    return Euclidean()
+def Vector(x, y, z):
+    return Euclidean(0, x, y, z)
 
-def Scalar():
-    return Euclidean()
+def Scalar(w):
+    return Euclidean(w, 0, 0, 0)
 
-f = Vector()
-g = Vector()
-h = Vector()
-k = Scalar()
-n = Vector()
+f = Vector(1, 2, 3)
+g = Vector(3, 5, 7)
+h = Vector(11, 13, 17)
+k = Scalar(4)
 
-V = [f, g, h, n] 
+print "f => " + str(f)
+print "g => " + str(g)
+print "k => " + str(k)
+print "repr(f) => " + repr(f)
+print "repr(g) => " + repr(g)
+print "repr(k) => " + repr(k)
 
-print "f : " + repr(f)
-print "g : " + repr(g)
-print "f + g : " + repr(f + g)
-print "k * f : " + repr(k * f)
+
+# We want f * g to be in the vector space (a Euclidean vector).
+# Python allows us to do this using __mul__.
+print "f + g => " + str(f + g)
+print str(k) + " * " + str(f) + " => " + str(k * f)
+
 print (f + g) + h == f + (g + h)
 print f + g == g + f
+
 print f + n == f
 print n != f
