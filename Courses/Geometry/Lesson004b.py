edfd3ae083467fc8f3a008547cb867ed5f40bbd1
David Geo Holmes <geometryzen@gmail.com>
2013-10-08 21:02:02 -0700

    Save file.

--- Courses/Geometry/Lesson004b.py
+++ Courses/Geometry/Lesson004b.py
@@ -86,7 +86,7 @@ def onDocumentKeyUp(event):
 def tick(t):
     global f
     if moveLeft:
-        f = f + VectorE3(0.0, -0.1, 0.0)
+        f -= 0.1 * i
     if moveRight:
         f = f + VectorE3(0.0, +0.1, 0.0)
     if moveForward:
