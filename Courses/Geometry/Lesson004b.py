ba792405dbeb0c2775d017e85b00ce041baef519
David Geo Holmes <geometryzen@gmail.com>
2013-10-08 19:13:12 -0700

    Save file.

--- Courses/Geometry/Lesson004b.py
+++ Courses/Geometry/Lesson004b.py
@@ -69,9 +69,9 @@ def Vector(x, y):
 def Scalar(w):
     return Euclidean(w, 0.0, 0.0)
 
-f = Vector(3.0, 0.0)
-g = Vector(3.0, 5.0)
-h = Vector(7.0, 11.0)
+f = Vector(3.0, 1.0)
+g = Vector(0.0, 2.0)
+h = Vector(1.0, 1.0)
 u = f + g
 v = g + h
 i = f + g + h
