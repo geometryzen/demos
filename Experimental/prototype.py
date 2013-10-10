f1e7f7e91d5fa706d8085b874d00d2c17be48eba
David Geo Holmes <geometryzen@gmail.com>
2013-10-10 07:07:27 -0700

    Save file.

--- Experimental/prototype.py
+++ Experimental/prototype.py
@@ -1,1 +1,47 @@
-# TODO:
+from browser import *
+from geometry import *
+from math import *
+from workbench import *
+
+T = 5 # Periodic Time
+omega = 2 * pi / T # Angular Velocity
+R = 4 # radius
+
+space = CartesianSpace()
+
+sphere = SphereBuilder().color(0x0000FF).build()
+space.add(sphere)
+
+workbench = Workbench(space.renderer, space.camera)
+
+timeOut = 1.5 * T
+
+def tick(t):
+    sphere.position.set(R * cos(omega*t), R * sin(omega*t), 0)
+    space.render()
+
+def terminate(time):
+    return time > timeOut
+
+def setUp():
+    workbench.setUp()
+    document.addEventListener("keydown", onDocumentKeyDown, False)
+
+def tearDown():
+    document.removeEventListener("keydown", onDocumentKeyDown, False)
+    workbench.tearDown()
+
+def escKey(event, downFlag):
+    event.preventDefault()
+    global timeOut
+    timeOut = 0
+
+keyHandlers = {27: escKey}
+    
+def onDocumentKeyDown(event):
+    try:
+        keyHandlers[event.keyCode](event, True)
+    except:
+        pass
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
