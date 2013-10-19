ecf6f8d4202bf7aed05075d4205a241cc01e6e35
David Geo Holmes <geometryzen@gmail.com>
2013-10-19 13:06:21 -0700

    Save file.

--- Courses/SpecialRelativity/Lesson002-Geometric-LT-E2-Bondi.py
+++ Courses/SpecialRelativity/Lesson002-Geometric-LT-E2-Bondi.py
@@ -1,4 +1,4 @@
-from e3ga import *
+from e2ga import *
 from math import *
 
 def gamma(beta):
@@ -8,24 +8,46 @@ def boost(beta):
     g = gamma(beta)
     return (1 + g + g * beta) / sqrt(2 * (1 + g))
 
-beta = VectorE3(3.0/5.0, 0, 0)
+def LccSquared(k, direction):
+    return (k * k + 1) * (1 - direction) / (2 * k)
 
-print "beta: " + repr(beta)
+k = ScalarE2(2.0)
+
+print "k: " + str(k)
+
+B = (k * k - 1) / (k * k + 1)
+
+i = VectorE2(1, 0)
+
+print "beta: " + str(beta)
 
 g = gamma(beta)
 
-print "g: " + repr(g)
+print "gamma: " + str(g)
 
-L = boost(beta)
+Lcc2 = LccSquared(k, i)
 
-print "L: " + repr(L)
-
-x = ScalarE3(4) + VectorE3(3, 0, 0)
-
-print "x: " + repr(x)
+print ""
+print "Lcc2: " + str(Lcc2)
 
 Lcc = cliffordConjugate(L)
 
-xPrimed = Lcc * x * Lcc
+print "cliffordConjugate(L): " + str(Lcc)
 
-print "x': " + repr(xPrimed)
+print "L * cliffordConjugate(L): " + str(L * Lcc)
+
+X = ScalarE2(4) + VectorE2(3, 1)
+
+print ""
+print "X : " + str(X)
+print "ct: " + str(X[0])
+print "r : " + str(X[1])
+print "Interval Quadrance : " + str(X * cliffordConjugate(X))
+
+xPrimed = Lcc * X * Lcc
+
+print ""
+print "X' : " + str(xPrimed)
+print "ct': " + str(xPrimed[0])
+print "r' : " + str(xPrimed[1])
+print "Interval Quadrance : " + str(xPrimed * cliffordConjugate(xPrimed))
