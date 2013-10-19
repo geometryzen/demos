3ff9bd89b31156a25c4f03fb8fda52dcec979ecc
David Geo Holmes <geometryzen@gmail.com>
2013-10-18 21:35:44 -0700

    Save file.

--- Courses/SpecialRelativity/lesson001.py
+++ Courses/SpecialRelativity/lesson001.py
@@ -1,3 +1,12 @@
+'''
+I'm starting here with the representation in which time is the scalar part of a multivector.
+It's fascinating to see this work using just the Euclidean2 numbers.
+Obviously this requires the invariant interval to be calculated as X * cliffordConjugate(X).
+The next steps will be:
+1. Perform the same calculation using Spacetime Algebra.
+2. Experiment with an arbitrary field. e.g. Rational or Complex (prelude to symbolic calculation).
+3. Visualization technigues. Should be able to show and improve upon Spacetime diagrams.
+'''
 from e2ga import *
 from math import *
 
@@ -30,6 +39,7 @@ print ""
 print "X : " + str(X)
 print "ct: " + str(X[0])
 print "r : " + str(X[1])
+print "Interval : " + str(X * cliffordConjugate(X))
 
 xPrimed = Lcc * X * Lcc
 
@@ -37,3 +47,4 @@ print ""
 print "x' : " + str(xPrimed)
 print "ct': " + str(xPrimed[0])
 print "r' : " + str(xPrimed[1])
+print "Interval : " + str(xPrimed * cliffordConjugate(xPrimed))
