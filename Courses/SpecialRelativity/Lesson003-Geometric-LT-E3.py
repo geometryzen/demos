506cd62e4a90bc7b72c56782607ccaf179a19dda
David Geo Holmes <geometryzen@gmail.com>
2013-10-19 12:53:50 -0700

    Save file.

--- Courses/SpecialRelativity/Lesson003-Geometric-LT-E3.py
+++ Courses/SpecialRelativity/Lesson003-Geometric-LT-E3.py
@@ -1,1 +1,31 @@
-# Lesson003-Geometric-LT-E3.py
+from e3ga import *
+from math import *
+
+def gamma(beta):
+    return 1 / sqrt(1 - beta * beta)
+
+def boost(beta):
+    g = gamma(beta)
+    return (1 + g + g * beta) / sqrt(2 * (1 + g))
+
+beta = VectorE3(3.0/5.0, 0, 0)
+
+print "beta: " + repr(beta)
+
+g = gamma(beta)
+
+print "g: " + repr(g)
+
+L = boost(beta)
+
+print "L: " + repr(L)
+
+x = ScalarE3(4) + VectorE3(3, 0, 0)
+
+print "x: " + repr(x)
+
+Lcc = cliffordConjugate(L)
+
+xPrimed = Lcc * x * Lcc
+
+print "x': " + repr(xPrimed)
