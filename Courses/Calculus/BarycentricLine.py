bb543c5131f1a9b8cedf465d027cae067a77aa99
David Geo Holmes <geometryzen@gmail.com>
2013-10-21 12:18:20 -0700

    Save file.

--- Courses/Calculus/BarycentricLine.py
+++ Courses/Calculus/BarycentricLine.py
@@ -5,20 +5,20 @@ j = VectorE3(0,1,0)
 k = VectorE3(0,0,1)
 
 # Endpoints of the Barycentric Line.
-a1 = i + j
-a2 = 3 * i + 4 * j
+a0 = i + j
+a1 = 3 * i + 4 * j
 
 # The Barycentric coordinates sum to unity.
-t1 = 0.6
-t2 = 0.4
+t0 = 0.6
+t1 = 0.4
 
 # Compute the vector from the coordinates.
-a = t1 * a1 + t2 * a2
+a = t0 * a0 + t1 * a1
 
 print a
 # Verify theat a does actually lie on the line.
-print (a2 - a1) * (0.8 / 2.0) + a1
+print (a1 - a0) * (0.8 / 2.0) + a0
 # Alternatively.
-print a ^ a1 ^ a2
+print a ^ a0 ^ a1
 # Reconstruct a from ratios of outer products with alternating signs and one missing in numerator.
-print ((a ^ a2)/(a1 ^ a2)) * a1 - ((a ^ a1)/(a1 ^ a2)) * a2
+print ((a ^ a1)/(a0 ^ a1)) * a0 - ((a ^ a0)/(a0 ^ a1)) * a1
