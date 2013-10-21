d695c12ca7d1732ebb1d138b536ce62c9d3040a1
David Geo Holmes <geometryzen@gmail.com>
2013-10-21 12:13:20 -0700

    Save file.

--- Courses/Calculus/BarycentricLine.py
+++ Courses/Calculus/BarycentricLine.py
@@ -4,18 +4,18 @@ i = VectorE3(1,0,0)
 j = VectorE3(0,1,0)
 k = VectorE3(0,0,1)
 
+# Endpoints of the Barycentric Line.
 a1 = i + j
 a2 = 3 * i + 4 * j
 
+# The Barycentric coordinates sum to unity.
 t1 = 0.6
 t2 = 0.4
 
+# 
 a = t1 * a1 + t2 * a2
 
 print a
 print (a2 - a1) * (0.8 / 2.0) + a1
+
 print ((a ^ a2)/(a1 ^ a2)) * a1 - ((a ^ a1)/(a1 ^ a2)) * a2
-print a1 ^ a2
-print a ^ a1
-print a ^ a2
-print -(a ^ a1) * a2 / (a1 ^ a2)
