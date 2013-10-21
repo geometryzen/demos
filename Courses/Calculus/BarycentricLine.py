4671daeaa258d8137bf5ae3aadbc31756617d7ee
David Geo Holmes <geometryzen@gmail.com>
2013-10-21 11:51:05 -0700

    Save file.

--- Courses/Calculus/BarycentricLine.py
+++ Courses/Calculus/BarycentricLine.py
@@ -1,1 +1,15 @@
-# BarycentricLine.py
+from e3ga import *
+
+i = VectorE3(1,0,0)
+j = VectorE3(0,1,0)
+k = VectorE3(0,0,1)
+
+a1 = i + j
+a2 = 3 * i + 4 * j
+
+t1 = 0.6
+t2 = 0.4
+
+a = t1 * a1 + t2 * a2
+
+print a
