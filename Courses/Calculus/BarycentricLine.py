430bce55baa1dab5c767ba09833951617d1c3d5a
David Geo Holmes <geometryzen@gmail.com>
2013-10-21 12:16:50 -0700

    Save file.

--- Courses/Calculus/BarycentricLine.py
+++ Courses/Calculus/BarycentricLine.py
@@ -12,12 +12,13 @@ a2 = 3 * i + 4 * j
 t1 = 0.6
 t2 = 0.4
 
-# 
+# Compute the vector from the coordinates.
 a = t1 * a1 + t2 * a2
 
 print a
 # Verify theat a does actually lie on the line.
 print (a2 - a1) * (0.8 / 2.0) + a1
+# Alternatively.
 print a ^ a1 ^ a2
-
+# Reconstruct a from ratios of outer products with alternating signs and one missing in numerator.
 print ((a ^ a2)/(a1 ^ a2)) * a1 - ((a ^ a1)/(a1 ^ a2)) * a2
