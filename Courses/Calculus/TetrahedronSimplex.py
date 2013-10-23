b54050c551e7bb167c2ab355bc95b63b27a10d24
David Geo Holmes <geometryzen@gmail.com>
2013-10-22 19:11:15 -0700

    Save file.

--- Courses/Calculus/TetrahedronSimplex.py
+++ Courses/Calculus/TetrahedronSimplex.py
@@ -1,1 +1,12 @@
-# TetrahedronSimplex.py
+'''
+This program is used to illustrate some theorems for simplices in the case of a 3-simplex (tetrahedron) in 3D Euclidean space.
+The initial approach will be purely alhebraic.
+This will be later illustrated with 3D geometry.
+Finally, it would be nice to prove the result symbolically.
+'''
+from e3ga import *
+
+# Let the tetrahedron be described by 4 points.
+# For simplicity we will use a standard simplex.
+i = VectorE3(1,0,0)
+x0 = VectorE3()
