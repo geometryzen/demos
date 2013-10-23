926f9321ab11e314318e61f98cf3164392f362a8
David Geo Holmes <geometryzen@gmail.com>
2013-10-22 19:13:51 -0700

    Save file.

--- Courses/Calculus/TetrahedronSimplex.py
+++ Courses/Calculus/TetrahedronSimplex.py
@@ -5,8 +5,17 @@ This will be later illustrated with 3D geometry.
 Finally, it would be nice to prove the result symbolically.
 '''
 from e3ga import *
+from math import *
 
 # Let the tetrahedron be described by 4 points.
 # For simplicity we will use a standard simplex.
 i = VectorE3(1,0,0)
-x0 = VectorE3()
+j = VectorE3(0,1,0)
+k = VectorE3(0,0,1)
+
+x0 = VectorE3(0,0,0)
+x1 = i
+x2 = j
+x3 = k
+
+volume = (x1 - x0) ^ (x2 - x0) ^ (x3 - x0) / factorial(3)
