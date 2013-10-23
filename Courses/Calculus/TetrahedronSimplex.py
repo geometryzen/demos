c98fdda5e6d3290dd5514c7272dd1dde12d73e6c
David Geo Holmes <geometryzen@gmail.com>
2013-10-22 19:25:48 -0700

    Save file.

--- Courses/Calculus/TetrahedronSimplex.py
+++ Courses/Calculus/TetrahedronSimplex.py
@@ -1,6 +1,6 @@
 '''
-This program is used to illustrate some theorems for simplices in the case of a 3-simplex (tetrahedron) in 3D Euclidean space.
-The initial approach will be purely alhebraic.
+This program is used to illustrate the measure-boundary theorem for a 3-simplex (tetrahedron) in 3D Euclidean space.
+The initial approach will be purely algebraic.
 This will be later illustrated with 3D geometry.
 Finally, it would be nice to prove the result symbolically.
 '''
@@ -8,12 +8,12 @@ from e3ga import *
 from math import *
 
 # Let the tetrahedron be described by 4 points.
-# For simplicity we will use a standard simplex.
+# Set up some basis vectors.
 i = VectorE3(1,0,0)
 j = VectorE3(0,1,0)
 k = VectorE3(0,0,1)
 
-x0 = VectorE3(1,2,3)
+x0 = i + 2 * j + 3 * k
 x1 = i + 3 * j
 x2 = j + 4 * k
 x3 = i + j + k
