c9b5e063c4b9116b9dfe3555546861ebe1cbfaa4
David Geo Holmes <geometryzen@gmail.com>
2013-10-22 19:21:51 -0700

    Save file.

--- Courses/Calculus/TetrahedronSimplex.py
+++ Courses/Calculus/TetrahedronSimplex.py
@@ -24,7 +24,7 @@ print "directed volume measure of tetrahedron: " + str(volume)
 # Now we calculate the directed measure of each of the faces.
 a = -0.5 * (x1 - x0) ^ (x2 - x0)
 print "directed measure of face (x0, x1, x2): " + str(a)
-b = +0.5
+b = +0.5 * (x1 - x0) ^ (x3 - x0)
 print "directed measure of face (x0, x1, x3): " + str(b)
 c = +0.5
 print "directed measure of face (x0, x1, x3): " + str(c)
