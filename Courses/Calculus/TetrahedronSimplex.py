cb6e9a33cc7482a82cd18ed9aafe98ca0cd9ebe0
David Geo Holmes <geometryzen@gmail.com>
2013-10-22 19:31:57 -0700

    Save file.

--- Courses/Calculus/TetrahedronSimplex.py
+++ Courses/Calculus/TetrahedronSimplex.py
@@ -36,5 +36,3 @@ d = +0.5 * (x2 - x1) ^ (x3 - x1)
 print "directed measure of face (x0, x1, x3): " + str(d)
 print ""
 print "directed measure of surface of tetrahedron: " + str(a+b+c+d)
-print "(You will probably get a small bivector measure owing to rounding errors)"
-print "The point is that it is actually zero."
