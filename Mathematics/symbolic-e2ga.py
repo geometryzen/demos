bd45397f563acd929519275de29fe3134dc4c5bd
David Geo Holmes <geometryzen@gmail.com>
2013-10-29 10:32:25 -0700

    Save file.

--- Mathematics/symbolic-e2ga.py
+++ Mathematics/symbolic-e2ga.py
@@ -3,6 +3,7 @@ Under Construction. This may not work for you. Oct 29, 2013.
 '''
 from e2ga import *
 
-p = PointE2(2, 3)
-print p
-print repr(p)
+A = PointE2(2, 3)
+B = PointE2(5, 2)
+print A
+print B
