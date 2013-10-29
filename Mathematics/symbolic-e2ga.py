0381128dce7171f6e349941621d4178a36c1efc7
David Geo Holmes <geometryzen@gmail.com>
2013-10-29 16:13:49 -0700

    Save file.

--- Mathematics/symbolic-e2ga.py
+++ Mathematics/symbolic-e2ga.py
@@ -14,5 +14,5 @@ s = Variable("s")
 
 p = Euclidean2(w, x, y, s)
 expr = p
-env = Environment() + Binding("x", 2) + Binding("y", 3)
+env = Environment() + Binding("x", 2) + Binding("y", 3) + Binding("s", 4)
 print Binding(str(expr), expr.evaluate(env))
