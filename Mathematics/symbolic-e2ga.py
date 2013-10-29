98ba7dff4d07b9ab4c49e107eb6d576afa5bd0b2
David Geo Holmes <geometryzen@gmail.com>
2013-10-29 16:38:52 -0700

    Save file.

--- Mathematics/symbolic-e2ga.py
+++ Mathematics/symbolic-e2ga.py
@@ -8,13 +8,4 @@ x = Variable("x")
 y = Variable("y")
 s = Variable("s")
 
-A = PointE2(x, y)
-B = PointE2(5, 2)
-print Binding("A", A)
-print Binding("B", B)
-#print p.x
-#print p.y
-
-expr = x + 1
-env = Environment() + Binding("w", 1.0) + Binding("x", 2) + Binding("y", 3) + Binding("s", 4)
-print Binding(str(expr), expr.evaluate(env))
+print x * x
