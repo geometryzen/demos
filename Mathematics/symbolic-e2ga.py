dd585fc1fbbbe67652a501f4f0f2ca47489f8f8b
David Geo Holmes <geometryzen@gmail.com>
2013-10-29 12:31:07 -0700

    Save file.

--- Mathematics/symbolic-e2ga.py
+++ Mathematics/symbolic-e2ga.py
@@ -5,17 +5,6 @@ from e2ga import *
 from symbolic import *
 
 x = Variable("x")
-y = Variable("y")
-z = x + 2
-print z
 env = Environment() + Binding("x", 3)
 print env
 print env.evaluate(z)
-
-p = PointE2(x, y)
-print p
-print repr(p)
-A = PointE2(2, 3)
-B = PointE2(5, 2)
-print A
-print B
