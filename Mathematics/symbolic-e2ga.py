c21016878047f3c3fdbad964f3c87e21bbb0c9b0
David Geo Holmes <geometryzen@gmail.com>
2013-10-29 14:28:08 -0700

    Save file.

--- Mathematics/symbolic-e2ga.py
+++ Mathematics/symbolic-e2ga.py
@@ -4,6 +4,8 @@ Under Construction. This may not work for you. Oct 29, 2013.
 from e2ga import *
 from symbolic import *
 
+i = VectorE2(1,0)
+
 x = Variable("x")
 expr = x * x + 2
 b = Binding("x", 2)
