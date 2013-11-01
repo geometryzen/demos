2db9509cabeee83079e03798531fe914b6cc2590
David Geo Holmes <geometryzen@gmail.com>
2013-11-01 09:05:38 -0700

    Save file.

--- Mathematics/e2ga-constants.py
+++ Mathematics/e2ga-constants.py
@@ -1,6 +1,5 @@
 '''
 The e2ga module pre-defines the standard basis vectors e1 and e2, as well as the pseudoscalar I.
-The alias constants i and j are also defined.
 '''
 from e2ga import *
 
@@ -8,6 +7,3 @@ print e1
 print e2
 print e1 * e2
 print e2 << I
-
-print i
-print j
