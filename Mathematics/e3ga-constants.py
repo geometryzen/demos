c5f93d81a51661262dc9bceedbb5b70c51dd23ad
David Geo Holmes <geometryzen@gmail.com>
2013-11-01 09:04:30 -0700

    Save file.

--- Mathematics/e3ga-constants.py
+++ Mathematics/e3ga-constants.py
@@ -1,6 +1,5 @@
 '''
 The e3ga module pre-defines the standard basis vectors e1, e2 and e3, as well as the pseudoscalar I.
-The alias constants i, j and k are also defined.
 '''
 from e3ga import *
 
@@ -12,7 +11,3 @@ print e1 ^ e2
 print e1 << I
 print I * e3
 print e3 * I
-
-print i
-print j
-print k
