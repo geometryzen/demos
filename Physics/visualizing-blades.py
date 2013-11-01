c792cce75e1afbb24cb6b2a5a60b22ea1b89c5b3
David Geo Holmes <geometryzen@gmail.com>
2013-11-01 13:18:51 -0700

    Save file.

--- Physics/visualizing-blades.py
+++ Physics/visualizing-blades.py
@@ -29,7 +29,7 @@ space2D.addChild(output)
 
 quantity = ScalarE3(1) + VectorE3(3, 4, 0) + BivectorE3(4,3,0) + PseudoscalarE3(1)
 
-probe  = ProbeBuilderE3().build()
+probe  = ProbeBuilderE3().segments(6).build()
 
 space3D.add(probe.grade0)
 space3D.add(probe.grade1)
