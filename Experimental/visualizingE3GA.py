f32fe446fdd2f04cbf6665526de6ee0916160082
David Geo Holmes <geometryzen@gmail.com>
2013-11-01 11:17:14 -0700

    Save file.

--- Experimental/visualizingE3GA.py
+++ Experimental/visualizingE3GA.py
@@ -24,14 +24,7 @@ output.x = 100
 output.y = 100
 space2D.addChild(output)
 
-quantity = ScalarE3(1) + VectorE3(3, 4, 0) + BivectorE3(4,3,0) + PseudoscalarE3(1)
-
-# Define the Meshes that you want to represent each grade of the quantity.
-wireframe = False
-grade0 = SphereBuilder().wireframe(True).color(0xFFFF00).segments(12).build()
-grade1 = ArrowBuilder().color(0x0000FF).wireframe(wireframe).segments(32).build()
-grade2 = VortexBuilder().wireframe(wireframe).color(0x0000FF).build()
-grade3 = CubeBuilder().wireframe(True).color(0xFFFF00).segments(1).build()
+quantity = e1
 
 probe  = ProbeBuilderE3().color(0x0000FF).build()
 
