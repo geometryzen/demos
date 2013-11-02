68b14f15aaed77f90d4c8de20a8ea6eec4246797
David Geo Holmes <geometryzen@gmail.com>
2013-11-02 08:42:15 -0700

    Save file.

--- Canvas-3D/Math/Sphere.py
+++ Canvas-3D/Math/Sphere.py
@@ -1,1 +1,15 @@
-# Sphere.py
+'''
+Under construction. Nov 2, 2013
+'''
+from three import *
+
+center = VectorE3(1,2,3)
+radius = e3
+
+sphere = Sphere(center, radius)
+
+print sphere.center
+print sphere.radius
+
+print sphere
+print repr(sphere)
