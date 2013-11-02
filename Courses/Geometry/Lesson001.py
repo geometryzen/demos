0e0e93e859d03cf7b6944912242aad7c029a590c
David Geo Holmes <geometryzen@gmail.com>
2013-11-01 18:14:02 -0700

    Save file.

--- Courses/Geometry/Lesson001.py
+++ Courses/Geometry/Lesson001.py
@@ -93,7 +93,7 @@ space3D.add(probeC.grade1)
 space3D.add(probeC.grade2)
 space3D.add(probeC.grade3)
 
-probeA  = ProbeBuilderE3().wireframe(True).segments(12).color(0xFF0000).build()
+probeA  = ProbeBuilderE3().wireframe(True).segments(12).color(0xEEEEEE).build()
 space3D.add(probeA.grade0)
 space3D.add(probeA.grade1)
 space3D.add(probeA.grade2)
