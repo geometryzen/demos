02fe9e9fe3511605b07ec47a47ba840e3bd35190
David Geo Holmes <geometryzen@gmail.com>
2013-11-02 09:30:54 -0700

    Save file.

--- Canvas-3D/Math/Sphere.py
+++ Canvas-3D/Math/Sphere.py
@@ -15,7 +15,9 @@ print sphere
 print repr(sphere)
 
 s1 = Sphere(VectorE3(0, 0, 0), 1.0)
-s2 = Sphere(VectorE3(2, 2, 2), 1.0)
+s2 = Sphere(VectorE3(2, 2, 2), 1.5)
 
 print sphere.intersectsSphere(sphere)
 print s1.intersectsSphere(s2)
+
+print Sphere().copy(s2)
