77732a5cf3d196f8e1a86466142fa39dd61986e8
David Geo Holmes <geometryzen@gmail.com>
2013-11-03 05:40:43 -0800

    Save file.

--- Canvas-3D/Math/Plane.py
+++ Canvas-3D/Math/Plane.py
@@ -1,1 +1,18 @@
-# Plane.py
+'''
+Under Construction. Nov 3, 2013
+'''
+from three import *
+
+normal = VectorE3(0, 0, 1)
+offset = 1.0
+
+plane = Plane(normal, offset)
+
+print plane.normal
+print plane.offset
+
+print plane
+print repr(plane)
+
+s1 = Plane(VectorE3(0, 0, 0), 0.1)
+s2 = Plane(VectorE3(2, 2, 2), 1.5)
