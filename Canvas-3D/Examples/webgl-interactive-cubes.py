477c0ae37c4999d25b167bbf87e7918d16cd2f6d
David Geo Holmes <geometryzen@gmail.com>
2013-11-03 07:32:52 -0800

    Save file.

--- Canvas-3D/Examples/webgl-interactive-cubes.py
+++ Canvas-3D/Examples/webgl-interactive-cubes.py
@@ -1,1 +1,39 @@
 # webgl-interactive-cubes.py
+from three import *
+from browser import *
+from workbench import *
+
+space = CartesianSpace()
+
+# All arguments are optional and the defaults, in order, are as follows.
+length = 6
+segments = 12
+radiusShaft = 0.01
+radiusCone = 0.08
+lengthCone = 0.2
+material = MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 1})
+arrow = ArrowBuilder().scale(length).segments(segments).material(material).build()
+arrow.geometry.name = 'Foo'
+
+print repr(arrow.geometry)
+print "uuid:            " + str(arrow.geometry.uuid)
+print "name:            " + str(arrow.geometry.name)
+print arrow.geometry
+
+space.add(arrow)
+
+workbench = Workbench(space.renderer, space.camera)
+
+def tick(t):
+    space.render()
+    
+def terminate(t):
+    return t > 4
+
+def setUp():
+    workbench.setUp()
+
+def tearDown():
+    workbench.tearDown()
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
