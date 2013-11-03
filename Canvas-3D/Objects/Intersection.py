ae14f6a7bd9b8087bf0a13f39946f4c30e883290
David Geo Holmes <geometryzen@gmail.com>
2013-11-03 12:38:24 -0800

    Save file.

--- Canvas-3D/Objects/Intersection.py
+++ Canvas-3D/Objects/Intersection.py
@@ -6,7 +6,8 @@ from three import *
 distance = 10.0
 point = VectorE3(1.0, 2.0, 3.0)
 
-face = Face3(0, 1, 2)
+normal = VectorE3(0, 1, 0)
+face = Face3(0, 1, 2, normal)
 
 print face
 print repr(face)
