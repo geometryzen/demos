37ccd54ee1c102c4397cb9e158872a742c3ea634
David Geo Holmes <geometryzen@gmail.com>
2013-11-03 12:16:46 -0800

    Save file.

--- Canvas-3D/Objects/Intersection.py
+++ Canvas-3D/Objects/Intersection.py
@@ -8,7 +8,7 @@ point = VectorE3(1.0, 2.0, 3.0)
 
 face = Face3(e1, e2, e3)
 
-intersection = Intersection(distance, point)
+intersection = Intersection(distance, point, face)
 
 print intersection
 print repr(intersection)
