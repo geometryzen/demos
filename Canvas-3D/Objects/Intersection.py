4a7aa35b5add2742e3c13d414e42e9c6de8c0f74
David Geo Holmes <geometryzen@gmail.com>
2013-11-03 11:50:56 -0800

    Save file.

--- Canvas-3D/Objects/Intersection.py
+++ Canvas-3D/Objects/Intersection.py
@@ -3,4 +3,9 @@ Under Construction. Nov 3, 2013.
 '''
 from three import *
 
-i = Intersection()
+distance = 10.0
+
+intersection = Intersection(distance)
+
+print intersection
+print repr(intersection)
