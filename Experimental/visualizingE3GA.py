36d9913623c5277d8febcee099bd0d27a280176d
David Geo Holmes <geometryzen@gmail.com>
2013-11-04 05:32:05 -0800

    Save file.

--- Experimental/visualizingE3GA.py
+++ Experimental/visualizingE3GA.py
@@ -116,6 +116,7 @@ cylinder = CylinderBuilder().wireframe(True).color(colorB).build()
 cube = CubeBuilder().wireframe(True).color(colorB).build()
 composite.add(cylinder)
 composite.add(cube)
+space3D.add(composite)
 #probeB.grade3 = composite
 space3D.add(probeB.grade0)
 space3D.add(probeB.grade1)
