79c6d9bc71a3a6eb3efd52ddf68c35b6089a8798
David Geo Holmes <geometryzen@gmail.com>
2013-11-05 07:28:50 -0800

    Save file.

--- Experimental/visualizingE3GA.py
+++ Experimental/visualizingE3GA.py
@@ -7,7 +7,7 @@ from geometry import *
 blades = [ScalarE3(1), e1, e2, e3, e1 ^ e2, e2 ^ e3, e3 ^ e1, e1 ^ e2 ^ e3, e1 ^ e3 ^ e2]
 binops = ["+", "-", "*", "<<", "^", ">>"]
 indexA = 1
-indexB = 7
+indexB = 6
 indexOp = 2
 
 timeOut = 600
