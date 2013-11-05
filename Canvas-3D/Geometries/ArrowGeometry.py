01d46d86f0e6e95a0e5709c6b42d4b0215e8c757
David Geo Holmes <geometryzen@gmail.com>
2013-11-05 11:08:39 -0800

    Save file.

--- Canvas-3D/Geometries/ArrowGeometry.py
+++ Canvas-3D/Geometries/ArrowGeometry.py
@@ -17,6 +17,7 @@ material = MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 1})
 arrow = ArrowBuilder().axis(e3).scale(scale).segments(segments).material(material).build()
 arrow.geometry.name = 'Foo'
 for face in arrow.geometry.faces:
+    print face.centroid
     print face.normal
 
 
