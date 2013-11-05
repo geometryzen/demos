c55f19e29e3a1745ce01a5d8b3c91bedd4454091
David Geo Holmes <geometryzen@gmail.com>
2013-11-04 19:08:49 -0800

    Save file.

--- Experimental/visualizingE3GA.py
+++ Experimental/visualizingE3GA.py
@@ -155,7 +155,7 @@ pipe.position = VectorE3(+0.5, +0.0, +0.5)
 composite.add(pipe)
 
 pipe = CylinderBuilder().radius(0.01).axis(e2).wireframe(wireframe).color(colorB).build()
-pipe.position = VectorE3(+0.0, +0.0, +0.0)
+pipe.position = VectorE3(+0.0, -0.5, -0.5)
 composite.add(pipe)
 
 probeB.grade3 = composite
