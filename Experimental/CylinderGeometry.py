c0beed2fc9431f8c238a16285b64a6e0ea502b36
David Geo Holmes <geometryzen@gmail.com>
2013-11-04 17:51:02 -0800

    Save file.

--- Experimental/CylinderGeometry.py
+++ Experimental/CylinderGeometry.py
@@ -15,7 +15,7 @@ def setUp():
     workbench.setUp()
 
     material = MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 2})
-    mesh = CylinderBuilder().axis(e3).radius(1).height(4).segments(4).material(material).build()
+    mesh = CylinderBuilder().axis(e3).radius(1).height(4).segments(2).material(material).build()
     geometry = mesh.geometry
     for vertex in geometry.vertices:
         print vertex
