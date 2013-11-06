6302aa47ac90441758b2ec6b675c86c4b492f59b
David Geo Holmes <geometryzen@gmail.com>
2013-11-06 13:33:41 -0800

    Save file.

--- Canvas-3D/Materials/ParticleSystemMaterial.py
+++ Canvas-3D/Materials/ParticleSystemMaterial.py
@@ -17,6 +17,7 @@ camera = PerspectiveCamera(60, 1, 1, 10000)
 camera.position.set(10000, 0, 3200)
 
 material = ParticleSystemMaterial({"size": 1, "vertexColors": True, "depthTest": False, "opacity": 0.5, "sizeAttenuation": False, "transparent": True})
+material.size = 2
 print material.size
 print material.color
 print material.sizeAttenuation
