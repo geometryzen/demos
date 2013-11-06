f094ec38226a2a6631dc66062e7e90e949699a80
David Geo Holmes <geometryzen@gmail.com>
2013-11-06 15:46:00 -0800

    Save file.

--- Canvas-3D/Examples/canvas-geometry-cube.py
+++ Canvas-3D/Examples/canvas-geometry-cube.py
@@ -36,7 +36,7 @@ camera.position.z = 500
 scene = Scene()
 
 geometry = CubeGeometry(200, 200, 200)
-for i in range(0, len(geometry.faces)):
+for i in range(0, len(geometry.faces), 2):
     hex = int(random() * 0xFFFFFF)
     geometry.faces[i].color.setHex(hex)
     geometry.faces[i+1].color.setHex(hex)
