a2886739faacef337c3babada269c01dc4fdea10
David Geo Holmes <geometryzen@gmail.com>
2013-11-07 07:44:04 -0800

    Save file.

--- Mathematics/reciprocal-vectors.py
+++ Mathematics/reciprocal-vectors.py
@@ -33,8 +33,8 @@ print r1 ^ e1 + r2 ^ e2
 i = VectorE3(1.0, 0.0, 0.0)
 j = VectorE3(0.0, 1.0, 0.0)
 k = VectorE3(0.0, 0.0, 1.0)
-e1 = 12 * i
-e2 = 3 * i + 4 * j
+e1 = 12.0 * i
+e2 = 3.0 * i + 4.0 * j
 e3 = k
 v = (e1.cross(e2)) % e3
 r1 = e2.cross(e3) / v
