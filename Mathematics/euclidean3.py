7c0f4508d3333c4a1825284e86926d6df14302ee
David Geo Holmes <geometryzen@gmail.com>
2013-11-07 07:32:08 -0800

    Save file.

--- Mathematics/euclidean3.py
+++ Mathematics/euclidean3.py
@@ -176,7 +176,7 @@ assertEqual("e2", str(e2))
 assertEqual("e3", str(e3))
 assertEqual("3*e1+4*e2+12*e3", str(VectorE3(3.0, 4.0, 12.0)))
 
-assertEqual(VectorE3(3, 4, 12).magnitude(), 13)
+assertEqual(VectorE3(3.0, 4.0, 12.0).magnitude(), 13)
 
 print "The scalar product is available using the Python % operator."
 print "Between vectors this is referred to as the dot product."
