ddf4e95a543506a028442d3673f3b1bd557db5e5
David Geo Holmes <geometryzen@gmail.com>
2013-11-08 07:13:39 -0800

    Save file.

--- Canvas-3D/Math/Matrix3.py
+++ Canvas-3D/Math/Matrix3.py
@@ -5,9 +5,11 @@ from three import *
 
 m = Matrix3(1.0, 2.1, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
 
+print Matrix()
 print m
 
 print Matrix3().determinant()
 print m.determinant()
+
 print m.transpose()
 print m
