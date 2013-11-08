4afc3b96c3376ca5d42ea32854b0c9a910846791
David Geo Holmes <geometryzen@gmail.com>
2013-11-07 19:25:48 -0800

    Save file.

--- Units of Measure/units-of-measure.py
+++ Units of Measure/units-of-measure.py
@@ -5,7 +5,7 @@ m = ScalarE3(10) * kilogram
  
 print "m => " + str(m)
  
-g = 9.81 * VectorE3(0, 0, -1) * newton / kilogram
+g = 9.81 * VectorE3(0.0, 0.0, -1.0) * newton / kilogram
 print "g => " + str(g)
  
 F = m * g
