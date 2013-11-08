7fb05dc5bcf1d8da48b828612b484dafcf628c10
David Geo Holmes <geometryzen@gmail.com>
2013-11-08 07:57:30 -0800

    Save file.

--- Canvas-3D/Math/Matrix3.py
+++ Canvas-3D/Math/Matrix3.py
@@ -7,7 +7,9 @@ m = Matrix3(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
 
 print "construction"
 print Matrix3()
+print repr(Matrix3())
 print m
+print repr(m)
 print ""
 print "determinant()"
 print Matrix3().determinant()
