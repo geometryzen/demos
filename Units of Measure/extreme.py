6b7141b14f203c7e3d3311c8813ea7ea9b55d709
David Geo Holmes <geometryzen@gmail.com>
2013-11-07 19:24:43 -0800

    Save file.

--- Units of Measure/extreme.py
+++ Units of Measure/extreme.py
@@ -6,7 +6,7 @@ r = VectorE3(1.0, 0.0, 0.0) * meter
  
 print "m => " + str(m)
  
-g = 9.81 * VectorE3(0, 0, -1) * newton / kilogram
+g = 9.81 * VectorE3(0.0, 0.0, -1.0) * newton / kilogram
 print "g => " + str(g)
  
 F = m * g
