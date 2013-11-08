2a44ec47381e16d91fa36ac80557c7560db1ce96
David Geo Holmes <geometryzen@gmail.com>
2013-11-07 20:05:03 -0800

    Save file.

--- Canvas-3D/Geometries/OctahedronGeometry.py
+++ Canvas-3D/Geometries/OctahedronGeometry.py
@@ -5,7 +5,7 @@ from workbench import *
 scene = Scene()
 
 camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 2
+camera.position.z = 2.0
 
 renderer = WebGLRenderer()
 renderer.setClearColor(Color(0x080808), 1.0)
