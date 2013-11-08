fa3be735237b31aec429c81e3323e5e8ac0fdbbc
David Geo Holmes <geometryzen@gmail.com>
2013-11-08 07:00:54 -0800

    Save file.

--- Canvas-3D/Math/Matrix3.py
+++ Canvas-3D/Math/Matrix3.py
@@ -3,7 +3,7 @@ Under Construction. Nov 8, 2013
 '''
 from three import *
 
-m = Matrix3(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
+m = Matrix3(1.0, 2.1, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
 
 print m
 
