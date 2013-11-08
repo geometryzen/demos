7d58ed039526c442392defa24284e2e381fe19e3
David Geo Holmes <geometryzen@gmail.com>
2013-11-08 07:19:17 -0800

    Save file.

--- Canvas-3D/Math/Matrix3.py
+++ Canvas-3D/Math/Matrix3.py
@@ -5,11 +5,14 @@ from three import *
 
 m = Matrix3(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
 
+print "construction"
 print Matrix3()
 print m
+print ""
 print "determinant()"
 print Matrix3().determinant()
 print m.determinant()
+print ""
 print "transpose()"
 print m.transpose()
 print m
