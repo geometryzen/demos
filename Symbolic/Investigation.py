535cb6b1aca890fc71835b2a0740ab91e6557201
David Geo Holmes <geometryzen@gmail.com>
2013-11-09 14:48:16 -0800

    Save file.

--- Symbolic/Investigation.py
+++ Symbolic/Investigation.py
@@ -2,6 +2,34 @@
 Investigating the feasibility of CAS.
 '''
 from symbolic import *
+from math import exp, cos, sin, sqrt, pi
+
+# The following functions are provided for testing purposes.
+def assertEqual(actual, expect):
+    if expect == actual:
+        pass
+    else:
+        print {"actual": actual, "expect": expect}
+
+def assertTrue(actual):
+    return assertEqual(actual, True)
+
+def assertFalse(actual):
+    return assertEqual(actual, False)
+
+def explain(m):
+    print str(m) + " is " + repr(m)
+    return m
+
+def showValue(name, m):
+    print name + " => " + str(m)
+    return m
+
+def isCloseTo(actual, expect):
+    epsilon = 1e-10
+    error = actual - expect
+    quadrance = error % error
+    return sqrt(quadrance) < epsilon
 
 a = Variable("a")
 b = Variable("b")
