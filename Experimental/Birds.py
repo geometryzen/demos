15b7fbe0d0d4fc5b09c38e159f335fcf3dcea24b
David Geo Holmes <geometryzen@gmail.com>
2013-11-12 19:20:57 -0800

    Save file.

--- Experimental/Birds.py
+++ Experimental/Birds.py
@@ -23,7 +23,7 @@ class Bird:
 def borgMesh():
     g = Geometry();
     vertices = g.vertices
-    vertices.append(VectorE3(5,0,0))
+    vertices.append(Vector3(5,0,0))
     m = MeshBasicMaterial({"color": random() * 0xFFFFFF, "side": DoubleSide})
 
 # global timeOut allows us to terminate the program gracefully on time or Esc key.
