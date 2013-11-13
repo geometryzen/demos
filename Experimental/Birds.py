43cb3c5ebb266264efac5be39f9d2afe0992e5ef
David Geo Holmes <geometryzen@gmail.com>
2013-11-13 11:27:04 -0800

    Save file.

--- Experimental/Birds.py
+++ Experimental/Birds.py
@@ -58,6 +58,16 @@ class Bird:
             vector = self.avoid(vector)
             vector.multiplyScalar(5.0)
             self._acceleration.add(vector)
+
+            vector.set(self.position.x, self.position.y, -self._depth)
+            vector = self.avoid(vector)
+            vector.multiplyScalar(5.0)
+            self._acceleration.add(vector)
+
+            vector.set(self.position.x, self.position.y, +self._depth)
+            vector = self.avoid(vector)
+            vector.multiplyScalar(5.0)
+            self._acceleration.add(vector)            
         else:
             pass
         if random() > 0.5:
