d4ff659ce5744b82ae70f896d9a35970012ffed9
David Geo Holmes <geometryzen@gmail.com>
2013-11-12 19:00:51 -0800

    Save file.

--- Experimental/Birds.py
+++ Experimental/Birds.py
@@ -5,7 +5,7 @@ from random import random
 
 class Bird:
     def __init__(self):
-        self.position = VectorE3()
+        self.position = VectorE3(0.0, 0.0, 0.0)
         pass
 
 # global timeOut allows us to terminate the program gracefully on time or Esc key.
