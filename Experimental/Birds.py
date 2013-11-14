ae86b70a18b1391b71154d81ef7f68cce5406e53
David Geo Holmes <geometryzen@gmail.com>
2013-11-13 17:45:07 -0800

    Save file.

--- Experimental/Birds.py
+++ Experimental/Birds.py
@@ -208,7 +208,7 @@ birds = []
 borgs = []
     
 def setUp():
-    for i in range(0, 4):
+    for i in range(0, 100):
         bird = Bird()
         bird.setAvoidWalls(True)
         bird.setWorldSize(500, 500, 500)
