6eec4c20c6d0df46e484b1073b3fc14e49319fc9
David Geo Holmes <geometryzen@gmail.com>
2013-11-15 06:12:41 -0800

    Save file.

--- Courses/Calculus/ContourIntegral.py
+++ Courses/Calculus/ContourIntegral.py
@@ -1,4 +1,11 @@
 '''
 Experiment with the calculation and visualization of a path integral in the complex plane.
+
+f(z) = 1/(z(z+1))
+
 '''
 from cmath import *
+
+def f(z):
+    return 1 / (z * (z + 1))
+
