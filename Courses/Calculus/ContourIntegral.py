f773f878061066418e4ec5e703367a4c52286afa
David Geo Holmes <geometryzen@gmail.com>
2013-11-15 10:24:07 -0800

    Save file.

--- Courses/Calculus/ContourIntegral.py
+++ Courses/Calculus/ContourIntegral.py
@@ -17,7 +17,7 @@ def f(z):
 Integrate the simplified integral in polar coordinates.
 '''
 def polarIntegral(R):
-    N = 10000
+    N = 100
     dTheta = pi / N
     sum = 0
     for index in range(1, N + 1):
