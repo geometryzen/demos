15b4fddcdc68bb6c9fd18f4a38214400d95255fb
David Geo Holmes <geometryzen@gmail.com>
2013-11-15 09:45:31 -0800

    Save file.

--- Courses/Calculus/ContourIntegral.py
+++ Courses/Calculus/ContourIntegral.py
@@ -38,7 +38,7 @@ def directIntegral(R):
     dTheta = tao / N
     sum = 0
     for index in range(1, N + 1):
-        theta = index * tao / N 
+        theta = index * pi / N 
         cosT = cos(theta)
         sinT = sin(theta)
         dz = R * (-sinT + i * cosT) * dTheta
