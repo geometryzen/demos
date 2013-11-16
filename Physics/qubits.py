3b113e8c78fada57ec9ead7ae40efc548d4aa6bf
David Geo Holmes <geometryzen@gmail.com>
2013-11-16 15:00:19 -0800

    Save file.

--- Physics/qubits.py
+++ Physics/qubits.py
@@ -20,18 +20,19 @@ def rotor(theta, phi):
     return exp(-I * e3 * phi / 2.0) * exp(-I * e2 * theta / 2.0)
 
 xp = rotor(pi / 2.0,  0.0)
+xm = rotor(pi / 2.0,  pi)
 yp = rotor(pi / 2.0, +pi / 2.0)
 ym = rotor(pi / 2.0, -pi / 2.0)
 
 print "|z+> => " + str(rotor(0.0,       0.0))
 print "|z-> => " + str(rotor(pi,        0.0))
 print "|x+> => " + str(xp)
-print "|x-> => " + str(rotor(pi / 2.0,  pi))
+print "|x-> => " + str(xm)
 print "|y+> => " + str(yp)
 print "|y-> => " + str(ym)
 
 print e1 * xp * e3 / xp
-print e2 * ym * e3 / ym
+print e1 * xm * e3 / xm
 
 print e2 * yp * e3 / yp
 print e2 * ym * e3 / ym
