6ee736e737891ce7c6c40bfc89c9f9ae1979247e
David Geo Holmes <geometryzen@gmail.com>
2013-11-16 15:28:54 -0800

    Save file.

--- Physics/qubits.py
+++ Physics/qubits.py
@@ -35,6 +35,7 @@ print "x- => " + str(xm)
 print "y+ => " + str(yp)
 print "y- => " + str(ym)
 
+# Exact values:
 zp = ScalarE3(1.0)
 zm = -I * e2
 
@@ -44,6 +45,7 @@ xm = e2 * e3 - e1 * e2
 yp = ScalarE3(1.0) - e1 * e2 + e2 * e3 - e3 * e1
 ym = ScalarE3(1.0) + e1 * e2 - e2 * e3 - e3 * e1
 
+# For E3 we can compute the eigenvalues by division.
 print e3 * zp * e3 / zp
 print e3 * zm * e3 / zm
 
