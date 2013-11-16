460327aea72e5bcb0a6bdcc1e998720843e4ca27
David Geo Holmes <geometryzen@gmail.com>
2013-11-16 14:36:40 -0800

    Save file.

--- Physics/qubits.py
+++ Physics/qubits.py
@@ -1,7 +1,7 @@
 from e3ga import *
 from math import exp
 
-def rotor(theat, phi):
+def rotor(theta, phi):
     return exp(-I * e3 * phi / 2) * exp(-I * e2 * theta / 2)
 
 print "z => " + str(rotor(0,0))
