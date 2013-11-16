ef474dd0f97dfd78d9b3be7eb4c955432d770a42
David Geo Holmes <geometryzen@gmail.com>
2013-11-16 14:42:21 -0800

    Save file.

--- Physics/qubits.py
+++ Physics/qubits.py
@@ -4,9 +4,9 @@ from math import exp
 def rotor(theta, phi):
     return exp(-I * e3 * phi / 2) * exp(-I * e2 * theta / 2)
 
-print "|0> => " + str(rotor(0,0))
-print "|1> => " + str(rotor(pi,0))
-print "|+> => " + str(rotor(pi/2,0))
-print "|-> => " + str(rotor(pi/2,pi))
+print "|z+> => " + str(rotor(0,0))
+print "|z-> => " + str(rotor(pi,0))
+print "|x+> => " + str(rotor(pi/2,0))
+print "|x-> => " + str(rotor(pi/2,pi))
 print "|y+> => " + str(rotor(pi/2,+pi/2))
 print "|y-> => " + str(rotor(pi/2,-pi/2))
