1be177dd48523f368a90beb0fe1a25c57df87a34
David Geo Holmes <geometryzen@gmail.com>
2013-11-16 14:46:59 -0800

    Save file.

--- Physics/qubits.py
+++ Physics/qubits.py
@@ -1,12 +1,15 @@
+'''
+Rotors for the cardinal points in 3D space.
+'''
 from e3ga import *
 from math import exp
 
 def rotor(theta, phi):
     return exp(-I * e3 * phi / 2.0) * exp(-I * e2 * theta / 2.0)
 
-print "|z+> => " + str(rotor(0,0))
-print "|z-> => " + str(rotor(pi,0))
-print "|x+> => " + str(rotor(pi/2,0))
-print "|x-> => " + str(rotor(pi/2,pi))
-print "|y+> => " + str(rotor(pi/2,+pi/2))
-print "|y-> => " + str(rotor(pi/2,-pi/2))
+print "|z+> => " + str(rotor(0.0,       0.0))
+print "|z-> => " + str(rotor(pi,        0.0))
+print "|x+> => " + str(rotor(pi / 2.0,  0.0))
+print "|x-> => " + str(rotor(pi / 2.0,  pi))
+print "|y+> => " + str(rotor(pi / 2.0, +pi / 2.0))
+print "|y-> => " + str(rotor(pi / 2.0, -pi / 2.0))
