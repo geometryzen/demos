06501c3f6d63823cced2dab750da3c0212416495
David Geo Holmes <geometryzen@gmail.com>
2013-11-16 14:56:46 -0800

    Save file.

--- Physics/qubits.py
+++ Physics/qubits.py
@@ -9,6 +9,9 @@ z- <-> -e31 = - I * e2
 x+ <-> (1 - e31)   / sqrt(2)
 x- <-> (e23 - e12) / sqrt(2)
 
+y+ <-> (1 - e12 + e23 - e31) / 2
+y- <-> (1 + e12 - e23 - e31) / 2
+
 '''
 from e3ga import *
 from math import exp
@@ -16,11 +19,13 @@ from math import exp
 def rotor(theta, phi):
     return exp(-I * e3 * phi / 2.0) * exp(-I * e2 * theta / 2.0)
 
+yp = rotor(pi / 2.0, +pi / 2.0)
+
 print "|z+> => " + str(rotor(0.0,       0.0))
 print "|z-> => " + str(rotor(pi,        0.0))
 print "|x+> => " + str(rotor(pi / 2.0,  0.0))
 print "|x-> => " + str(rotor(pi / 2.0,  pi))
-print "|y+> => " + str(rotor(pi / 2.0, +pi / 2.0))
+print "|y+> => " + str(yp)
 print "|y-> => " + str(rotor(pi / 2.0, -pi / 2.0))
 
 print e2 * (e2 * e3 - e1 * e2) * e3
