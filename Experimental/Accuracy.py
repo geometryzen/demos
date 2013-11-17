8b915a3206a0fa2f316d21045a6274fcee468d8f
David Geo Holmes <geometryzen@gmail.com>
2013-11-17 15:57:43 -0800

    Save file.

--- Experimental/Accuracy.py
+++ Experimental/Accuracy.py
@@ -3,23 +3,5 @@ Investigation into improving accuracy in the use of trigonometric functions.
 '''
 from math import *
 
-print sqrt1_2
-print cos(pi / 4 + 0.00000000000000001)
-print cos(pi / 4 + 0.0000000000000001)
-print cos(pi / 4 + 0.000000000000001)
-print cos(pi / 4 + 0.00000000000001)
-print cos(pi / 4 + 0.0000000000001)
-print cos(pi / 4 + 0.000000000001)
-print cos(pi / 4 + 0.00000000001)
-print cos(pi / 4 + 0.0000000001)
-print cos(pi / 4 + 0.000000001)
-print cos(pi / 4 + 0.00000001)
-print pi
-print pi / 2
-print 3.14159265359
-print degrees(3.14159265359)
-print degrees(pi/2)
-print degrees(1.57079632679)
-
 for i in range(-8, 9):
     print i * tao / 8
