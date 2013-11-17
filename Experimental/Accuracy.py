e17673c9822919f0a7da773997f64daf1f1f4acc
David Geo Holmes <geometryzen@gmail.com>
2013-11-17 15:58:11 -0800

    Save file.

--- Experimental/Accuracy.py
+++ Experimental/Accuracy.py
@@ -4,4 +4,5 @@ Investigation into improving accuracy in the use of trigonometric functions.
 from math import *
 
 for i in range(-8, 9):
-    print i * tao / 8
+    theta = i * tao / 8
+    print cos(theta)
