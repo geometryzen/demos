f4b282617682f80244bae5c0da7780fa12b5a92d
David Geo Holmes <geometryzen@gmail.com>
2013-11-17 13:07:00 -0800

    Save file.

--- Experimental/Accuracy.py
+++ Experimental/Accuracy.py
@@ -2,33 +2,5 @@
 Investigation into improving accuracy in the use of trigonometric functions.
 '''
 from math import *
-from e3ga import Euclidean3, I, e3, e2
 
-def rotor(theta, phi):
-    R2 = exp(-I * e3 * phi / 2.0)
-    R1 = exp(-I * e2 * theta / 2.0)
-    return R1
-    #return R2 * R1
-
-print "sqrt(2) => " + str(sqrt(2))
-print e
-print pi
-print tao
-print cos(pi / 4.0)
-print sin(pi / 4.0)
-print sqrt1_2
-print sqrt2
-print sqrt2 * sqrt2
-print sqrt1_2 * sqrt1_2
-print cos(pi / 4.0) * cos(pi / 4)
-
-print cos((pi / 2.0) / 2.0)
-print sin((pi / 2.0) / 2.0)
-print exp((pi / 2.0) / 2.0)
-
-print rotor(pi / 2.0, 0.0)
-print (I * e2 * (pi / 2.0)).vector
-print (I * e2 * (pi / 2.0)).quaternion
-print (I * e2 * (pi / 2.0)).quaternion.z
-print sin((-pi / 2.0)/2.0)
-print cos(pi/4)
+print cos(pi / 4)
