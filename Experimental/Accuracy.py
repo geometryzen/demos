046abe799d219ed18db54378b3997e2d99c5c08a
David Geo Holmes <geometryzen@gmail.com>
2013-11-17 13:08:22 -0800

    Save file.

--- Experimental/Accuracy.py
+++ Experimental/Accuracy.py
@@ -3,5 +3,7 @@ Investigation into improving accuracy in the use of trigonometric functions.
 '''
 from math import *
 
-print cos(pi / 4 + 0.00000000000000001)
 print sqrt1_2
+print cos(pi / 4 + 0.00000000000000001)
+print cos(pi / 4 + 0.0000000000000001)
+print cos(pi / 4 + 0.000000000000001)
