adf680422dc7b7ecf253e24166f9cc13f6385209
David Geo Holmes <geometryzen@gmail.com>
2013-11-17 12:50:01 -0800

    Save file.

--- Experimental/Accuracy.py
+++ Experimental/Accuracy.py
@@ -30,4 +30,4 @@ print rotor(pi / 2.0, 0.0)
 print (I * e2 * (pi / 2.0)).quaternion.x
 print (I * e2 * (pi / 2.0)).quaternion.y
 print (I * e2 * (pi / 2.0)).quaternion.z
-print cos((-pi / 2.0)/2.0)
+print sin((-pi / 2.0)/2.0)
