ba9c5147fabbea713fcafed26edc8fb6bebd96a9
David Geo Holmes <geometryzen@gmail.com>
2013-11-17 21:03:52 -0800

    Save file.

--- Mathematics/Matrix2x2.py
+++ Mathematics/Matrix2x2.py
@@ -9,7 +9,7 @@ from cmath import *
 zp = Matrix2x1(complex(1.0, 0.0), complex(0.0, 0.0))
 zm = Matrix2x1(complex(0.0, 0.0), complex(1.0, 0.0))
 
-print zp
+print zp[0]
 
 hxp = (zp + zm) / sqrt(2.0)
 hxm = (zp - zm) / sqrt(2.0)
