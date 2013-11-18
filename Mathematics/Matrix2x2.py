f9d094f882433430be28ca4aa884fc3355936786
David Geo Holmes <geometryzen@gmail.com>
2013-11-17 20:24:49 -0800

    Save file.

--- Mathematics/Matrix2x2.py
+++ Mathematics/Matrix2x2.py
@@ -3,6 +3,7 @@ Support for 2x2, 2x1, 1x2 matrices over an arbitrary field.
 Under Construction. Nov 17, 2013.
 '''
 from matrix import *
+from math import *
 from cmath import *
 
 hxp = Matrix2x1(complex(1,0), complex(0,1)) / sqrt(2.0)
