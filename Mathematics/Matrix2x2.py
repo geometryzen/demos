f7e8eb4bed1b7dbb52164df2ddea4f5b85f8d22b
David Geo Holmes <geometryzen@gmail.com>
2013-11-18 15:51:28 -0800

    Save file.

--- Mathematics/Matrix2x2.py
+++ Mathematics/Matrix2x2.py
@@ -29,7 +29,7 @@ b = Variable("b")
 
 state = Matrix2x1(a, b)
 
-print "H * " + str(state) + " => " + str(H * state * sqrt1_2)
+print "H * " + str(state) + " => " + str(H * state * (1/sqrt(2)))
 
 
 
