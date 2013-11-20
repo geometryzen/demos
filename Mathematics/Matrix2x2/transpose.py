f07a22fdd722e501ffe91ed8e30ae062bc588058
David Geo Holmes <geometryzen@gmail.com>
2013-11-20 12:47:46 -0800

    Save file.

--- Mathematics/Matrix2x2/transpose.py
+++ Mathematics/Matrix2x2/transpose.py
@@ -11,6 +11,8 @@ M = Matrix2x2(Matrix2x1(1,3), Matrix2x1(2,4))
 print "M is a 2x2 matrix."
 print "repr(M) => " + repr(M)
 print "str(M) => " + str(M)
+print ""
+print "Extraction of the column vectors..."
 print "M[0] => " + str(M[0])
 print "M[1] => " + str(M[1])
 print "M[0][0] => " + str(M[0][0])
