aae6a9dc87ea2bbbf4a3c0cca55d6ad60eba0f61
David Geo Holmes <geometryzen@gmail.com>
2013-11-20 13:56:04 -0800

    Save file.

--- Mathematics/Matrix2x2/conjugate.py
+++ Mathematics/Matrix2x2/conjugate.py
@@ -8,6 +8,7 @@ zero = complex(0.0, 0.0)
 one = complex(1.0, 0.0)
 i = complex(0.0, 1.0)
 
+I = Matrix2x2(Matrix2x1(one, zero), Matrix2x1(zero, one))
 X = Matrix2x2(Matrix2x1(zero, one), Matrix2x1(one, zero))
 Y = Matrix2x2(Matrix2x1(zero, i), Matrix2x1(-i, zero))
 Z = Matrix2x2(Matrix2x1(one, zero), Matrix2x1(zero, -one))
