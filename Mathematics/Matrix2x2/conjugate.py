7aa15c75eea7d2e4cf8324026935a5633ad39312
David Geo Holmes <geometryzen@gmail.com>
2013-11-20 13:57:08 -0800

    Save file.

--- Mathematics/Matrix2x2/conjugate.py
+++ Mathematics/Matrix2x2/conjugate.py
@@ -3,6 +3,7 @@ Matrices over a complex field.
 '''
 from matrix import *
 from cmath import *
+from math import *
 
 zero = complex(0.0, 0.0)
 one = complex(1.0, 0.0)
@@ -12,6 +13,7 @@ I = Matrix2x2(Matrix2x1(one, zero), Matrix2x1(zero, one))
 X = Matrix2x2(Matrix2x1(zero, one), Matrix2x1(one, zero))
 Y = Matrix2x2(Matrix2x1(zero, i), Matrix2x1(-i, zero))
 Z = Matrix2x2(Matrix2x1(one, zero), Matrix2x1(zero, -one))
+H = (X + Z) * sqrt1_2
 
 print "I => " + str(I)
 print "X => " + str(X)
