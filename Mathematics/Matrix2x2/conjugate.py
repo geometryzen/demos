f6c4d14cbf8862774ed18f11aa124536a51bdecd
David Geo Holmes <geometryzen@gmail.com>
2013-11-20 13:54:34 -0800

    Save file.

--- Mathematics/Matrix2x2/conjugate.py
+++ Mathematics/Matrix2x2/conjugate.py
@@ -8,26 +8,6 @@ zero = complex(0.0, 0.0)
 one = complex(1.0, 0.0)
 i = complex(0.0, 1.0)
 
-M = Matrix2x2(Matrix2x1(zero, one), Matrix2x1(one, zero))
-
-print "M is a 2x2 matrix."
-print ""
-print "Construction..."
-print "repr(M) => " + repr(M)
-print ""
-print "Notation..."
-print "str(M) => " + str(M)
-print ""
-print "Extraction of the column vectors..."
-print "M[0] => " + str(M[0])
-print "M[1] => " + str(M[1])
-print ""
-print "Extraction of the elements of each column vector..."
-print "M[0][0] => " + str(M[0][0])
-print "M[0][1] => " + str(M[0][1])
-print "M[1][0] => " + str(M[1][0])
-print "M[1][1] => " + str(M[1][1])
-print ""
-print "Transposition..."
-print "M.transpose() => " + str(M.transpose())
-print "M => " + str(M)
+X = Matrix2x2(Matrix2x1(zero, one), Matrix2x1(one, zero))
+Y = Matrix2x2(Matrix2x1(zero, i), Matrix2x1(-i, zero))
+Z = Matrix2x2(Matrix2x1(one, zero), Matrix2x1(zero, -one))
