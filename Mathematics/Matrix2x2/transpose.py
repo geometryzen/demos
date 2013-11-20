3d13d7393b01a219ea87b870ab4185c5b5582ab6
David Geo Holmes <geometryzen@gmail.com>
2013-11-20 05:42:19 -0800

    Save file.

--- Mathematics/Matrix2x2/transpose.py
+++ Mathematics/Matrix2x2/transpose.py
@@ -8,9 +8,11 @@ d = Variable("d")
 
 M = Matrix2x2(Matrix2x1(a,c), Matrix2x1(b,d))
 
-print "M"
+print "M:"
 print M
 print ""
-print "M[0]"
+print "M[0]:"
 print M[0]
+print ""
+print "M[1]:"
 print M[1]
