c455a3b94c0ad658a4caf23bbf07dc4a8c5ad396
David Geo Holmes <geometryzen@gmail.com>
2013-11-20 18:00:08 -0800

    Save file.

--- Mathematics/Matrix2x2/pauli.py
+++ Mathematics/Matrix2x2/pauli.py
@@ -10,9 +10,9 @@ zero = complex(0.0, 0.0)
 one = complex(1.0, 0.0)
 i = complex(0.0, 1.0)
 
-I = Matrix2x2(Matrix2x1(1.0, 0.0), Matrix2x1(0.0, 1.0))
+I = Matrix2x2(Matrix2x1(one, zero), Matrix2x1(zero, one))
 X = Matrix2x2(Matrix2x1(zero, one), Matrix2x1(one, zero))
-Y = Matrix2x2(Matrix2x1(0, i), Matrix2x1(-i, 0))
+Y = Matrix2x2(Matrix2x1(zero, i), Matrix2x1(-i, zero))
 Z = Matrix2x2(Matrix2x1(one, zero), Matrix2x1(zero, -one))
 
 H = (X + Z) * sqrt1_2
