b2646d1e99896ba0d1f18b4d605244bf35967d30
David Geo Holmes <geometryzen@gmail.com>
2013-11-20 17:35:29 -0800

    Save file.

--- Mathematics/Matrix2x2/pauli.py
+++ Mathematics/Matrix2x2/pauli.py
@@ -23,7 +23,8 @@ print ""
 print "H => " + str(H)
 print "H * H => " + str(H * H)
 
-x = Matrix2x1(complex(random(),random()), complex(random(),random())) / sqrt(abs(conjugate(x.transpose()) * x))
+x = Matrix2x1(complex(random(),random()), complex(random(),random()))
+x = x / sqrt(abs(conjugate(x.transpose()) * x))
 print repr(x.transpose())
 print repr(x)
 print repr(conjugate(x.transpose()) * x)
