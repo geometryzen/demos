46f5ca499677bdb5dfee3d76292aa22f858eeb1e
David Geo Holmes <geometryzen@gmail.com>
2013-11-22 12:57:45 -0800

    Save file.

--- Courses/Geometry/Lesson001.py
+++ Courses/Geometry/Lesson001.py
@@ -4,7 +4,9 @@ from browser import *
 from workbench import *
 from geometry import *
 
-e1 = VectorE3(1.0, 0, 0)
+e1 = VectorE3(1.0, 0.0, 0.0, False)
+e2 = VectorE3(0.0, 1.0, 0.0, False)
+e3 = VectorE3(0.0, 0.0, 1.0, False)
 
 blades = [ScalarE3(+1), ScalarE3(-1), +e1, -e1, +e2, -e2, +e3, -e3, e1 ^ e2, e2 ^ e1, e2 ^ e3, e3 ^ e2, e3 ^ e1, e1 ^ e3, I, -I]
 binops = ["+", "-", "*", "<<", "^", ">>"]
