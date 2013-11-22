35eb37a19c80fa9c3c7ea06e5fe0656f213fb65a
David Geo Holmes <geometryzen@gmail.com>
2013-11-22 07:25:03 -0800

    Save file.

--- Physics/qubit-visualization.py
+++ Physics/qubit-visualization.py
@@ -179,7 +179,7 @@ def onDocumentMouseOut(event):
         
 def render(t):
     R = exp(- e1 ^ e3 * radians(coords.theta))
-    txtScratch.text = coords
+    txtScratch.text = R
     renderer.render(scene, camera)
     space2D.render()
     
