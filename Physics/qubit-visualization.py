ee120f647ffb7fbc2d8685e91329143f8a822998
David Geo Holmes <geometryzen@gmail.com>
2013-11-21 17:32:07 -0800

    Save file.

--- Physics/qubit-visualization.py
+++ Physics/qubit-visualization.py
@@ -151,7 +151,7 @@ def tick(t):
 
     attitude = (e2 * e3 - e1 * e2)
     txtSpinor.text = repr(attitude)
-#    cube.attitude = attitude    
+    cube.attitude = attitude    
 #    cube.rotation.y += (targetRotation - cube.rotation.y) * 0.05
     renderer.render(scene, camera)
     space2D.render()
