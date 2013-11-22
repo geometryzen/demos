dffd63d58f7d36525a58c4caae7a6427f61f0d17
David Geo Holmes <geometryzen@gmail.com>
2013-11-21 17:30:54 -0800

    Save file.

--- Physics/qubit-visualization.py
+++ Physics/qubit-visualization.py
@@ -5,6 +5,8 @@ from workbench import *
 from random import random
 from math import *
 
+e1 = VectorE3(1.0, 0.0, 0.0, 3)
+e2 = VectorE3(0.0, 1.0, 0.0)
 e2 = VectorE3(0.0, 1.0, 0.0)
 
 canvas2D = document.createElement("canvas")
