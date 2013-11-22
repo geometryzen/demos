9ecaab66900848b373777f39381094c5a1fc85fe
David Geo Holmes <geometryzen@gmail.com>
2013-11-22 07:49:17 -0800

    Save file.

--- Physics/qubit-visualization.py
+++ Physics/qubit-visualization.py
@@ -89,7 +89,7 @@ scene.add(cube)
 cube.position = e2
 
 geometry = SphereGeometry(1.0)
-material = MeshBasicMaterial()
+material = MeshNormalMaterial({"wireframe":True, "wireframeLinewidth":1})
 sphere = Mesh(geometry, material)
 scene.add(sphere)
 sphere.position = -e2
