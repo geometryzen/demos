fc29dc577f1eb858012c79159f8a9624bd22904c
David Geo Holmes <geometryzen@gmail.com>
2013-11-22 13:09:10 -0800

    Save file.

--- Canvas-3D/Materials/MeshNormalMaterial.py
+++ Canvas-3D/Materials/MeshNormalMaterial.py
@@ -25,7 +25,7 @@ material = MeshNormalMaterial()
 print repr(material)
 print str(material)
 
-mesh = Mesh(SphereGeometry(50, 32, 24), material)
+mesh = Mesh(SphereGeometry(50.0, 32, 24), material)
 
 scene.add(mesh)
 
