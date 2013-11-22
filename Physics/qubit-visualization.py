27493a9095ce4e3444b3207a25794d8be4fe36f5
David Geo Holmes <geometryzen@gmail.com>
2013-11-21 17:30:06 -0800

    Save file.

--- Physics/qubit-visualization.py
+++ Physics/qubit-visualization.py
@@ -5,6 +5,8 @@ from workbench import *
 from random import random
 from math import *
 
+e2 = VectorE3(0.0, 0.0, 0.0)
+
 canvas2D = document.createElement("canvas")
 canvas2D.style.position = "absolute"
 canvas2D.style.top = "0px"
