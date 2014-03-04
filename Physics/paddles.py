a0ad7bdb6bc14a1c8563306f4c4c4f133632055a
David Geo Holmes <geometryzen@gmail.com>
2014-03-04 12:55:57 -0500

    Save file.

--- Physics/paddles.py
+++ Physics/paddles.py
@@ -7,6 +7,10 @@ from math import pi, exp
 space = CartesianSpace()
 timeOut = 10
 
+e1 = VectorE3(1.0, 0.0, 0.0, False)
+e2 = VectorE3(0.0, 1.0, 0.0, False)
+e3 = VectorE3(0.0, 0.0, 1.0, False)
+
 e1 = ArrowBuilder().color(0xFF0000).axis(e1).build()#.attitude(exp(-BivectorE3( 0,  0, 1)*pi/4)).build()
 e2 = ArrowBuilder().color(0x00FF00).axis(e2).build()#.attitude(exp(-BivectorE3( 0, -1, 0)*pi/4)).build()
 e3 = ArrowBuilder().color(0x0000FF).axis(e3).build()#.attitude(exp(-BivectorE3( 1,  0, 0)*pi/4)).build()
