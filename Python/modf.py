bf1cc0ddecbff133320fd20ec8f11ce7c172393a
David Geo Holmes <geometryzen@gmail.com>
2014-03-04 13:02:14 -0500

    Delete modf.py

--- Python/modf.py
+++
@@ -1,6 +0,0 @@
-import math
-
-print "math.modf(100.12) : ", math.modf(100.12)
-print "math.modf(100.72) : ", math.modf(100.72)
-print "math.modf(119L) : ", math.modf(119L)
-print "math.modf(math.pi) : ", math.modf(math.pi)
