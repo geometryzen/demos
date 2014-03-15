1f05a36eb0f1ff179d4e9e33e01582cac3dffa6e
David Geo Holmes <geometryzen@gmail.com>
2014-03-15 14:07:45 -0400

    Save file.

--- Experimental/visualizingE3GA.py
+++ Experimental/visualizingE3GA.py
@@ -5,6 +5,7 @@ from workbench import *
 from geometry import *
 
 e1 = VectorE3(1,0,0)
+e2 = VectorE3(0,1,0)
 
 blades = [ScalarE3(1), e1, e2, e3, e1 ^ e2, e2 ^ e3, e3 ^ e1, e1 ^ e2 ^ e3, e1 ^ e3 ^ e2]
 binops = ["+", "-", "*", "<<", "^", ">>"]
