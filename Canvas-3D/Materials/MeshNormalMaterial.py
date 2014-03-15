46b35b2f6df9e5432d6d93920543f7656dadd144
David Geo Holmes <geometryzen@gmail.com>
2014-03-15 17:22:54 -0400

    Save file.

--- Canvas-3D/Materials/MeshNormalMaterial.py
+++ Canvas-3D/Materials/MeshNormalMaterial.py
@@ -34,37 +34,18 @@ progress = None
 progressEnd = 6000
 startTime =  None
 
-def render():
-    mesh.rotation.x = mesh.rotation.x + 0.02
-    mesh.rotation.y = mesh.rotation.y + 0.02
-    mesh.rotation.z = mesh.rotation.z + 0.02
-        
+workbench = Workbench(renderer, camera)
+
+def setUp():
+    workbench.setUp()
+
+def tick(t):
     renderer.render(scene, camera)
 
-def onWindowResize(event):
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.size = (window.innerWidth, window.innerHeight)
-    
-def step(timestamp):
-    global requestID, progress, startTime
-    if (startTime):
-        progress = timestamp - startTime
-    else:
-        if (timestamp):
-            startTime = timestamp
-        else:
-            progress = 0
-        
-    if (progress < progressEnd):
-        requestID = window.requestAnimationFrame(step)
-        render()
-    else:
-        window.cancelAnimationFrame(requestID)
-        # container.removeChild(renderer.domElement)
+def terminate(t):
+    return t > 6
 
-window.addEventListener("resize", onWindowResize, False)
+def tearDown():
+    workbench.tearDown()
 
-onWindowResize(None)
-
-step(None)
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
