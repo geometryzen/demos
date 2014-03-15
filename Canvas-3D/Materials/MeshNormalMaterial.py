e21307732319b61128e9076675b96487fc82e3cf
David Geo Holmes <geometryzen@gmail.com>
2014-03-15 17:23:14 -0400

    Save file.

--- Canvas-3D/Materials/MeshNormalMaterial.py
+++ Canvas-3D/Materials/MeshNormalMaterial.py
@@ -1,6 +1,7 @@
 # MeshNormalMaterial demonstration.
 from three import *
 from browser import *
+from workbench import *
 from math import pi
 
 for canvas in document.getElementsByTagName("canvas"):
