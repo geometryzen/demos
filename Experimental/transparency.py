57dca7e1a87e8dd911f5469e020b2053b8568cd8
David Geo Holmes <geometryzen@gmail.com>
2014-03-16 18:20:06 -0400

    Save file.

--- Experimental/transparency.py
+++ Experimental/transparency.py
@@ -22,9 +22,9 @@ scene.add(directionalLight)
 renderer = WebGLRenderer()
 renderer.setClearColor(Color(0x080808), 1.0)
 
-for i in range(0,10):
+for i in range(0,20):
     mesh = Mesh(CubeGeometry(0.1, 5, 5), MeshLambertMaterial({"color": 0x0000FF,"opacity": 0.25,"transparent": True}))
-    mesh.position = VectorE3(i-5,0,0)
+    mesh.position = VectorE3(i-10,0,0)
     scene.add(mesh)
 
 workbench = Workbench(renderer, camera)
