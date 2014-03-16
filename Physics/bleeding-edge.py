e1a5bbc07650ee5ad874c6e8efaf53ccada1ba12
David Geo Holmes <geometryzen@gmail.com>
2014-03-16 00:29:41 -0400

    Save file.

--- Physics/bleeding-edge.py
+++ Physics/bleeding-edge.py
@@ -20,8 +20,8 @@ particle.position = VectorE3(0.0, 1.0, 0.0)
 particle.velocity = VectorE3(0.0, -1.0, 0.0)
 space3D.add(particle)
 
-#wire = CylinderBuilder().radius(0.1).height(100).color("blue").build()
-#space3D.add(wire)
+wire = CylinderBuilder().radius(0.1).height(100).color("blue").build()
+space3D.add(wire)
 
 # Probe to show the velocity of the particle.
 probeV = ProbeBuilderE3().color(particle.material.color.getHex()).build()
