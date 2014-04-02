d19e97e9c7285175c1212a1cff8c6fdc90867948
David Geo Holmes <geometryzen@gmail.com>
2014-04-01 21:48:59 -0400

    Save file.

--- Education/bisection.py
+++ Education/bisection.py
@@ -11,4 +11,16 @@ def f(x):
     return exp(x)*log(x) - x * x
 
 a = window.prompt("Enter first guess", "0")
-b = window.prompt("Enter second guess", "0")
+b = window.prompt("Enter second guess", "1")
+
+dx = abs(b-a)
+
+while dx > tolerance:
+    x = (a+b)/2.0
+    if (f(a)*f(x)) < 0:
+        b = x
+    else:
+        a = x
+    dx = abs(b-a)
+
+print 'Found f(x) = 0 at x = %0.8f +/- %0.8f ' % (x, tolerance)
