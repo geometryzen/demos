e0a1c831c896867814832223d3322d7c2761cc43
David Geo Holmes <geometryzen@gmail.com>
2014-04-01 21:45:57 -0400

    Save file.

--- Education/bisection.py
+++ Education/bisection.py
@@ -3,8 +3,11 @@ This program uses the bisection method to find the root of f(x)
 """
 
 from math import *
+from browser import window
 
 tolerance = 1.0e-6
 
 def f(x):
     return exp(x)*log(x) - x * x
+
+a = window.prompt("Enter first guess")
