ee91e420260de7877de317efe36f1f84da0e0441
David Geo Holmes <geometryzen@gmail.com>
2014-04-03 12:56:34 -0400

    Save file.

--- Window/graph.py
+++ Window/graph.py
@@ -3,7 +3,6 @@ from browser import window, WindowAnimationRunner
 w = None
 
 def tick(time):
-    # w.document.body.innerHTML = '<h1>' + str(time) + '</h1>'
     pass
 
 def terminate(time):
@@ -13,7 +12,7 @@ def setUp():
     print "Press Esc key with this window as focus to termintate the animation."
     global w
     w = window.open("", "", "height=400, width=600")
-    # w.document.write("<h1>Hello</h1>")
+    w.document.body.innerHTML = '<h1>' + str(time) + '</h1>'
 
 def tearDown():
     w.close()
