2e8a3e1380dee444f86007e05dc3af0ce62d141d
David Geo Holmes <geometryzen@gmail.com>
2014-04-02 22:06:27 -0400

    Save file.

--- Window/open.py
+++ Window/open.py
@@ -3,4 +3,7 @@ from browser import window, WindowAnimationRunner
 #window.open("http://www.geometryzen.org")
 w = window.open("https://www.google.com","_blank")
 
+def tick():
+    pass
+
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
