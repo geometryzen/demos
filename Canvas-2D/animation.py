8e9bb2f85f9dcd8f27ecb94bacff5a53cfc00035
David Geo Holmes <geometryzen@gmail.com>
2014-04-03 19:58:38 -0400

    Save file.

--- Canvas-2D/animation.py
+++ Canvas-2D/animation.py
@@ -1,3 +1,5 @@
+#2345678901234567890123456789012345678901234567890123456789012345678901234567890
+#        1         2         3         4         5         6         7         8
 from browser import *
 from workbench import *
 from math import pi
