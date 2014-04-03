5ce97f5a58db40380244d2fb4b2e80863b608141
David Geo Holmes <geometryzen@gmail.com>
2014-04-02 22:22:47 -0400

    Save file.

--- Window/open.py
+++ Window/open.py
@@ -1,6 +1,6 @@
 from browser import window, WindowAnimationRunner
 
-w = window.open("","", "height=200, width=100, top=600")
+w = window.open("","", "height=200, width=100, top=400")
 
 #w.document.write("Hello")
 
