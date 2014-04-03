dc564dc065b9eca2b1f1f100b7d6a3ff4e958e97
David Geo Holmes <geometryzen@gmail.com>
2014-04-03 19:35:33 -0400

    Save file.

--- Window/graph.py
+++ Window/graph.py
@@ -1,27 +1,2 @@
-from browser import window, WindowAnimationRunner
+from browser import window
 from jxg import JSXGraph
-
-w = None
-board = None
-
-def tick(time):
-    pass
-
-def terminate(time):
-    return False
-
-def setUp():
-    print "Press Esc key with this window as focus to termintate the animation."
-    global w, board
-    w = window.open("", "", "height=400, width=600")
-    w.document.body.innerHTML = '<div id="box" style="width:200px;height:200px"></div>'
-    w.document.title = "Hello, World!"
-    e = w.document.getElementById("box")
-    print e
-    board = JSXGraph.initBoard("box")
-
-def tearDown():
-    w.close()
-
-WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
-
