a588c30ecc938723644c146514b01b58ca2dfefe
David Geo Holmes <geometryzen@gmail.com>
2014-04-03 21:24:09 -0400

    Save file.

--- Window/graph.py
+++ Window/graph.py
@@ -4,7 +4,7 @@ from jxg import JSXGraph
 document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
 div = document.getElementById("box")
 
-div.style.width  = "400px"
+div.style.width  = "600px"
 div.style.height = "400px"
 
 JSXGraph.initBoard("box", 
