eb662877d6b7a6da16480acb1d4db41f497e1f23
David Geo Holmes <geometryzen@gmail.com>
2014-04-03 21:04:22 -0400

    Save file.

--- Window/graph.py
+++ Window/graph.py
@@ -2,13 +2,9 @@ from browser import document
 from jxg import JSXGraph
 
 gc = document.getElementById("graph-container").innerHTML = '<div id="box"></div>'
-pc = document.getElementById("printer-container")
+div = document.getElementById("box")
 
-div = document.createElement("div")
-div.id = "box"
 div.style.width = "400px"
 div.style.height = "400px"
-
-gc.appendChild(div)
 
 JSXGraph.initBoard("box", {"boundingbox":[-2,4,6,-4]})
