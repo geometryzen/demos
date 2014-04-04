5eda4156fefcb334a0a368a9b1d6a1610dea2e95
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 15:10:28 -0400

    Save file.

--- JSXGraph/plotting.py
+++ JSXGraph/plotting.py
@@ -15,4 +15,4 @@ b = JSXGraph.initBoard("box", {"boundingbox":[-10,10,20,-10],"axis":True})
 p = b.create('point',[1,1])
 dataX = [1,  2,   3,4,5,6,7,8]
 dataY = [0.3,4.0,-1,2,3,7,9,8]
-b.create('curve',[dataX,dataY])
+b.create('curve',[dataX,dataY],{"strokeColor":'red'})
