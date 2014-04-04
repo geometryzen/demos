88521220986feff955d4a921026867fdc6ea2ef3
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 14:54:08 -0400

    Save file.

--- JSXGraph/curve.py
+++ JSXGraph/curve.py
@@ -1,5 +1,6 @@
 from browser import document, window
 from jxg import require
+from math import sin
 
 JXG = require('JXG')
 JSXGraph = JXG.JSXGraph
@@ -11,3 +12,5 @@ div.style.width  = "400px"
 div.style.height = "400px"
 
 b = JSXGraph.initBoard("box", {"boundingbox":[-10,10,20,-10]})
+
+b.create('functiongraph',[lambda x: sin(x)])
