0704f557e472a78794874352d98a1369b4157dbd
David Geo Holmes <geometryzen@gmail.com>
2014-04-03 21:22:40 -0400

    Save file.

--- Window/graph.py
+++ Window/graph.py
@@ -7,4 +7,5 @@ div = document.getElementById("box")
 div.style.width  = "400px"
 div.style.height = "400px"
 
-JSXGraph.initBoard("box", {"boundingbox":[-2,4,6,-4], "axis":True, "grid":True})
+JSXGraph.initBoard("box", 
+                   {"boundingbox":[-2,4,6,-4], "axis":True, "grid":True})
