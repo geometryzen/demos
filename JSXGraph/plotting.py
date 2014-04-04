b8a2485b8dcee3c77d0956d36804053737005e18
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 17:02:25 -0400

    Save file.

--- JSXGraph/plotting.py
+++ JSXGraph/plotting.py
@@ -15,11 +15,12 @@ b = JSXGraph.initBoard("box", {"boundingbox":[-10,10,20,-10],"axis":True})
 p = b.create('point',[1,4])
 dataX = [1,2,3,4,5,6,7,8]
 dataY = [0.3,4.0,-1,2.3,7,9,8,9]
-b.create('curve',[dataX,dataY],{"strokeColor":'red'})
 
 # It does seem that JXG calls with a second Boolean argument.
 def foo(x, unused):
     return p.X()
 
+b.create('curve',[dataX,dataY],{"strokeColor":'red'})
+
 # FIXME: Some bug prevents us from doing this.
-b.create('curve',[dataX,foo],{"strokeColor":'blue'})
+#b.create('curve',[dataX,foo],{"strokeColor":'blue'})
