8f6160f0da3b1c79ab296efb6c7be052b1d64513
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 14:52:36 -0400

    Save file.

--- JSXGraph/curve.py
+++ JSXGraph/curve.py
@@ -11,16 +11,3 @@ div.style.width  = "400px"
 div.style.height = "400px"
 
 b = JSXGraph.initBoard("box", {"boundingbox":[-5,5,5,-5]})
-
-p1 = b.create('point',[0,0],{"name":'A',"size":4})
-p2 = b.create('point',[ 2,-1],{"name":'B',"size":4})
-
-ci = b.create(
-    'circle',
-    [p1,p2],
-    {
-        "fillColor":"#555500",
-        "dash":2,
-        "fillOpacity": lambda: p2.X() * 0.25
-    }
-)
