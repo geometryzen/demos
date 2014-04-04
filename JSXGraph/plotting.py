ff014bee06dee35521a88e104306a0f6e7b2ee6b
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 17:54:03 -0400

    Save file.

--- JSXGraph/plotting.py
+++ JSXGraph/plotting.py
@@ -18,6 +18,7 @@ dataY = [0.3,4.0,-1,2.3,7,9,8,9]
 
 # It does seem that JXG calls with a second Boolean argument.
 def foo(x, unused):
+    print x, unused
     return p.X() * sin(x) * x
 
 b.create('curve',[dataX,dataY],{"strokeColor":'red'})
