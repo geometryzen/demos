625ae090ea8250b189c3b27e53cba73dc4eb4ce6
David Geo Holmes <geometryzen@gmail.com>
2014-04-03 23:27:44 -0400

    Save file.

--- JSXGraph/poc.py
+++ JSXGraph/poc.py
@@ -8,7 +8,6 @@ div.style.width  = "400px"
 div.style.height = "400px"
 
 JSXGraph.initBoard("box", 
-                   {"boundingbox":[-2,4,6,-4],
-                    "axis":True,
+                   {"axis":True,
                     "grid":True,
                     "showCopyright":False})
