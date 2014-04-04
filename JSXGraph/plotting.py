bc67aa4ffcab1776923aae130d9cf78ba9270fa3
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 15:14:47 -0400

    Save file.

--- JSXGraph/plotting.py
+++ JSXGraph/plotting.py
@@ -17,7 +17,7 @@ dataX = [1,  2,   3,4,5,6,7,8]
 dataY = [0.3,4.0,-1,2,3,7,9,8,9]
 b.create('curve',[dataX,dataY],{"strokeColor":'red'})
 
-def foo(x):
+def foo(x, unused):
     return p.X()
 
 b.create('curve',[dataX,foo],{"strokeColor":'blue'})
