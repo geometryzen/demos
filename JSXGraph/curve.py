f69e16320207ad515c673b8432c5b1628eef3726
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 15:03:18 -0400

    Save file.

--- JSXGraph/curve.py
+++ JSXGraph/curve.py
@@ -2,7 +2,7 @@ from browser import document, window
 import jxg
 from math import *
 
-JSXGraph = require('JXG').JSXGraph
+JSXGraph = jxg.require('JXG').JSXGraph
 
 document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
 div = document.getElementById("box")
