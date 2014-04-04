b4336387746d37f3a2478f24e318da64ac846ca5
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 15:05:45 -0400

    Save file.

--- JSXGraph/curve.py
+++ JSXGraph/curve.py
@@ -13,4 +13,4 @@ div.style.height = "400px"
 b = JSXGraph.initBoard("box", {"boundingbox":[-10,10,20,-10],"axis":True})
 
 p = b.create('point',[1,1])
-b.create('functiongraph',[lambda x,unk: p.X() * sin(x)])
+b.create('functiongraph',[lambda x,unused: p.X() * sin(x)])
