7fb9f55a9e1de455fb104bc805a76df050aa38d2
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 13:28:27 -0400

    Save file.

--- JSXGraph/poc.py
+++ JSXGraph/poc.py
@@ -10,16 +10,9 @@ div.style.height = "400px"
 board = JSXGraph.initBoard("box", {"axis":True,"grid":True,"showCopyright":False})
 
 free = board.create('point',[3,4],{"name":'A',"size":3})
-
-def foo():
-    return free.X()
-
-bar = lambda: free.X()
-
 dep = board.create('point',[lambda: free.X(),1],{"name":'B',"size":3})
 
 #p.setAttribute({"fixed":True});
 
-print foo()
 print free.X()
 print free.Y()
