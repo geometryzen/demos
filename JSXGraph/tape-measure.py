a86ee6d69a4394efefb4916c39f6d0967a6f3556
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 00:30:58 -0400

    Save file.

--- JSXGraph/tape-measure.py
+++ JSXGraph/tape-measure.py
@@ -1,1 +1,27 @@
-# tape-measure.py
+from browser import document, window
+from jxg import require
+
+# WARNING: This method for acquiring the library is subject to change,
+#          but the API will otherwise stay the same.
+graph = require('JXG').JSXGraph
+
+document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
+div = document.getElementById("box")
+
+div.style.width  = "400px"
+div.style.height = "400px"
+
+b = graph.initBoard("box", {"boundingbox":[-5,5,5,-5]})
+
+p1 = b.create('point',[0,0],{"name":'A',"size":4})
+p2 = b.create('point',[ 2,-1],{"name":'B',"size":4})
+
+ci = b.create(
+    'circle',
+    [p1,p2],
+    {
+        "fillColor":"#555500",
+        "dash":2,
+        "fillOpacity": lambda: p2.X() * 0.25
+    }
+)
