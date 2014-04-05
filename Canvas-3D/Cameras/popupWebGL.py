0c0ac156186097bf41e28a8650f02270aa1dcd0f
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 15:22:28 -0400

    Save file.

--- Canvas-3D/Cameras/popupWebGL.py
+++ Canvas-3D/Cameras/popupWebGL.py
@@ -4,7 +4,7 @@ from random import random
 
 THREE = window.THREE
 
-glwin = window.open("","","width=600,height=480")
+glwin = window.open("","","width=1200,height=960")
 
 # Global variables
 camera = THREE.OrthographicCamera(-1.0, 1.0, 1.0, -1.0, -500.0, 1000.0)
