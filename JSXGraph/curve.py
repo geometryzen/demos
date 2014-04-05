331069f5fc0ed2bc14fe9aafe16e3c5f68872d0d
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 10:54:07 -0400

    Save file.

--- JSXGraph/curve.py
+++ JSXGraph/curve.py
@@ -1,8 +1,7 @@
 from browser import document, window
-import jxg
 from math import *
 
-JSXGraph = jxg.require('JXG').JSXGraph
+graph = window.JXG.JSXGraph
 
 document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
 div = document.getElementById("box")
@@ -10,7 +9,7 @@ div = document.getElementById("box")
 div.style.width  = "400px"
 div.style.height = "400px"
 
-b = JSXGraph.initBoard("box", {"axis":True,"grid":True})
+b = graph.initBoard("box", {"axis":True,"grid":True})
 
 p = b.create('point',[1,1])
 b.create('functiongraph',[lambda x: p.X() * sin(x)])
