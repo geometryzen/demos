aa8690d3f9b719609fe3fed378bb2d6f617fc21c
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 00:28:11 -0400

    Save file.

--- JSXGraph/plotting.py
+++ JSXGraph/plotting.py
@@ -10,7 +10,7 @@ div = document.getElementById("box")
 div.style.width  = "400px"
 div.style.height = "400px"
 
-b = JSXGraph.initBoard("box", {"boundingbox":[-10,10,20,-10],"axis":True})
+b = graph.initBoard("box", {"boundingbox":[-10,10,20,-10],"axis":True})
 
 p = b.create('point',[1,4])
 dataX = [1,2,3,4,5,6,7,8]
