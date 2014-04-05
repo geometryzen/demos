b4cb24a9475c0a2734aeb0a3d75aa4afdf4c259a
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 15:13:38 -0400

    Save file.

--- Canvas-3D/Cameras/popupWebGL.py
+++ Canvas-3D/Cameras/popupWebGL.py
@@ -1,19 +1,20 @@
-from three import *
 from browser import *
 from math import cos, sin, floor
 from random import random
 
 useLargeCanvas = True
 
+THREE = window.THREE
+
 # Global variables
-camera = OrthographicCamera(-1.0, 1.0, 1.0, -1.0, -500.0, 1000.0)
+camera = THREE.OrthographicCamera(-1.0, 1.0, 1.0, -1.0, -500.0, 1000.0)
 camera.position.x = 200.0
 camera.position.y = 100.0
 camera.position.z = 200.0
 
-renderer = WebGLRenderer({"antialias": True})
-renderer.setClearColor(Color(0xFFFFFF), 1.0)
-scene = Scene()
+renderer = THREE.WebGLRenderer({"antialias": True})
+renderer.setClearColor(THREE.Color(0xFFFFFF), 1.0)
+scene = THREE.Scene()
 view = document.getElementById("view")
 progressEnd = 5
 
@@ -57,24 +58,24 @@ def setUp():
     # Grid
     size = 500
     step = 50
-    geometry = Geometry()
+    geometry = THREE.Geometry()
     for i in range(-size, size+step, step):
-        geometry.vertices.append(VectorE3(float(-size), 0.0, float(i)))
-        geometry.vertices.append(VectorE3(float(+size), 0.0, float(i)))
-        geometry.vertices.append(VectorE3( float(i), 0.0, float(-size)))
-        geometry.vertices.append(VectorE3( float(i), 0.0, float(+size)))
+        geometry.vertices.append(THREE.Vector3(float(-size), 0.0, float(i)))
+        geometry.vertices.append(THREE.Vector3(float(+size), 0.0, float(i)))
+        geometry.vertices.append(THREE.Vector3( float(i), 0.0, float(-size)))
+        geometry.vertices.append(THREE.Vector3( float(i), 0.0, float(+size)))
         
-    material = LineBasicMaterial({"color":0x000000,"opacity":0.2})
+    material = THREE.LineBasicMaterial({"color":0x000000,"opacity":0.2})
     
-    line = Line(geometry, material)
-    line.type = LinePieces
+    line = THREE.Line(geometry, material)
+    line.type = THREE.LinePieces
     scene.add(line)
     
     # Cubes
-    geometry = CubeGeometry(50, 50, 50)
-    material = MeshLambertMaterial({"color":0xFFFFFF,"shading":FlatShading, "overdraw":True})
+    geometry = THREE.CubeGeometry(50, 50, 50)
+    material = THREE.MeshLambertMaterial({"color":0xFFFFFF,"shading":THREE.FlatShading, "overdraw":True})
     for i in range(0, 100):
-        cube = Mesh(geometry, material)
+        cube = THREE.Mesh(geometry, material)
         cube.scale.y = floor(random() * 2 + 1)
         cube.position.x = floor((random() * 1000 - 500) / 50) * 50 + 25
         cube.position.y = (cube.scale.y * 50) / 2
@@ -82,17 +83,17 @@ def setUp():
         scene.add(cube)
         
     # Lights
-    ambientLight = AmbientLight(random() * 0x10)
+    ambientLight = THREE.AmbientLight(random() * 0x10)
     scene.add(ambientLight)
     
-    directionalLight = DirectionalLight(random() * 0xFFFFFF)
+    directionalLight = THREE.DirectionalLight(random() * 0xFFFFFF)
     directionalLight.position.x = random() - 0.5
     directionalLight.position.y = random() - 0.5
     directionalLight.position.z = random() - 0.5
     directionalLight.position.normalize()
     scene.add(directionalLight)
     
-    directionalLight = DirectionalLight(random() * 0xFFFFFF)
+    directionalLight = THREE.DirectionalLight(random() * 0xFFFFFF)
     directionalLight.position.x = random() - 0.5
     directionalLight.position.y = random() - 0.5
     directionalLight.position.z = random() - 0.5
