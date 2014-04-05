af8be992e25c31062593634c91533f28917d433a
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 14:15:23 -0400

    Save file.

--- JSXGraph/popup.py
+++ JSXGraph/popup.py
@@ -1,1 +1,17 @@
-# popup.py
+from browser import document, window
+from math import *
+
+graph = window.JXG.JSXGraph
+
+document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
+div = document.getElementById("box")
+
+div.style.width  = "400px"
+div.style.height = "400px"
+
+board = graph.initBoard("box", {"axis":True,"grid":True})
+
+A = board.create('point',[1,1],{"name": 'Alice'})
+B = board.create('point',[2,2],{"name":'Bob'})
+
+f = board.create('functiongraph',[lambda x: A.X() * sin(x)])
