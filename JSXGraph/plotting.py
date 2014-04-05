d7174a33dfb284e6ac9b135ee5c547ec21ac5d4c
David Geo Holmes <geometryzen@gmail.com>
2014-04-04 23:53:24 -0400

    Save file.

--- JSXGraph/plotting.py
+++ JSXGraph/plotting.py
@@ -16,9 +16,7 @@ p = b.create('point',[1,4])
 dataX = [1,2,3,4,5,6,7,8]
 dataY = [0.3,4.0,-1,2.3,7,9,8,9]
 
-# It does seem that JXG calls with a second Boolean argument.
-def foo(x):
-    return p.X() * sin(x) * x
+# It does seem that JXG ca
 
 b.create('curve',[dataX,dataY],{"strokeColor":'red'})
 
