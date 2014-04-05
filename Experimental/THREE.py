2bff78132c6a92e73d5628cc4e38f2ef342fa523
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 12:25:28 -0400

    Save file.

--- Experimental/THREE.py
+++ Experimental/THREE.py
@@ -6,7 +6,9 @@ v = THREE.Vector3(1,2,3)
 w = THREE.Vector3(0.5,0.5,0.5)
 
 print v.x, v.y, v.z
+# Unfortunately, THREE does not support toString.
 print str(v)
 print repr(v)
 
-print v + w
+z = v + w
+print z.x, z.y, z.z
