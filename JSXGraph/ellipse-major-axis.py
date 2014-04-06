f0d3f7c4248a8a0956fc41324d3d925c9c61d434
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 20:26:45 -0400

    Save file.

--- JSXGraph/ellipse-major-axis.py
+++ JSXGraph/ellipse-major-axis.py
@@ -1,1 +1,18 @@
 # ellipse-major-axis.py
+from browser import document, window
+from math import *
+
+graph = window.JXG.JSXGraph
+
+document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
+div = document.getElementById("box")
+
+div.style.width  = "400px"
+div.style.height = "400px"
+
+board = graph.initBoard("box", {"axis":True,"grid":True})
+
+A = board.create('point',[-1,0])
+B = board.create('point',[1,0])
+s = board.create('slider',[[-1,-2],[1,-2],[0,4,10]])
+E = board.create('ellipse',[A,B,lambda : s.Value()])
