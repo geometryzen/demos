3bfb6468ba62da55e08dbcf8e1d6a76927589f91
David Geo Holmes <geometryzen@gmail.com>
2014-04-05 21:01:36 -0400

    Save file.

--- JSXGraph/turtle-eight.py
+++ JSXGraph/turtle-eight.py
@@ -1,22 +1,31 @@
-# turtle-eight.py
-var t = brd.create('turtle',[0, 0], {strokeOpacity:0.5});
-t.setPenSize(3);
-t.right(90);
-var alpha = 0;
+from browser import document, window
+from math import *
+
+graph = window.JXG.JSXGraph
+
+document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
+div = document.getElementById("box")
+
+div.style.width  = "400px"
+div.style.height = "400px"
+
+board = graph.initBoard("box", {"boundingbox":[-250,250,250,-250]})
+t = board.create('turtle',[0, 0], {"strokeOpacity":0.5})
+
+t.setPenSize(3)
+t.right(90)
+alpha = 0
  
-var run = function() {
-   t.forward(2);
-   if (Math.floor(alpha / 360) % 2 === 0) {
-      t.left(1);        // turn left by 1 degree
-   } else {
-      t.right(1);       // turn right by 1 degree
-   }
- 
-   alpha += 1;
- 
-   if (alpha < 1440) {  // stop after two rounds
-       setTimeout(run, 20); 
-   }
-}
- 
-run();
+def run():
+    t.forward(2)
+    if (Math.floor(alpha / 360) % 2 == 0):
+        t.left(1)
+    else:
+        t.right(1)
+
+    alpha += 1;
+
+    if (alpha < 1440):
+        setTimeout(run, 20)
+
+run()
