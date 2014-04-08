9cd1e0e8b65dc408c0b09f84923fe1b0bb1d9fc4
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 19:57:12 -0400

    Save file.

--- Mathematics/NumPy/linspace.py
+++ Mathematics/NumPy/linspace.py
@@ -9,9 +9,9 @@ print x[2]
 print x[3]
 print x[4]
 
-x = np.linspace(2.0, 3.0, 5, False)
+x = np.linspace(2.0, 3.0, 5, False, False)
 
-print "linspace(2.0, 3.0, 5, False)"
+print "linspace(2.0, 3.0, 5, False, False)"
 print x[0]
 print x[1]
 print x[2]
