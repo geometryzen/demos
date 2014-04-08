0dc4ddc77016256c079447fbb0de1a63e3f8971f
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 14:48:03 -0400

    Save file.

--- Mathematics/NumPy/basic.py
+++ Mathematics/NumPy/basic.py
@@ -8,7 +8,7 @@ e4 = VectorE3(4,0,0)
 e5 = VectorE3(5,0,0)
 e6 = VectorE3(6,0,0)
 
-x = np.array([[e1,e2,e3],[e4,e5,e6]])
+x = np.array([[e1,e2][e3,e3]],[[e5,e6],[e7,e8]])
 print type(x)
 print x.shape
 print x.sizes
