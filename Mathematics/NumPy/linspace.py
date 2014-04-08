36710cf125ee428c93764b3028baa0486159b285
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 19:55:16 -0400

    Save file.

--- Mathematics/NumPy/linspace.py
+++ Mathematics/NumPy/linspace.py
@@ -2,14 +2,11 @@ import numpy as np
 
 x = np.linspace(2.0, 3.0, 5)
 
+print "linspace(2.0, 3.0, 5)"
 print x[0]
 print x[1]
 print x[2]
 print x[3]
 print x[4]
-print ""
-print x.ndim
-print x.size
-print x.shape
 
 
