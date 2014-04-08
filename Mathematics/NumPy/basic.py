253426b1cab52a34c83e7dc0b17150a2b5077759
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 15:18:35 -0400

    Save file.

--- Mathematics/NumPy/basic.py
+++ Mathematics/NumPy/basic.py
@@ -1,14 +1,4 @@
 import numpy as np
-from e3ga import *
-
-e1 = VectorE3(1,0,0)
-e2 = VectorE3(2,0,0)
-e3 = VectorE3(3,0,0)
-e4 = VectorE3(4,0,0)
-e5 = VectorE3(5,0,0)
-e6 = VectorE3(6,0,0)
-e7 = VectorE3(6,0,0)
-e8 = VectorE3(6,0,0)
 
 x = np.array([[
                ['a','b','c','d'],
