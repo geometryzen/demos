013326cb2f961e2cc45d4f73af708438497b5476
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 12:33:31 -0400

    Save file.

--- Mathematics/NumPy/basic.py
+++ Mathematics/NumPy/basic.py
@@ -2,8 +2,13 @@ import numpy as np
 from e3ga import *
 
 e1 = VectorE3(1,0,0)
+e2 = VectorE3(2,0,0)
+e3 = VectorE3(3,0,0)
+e4 = VectorE3(4,0,0)
+e5 = VectorE3(5,0,0)
+e6 = VectorE3(6,0,0)
 
-x = np.array([[e1,2,3],[4,5,6]])
+x = np.array([[e1,e2,e3],[e4,e5,e6]])
 print type(x)
 print x.shape
 print x[1,2]
