6adb9c5512ccca21a35929d2ae18d7b439a82e8e
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 00:20:14 -0400

    Save file.

--- Modeling/ode/euler3D.py
+++ Modeling/ode/euler3D.py
@@ -8,6 +8,7 @@ from browser import document, window
 e1 = VectorE3(1, 0, 0)
 e2 = VectorE3(0, 1, 0)
 e3 = VectorE3(0, 0, 1)
+origin = VectorE3(0,0,0)
 
 #def FreeFall(state,time):
 #    g0 = state[1]
@@ -17,7 +18,7 @@ e3 = VectorE3(0, 0, 1)
 # Simple Harmonic Motion
 def SHO(state, time):
     g0 = state[1]
-    g1 = - k/m * state[0] - gravity
+    g1 = - k/m * state[0] + gravity
     return numpy.array([g0, g1])
 
 def euler(y, t, dt, derivs):
@@ -29,8 +30,8 @@ N = 1000
 
 y = numpy.zeros((N,2))
 
-y[0,0] = 0 * e1 + 0 * e2 + 0 * e3
-y[0,1] = 0.0
+y[0,0] = VectorE3(0, 0, 0)
+y[0,1] = VectorE3(0, 0, 0)
 
 tau = 5.0
 
@@ -40,7 +41,7 @@ dt = tau / float(N-1)
 
 k = 3.5
 m = 0.2
-gravity = 9.8
+gravity = VectorE3(0.0, 0.0, -9.8)
 
 for j in range(N-1):
     y[j+1] = euler(y[j], t[j], dt, SHO)
