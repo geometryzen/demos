7a4020cacbd937242dbd7842203554b996ae0c03
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 15:59:58 -0400

    Save file.

--- Python/UrlLib.py
+++ Python/UrlLib.py
@@ -1,6 +1,12 @@
 # UrlLib.py
-import urllib
+import urllib3
+
+http = urllib3.PoolManager()
+
+
+'''
 u = urllib.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
 data = u.read()
 
 print data
+'''
