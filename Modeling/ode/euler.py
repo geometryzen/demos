32e2da21a4cd0b37032b73f19028a256998c650c
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 00:02:41 -0400

    Save file.

--- Modeling/ode/euler.py
+++ Modeling/ode/euler.py
@@ -1,10 +1,10 @@
 import numpy
 from browser import document, window
 
-def FreeFall(state,time):
-    g0 = state[1]
-    g1 = -9.8
-    return numpy.array([g0, g1])
+#def FreeFall(state,time):
+#    g0 = state[1]
+#    g1 = -9.8
+#    return numpy.array([g0, g1])
 
 def SHO(state, time):
     g0 = state[1]
