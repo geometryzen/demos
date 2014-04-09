7a3f8dd6d52cb15c0b54f60a816cc1d15c77e62f
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 22:52:32 -0400

    Save file.

--- Modeling/ode/euler.py
+++ Modeling/ode/euler.py
@@ -36,9 +36,10 @@ for j in range(N-1):
     print "time[",j,"] =", time[j]
     print "y[",j,"][0] =", y[j][0]
     print "y[",j,"][1] =", y[j][1]
-    y[j+1] = euler(y[j], time[j], dt, SHO)
-    print "y[",j+1,"][0]:", y[j+1][0]
-    print "y[",j+1,"][1]:", y[j+1][1]
+    e = euler(y[j], time[j], dt, SHO)
+    print "y[",j+1,"][0]:", e[0]
+    print "y[",j+1,"][1]:", e[1]
+    y[j+1]=e
     pass
     
 #xdata = [y[j,0] for j in range(N)]
