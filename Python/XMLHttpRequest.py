8e608c7272cd5456e14c9a7255a681dd6db742ab
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 15:42:37 -0400

    Save file.

--- Python/XMLHttpRequest.py
+++ Python/XMLHttpRequest.py
@@ -1,13 +1,1 @@
-from browser import window
-
-url = 'http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22'
-clazz = window.XMLHttpRequest
-
-print clazz
-
-request = clazz()
-
-request.open("GET", url, False)
-request.send()
-
-print request.responseText
+à€€à€€
