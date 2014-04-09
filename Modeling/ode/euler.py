b9b1bc3f1afc9beed10356e7cd2b9d147e551bbb
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 22:44:08 -0400

    Save file.

--- Modeling/ode/euler.py
+++ Modeling/ode/euler.py
@@ -11,8 +11,7 @@ def SHO(state, time):
     return numpy.array([g0, g1])
 
 def euler(y, t, dt, derivs):
-    y.next = y + derivs(y,t) * dt
-    return y.next
+    return y + derivs(y,t) * dt
 
 N = 1000
 x0 = 0.0
