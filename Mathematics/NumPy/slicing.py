5e604e33d9e122b0f85886da0bfe42a80075e776
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 14:30:19 -0400

    Save file.

--- Mathematics/NumPy/slicing.py
+++ Mathematics/NumPy/slicing.py
@@ -10,7 +10,7 @@ print word[0:2]
 print word[2:4]
 print word[:2] # omitting first index defaults to zero
 print word[2:] # omitting second default to len(word)
-print word[:]
+print word[::2]
 #print word[1:100]
 #rint word[10:0]
 print "Watch those minus signs..."
