16edd8ad7cf9c8745e31d1f6c6dd309b778b984d
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 17:37:21 -0400

    Save file.

--- Python/XMLHttpRequest.py
+++ Python/XMLHttpRequest.py
@@ -8,6 +8,8 @@ xhr = XMLHttpRequest()
 
 xhr.open('GET', 'http://localhost:8080/users/geometryzen/repos/demos/blob/master/Python/XMLHttpRequest.py', True)
 
+xhr.responseType = "text"
+
 xhr.onload = reqListener
 
 xhr.send()
