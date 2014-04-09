c998cb741ce3fcfd4fddae29d734c239c2c73b2f
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 17:04:41 -0400

    Save file.

--- Python/XMLHttpRequest.py
+++ Python/XMLHttpRequest.py
@@ -7,17 +7,9 @@ request = XMLHttpRequest()
 print "request created"
 request.onload = reqListener
 print "handler connected"
-request.open('GET', 'localhost:8080/', True)
+request.open('GET', 'http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22', True)
 print "connection open"
 request.send()
 print "request sent"
 
 print "Done"
-
-
-'''
-u = urllib.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
-data = u.read()
-
-print data
-'''
