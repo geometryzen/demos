e9fd879f2fc1902dd88dc9cdade02676ba3c89a8
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 09:41:25 -0400

    Save file.

--- Python/Directives.py
+++ Python/Directives.py
@@ -6,8 +6,9 @@ b = 73
 print("decimal 73 as integer b = %d " %(b)) # for integer
 
 print("as octal b = %o" %(b)) # octal
+
+print("as hexadecimal b = %x "%(b)) # works hexadecimal
 '''
-p r i n t ("as hexadecimal b = %x "%(b ) ) # wo rks h e x a d e cim al
 p r i n t ("learn \"Python\" ") # u s e o f d o u bl e q u ot e symbol
 p r i n t ("shows a backslash \\") # u s e o f nn
 p r i n t (’use of single \’ quotes \’ ’) # p r i n t s i n g l e q u o t e s
