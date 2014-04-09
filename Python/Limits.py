ed03c8bf75b25bcdd97f36f4478e94582484d19b
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 09:56:22 -0400

    Save file.

--- Python/Limits.py
+++ Python/Limits.py
@@ -1,1 +1,8 @@
-# Limits.py
+# Limits.py : determines approximate machine precision
+N = 3
+eps = 1.0
+for i in range(N) :
+    eps = eps / 2
+    onePluseps = 1.0 + eps
+    print("one + eps = ", onePluseps)
+    print("eps = ", eps)
