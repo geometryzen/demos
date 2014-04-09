d4f903d0641b1ff551e6148e653694c80d9d094c
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 23:15:57 -0400

    Save file.

--- Modeling/ode/euler.py
+++ Modeling/ode/euler.py
@@ -54,7 +54,6 @@ div.style.height = "400px"
 
 b = graph.initBoard("box", {"boundingbox":[-10,10,20,-10],"axis":True})
 
-p = b.create('point',[1,4])
 dataX = [1,2,3,4,5,6,7,8]
 dataY = [0.3,4.0,-1,2.3,7,9,8,9]
 
