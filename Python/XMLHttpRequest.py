c6df7eceb0e4508c1ee35372a027753c62626130
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 16:39:48 -0400

    Save file.

--- Python/XMLHttpRequest.py
+++ Python/XMLHttpRequest.py
@@ -1,1 +1,14 @@
-à€€à€€
+# UrlLib.py
+from http import *
+
+request = XMLHttpRequest()
+
+request.open('GET', 'gttp://google.com/')
+
+
+'''
+u = urllib.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
+data = u.read()
+
+print data
+'''
