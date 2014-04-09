2a92db5bf956f5260c0b77fb5ae3b482cb62d50a
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 17:17:38 -0400

    Save file.

--- Python/XMLHttpRequest.py
+++ Python/XMLHttpRequest.py
@@ -7,7 +7,7 @@ request = XMLHttpRequest()
 print "request created"
 request.onload = reqListener
 print "handler connected"
-request.open('GET', 'https://www.google.com', True)
+request.open('GET', 'https://github.com/david-geo-holmes', True)
 print "connection open"
 request.send()
 print "request sent"
