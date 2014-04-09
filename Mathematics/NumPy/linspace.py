899243ba31815090c28441984827c1efbaa74c93
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 20:01:50 -0400

    Save file.

--- Mathematics/NumPy/linspace.py
+++ Mathematics/NumPy/linspace.py
@@ -23,8 +23,8 @@ x = np.linspace(2.0, 3.0, 5, True, True)
 print "linspace(2.0, 3.0, 5, True, True)"
 print x[0]
 print x[1]
-print x[2]
-print x[3]
-print x[4]
+print x[0][0]
+print x[0][1]
+print x[0][2]
 
 
