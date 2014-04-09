723eb963a394419dd96208fb4f317b7b5f1ad552
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 17:24:23 -0400

    Save file.

--- Python/XMLHttpRequest.py
+++ Python/XMLHttpRequest.py
@@ -4,12 +4,12 @@ def reqListener():
     print "Aha!"
 
 request = XMLHttpRequest()
-print "request created"
+
 request.onload = reqListener
-print "handler connected"
+
 request.open('GET', 'http://localhost:8080/users/geometryzen/repos/demos/blob/master/Python/XMLHttpRequest.py', True)
-print "connection open"
+
 request.send()
-print "request sent"
+
 
 print "Done"
