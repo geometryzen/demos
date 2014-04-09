70929b1f1c31c9ad00a228f74d8e92333f4b60fc
David Geo Holmes <geometryzen@gmail.com>
2014-04-08 22:50:39 -0400

    Save file.

--- Modeling/ode/euler.py
+++ Modeling/ode/euler.py
@@ -34,8 +34,8 @@ y[0,1] = v0
 
 for j in range(N-1):
     print "time[",j,"] =", time[j]
-    print "y[",j,"][0]:", y[j][0]
-    print "y[",j,"][1]:", y[j][1]
+    print "y[",j,"][0] =", y[j][0]
+    print "y[",j,"][1] =", y[j][1]
     y[j+1] = euler(y[j], time[j], dt, SHO)
     print "y[",j+1,"][0]:", y[j+1][0]
     print "y[",j+1,"][1]:", y[j+1][1]
