d215a31b5eb94178d63e0460311b11fa1d5ec84e
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 14:19:02 -0400

    Save file.

--- Mathematics/NumPy/slicing.py
+++ Mathematics/NumPy/slicing.py
@@ -11,7 +11,7 @@ print word[2:4]
 print word[:2] # omitting first index defaults to zero
 print word[2:] # omitting second default to len(word)
 print word[:]
-print word[1:100]
+#print word[1:100]
 #rint word[10:0]
 print "Watch those minus signs..."
 print word[-1] # counting from the right
