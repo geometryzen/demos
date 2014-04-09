00deba10271c665aa7fae4324c7e64f30f13fdf9
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 17:02:22 -0400

    Save file.

--- Python/XMLHttpRequest.py
+++ Python/XMLHttpRequest.py
@@ -7,7 +7,7 @@ request = XMLHttpRequest()
 print "request created"
 request.onload = reqListener
 print "handler connected"
-request.open('GET', 'http://google.com/', True)
+request.open('GET', 'localhost:8080/', True)
 print "connection open"
 request.send()
 print "request sent"
