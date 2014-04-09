5ade6e210d7447817381f76e0c85c06ac79e90c3
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 00:15:49 -0400

    Save file.

--- Modeling/ode/euler3D.py
+++ Modeling/ode/euler3D.py
@@ -4,6 +4,10 @@ import numpy
 from e3ga import *
 from browser import document, window
 
+e1 = VectorE3(1, 0, 0)
+e2 = VectorE3(0, 1, 0)
+e3 = VectorE3(0, 0, 1)
+
 #def FreeFall(state,time):
 #    g0 = state[1]
 #    g1 = -9.8
