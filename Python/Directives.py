4fcfb7342b0f900693b1c341d374d562e171b990
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 09:38:35 -0400

    Save file.

--- Python/Directives.py
+++ Python/Directives.py
@@ -1,1 +1,11 @@
-# Directives.py
+print ("hello \n")
+'''
+p r i n t ("\t it’s me") # t a b u l a t o r
+b = 73
+p r i n t ("decimal 73 as integer b = %d "%(b ) ) # f o r i n t e g e r
+p r i n t ("as octal b = %o"%(b ) ) # o c t a l
+p r i n t ("as hexadecimal b = %x "%(b ) ) # wo rks h e x a d e cim al
+p r i n t ("learn \"Python\" ") # u s e o f d o u bl e q u ot e symbol
+p r i n t ("shows a backslash \\") # u s e o f nn
+p r i n t (’use of single \’ quotes \’ ’) # p r i n t s i n g l e q u o t e s
+'''
