7e029f5d8429de45361c39e7c0cee88c94ffd011
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 10:18:51 -0400

    Save file.

--- Python/Range.py
+++ Python/Range.py
@@ -1,1 +1,24 @@
 # Range.py
+import numpy as np
+import sys
+
+sys.version
+# out: '2.7.3rc2 (default, Mar 22 2012, 04:35:15) \n[GCC 4.6.3]'
+np.version.version
+# out: '1.6.2'
+
+size = int(1E6)
+
+%timeit for x in range(size): x ** 2
+# out: 10 loops, best of 3: 136 ms per loop
+
+%timeit for x in xrange(size): x ** 2
+# out: 10 loops, best of 3: 88.9 ms per loop
+
+# avoid this
+%timeit for x in np.arange(size): x ** 2
+#out: 1 loops, best of 3: 1.16 s per loop
+
+# use this
+%timeit np.arange(size) ** 2
+#out: 100 loops, best of 3: 19.5 ms per loop
