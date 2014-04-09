480a42ca426636393112c6228a5ea0121427cf34
David Geo Holmes <geometryzen@gmail.com>
2014-04-09 14:17:45 -0400

    Save file.

--- Mathematics/NumPy/slicing.py
+++ Mathematics/NumPy/slicing.py
@@ -10,7 +10,6 @@ print word[0:2]
 print word[2:4]
 print word[:2] # omitting first index defaults to zero
 print word[2:] # omitting second default to len(word)
-print word[2:len(word)]
 print word[:]
 print 'x' + word[1:]
 print word[1:100]
