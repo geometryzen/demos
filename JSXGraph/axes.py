7f11f640a63386ad089d485d839f3b45c1545d9e
David Geo Holmes <geometryzen@gmail.com>
2014-04-14 10:23:42 -0400

    Save file.

--- JSXGraph/axes.py
+++ JSXGraph/axes.py
@@ -12,7 +12,13 @@ board = graph.initBoard("box",{"grid":True,"boundingbox":[-10,10,10,-10]})
 
 board.create(
              'axis',
-             [[0.0,1.0], [1.0,1.3]],
-             {"withLabel": True,
-              "name": "x-axis",
-              "label": {"offset": [280,130]}})
+             [
+                 [0.0,1.0],
+                 [1.0,1.3]
+             ],
+             {
+                 "withLabel": True,
+                 "name": "x-axis",
+                 "label": 
+                 {
+                     "offset": [280,130]}})
