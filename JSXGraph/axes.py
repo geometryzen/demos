203d917f348fca00d854f2bf0eadf18b9eec1709
David Geo Holmes <geometryzen@gmail.com>
2014-04-14 10:03:39 -0400

    Save file.

--- JSXGraph/axes.py
+++ JSXGraph/axes.py
@@ -1,1 +1,12 @@
-# axes.py
+from browser import document, window
+from math import *
+
+graph = window.JXG.JSXGraph
+
+document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
+div = document.getElementById("box")
+
+div.style.width  = "400px"
+div.style.height = "400px"
+
+board = graph.initBoard("box", {"axis":True,"grid":True})
