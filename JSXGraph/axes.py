ce6ffc36f138c1adb9fb8e36d12d9aed81aa9f20
David Geo Holmes <geometryzen@gmail.com>
2014-04-14 10:03:48 -0400

    Save file.

--- JSXGraph/axes.py
+++ JSXGraph/axes.py
@@ -6,7 +6,7 @@ graph = window.JXG.JSXGraph
 document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
 div = document.getElementById("box")
 
-div.style.width  = "400px"
-div.style.height = "400px"
+div.style.width  = "600px"
+div.style.height = "600px"
 
 board = graph.initBoard("box", {"axis":True,"grid":True})
