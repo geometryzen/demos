d10923fd208cdbc674f3928125acc774b5d0f6f0
David Geo Holmes <geometryzen@gmail.com>
2014-04-14 10:11:38 -0400

    Save file.

--- JSXGraph/axes.py
+++ JSXGraph/axes.py
@@ -9,6 +9,6 @@ div = document.getElementById("box")
 div.style.width  = "600px"
 div.style.height = "600px"
 
-board = graph.initBoard("box",{"grid":False})
+board = graph.initBoard("box",{"grid":True})
 
 board.create('axis', [[0.0,1.0], [1.0,1.3]], {"withLabel": True, "name": "abc", "label": {"offset": [10,20]}});
