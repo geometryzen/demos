886d8fd46f6914a36d6a3453f157de39c1a56c21
David Geo Holmes <geometryzen@gmail.com>
2014-04-14 10:22:53 -0400

    Save file.

--- JSXGraph/axes.py
+++ JSXGraph/axes.py
@@ -10,6 +10,9 @@ div.style.height = "600px"
 
 board = graph.initBoard("box",{"grid":True,"boundingbox":[-10,10,10,-10]})
 
-board.create('axis', [[0.0,1.0], [1.0,1.3]], {"withLabel": True, "name": "x-axis", "label": {"offset": [280,130]}});
-
-t1 = board.create('text',[0,1,"\theta"],{"useFlop":True});
+board.create(
+             'axis',
+             [[0.0,1.0], [1.0,1.3]],
+             {"withLabel": True,
+              "name": "x-axis",
+              "label": {"offset": [280,130]}})
