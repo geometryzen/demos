02dd42d8b291c5211b23d2eb3790fa72510e0f4d
David Geo Holmes <geometryzen@gmail.com>
2014-04-14 10:19:50 -0400

    Save file.

--- JSXGraph/axes.py
+++ JSXGraph/axes.py
@@ -11,3 +11,5 @@ div.style.height = "600px"
 board = graph.initBoard("box",{"grid":True,"boundingbox":[-10,10,10,-10]})
 
 board.create('axis', [[0.0,1.0], [1.0,1.3]], {"withLabel": True, "name": "x-axis", "label": {"offset": [280,130]}});
+
+t1 = board.create('text',[0,1,"Hello World"]);
