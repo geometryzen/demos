c20b3ff5d16a36f1b74b433b93a449838270e3d7
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 11:12:43 -0400

    Save file.

--- Physics/thermal-equilibrium-of-blocks-in-contact.py
+++ Physics/thermal-equilibrium-of-blocks-in-contact.py
@@ -1,3 +1,4 @@
+from math import factorial
 # Total quanta
 Q = 4
 N1 = 3
@@ -5,5 +6,5 @@ N2 = 3
 
 for q1 in range(Q):
     q2 = Q - q1
-    w1 = (q1 + N1 -1)! / q1!(N1-1)!
+    w1 = factorial(q1 + N1 -1) / factorial(q1) * factorial(N1-1)
     print q1, q2
