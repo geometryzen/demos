649f2312cb89c8d3008b74c36b7161da93db783a
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 13:36:50 -0400

    Save file.

--- Physics/thermal-equilibrium-of-blocks-in-contact.py
+++ Physics/thermal-equilibrium-of-blocks-in-contact.py
@@ -4,6 +4,9 @@ Q = 10  # Total number of quanta shared between the blocks
 N1 = 30 # number of oscillators in first block
 N2 = 20 # number of oscillators in second block
 
+'''
+Note. To make this work for large numbers requires Strirling's approximation. We can use the approximation obtained from considering the Trapezoid rule.
+'''
 def factorial(n):
     return exp(n * log(n) - n + 1)
 
@@ -12,7 +15,3 @@ for q1 in range(Q+1):
     w1 = factorial(q1 + N1 - 1) / factorial(q1) * factorial(N1-1)
     w2 = factorial(q2 + N2 - 1) / factorial(q2) * factorial(N2-1)
     print q1, q2, w1, w2, w1 * w2
-
-'''
-Note. To make this work for large numbers would require Strirling's approximation. We can use the approximation obtained from considering the Trapezoid rule.
-'''
