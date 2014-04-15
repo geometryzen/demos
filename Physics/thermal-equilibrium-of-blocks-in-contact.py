ba060f324ed4a5e2127d70f8ed7025195e0ec5f4
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 11:23:12 -0400

    Save file.

--- Physics/thermal-equilibrium-of-blocks-in-contact.py
+++ Physics/thermal-equilibrium-of-blocks-in-contact.py
@@ -4,7 +4,7 @@ Q = 10  # Total number of quanta shared between the blocks
 N1 = 6 # number of oscillators in first block
 N2 = 4 # number of oscillators in second block
 
-for q1 in range(Q):
+for q1 in range(Q+1):
     q2 = Q - q1
     w1 = factorial(q1 + N1 - 1) / factorial(q1) * factorial(N1-1)
     w2 = factorial(q2 + N2 - 1) / factorial(q2) * factorial(N2-1)
