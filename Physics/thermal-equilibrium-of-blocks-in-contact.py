52f9deb2fb052d1d9bac61e35fa904c8366d46b1
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 11:13:25 -0400

    Save file.

--- Physics/thermal-equilibrium-of-blocks-in-contact.py
+++ Physics/thermal-equilibrium-of-blocks-in-contact.py
@@ -6,5 +6,6 @@ N2 = 3
 
 for q1 in range(Q):
     q2 = Q - q1
-    w1 = factorial(q1 + N1 -1) / factorial(q1) * factorial(N1-1)
-    print q1, q2, w1
+    w1 = factorial(q1 + N1 - 1) / factorial(q1) * factorial(N1-1)
+    w2 = factorial(q2 + N2 - 1) / factorial(q2) * factorial(N2-1)
+    print q1, q2, w1, w2
