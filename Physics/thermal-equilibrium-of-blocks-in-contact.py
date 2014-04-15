da4e4d14e2b0ef215c3333c79786583795f87c3b
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 12:49:11 -0400

    Save file.

--- Physics/thermal-equilibrium-of-blocks-in-contact.py
+++ Physics/thermal-equilibrium-of-blocks-in-contact.py
@@ -5,7 +5,7 @@ N1 = 6 # number of oscillators in first block
 N2 = 4 # number of oscillators in second block
 
 def factorial(n):
-    return 
+    return n
 
 for q1 in range(Q+1):
     q2 = Q - q1
