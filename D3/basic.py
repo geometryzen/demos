f4e8ec06c7176981995f4fbf2331f10b2a5ebd89
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 21:25:57 -0400

    Save file.

--- D3/basic.py
+++ D3/basic.py
@@ -10,6 +10,8 @@ i = 0
 svg = d3.select("body").append("svg").attr("width", width).attr("height", height)
 
 def particle():
+    print "Hello"
+'''
     global i
     i = (i + 1) % 360
     print i
@@ -25,5 +27,6 @@ def particle():
     circle.transition().duration(2000).ease(window.Math.sqrt).attr("r", 100).style("stroke-opacity", 1e-6).remove()
 
     d3.event.preventDefault()
+'''
     
 svg.append("rect").attr("width", width).attr("height", height).on("mousemove", particle)
