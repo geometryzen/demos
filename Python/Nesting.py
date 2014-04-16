6c359e88274a528950f7d12195a50a93b11a82b0
David Geo Holmes <geometryzen@gmail.com>
2014-04-16 16:53:38 -0400

    Save file.

--- Python/Nesting.py
+++ Python/Nesting.py
@@ -1,1 +1,12 @@
-# Nesting.py
+def outer(x):
+    def middle(y):
+        # a = x
+        def inner(z):
+            print "z", str(z)
+            print "y", str(y)
+            print "x", str(x)
+            return x
+        return inner
+    return middle
+
+print outer(10)(20)(30)
