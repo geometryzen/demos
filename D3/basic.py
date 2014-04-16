eec509056d590cf4d882adb095f5bc73a47aef72
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 22:49:13 -0400

    Save file.

--- D3/basic.py
+++ D3/basic.py
@@ -1,4 +1,5 @@
-from browser import window
+from browser import *
+from workbench import *
 import math
 
 d3 = window.d3
@@ -45,3 +46,46 @@ def particle():
 #rect.attr("width", width).attr("height", height)#.on("mousemove", particle)
 
 particle()
+
+canvas = document.createElement("canvas")
+
+workbench = Workbench2D(canvas)
+
+context = canvas.getContext("2d")
+
+progressEnd = 3
+        
+step = 0
+steps = 50
+addAngle = 2 * pi / steps
+addScale = 1.0 / steps
+
+def setUp():
+    workbench.setUp()
+
+def tick(t):
+    global step
+    if step < steps:
+        step += 1
+    # Changing the canvas width or height resets the canvas.
+    context.fillStyle = "blue"
+    context.font = "24pt Helvetica"
+    context.textAlign = "center"
+    context.textBaseline = "middle"
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.save()
+    context.translate(canvas.width / 2, canvas.height / 2)
+    context.scale(addScale * step, addScale * step)
+    context.rotate(addAngle * step)
+    context.fillText("Geometry Zen", 0, 0)
+    context.restore()
+
+def terminate(t):
+    return t > progressEnd
+    
+def tearDown():
+    workbench.tearDown()
+
+war = WindowAnimationRunner(tick, terminate, setUp, tearDown)
+war.start()
+
