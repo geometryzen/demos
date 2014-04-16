fa5cf974371875d9d6da97c1289553e674bc665d
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 21:23:16 -0400

    Save file.

--- D3/basic.py
+++ D3/basic.py
@@ -19,7 +19,7 @@ def particle():
     circle.attr("cx", m[0])
     circle.attr("cy", m[1])
     circle.attr("r", 1e-6)
-#   circle.style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
+    circle.style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
     circle.style("stroke-opacity", 1)
     circle.transition().duration(2000).ease(window.Math.sqrt).attr("r", 100).style("stroke-opacity", 1e-6).remove()
 
