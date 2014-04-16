2cf932e60cc6ad6b0e2048b6f7c9d5e4238eb274
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 23:56:06 -0400

    Save file.

--- D3/basic.py
+++ D3/basic.py
@@ -31,9 +31,7 @@ workbench = Workbench2D(canvas.node())
 
 context = canvas.node().getContext("2d")
 context.globalCompositionOperation = "lighter"
-context.lineWidth = 6
-print context.lineWidth
-print context.globalCompositionOperation
+context.lineWidth = 2
 
 def circle(t):
     s = c + str(1-t) + ")"
