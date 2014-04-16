2cea4fe0ebc2470ea144a308e4250860b5955994
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 21:13:23 -0400

    Save file.

--- D3/basic.py
+++ D3/basic.py
@@ -10,7 +10,7 @@ svg = d3.select("body").append("svg").attr("width", width).attr("height", height
 svg.append("rect")
     .attr("width", width)
     .attr("height", height)
-    .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
+    .on("ontouchstart" in document ? "touchmove" : "mousemove", particle)
 
 def particle():
     m = d3.mouse(this);
