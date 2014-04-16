8d9f37b80338953c4151554742c8ca1e202df099
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 21:13:19 -0400

    Save file.

--- D3/basic.py
+++ D3/basic.py
@@ -5,4 +5,27 @@ d3 = window.d3
 width = max(960, window.innerWidth)
 height = max(500, window.innerHeight)
 
-svg = d3.select("body").append("svg").attr("width", width).attr("height",height)
+svg = d3.select("body").append("svg").attr("width", width).attr("height", height)
+
+svg.append("rect")
+    .attr("width", width)
+    .attr("height", height)
+    .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
+
+def particle():
+    m = d3.mouse(this);
+
+    svg.insert("circle", "rect")
+        .attr("cx", m[0])
+        .attr("cy", m[1])
+        .attr("r", 1e-6)
+        .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
+        .style("stroke-opacity", 1)
+        .transition()
+        .duration(2000)
+        .ease(Math.sqrt)
+        .attr("r", 100)
+        .style("stroke-opacity", 1e-6)
+        .remove()
+
+    d3.event.preventDefault()
