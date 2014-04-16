c7380fe7ec09dda64d050db3ad356fc7f596ede0
David Geo Holmes <geometryzen@gmail.com>
2014-04-16 19:56:10 -0400

    Save file.

--- Python/Decorators.py
+++ Python/Decorators.py
@@ -1,1 +1,8 @@
-# Decorators.py
+def make_log(level):
+    def _(message):
+        print("{}: {}".format(level, message))
+    return _
+
+log_info = make_log("info")
+log_warning = make_log("warning")
+log_error = make_log("error")
