c4f196df9804c7a054096586e3a77a0dcb8e6e28
David Geo Holmes <geometryzen@gmail.com>
2014-04-15 21:19:21 -0400

    Save file.

--- D3/basic.py
+++ D3/basic.py
@@ -14,6 +14,8 @@ svg.append("rect").attr("width", width).attr("height", height).on("mousemove", p
 def particle():
     m = d3.mouse(this)
 
-    svg.insert("circle", "rect").attr("cx", m[0]).attr("cy", m[1]).attr("r", 1e-6).style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5)).style("stroke-opacity", 1).transition().duration(2000).ease(window.Math.sqrt).attr("r", 100).style("stroke-opacity", 1e-6).remove()
+    circle = svg.insert("circle", "rect")
+    print circle
+    .attr("cx", m[0]).attr("cy", m[1]).attr("r", 1e-6).style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5)).style("stroke-opacity", 1).transition().duration(2000).ease(window.Math.sqrt).attr("r", 100).style("stroke-opacity", 1e-6).remove()
 
     d3.event.preventDefault()
