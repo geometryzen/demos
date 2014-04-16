7cc33f5969bc1c996b38fd89b692d062ca55b929
David Geo Holmes <geometryzen@gmail.com>
2014-04-16 09:35:30 -0400

    Save file.

--- D3/basic.py
+++ D3/basic.py
@@ -55,7 +55,7 @@ def tick(t):
         z = x * y
         # print "c in tweeny: " + c
         def circle(t):
-            print "circle t: %d, c: %s" % (t, c)
+            print "circle t: %d, c: %s, x: %d" % (t, c, x)
             s = c + str(1-t) + ")"
             context.strokeStyle = s
             context.beginPath()
