855be6c619be0f9b0d93c1fdeff0181498f43f52
David Geo Holmes <geometryzen@gmail.com>
2014-04-18 20:59:28 -0400

    Save file.

--- Modeling/ode/trapezoid-euler.py
+++ Modeling/ode/trapezoid-euler.py
@@ -20,7 +20,7 @@ def SHO(time, state):
 def trapezoidEuler(y, x, h, f):
     print f(x,y)
     print h
-    print f(x,y) * h
+    print h * f(x,y)
     yBar = h * f(x,y)
     print yBar
     print type(yBar)
