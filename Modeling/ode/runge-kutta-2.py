7197a479775f048d4da9b722f2e3b00c34df56a1
David Geo Holmes <geometryzen@gmail.com>
2014-04-20 17:31:25 -0400

    Save file.

--- Modeling/ode/runge-kutta-2.py
+++ Modeling/ode/runge-kutta-2.py
@@ -17,7 +17,7 @@ def SHO(time, state):
     acceleration = - k/m * position + g - d * velocity
     return numpy.array([velocity, acceleration])
 
-alpha = 1.0
+alpha = 0.8
 b = 1.0 / (2.0 * alpha)
 a = 1.0 - b
 beta = 1 / (2.0 * b)
