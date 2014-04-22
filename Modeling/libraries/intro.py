8de17a6a2f26ef47cb570de3d54e68cdd647c1e1
David Geo Holmes <geometryzen@gmail.com>
2014-04-22 15:15:08 -0400

    Save file.

--- Modeling/libraries/intro.py
+++ Modeling/libraries/intro.py
@@ -2,29 +2,3 @@ import numpy as np
 
 x = np.array([1,2,3,4,5,6],float)
 y = x - 3
-
-print x
-print
-print x + 3
-print 3 + x
-print
-print x - 3
-print 3 - x
-print
-print x * 3
-print 3 * x
-print
-print x / 3
-print 3 / x
-print
-print x % 3
-print 3 % x
-print
-print x ^ 3
-print y ^ x
-print
-print x << 1
-print y << x
-print
-print x >> 1
-print y >> x
