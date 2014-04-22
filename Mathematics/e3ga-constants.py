6ebac6aa7dbfa80fd9d69b0ed412ad124e1ae88d
David Geo Holmes <geometryzen@gmail.com>
2014-04-22 17:32:08 -0400

    Save file.

--- Mathematics/e3ga-constants.py
+++ Mathematics/e3ga-constants.py
@@ -3,6 +3,11 @@ The e3ga module pre-defines the standard basis vectors e1, e2 and e3, as well as
 '''
 from e3ga import *
 
+e1 = VectorE3(1,0,0)
+e2 = VectorE3(0,1,0)
+e3 = VectorE3(0,0,1)
+I = e1 * e2 * e3
+
 print e1
 print e2
 print e3
