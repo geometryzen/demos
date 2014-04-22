355f670477102c27f3c99a14a9a0b32cb727c5db
David Geo Holmes <geometryzen@gmail.com>
2014-04-22 11:28:09 -0400

    Save file.

--- Mathematics/NumPy/repr.py
+++ Mathematics/NumPy/repr.py
@@ -5,6 +5,6 @@ print np.array([[],[]])
 print np.array([[],[],[]])
 
 print np.array([1])
-print np.array([1,2])
-print np.array([1,2,3])
+print repr(np.array([1,2]))
+print repr(np.array([1,2,3]))
 print repr(np.array([[1,2,3],[4,5,6]]))
