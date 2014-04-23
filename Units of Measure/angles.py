3c28daa034393fdf293f69f039cf523a27ed4ea1
David Geo Holmes <geometryzen@gmail.com>
2014-04-23 15:49:10 -0400

    Save file.

--- Units of Measure/angles.py
+++ Units of Measure/angles.py
@@ -1,17 +1,4 @@
 from e3ga import *
 from units import *
+from math import pi
 
-print "newton (N)"
-print newton.scale
-print newton.name
-print newton.dimensions
-print newton.labels
-print newton
-
-print
-print repr(radian)
-print radian.uom.scale
-print radian.uom.dimensions
-print radian.uom.labels
-print radian.uom.name
-print radian
