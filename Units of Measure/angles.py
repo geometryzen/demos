3c98385fd91d4c9b82cad2942047ea4a8685b79d
David Geo Holmes <geometryzen@gmail.com>
2014-04-23 16:14:25 -0400

    Save file.

--- Units of Measure/angles.py
+++ Units of Measure/angles.py
@@ -2,14 +2,13 @@ from e3ga import *
 from units import *
 from math import pi, cos
 
+angle0 = 1 * meter
 angle1 = pi * radian
 angle2 = 180 * degree
 
 print angle1
 print angle2
 
-print repr(angle1)
-print type(angle1.quantity)
 print "cos(angle1)", cos(angle1)
 print "cos(angle2)", cos(angle2)
 
