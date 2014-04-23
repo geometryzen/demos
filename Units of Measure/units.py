2ee47771901ca535165e5a83e8c5ba0ffff566bc
David Geo Holmes <geometryzen@gmail.com>
2014-04-23 19:04:12 -0400

    Save file.

--- Units of Measure/units.py
+++ Units of Measure/units.py
@@ -13,11 +13,8 @@ angle = omega * t
 
 print angle
 
-#print type(angle)
-#print repr(angle)
 print "cos(%s) -> %s" % (angle, cos(angle))
 print "sin(%s) -> %s" % (angle, sin(angle))
+print "tan(%s) -> %s" % (angle, tan(angle))
 
-#s = sin(angle)
-#print s
 
