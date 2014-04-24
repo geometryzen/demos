cbc0a6590940a5ae158882908c94026074b323f3
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 16:00:45 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -4,7 +4,7 @@ from browser import window
 from math import *
 
 # Spring Constant.
-k = 1.0 * newton / meter
+k = 2.0 * newton / meter
 x0 = 0.25 * meter
 
 # Compute the energy for a given extension.
@@ -26,7 +26,6 @@ dataY = map(lambda x: x.quantity, energies)
 
 JXG = window.JXG
 graph = JXG.JSXGraph
-
 
 def boundingBox():
     rangeX = abs(max(dataX)-min(dataX))
