dc0daa90a223f20b30dd0e1045de375a3a7dc4c3
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 10:42:23 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -22,5 +22,5 @@ p = b.create('point',[1,4])
 dataX = [1,2,3,4,5,6,7,8]
 dataY = [0.3,4.0,-1,2.3,7,9,8,9]
 
-b.create('curve',[dataX,dataY],{"strokeColor":'red'})
+b.create('curve',[dataX,dataY],{"strokeColor":'black'})
 b.create('curve',[dataX,lambda x: p.X()*sin(x)*x],{"strokeColor":'blue',"dash":1})
