5c87ad8937832a394217df38949034995ae2ca93
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 12:56:36 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -8,12 +8,6 @@ from math import *
 
 graph = window.JXG.JSXGraph
 
-document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
-div = document.getElementById("box")
-
-div.style.width  = "400px"
-div.style.height = "400px"
-
 b = graph.initBoard("box", {"boundingbox":[-10,10,10,-10],"axis":True,"showCopyright":False})
 
 dataX = [1,2,3,4,5,6,7,8]
