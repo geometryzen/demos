096902dd8d80c73e3386d3b193a5dda878d9b3da
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 17:27:07 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -51,6 +51,6 @@ b = graph.initBoard("box",
                      })
 
 b.create('curve',[dataX,dataY],{"strokeColor":'blue'})
-xLabel = b.create('text',[0.0, box[3], 'Extension / %s' % meter.uom], {"fontSize":15,"anchorX":'middle'})
+xLabel = b.create('text',[0.0, (minY+box[3])/2, 'Extension / %s' % meter.uom], {"fontSize":15,"anchorX":'middle'})
 yLabel = b.create('text',[minX, (maxY+minY)/2, 'Energy / %s' % f(meter).uom], {"fontSize":15, "display":'internal', "rotate":90,"anchorX":'middle'})
 title = b.create('text',[0.0, maxY, 'Energy versus Extension for a Spring'], {"fontSize":15,"anchorX":'middle'})
