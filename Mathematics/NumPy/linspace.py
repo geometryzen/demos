92a4ea65dff94540bfe1b6272a047fc11428cdbc
David Geo Holmes <geometryzen@gmail.com>
2014-04-23 21:23:29 -0400

    Save file.

--- Mathematics/NumPy/linspace.py
+++ Mathematics/NumPy/linspace.py
@@ -1,3 +1,4 @@
+from e3ga import *
 from units import *
 import numpy as np
 
@@ -40,6 +41,17 @@ print x[0][1]
 print x[0][2]
 
 x = np.linspace(2.0 * second, 3.0 * second, 5, False, True)
+print
+print x
+print x[0]
+print x[1]
+print x[0][0]
+print x[0][1]
+print x[0][2]
+print x[0][3]
+print x[0][4]
+
+x = np.linspace(VectorE3(1,0,0), VectorE3(0,1,0), 5, False, True)
 print
 print x
 print x[0]
