8f1f1aaeaee463ff4f8ce2da3757eea54a212c42
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 11:06:40 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -19,6 +19,6 @@ div.style.height = "400px"
 b = graph.initBoard("box", {"boundingbox":[-10,10,20,-10],"axis":True})
 
 dataX = [1,2,3,4,5,6,7,8]
-dataY = map()
+dataY = map(y)
 
 b.create('curve',[dataX,dataY],{"strokeColor":'black'})
