78d2712139cccaf2533733e608998ff385f09205
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 12:59:39 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -5,12 +5,11 @@ from math import *
 def f(x):
     return x
 
-
 graph = window.JXG.JSXGraph
 
 b = graph.initBoard("box", {"boundingbox":[-10,10,10,-10],"axis":True,"showCopyright":False})
 
-dataX = [1,2,3,4,5,6,7,8,9]
+dataX = range(10)
 dataY = map(f, dataX)
 
 b.create('curve',[dataX,dataY],{"strokeColor":'blue'})
