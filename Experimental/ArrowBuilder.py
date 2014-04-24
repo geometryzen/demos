6af4ea68a80e930f720f6ee687935d5cfc9ce1ee
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 00:05:19 -0400

    Save file.

--- Experimental/ArrowBuilder.py
+++ Experimental/ArrowBuilder.py
@@ -8,7 +8,7 @@ space = CartesianSpace()
 e3 = VectorE3(0,0,1)
 
 rotor = exp(-BivectorE3(0.0, 1.0, 0.0)*pi/4.0)
-shape = ArrowBuilder().scale(2).wireframe(False).color(0x0000FF).axis(e3).segments(6).build()
+shape = ArrowBuilder().scale(2).wireframe(False).color(0x0000FF).axis(e3).segments(12).build()
 space.add(shape)
 
 workbench = Workbench(space.renderer, space.camera)
