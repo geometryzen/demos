3a5263494b1a214e7d95e377ecd9a7d4eb07f6b9
David Geo Holmes <geometryzen@gmail.com>
2014-04-23 21:50:04 -0400

    Save file.

--- Modeling/ode/runge-kutta-2-with-units.py
+++ Modeling/ode/runge-kutta-2-with-units.py
@@ -30,7 +30,7 @@ def rungeKutta2(y, x, h, f):
 N = 10
 
 # Keep track of displacement and velocity.
-y = numpy.zeros((N,2)) * meter
+y = numpy.zeros((N,2)) * 5
 print y
 
 # Initial position goes in slot 0. We start at the origin.
