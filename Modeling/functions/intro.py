38a4d50bc1bb22220ec9100d5221712dab85625e
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 14:45:34 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -13,9 +13,7 @@ graph = window.JXG.JSXGraph
 b = graph.initBoard("box", {"boundingbox":[-1,1,1,-1],"axis":True,"showCopyright":False})
 
 domainX = np.linspace(-1.0 * meter, +1.0 * meter, 10)
-print type(domainX)
 rangeY = map(f, domainX)
-print type(rangeY)
 
 dataX = map(lambda x: x.quantity, domainX)
 dataY = map(lambda x: x.quantity, rangeY)
