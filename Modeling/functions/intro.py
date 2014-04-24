e03ef628e51443b8efc652225a0b7b185a3ec9af
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 17:28:33 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -4,7 +4,7 @@ from browser import window
 from math import *
 
 # Spring Constant.
-k = 2.0 * newton / meter
+k = 4.0 * newton / meter
 x0 = 0.0 * meter
 
 # Compute the energy for a given extension.
