4d679f4ded17a740c0fcc2e15f0521073ce68a30
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 12:57:06 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -10,7 +10,7 @@ graph = window.JXG.JSXGraph
 
 b = graph.initBoard("box", {"boundingbox":[-10,10,10,-10],"axis":True,"showCopyright":False})
 
-dataX = [1,2,3,4,5,6,7,8]
+dataX = [1,2,3,4,5,6,7,8,9]
 dataY = map(f, dataX)
 
 b.create('curve',[dataX,dataY],{"strokeColor":'blue'})
