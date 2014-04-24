69a777e47631da82b2c34354216120f25ac8ece0
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 15:33:51 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -34,5 +34,5 @@ b = graph.initBoard("box",
                      })
 
 b.create('curve',[dataX,dataY],{"strokeColor":'blue'})
-xLabel = b.create('text',[0,1, 'Extension/%s' % "m"], {})
+xLabel = b.create('text',[0,1, 'Extension/%s' % meter], {})
 yLabel = b.create('text',[0.5,0, 'Energy'], {})
