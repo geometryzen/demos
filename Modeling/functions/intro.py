2769dcc66cb066e164dbdd50a0b02ccfce513593
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 14:45:10 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -3,20 +3,10 @@ import numpy as np
 from browser import window
 from math import *
 
-x = 3
-
-def fake(p1):
-    pass
-
 k = 1.0 * newton / meter
 
-def makeFunction(s):
-    def f(x):
-        return s * x * x
-    return f
-
-print makeFunction(k)(2)
-print makeFunction(k)(3)
+def f(x):
+    return s * x * x
 
 graph = window.JXG.JSXGraph
 
@@ -24,7 +14,7 @@ b = graph.initBoard("box", {"boundingbox":[-1,1,1,-1],"axis":True,"showCopyright
 
 domainX = np.linspace(-1.0 * meter, +1.0 * meter, 10)
 print type(domainX)
-rangeY = map(makeFunction(k), domainX)
+rangeY = map(f, domainX)
 print type(rangeY)
 
 dataX = map(lambda x: x.quantity, domainX)
