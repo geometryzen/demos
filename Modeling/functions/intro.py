3068ffaa4aaf0d5659f7b04df34ef744e6408c92
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 14:07:46 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -12,7 +12,7 @@ graph = window.JXG.JSXGraph
 
 b = graph.initBoard("box", {"boundingbox":[-1,1,1,-1],"axis":True,"showCopyright":False})
 
-domainX = np.linspace(-1.0 * meter, +1.0 * meter, 100)
+domainX = np.linspace(-1.0 * meter, +1.0 * meter, 10)
 
 rangeY = map(f, domainX)
 
