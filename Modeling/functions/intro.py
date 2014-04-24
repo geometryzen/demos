c794ea952bf7b1301453b658ec13b984de7cbd0a
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 15:46:34 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -36,7 +36,7 @@ b = graph.initBoard("box",
                      })
 
 b.create('curve',[dataX,dataY],{"strokeColor":'blue'})
-xLabel = b.create('text',[0.7,0.05, 'Extension / %s' % meter.uom], {})
+xLabel = b.create('text',[0.7,0.05, 'Extension / %s' % meter.uom], {"fontSize":20})
 yLabel = b.create('text',[0.0,0.0, 'Energy / %s' % f(meter).uom], {})
 
 tRot = b.create('transform', [0.0, 0.0,0.0], {"type":'rotate'}) 
