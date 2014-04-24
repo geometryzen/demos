16c45b41d75fe1710b65cb235ff32e11eb042e89
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 15:30:10 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -34,4 +34,4 @@ b = graph.initBoard("box",
                      })
 
 b.create('curve',[dataX,dataY],{"strokeColor":'blue'})
-txt = b.create('text',[-2,-1, 'Hello World'], {fontSize:30})
+txt = b.create('text',[-2,-1, 'Hello World'], {"fontSize":30})
