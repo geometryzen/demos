06fbb231ce8f411e7974fb3b8b7633c96d139b25
David Geo Holmes <geometryzen@gmail.com>
2014-04-24 13:26:59 -0400

    Save file.

--- Modeling/functions/intro.py
+++ Modeling/functions/intro.py
@@ -11,6 +11,7 @@ graph = window.JXG.JSXGraph
 b = graph.initBoard("box", {"boundingbox":[-10,10,10,-10],"axis":True,"showCopyright":False})
 
 domainX = linspace(0,5,10)
+print domainX
 dataX = map(lambda x: x, domainX)
 print repr(dataX)
 dataY = map(f, dataX)
