0cebc3076eb94f2590cd456f18c2eb729a920ce9
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 17:51:54 -0400

    Save file.

--- JSXGraph/popup.py
+++ JSXGraph/popup.py
@@ -20,10 +20,7 @@ B = board.create('point',[2,2],{"name":'Bob'})
 f = board.create('functiongraph',[lambda x: A.X() * sin(x)])
 
 def tick(time):
-    try:
-        A.moveTo([sin(time*pi*2/12),Math.cos(time*pi*2/12)],800)
-    except Exception as e:
-        print "Oops" + e
+    A.moveTo([sin(time*pi*2/12),cos(time*pi*2/12)],800)
 
 def terminate(time):
     return time > 10
