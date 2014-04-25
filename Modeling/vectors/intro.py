dc83fbfe5201e8a09c155989d1d46bab92c44877
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 09:36:26 -0400

    Save file.

--- Modeling/vectors/intro.py
+++ Modeling/vectors/intro.py
@@ -8,7 +8,7 @@ JXG = window.JXG
 graph = JXG.JSXGraph
 
 b = graph.initBoard("box", 
-                    {"boundingbox": [-1,1,1,-1],
+                    {"boundingbox": [-5,5,5,-5],
                      "axis":True,
                      "showCopyright":False,
                      "showNavigation":False
