fbb802b3ef9e1fc40e0a54b78dcb1ca8a9340311
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 15:17:35 -0400

    Save file.

--- Modeling/vectors/intro.py
+++ Modeling/vectors/intro.py
@@ -9,7 +9,7 @@ JXG = window.JXG
 JXG.Options.text.useMathJax = True
 graph = JXG.JSXGraph
 
-scale = 10
+scale = 12
 
 board = graph.initBoard("box", 
                     {"boundingbox": [-scale,scale,scale,-scale],
