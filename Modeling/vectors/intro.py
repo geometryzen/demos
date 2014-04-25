b092ac7ab3bb784cca8966cfa4568051431d85d3
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 13:19:52 -0400

    Save file.

--- Modeling/vectors/intro.py
+++ Modeling/vectors/intro.py
@@ -17,13 +17,13 @@ board = graph.initBoard("box",
 
 a = VectorE2(1,1)
 
+def toString(mv):
+    return "\\[ a_x=%0.3f, a_y=%0.3f \\]" % (mv.x, mv.y)
+
 tail = board.create('point',[0,0], {'name':lambda: toString(a),'size':5,'fillOpacity':0.3,'strokeOpacity':0.3})
 head = board.create('point',[a.x,a.y], {'size':5,'fillOpacity':0.3,'strokeOpacity':0.3})
 
 line = board.create('arrow',[tail,head],{'strokeWidth':5, 'strokeOpacity':0.7, 'strokeColor':'blue'});
-
-def toString(mv):
-    return "\\[ a_x=%0.3f, a_y=%0.3f \\]" % (mv.x, mv.y)
 
 txt = board.create('text',[-1.5,+1.5, lambda: toString(a)], {'fontSize':30})
 
