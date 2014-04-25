ce817ef248bf3462d13cb753147a175e819b07fc
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 17:06:09 -0400

    Save file.

--- Experimental/PopUpAnimation.py
+++ Experimental/PopUpAnimation.py
@@ -3,6 +3,18 @@ from math import *
 
 win = window.open("","","width=415,height=400")
 
-WindowAnimationRunner().start()
+def tick(time):
+    pass
+
+def terminate(time):
+    pass
+
+def setUp():
+    pass
+
+def tearDown():
+    pass
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
 
 win.close()
