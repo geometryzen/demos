f635d78f0584c1654493f90adda2d287b2bce525
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 13:21:34 -0400

    Save file.

--- Modeling/vectors/intro.py
+++ Modeling/vectors/intro.py
@@ -21,7 +21,7 @@ def toString(mv):
     return "\\[ a_x=%0.3f, a_y=%0.3f \\]" % (mv.x, mv.y)
 
 tail = board.create('point',[0,0], {'name':'','size':5,'fillOpacity':0.3,'strokeOpacity':0.3})
-head = board.create('point',[a.x,a.y], {'size':5,'fillOpacity':0.3,'strokeOpacity':0.3})
+head = board.create('point',[a.x,a.y], {'name':'','size':5,'fillOpacity':0.3,'strokeOpacity':0.3})
 
 line = board.create('arrow',[tail,head],{'strokeWidth':5, 'strokeOpacity':0.7, 'strokeColor':'blue'});
 
