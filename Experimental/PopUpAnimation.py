e3e9455c21a6adbc9f4521d0f3d58b34c506fb24
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 17:30:29 -0400

    Save file.

--- Experimental/PopUpAnimation.py
+++ Experimental/PopUpAnimation.py
@@ -1,3 +1,7 @@
+'''
+Demonstration of opening a window and running it as an animation.
+The animation is suspended when the window is not visible.
+'''
 import browser
 
 win = browser.window.open("","","width=415,height=400")
@@ -16,6 +20,7 @@ def setUp():
 
 def tearDown():
     win.close()
+    print "The window was closed."
 
 # Construct 
 browser.WindowAnimationRunner(tick, terminate, setUp, tearDown, win).start()
