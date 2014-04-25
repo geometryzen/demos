9f07ae60fd9e9cf61705757ca9e21686af7b4330
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 14:55:15 -0400

    Save file.

--- Modeling/vectors/intro.py
+++ Modeling/vectors/intro.py
@@ -36,7 +36,7 @@ def Arrow(name, vector, x, y, color):
                 'snapToGrid':True}
     tail = board.create('point',[random()*50, random()*50], pointDef)
     head = board.create('point',[tail.X() + vector.x, tail.Y() + vector.y], pointDef)
-    txt = board.create('text',[x, y, lambda: toString(vector)], {'fontSize':20, 'strokeColor':color,'fixed':True})
+    txt = board.create('text',[x, y, lambda: toString(name, vector)], {'fontSize':20, 'strokeColor':color,'fixed':True})
     return board.create('arrow',[tail,head],{'strokeWidth':5, 'strokeOpacity':0.7, 'strokeColor':color});
 
 def updateVector(vector, arrow):
