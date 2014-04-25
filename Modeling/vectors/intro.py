07a8750b010dd1a7900544f5dbc24a0bb8fb25ab
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 09:50:51 -0400

    Save file.

--- Modeling/vectors/intro.py
+++ Modeling/vectors/intro.py
@@ -16,15 +16,8 @@ b = graph.initBoard("box",
 
 e1 = VectorE2(1,0)
 
-print e1
-print e1.x
-print e1.y
-print repr(e1)
-print e1.w
-print e1.xy
-
 tail = b.create('point',[0,0], {'name':'A'})
-head = b.create('point',[1,0], {'name':'B'})
+head = b.create('point',[e1.x,e1.y], {'name':'B'})
 
 line = b.create('line',[tail,head], 
  {'straightFirst':False, 'straightLast':False,'lastArrow':True})
