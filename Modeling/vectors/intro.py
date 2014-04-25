f4c56f972a027f401ecd8f82a22a7d00f6c24ce5
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 14:22:22 -0400

    Save file.

--- Modeling/vectors/intro.py
+++ Modeling/vectors/intro.py
@@ -21,11 +21,11 @@ b = VectorE2(0,1)
 def toString(mv):
     return "\\[ a_x=%0.3f, a_y=%0.3f \\]" % (mv.x, mv.y)
 
-def Arrow(strokeColor):
+def Arrow(color):
     pointDef = {'name':'','size':5,'fillOpacity':0.3,'strokeOpacity':0.3,'strokeColor':'gray','fillColor':'gray'}
     tail = board.create('point',[0,0], pointDef)
     head = board.create('point',[a.x,a.y], pointDef)
-    return board.create('arrow',[tail,head],{'strokeWidth':5, 'strokeOpacity':0.7, 'strokeColor':strokeColor});
+    return board.create('arrow',[tail,head],{'strokeWidth':5, 'strokeOpacity':0.7, 'strokeColor':color});
 
 txt = board.create('text',[-1.5,+1.5, lambda: toString(a)], {'fontSize':30, 'strokeColor':'red'})
 
