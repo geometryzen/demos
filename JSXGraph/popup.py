173d72c7fa09cfe642462787f7876fd7d3d1177f
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 17:41:39 -0400

    Save file.

--- JSXGraph/popup.py
+++ JSXGraph/popup.py
@@ -1,27 +1,35 @@
-from browser import window
+from browser import *
 from math import *
-
-print "Starting..."
 
 graph = window.JXG.JSXGraph
 
-plotWin = window.open("","JSXGraph-Window","width=415,height=400")
-plotDoc = plotWin.document
+win = window.open("","","width=600,height=600")
 
-plotDoc.body.innerHTML = '<div id="box" class="jxgbox"></div>'
+win.document.body.innerHTML = '<div id="box" class="jxgbox"></div>'
 
-div = plotDoc.getElementById("box")
+div = win.document.getElementById("box")
 
 div.style.width  = "400px"
 div.style.height = "400px"
 
-board = graph.initBoard("box", {"document":plotDoc,"axis":True,"grid":True})
-
-print "Got board!"
+board = graph.initBoard("box", {"document":win.document,"axis":True,"grid":True})
 
 A = board.create('point',[1,1],{"name": 'Alice'})
 B = board.create('point',[2,2],{"name":'Bob'})
 
 f = board.create('functiongraph',[lambda x: A.X() * sin(x)])
 
-print "Hmmm."
+def tick(time):
+    pass
+
+def terminate(time):
+    return time > 10
+
+def setUp():
+    pass
+
+def tearDown():
+    win.close()
+    print "The window was closed."
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown, win).start()
