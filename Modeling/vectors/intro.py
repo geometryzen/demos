f9b216c763c408ef483d1e1fb7a0fcc18db49634
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 14:55:40 -0400

    Save file.

--- Modeling/vectors/intro.py
+++ Modeling/vectors/intro.py
@@ -24,7 +24,7 @@ B = None
 C = None
 
 def toString(name, mv):
-    return "\\[ a_x=%0.3f, a_y=%0.3f \\]" % (mv.x, mv.y)
+    return "\\[ %s_x=%0.3f, a_y=%0.3f \\]" % (name, mv.x, mv.y)
 
 def Arrow(name, vector, x, y, color):
     pointDef = {'name':'',
