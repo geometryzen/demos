8a6c154b2a4bbf879e8c6b8fd8b2547e9d8677fe
David Geo Holmes <geometryzen@gmail.com>
2014-04-25 16:57:48 -0400

    Save file.

--- Experimental/PopUpAnimation.py
+++ Experimental/PopUpAnimation.py
@@ -1,1 +1,22 @@
-# PopUpAnimation.py
+from browser import window
+from math import *
+
+graph = window.JXG.JSXGraph
+
+plotWin = window.open("","JSXGraph-Window","width=415,height=400")
+plotDoc = plotWin.document
+
+plotDoc.body.innerHTML = '<div id="box" class="jxgbox"></div>'
+
+div = plotDoc.getElementById("box")
+
+div.style.width  = "400px"
+div.style.height = "400px"
+
+board = graph.initBoard("box", {"document":plotDoc,"axis":True,"grid":True})
+
+
+A = board.create('point',[1,1],{"name": 'Alice'})
+B = board.create('point',[2,2],{"name":'Bob'})
+
+f = board.create('functiongraph',[lambda x: A.X() * sin(x)])
