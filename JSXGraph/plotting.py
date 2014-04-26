620edaae73957787072639f873ef0b5e36382a66
David Geo Holmes <geometryzen@gmail.com>
2014-04-26 00:45:23 -0400

    Save file.

--- JSXGraph/plotting.py
+++ JSXGraph/plotting.py
@@ -16,4 +16,4 @@ dataX = [1,2,3,4,5,6,7,8]
 dataY = [0.3,4.0,-1,2.3,7,9,8,9]
 
 b.create('curve',[dataX,dataY],{"strokeColor":'red'})
-b.create('curve',[dataX,lambda x: p.X()*sin(x)*x],{"strokeColor":'blue',"dash":1})
+b.create('curve',[dataX,lambda x,unused: p.X()*sin(x)*x],{"strokeColor":'blue',"dash":1})
