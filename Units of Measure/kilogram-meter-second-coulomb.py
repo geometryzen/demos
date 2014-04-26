d6153e3fc72c57463a0463ad753c07b317638848
David Geo Holmes <geometryzen@gmail.com>
2014-04-26 14:24:25 -0400

    Save file.

--- Units of Measure/kilogram-meter-second-coulomb.py
+++ Units of Measure/kilogram-meter-second-coulomb.py
@@ -6,6 +6,10 @@ print "m = meter (" +str(meter) + ")"
 
 print "s = second (" +str(second) + ")"
 
+print "A = ampere (" +str(coulomb/second) + ")"
+
+print "thermodynamic temperatue (" +str(kelvin) + ")"
+
 print "C = coulomb (" +str(coulomb) + ")"
 
 print "N = newton (" +str(kilogram * meter / (second ** 2)) + ")"
@@ -13,8 +17,6 @@ print "N = newton (" +str(kilogram * meter / (second ** 2)) + ")"
 print "J = joule (" +str(newton * meter) + ")"
 
 print "W = watt (" +str(joule/second) + ")"
-
-print "A = ampere (" +str(coulomb/second) + ")"
 
 print "V = volt (" +str(joule/coulomb) + ")"
 
