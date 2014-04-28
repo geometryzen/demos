79a7bb0eb9f442f5f66c7ccdbaec62e601d594e0
David Geo Holmes <geometryzen@gmail.com>
2014-04-28 10:00:57 -0400

    Save file.

--- Mathematics/Vectors/vectors-with-units.py
+++ Mathematics/Vectors/vectors-with-units.py
@@ -3,7 +3,7 @@ from units import *
 
 e1 = VectorE3(1,0,0)
 e2 = VectorE3(0,1,0)
-r = VectorE3(4 * meter,3,2)
+r = VectorE3(4,3,2) * meter
 
 # Using the % symbol means scalar product.
 print e1 % e2
