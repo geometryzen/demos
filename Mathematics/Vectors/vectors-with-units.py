b63c29a2eb0c985574f3529889e76cc7d30d655b
David Geo Holmes <geometryzen@gmail.com>
2014-04-28 09:49:16 -0400

    Save file.

--- Mathematics/Vectors/vectors-with-units.py
+++ Mathematics/Vectors/vectors-with-units.py
@@ -1,4 +1,5 @@
 from e3ga import *
+from units import *
 
 e1 = VectorE3(1,0,0)
 e2 = VectorE3(0,1,0)
@@ -7,3 +8,5 @@ e2 = VectorE3(0,1,0)
 print e1 % e2
 
 print e1 ^ e2
+
+print (e1 * meter) % (e2 * meter)
