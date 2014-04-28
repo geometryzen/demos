d7211ed344ea456a259ab14e086b0bfa353240fd
David Geo Holmes <geometryzen@gmail.com>
2014-04-28 14:21:28 -0400

    Save file.

--- Mathematics/Vectors/quadrance.py
+++ Mathematics/Vectors/quadrance.py
@@ -8,12 +8,13 @@ Quadrance is a term introduced by NJ Wildberger.
 
 print quadrance(2)
 print quadrance(2.0)
+'''
 try:
     print quadrance("Hello")
 except Exception as e:
     print e
     pass
-
+'''
 c = complex(3.0,4.0)
 print str(c)
 print repr(c)
