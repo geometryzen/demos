4c05ff106b1d647d0295c94bab4011e15225396b
David Geo Holmes <geometryzen@gmail.com>
2014-04-29 17:42:41 -0400

    Save file.

--- Units of Measure/kilogram-meter-second-coulomb.py
+++ Units of Measure/kilogram-meter-second-coulomb.py
@@ -1,7 +1,7 @@
 from units import *
 
 def show(name, measure):
-    print "%s is '%s'" % (name, measure.uom)
+    print "%s : '%s'" % (name, measure.uom)
     
 show("mass", kilogram)
 show("length", meter)
