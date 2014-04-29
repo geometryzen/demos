3d0fb4e440fcf186ba6421f2a46538d0a52645e6
David Geo Holmes <geometryzen@gmail.com>
2014-04-29 11:24:18 -0400

    Save file.

--- Units of Measure/units-with-e3ga.py
+++ Units of Measure/units-with-e3ga.py
@@ -1,1 +1,15 @@
-# units-with-e3ga.py
+from units import *
+from e3ga import *
+
+e1 = VectorE3(1,0)
+e2 = VectorE3(0,1)
+
+print "e1 => %s" % e1
+print "e2 => %s" % e2
+
+x = VectorE3(3,4,12) * meter
+
+print "x => %s" % x
+
+print e1 % x
+print e2 % x
