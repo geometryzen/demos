d03363d02201511763a38f9f265f7a2174b5a402
David Geo Holmes <geometryzen@gmail.com>
2014-04-29 17:35:25 -0400

    Save file.

--- Units of Measure/units-with-fractions.py
+++ Units of Measure/units-with-fractions.py
@@ -4,6 +4,6 @@ from units import *
 f = Fraction(1,2)
 g = Fraction(3,2)
 
-print "f => %s" % f
-print "g => %s" % g
-print "f+g => %s" % f+g
+print "f => %s" % (f)
+print "g => %s" % (g)
+print "f+g => %s" % (f+g)
