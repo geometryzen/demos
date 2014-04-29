ee606a2a6abb280be89e8dbd450e1f6b3a83087a
David Geo Holmes <geometryzen@gmail.com>
2014-04-29 12:16:08 -0400

    Save file.

--- Units of Measure/units-with-complex.py
+++ Units of Measure/units-with-complex.py
@@ -5,7 +5,13 @@ u = complex(1,0)
 i = complex(0,1)
 z = complex(3,4)
 
+x = u * meter
+y = i * meter
+
 print "u = %s" % u
 print "i = %s" % i
 print "z = %s" % z
+
+print "x = %s" % x
+print "y = %s" % y
 
