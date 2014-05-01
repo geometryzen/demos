f6efde3fafa9132ce6478ba8361c4bc1483bb9c5
David Geo Holmes <geometryzen@gmail.com>
2014-05-01 19:51:27 -0400

    Save file.

--- Physics/ball-bounces-in-a-box.py
+++ Physics/ball-bounces-in-a-box.py
@@ -65,6 +65,7 @@ def tick(t):
     ball.position += (ball.momentum / ball.mass) * dt
     # Use a scalar product to project the ball position.
     # Use a geometric vector sandwich to compute the reflection.
+    print ball.position % e1
     print abs(ball.position % e1)
     print side
     if abs(ball.position % e1) >= side:
