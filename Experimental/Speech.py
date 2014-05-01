809498f957e46ab42867010c3fd2c8b1299e5ade
David Geo Holmes <geometryzen@gmail.com>
2014-05-01 12:54:02 -0400

    Save file.

--- Experimental/Speech.py
+++ Experimental/Speech.py
@@ -13,8 +13,8 @@ msg.voiceURI = 'native'
 msg.volume = 1 # 0 to 1
 msg.rate = 1 # 0.1 to 10
 msg.pitch = 2 # 0 to 2
-msg.text = 'Hello World'
-msg.lang = 'en-US'
+msg.text = 'Ola'
+#msg.lang = 'en-US'
 
 #msg.onend = function(e) {
 #  console.log('Finished in ' + event.elapsedTime + ' seconds.');
