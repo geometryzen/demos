ea0a49b91cd7858d955564c982ddffc29b768424
David Geo Holmes <geometryzen@gmail.com>
2014-05-01 17:36:47 -0400

    Save file.

--- JSXGraph/ellipse-major-axis.py
+++ JSXGraph/ellipse-major-axis.py
@@ -12,9 +12,6 @@ div.style.height = "400px"
 board = graph.initBoard("box", {"axis":True,"grid":True})
 
 A = board.create('point',[-1,0])
-print type(A)
-A.foo = A
 B = board.create('point',[1,0])
 s = board.create('slider',[[-1,-2],[1,-2],[0,4,10]])
-print s.Value()
 E = board.create('ellipse',[A,B,lambda : s.Value()])
