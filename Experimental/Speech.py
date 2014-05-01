85315952bf3caa9b55a366e381f77c0b7acbfd04
David Geo Holmes <geometryzen@gmail.com>
2014-05-01 13:12:02 -0400

    Save file.

--- Experimental/Speech.py
+++ Experimental/Speech.py
@@ -13,7 +13,7 @@ msg.voiceURI = 'native'
 msg.volume = 1 # 0 to 1
 msg.rate = 1 # 0.1 to 10
 msg.pitch = 2 # 0 to 2
-msg.text = 'Bonjour mon petit choux'
+msg.text = 'Bonjour Geometry Zen'
 #msg.lang = 'en-US'
 
 #def onEndHandler(event):
