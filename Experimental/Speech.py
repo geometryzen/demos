83cbd177c67977b9d863f306b9a4723427990c3f
David Geo Holmes <geometryzen@gmail.com>
2014-05-01 12:46:22 -0400

    Save file.

--- Experimental/Speech.py
+++ Experimental/Speech.py
@@ -4,6 +4,7 @@ msg = window.SpeechSynthesisUtterance('Hello Geometry Zen!')
 window.speechSynthesis.speak(msg)
 
 print type(window.speechSynthesis.getVoices())
+print window.speechSynthesis.getVoices()
 for voice in window.speechSynthesis.getVoices():
     print voice.name
 print map(window.speechSynthesis.getVoices(), lambda voice: (voice.name))
