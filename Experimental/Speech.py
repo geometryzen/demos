b2bd0474df77fc1addcaaf64b94763b88db536fe
David Geo Holmes <geometryzen@gmail.com>
2014-05-01 12:52:51 -0400

    Save file.

--- Experimental/Speech.py
+++ Experimental/Speech.py
@@ -7,4 +7,15 @@ for voice in window.speechSynthesis.getVoices():
     print voice.name
     
 msg = window.SpeechSynthesisUtterance()
+msg.voice = voices[0]
+msg.voiceURI = 'native'
+msg.volume = 1 # 0 to 1
+msg.rate = 1 # 0.1 to 10
+msg.pitch = 2 # 0 to 2
+msg.text = 'Hello World'
+msg.lang = 'en-US'
+
+#msg.onend = function(e) {
+#  console.log('Finished in ' + event.elapsedTime + ' seconds.');
+#};
 window.speechSynthesis.speak(msg)
