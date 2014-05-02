c4d550f402ce0e21d81d2e993fb7b40ba8683c51
David Geo Holmes <geometryzen@gmail.com>
2014-05-02 02:02:01 -0400

    Save file.

--- Mathematics/Functions/math.py
+++ Mathematics/Functions/math.py
@@ -4,15 +4,15 @@ print pi
 print str(pi)
 print repr(pi)
 print type(pi)
-
+print
 print cos(pi)
 print str(cos(pi))
 print repr(cos(pi))
 print type(cos(pi))
-
+print
 print log(100,2)
 print str(log(100,2))
 print repr(log(100,2))
 print type(log(100,2))
-
+print
 print pow(2,24)
