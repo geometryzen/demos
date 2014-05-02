27633a5d6d41ee86ece3a33a7fe05fd001838ede
David Geo Holmes <geometryzen@gmail.com>
2014-05-02 00:15:34 -0400

    Save file.

--- Canvas-3D/Cameras/OrthographicCamera.py
+++ Canvas-3D/Cameras/OrthographicCamera.py
@@ -114,8 +114,9 @@ def tick(t):
 def terminate(t):
     return t > progressEnd
 
-def tearDown():
+def tearDown(e):
     discardCanvases()
     print "Goodbye!"
+    print e
 
-WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
+WindowAnimationRunner(tick, terminate, setUp, tearDown, window).start()
