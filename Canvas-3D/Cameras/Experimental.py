3f6ebd80ec8c2775ea0522f78b1b185db06515fb
David Geo Holmes <geometryzen@gmail.com>
2014-05-02 00:11:23 -0400

    Save file.

--- Canvas-3D/Cameras/Experimental.py
+++ Canvas-3D/Cameras/Experimental.py
@@ -117,8 +117,9 @@ def tick(t):
 def terminate(t):
     return t > progressEnd
 
-def tearDown():
+def tearDown(e):
     discardCanvases()
     print "Goodbye!"
+    print e
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
