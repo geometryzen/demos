f3eb8c5183c7442cbb58aee91d5d2234cdb992c5
David Geo Holmes <geometryzen@gmail.com>
2014-05-01 22:28:47 -0400

    Save file.

--- Physics/vectors.py
+++ Physics/vectors.py
@@ -47,8 +47,9 @@ def tick(t):
 def terminate(t):
     return t > timeOut
 
-def tearDown():
+def tearDown(e):
     document.removeEventListener("keydown", onDocumentKeyDown, False)
     workbench.tearDown()
+    print e
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
