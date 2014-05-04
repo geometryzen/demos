99750969522e97e23b89e6763fa2fbfe666fdba8
David Geo Holmes <geometryzen@gmail.com>
2014-05-04 18:54:31 -0400

    Save file.

--- Python/Yield.py
+++ Python/Yield.py
@@ -1,1 +1,11 @@
-# Yield.py
+class GeneratorClass:
+    def __init__(self):
+        pass
+    def generator(self):
+        for i in range(10):
+            yield i
+
+gen = GeneratorClass()
+
+for g in gen.generator():
+    print g
