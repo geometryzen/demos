abfa5dc1f81bd1353381cd948eade404a69cde22
David Geo Holmes <geometryzen@gmail.com>
2014-05-05 16:15:55 -0400

    Save file.

--- Physics/rod-on-frictionless-surface.py
+++ Physics/rod-on-frictionless-surface.py
@@ -21,7 +21,6 @@ show("moment arm, r", r)
 
 # As a vector, the torque is given by
 N = - e1 * e2 * e3 * (r ^ F)
-N = r.cross(F)
 
 show("torque, N = r x F", N)
 show("torque, N = r x F", r.cross(F))
