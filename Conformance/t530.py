769b764a2126d5091ac31b80e04c373907baa97c
David Geo Holmes <geometryzen@gmail.com>
2014-05-05 09:14:29 -0400

    Save file.

--- Conformance/t530.py
+++ Conformance/t530.py
@@ -1,1 +1,13 @@
-# t530.py
+class GeneratorClass:
+    test = "hi"
+    def __init__(self):
+        pass
+    def generator(self):
+	print self.test
+        for i in range(10):
+            yield i
+
+gen = GeneratorClass()
+
+for g in gen.generator():
+    print g
