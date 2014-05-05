441d19df06acc2a1f68f4992f13f208d33f91c0e
David Geo Holmes <geometryzen@gmail.com>
2014-05-05 14:46:31 -0400

    Save file.

--- Physics/rod-on-frictionless-surface.py
+++ Physics/rod-on-frictionless-surface.py
@@ -1,4 +1,10 @@
+from e3ga import *
 from units import *
+
+e1 = VectorE3(1,0,0) # Imagine to right
+e2 = VectorE3(0,1,0) # Imaging into screen
+e3 = vectorE3(0,0,1) # Imaging pointing up
+
 
 alpha = (0.5 * meter) * (6 * newton) / ((0.3 * kilogram * meter ** 2)/12)
 
