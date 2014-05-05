a82048ed7e7076a347504657a03105fbe43fe200
David Geo Holmes <geometryzen@gmail.com>
2014-05-05 15:30:05 -0400

    Save file.

--- Physics/rod-on-frictionless-surface.py
+++ Physics/rod-on-frictionless-surface.py
@@ -3,6 +3,7 @@ from units import *
 from sys import *
 
 def show(name, measure):
+    sys.debug()
 #    print "%s => %s" % (name, measure)
     pass
 
@@ -28,7 +29,7 @@ print show("torque, N", N)
 # Moment of inertia for a rod of 300 grams, 1 meter long
 I = (300 * gram) * (meter ** 2) / 12
 
-#print show("moment of inertia, I", I)
+print show("moment of inertia, I", I)
 
 
 alpha = N/I
