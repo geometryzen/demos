3cda0cb8765762ed4d940f37550d3c17e9fcbc49
David Geo Holmes <geometryzen@gmail.com>
2014-05-05 14:48:39 -0400

    Save file.

--- Physics/rod-on-frictionless-surface.py
+++ Physics/rod-on-frictionless-surface.py
@@ -5,8 +5,13 @@ e1 = VectorE3(1,0,0) # Imagine to right
 e2 = VectorE3(0,1,0) # Imaging into screen
 e3 = VectorE3(0,0,1) # Imaging pointing up
 
-# Force is 6N to the right
+# Force is 6 N to the right
 F = 6 * newton * e1
+
+# Moment arm is 0.5 m upwards
+r = 0.5 * meter * e2
+
+N = r ^ F
 
 
 alpha = (0.5 * meter) * (6 * newton) / ((0.3 * kilogram * meter ** 2)/12)
