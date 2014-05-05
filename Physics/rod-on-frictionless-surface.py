8876252d4fb2c08c2af03923bab19527c23876fa
David Geo Holmes <geometryzen@gmail.com>
2014-05-05 15:29:01 -0400

    Save file.

--- Physics/rod-on-frictionless-surface.py
+++ Physics/rod-on-frictionless-surface.py
@@ -2,7 +2,7 @@ from e3ga import *
 from units import *
 
 def show(name, measure):
-    print "%s => %s" % (name, measure)
+#    print "%s => %s" % (name, measure)
 
 e1 = VectorE3(1,0,0) # Imagine pointing to the right
 e2 = VectorE3(0,1,0) # Imaging going into the screen
