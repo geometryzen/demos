a27c3685ade3bc6ef4ab93aa1e4dfd40ca082921
David Geo Holmes <geometryzen@gmail.com>
2014-05-06 12:30:35 -0400

    Save file.

--- Physics/angular-velocity-as-bivector.py
+++ Physics/angular-velocity-as-bivector.py
@@ -1,1 +1,10 @@
-# angular-velocity-as-bivector.py
+from e3ga import *
+from units import *
+
+e1 = VectorE3(1,0,0)
+e2 = VectorE3(0,1,0)
+e3 = VectorE3(0,0,1)
+
+# Conventionally, we think of the angular velocity as a vector.
+# Would be nice to have radians in units here!
+omegaConv = 5 * e3 / second
