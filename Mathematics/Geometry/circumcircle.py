449581453f464c9a39a0227891aabbc90287fd77
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 16:27:23 -0400

    Save file.

--- Mathematics/Geometry/circumcircle.py
+++ Mathematics/Geometry/circumcircle.py
@@ -22,8 +22,8 @@ def circumcircle(p1, p2, p3):
     return (center, radius)
 
 
-p1 = VectorE2(+0.1, 0.0)
-p2 = VectorE2( 0.0,+0.5)
+p1 = VectorE2(+1.0, 0.0)
+p2 = VectorE2( 0.0,+1.0)
 p3 = VectorE2(-0.5, 0.0)
 
 print circumcircle(p1,p2,p3);
