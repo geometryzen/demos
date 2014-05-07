a7ae7a908a405657167e97e3b496dab025800bda
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 08:16:52 -0400

    Save file.

--- Mathematics/Vectors/orthogonality.py
+++ Mathematics/Vectors/orthogonality.py
@@ -1,1 +1,10 @@
-# orthogonality.py
+'''
+Calculate the cross product of the vectors A, B,
+and verify that it is perpendicular to both A and B.
+'''
+from e3ga import *
+
+A = VectorE3(2,3,0)
+B = VectorE3(-1,1,4)
+
+print A
