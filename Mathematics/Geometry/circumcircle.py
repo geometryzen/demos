05619a9d3e9f18e7eadc490175e4cea345a32baf
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 16:34:29 -0400

    Save file.

--- Mathematics/Geometry/circumcircle.py
+++ Mathematics/Geometry/circumcircle.py
@@ -15,9 +15,11 @@ def circumcircle(p1, p2, p3):
     
     d = 2 * (x13 * x12 - x11 * x23)
     a1 = (2 * x13 * x12 - x23 * x11 + x12 * x33) / d
-    print "a1: %s" % a1
     a2 = (x33 * (x12 - x11) - (x13 + x23) * x11) / d
     a3 = x11 * (x13 + x33) / d
+    print "a1: %s" % a1
+    print "a2: %s" % a2
+    print "a3: %s" % a3
     center = (a1 * p1 + a2 * p3 + a3 * p3) / d
     radius = magnitude(p3 - center)
     return (center, radius)
