de1c4dd3caba2fd89f0ed23b24c07a105bde014d
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 16:07:24 -0400

    Save file.

--- Mathematics/Geometry/circumcircle.py
+++ Mathematics/Geometry/circumcircle.py
@@ -1,4 +1,10 @@
+from e2ga import *
+
 def circumcircle(p1, p2, p3):
     pass
 
-print circumcircle();
+p1 = VectorE2(+1, 0)
+p2 = VectorE2( 0,+1)
+p3 = VectorE3(-1, 0)
+
+print circumcircle(p1,p2,p3);
