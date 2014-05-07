76b90cf1966f9c1f305e806e909781effff2be69
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 09:44:11 -0400

    Save file.

--- Physics/inclined-plane.py
+++ Physics/inclined-plane.py
@@ -1,1 +1,7 @@
-# inclined-plane.py
+'''
+A block slides down an inclined plane that makes a 30 degree angle with the horizontal.
+If the coefficient of friction is 0.3, find the acceleration of the block.
+'''
+
+# First, compute the vector up the plane.
+theta = (30 / 180) * pi
