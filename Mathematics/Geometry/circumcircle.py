a2be9f4302375eac5113b434fd65e93401b6217f
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 18:34:23 -0400

    Save file.

--- Mathematics/Geometry/circumcircle.py
+++ Mathematics/Geometry/circumcircle.py
@@ -30,7 +30,7 @@ def circumcircle(p1, p2, p3):
 #    print "beta:  %s" % beta
 #    print "gamma: %s" % gamma
     
-    center = (a1 * p1 + a2 * p2 + a3 * p3) / d
+    center = (a1 * p1 + a2 * p2 + a3 * p3)
 #   center = (alpha * p1 + beta * p2 + gamma * p3) / d
     radius = magnitude(p3 - center)
     print "d1: %s" % magnitude(p1 - center)
