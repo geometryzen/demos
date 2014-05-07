3f9acf360bd40126155ea1e7c932cb0890fb77b7
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 16:23:48 -0400

    Save file.

--- Mathematics/Geometry/circumcircle.py
+++ Mathematics/Geometry/circumcircle.py
@@ -13,13 +13,14 @@ def circumcircle(p1, p2, p3):
     x33 = x3.dot(x3)
     
     d = 2 * (x13 * x12 - x11 * x23)
-    a1 = (2 * x13 * x12 - x23 * x11 + x12 * x33)/d
+    a1 = (2 * x13 * x12 - x23 * x11 + x12 * x33) / d
+    a2 = x33 * (x12 - x11) - () * x11
     a3 = x11 * (x13 + x33) / d
     return (a1 * p1 + a3 * p3) / d
 
 
-p1 = VectorE2(+0.5, 0)
-p2 = VectorE2( 0,+0.5)
-p3 = VectorE2(-0.5, 0)
+p1 = VectorE2(+0.5, 0.0)
+p2 = VectorE2( 0.0,+0.5)
+p3 = VectorE2(-0.5, 0.0)
 
 print circumcircle(p1,p2,p3);
