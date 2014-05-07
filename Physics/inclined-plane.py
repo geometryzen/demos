0da544a091b2685487401abeacc14da1c85b201d
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 10:33:33 -0400

    Save file.

--- Physics/inclined-plane.py
+++ Physics/inclined-plane.py
@@ -33,7 +33,7 @@ def acceleration(theta, mu):
     eNorm = eDown.cross(k)
     N = W.dot(eNorm) * eNorm
     print "N => %s" % (N / eNorm)
-    F = W.dot(eDown) * eDown - mu * N
+    F = W.dot(eDown) * eDown
     print "F => %s" % (F/eDown)
     
     
