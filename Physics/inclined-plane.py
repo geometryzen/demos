66effdba4aaf6b209c8952028b15458b2e89c32b
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 09:47:22 -0400

    Save file.

--- Physics/inclined-plane.py
+++ Physics/inclined-plane.py
@@ -11,7 +11,7 @@ mu = 0.3
 
 def acceleration(theta, mu):
     # First, compute the vector up the plane.
-    eUp = VectoreE3(cos(theta), sin(theta), 0)
+    eUp = VectorE3(cos(theta), sin(theta), 0)
     
     
 print acceleration(theta, mu)
