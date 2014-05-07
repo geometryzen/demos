afca8b4eba8179932ea5afc7373d7119391f6d0b
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 16:11:29 -0400

    Save file.

--- Mathematics/Geometry/circumcircle.py
+++ Mathematics/Geometry/circumcircle.py
@@ -11,6 +11,9 @@ def circumcircle(p1, p2, p3):
     x22 = x2.dot(x2)
     x23 = x2.dot(x3)
     x33 = x3.dot(x3)
+    
+    a1 = 2 * x13 * x12 - x23 * x11 + x12 * x33
+    return a1 * p1
     pass
 
 p1 = VectorE2(+1, 0)
