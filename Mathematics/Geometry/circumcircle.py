500ae449af3240fe5382708df31360fa8ed38963
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 16:34:06 -0400

    Save file.

--- Mathematics/Geometry/circumcircle.py
+++ Mathematics/Geometry/circumcircle.py
@@ -15,6 +15,7 @@ def circumcircle(p1, p2, p3):
     
     d = 2 * (x13 * x12 - x11 * x23)
     a1 = (2 * x13 * x12 - x23 * x11 + x12 * x33) / d
+    print "a1: %s" % a1
     a2 = (x33 * (x12 - x11) - (x13 + x23) * x11) / d
     a3 = x11 * (x13 + x33) / d
     center = (a1 * p1 + a2 * p3 + a3 * p3) / d
