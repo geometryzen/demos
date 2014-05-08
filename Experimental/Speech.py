daf8442181397e7b76d61b87256d33c0a9f691d7
David Geo Holmes <geometryzen@gmail.com>
2014-05-07 21:57:24 -0400

    Save file.

--- Experimental/Speech.py
+++ Experimental/Speech.py
@@ -1,25 +1,4 @@
 from browser import window
-import sys
 
 msg = window.SpeechSynthesisUtterance('Hello Geometric Physics!')
-window.speechSynthesis.speak(msg)
-
-for voice in window.speechSynthesis.getVoices():
-    print voice.name
-    
-msg = window.SpeechSynthesisUtterance()
-voices = window.speechSynthesis.getVoices()
-msg.voice = voices[4]
-msg.voiceURI = 'native'
-msg.volume = 1 # 0 to 1
-msg.rate = 1 # 0.1 to 10
-msg.pitch = 2 # 0 to 2
-msg.text = 'Bonjour, geometrique physique!'
-msg.lang = 'fr'
-
-#def onEndHandler(event):
-#    print 'Finished in ' + str(event.elapsedTime) + ' seconds.'
-    
-#msg.onend = onEndHandler
-    
 window.speechSynthesis.speak(msg)
