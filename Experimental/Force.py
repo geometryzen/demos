268d3445d8bdf3dff69e83a904bb680e9c59372a
David Geo Holmes <geometryzen@gmail.com>
2014-05-08 08:31:34 -0400

    Save file.

--- Experimental/Force.py
+++ Experimental/Force.py
@@ -1,25 +1,25 @@
 class Gravity:
-def __init__(self, m, M):
-    self.m = m # mass of object 1
-    self.M = M # mass of object 2
-    self.G = 6.67428E-11 # gravity constant, m**3/kg/s**2
+    def __init__(self, m, M):
+        self.m = m # mass of object 1
+        self.M = M # mass of object 2
+        self.G = 6.67428E-11 # gravity constant, m**3/kg/s**2
 
-def force(self, r):
-    G, m, M = self.G, self.m, self.M
-    return G*m*M/r**2
+    def force(self, r):
+        G, m, M = self.G, self.m, self.M
+        return G*m*M/r**2
 
-def visualize(self, r_start, r_stop, n=100):
-    #from numpy import linspace
-    #r = linspace(r_start, r_stop, n)
-    r = [1,2,3,4,5,6,7]
-    g = self.force(r)
-    title='Gravity force: m=%g, M=%g' % (self.m, self.M)
-    #plot(r, g, title=title)
+    def visualize(self, r_start, r_stop, n=100):
+        #from numpy import linspace
+        #r = linspace(r_start, r_stop, n)
+        r = [1,2,3,4,5,6,7]
+        g = self.force(r)
+        title='Gravity force: m=%g, M=%g' % (self.m, self.M)
+        #plot(r, g, title=title)
 
 class CoulumbsLaw(Gravity):
-def init(self, q1, q2):
-Gravity.init(self, q1, q2)
-self.G = 8.99E9
+    def init(self, q1, q2):
+        Gravity.init(self, q1, q2)
+        self.G = 8.99E9
 
 c = CoulumbsLaw(1E-6, -2E-6)
 print 'Electric force:', c.force(0.1)
