e0d4083ce5d9961c339f2a4e2e41fc0dbba361bb
David Geo Holmes <geometryzen@gmail.com>
2014-05-09 00:20:39 -0400

    Save file.

--- Physics/angular-velocity-as-bivector.py
+++ Physics/angular-velocity-as-bivector.py
@@ -8,6 +8,7 @@ e1 = VectorE3(1,0,0)
 e2 = VectorE3(0,1,0)
 e3 = VectorE3(0,0,1)
 I_3 = e1 * e2 * e3
+print e1
 
 # Conventionally, we think of the angular velocity as a vector.
 # Would be nice to have radians in units here!
