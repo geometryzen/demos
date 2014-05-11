d58a310a0f94b8e1ff384e28e0ac9964ca26ca57
David Geo Holmes <geometryzen@gmail.com>
2014-05-11 17:18:22 -0400

    Save file.

--- Experimental/complex.py
+++ Experimental/complex.py
@@ -1,6 +1,19 @@
 from units import *
 import sys
 
+print 0.001
+print 0.01
+print 0.1
+print 1.0
+print 10.0
+print 100.0
+print 1000.0
+print 10000.0
+print 100000.0
+print 1000000.0
+print 10000000.0
+print 100000000.0
+print 1000000000.0
 print 10000000000.0
 print 100000000000.0
 print 1000000000000.0
