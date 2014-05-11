543230991acd5b5f1dfba414b84a54aa0b9e05d3
David Geo Holmes <geometryzen@gmail.com>
2014-05-11 17:36:09 -0400

    Save file.

--- Units of Measure/units-with-fractions.py
+++ Units of Measure/units-with-fractions.py
@@ -1,8 +1,7 @@
 from fractions import *
-from units import *
 
-f = Fraction(9,4) * joule
-g = Fraction(3,8) * second
+f = Fraction(9,4)
+g = Fraction(3,8)
 
 print "f => %s" % (f)
 print "g => %s" % (g)
