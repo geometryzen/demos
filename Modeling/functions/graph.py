30033507047cce069e8a52f5d8c874ace880d7f1
David Geo Holmes <geometryzen@gmail.com>
2014-05-12 18:41:16 -0400

    Save file.

--- Modeling/functions/graph.py
+++ Modeling/functions/graph.py
@@ -43,7 +43,6 @@ div = win.document.getElementById("box")
 
 div.style.width  = "700px"
 div.style.height = "700px"
-div.style.font   = "20px"
 
 # Compute the bounding box for the graph so that the graph scales automagically.
 def boundingBox(padding):
