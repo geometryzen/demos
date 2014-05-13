6e7c8bf741f0ecc4d39870246f1e97c4ade54d93
David Geo Holmes <geometryzen@gmail.com>
2014-05-13 12:45:12 -0400

    Save file.

--- GA/pseudo.py
+++ GA/pseudo.py
@@ -7,7 +7,8 @@ I = i * j * k
 
 a = 5 * i + 3 * j + 2 * k
 b = 7 * i + 4 * j + 9 * k
+c = 9 * i + 5 * j + 16 * k
 
-print a.dot(b.cross(k))
-print a ^ b ^ k
-print a.dot(-I * (b^k))
+print a.dot(b.cross(c))
+print a ^ b ^ c
+print a.dot(-I * (b^c))
