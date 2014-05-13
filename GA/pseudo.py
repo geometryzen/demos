469de96f0b9f0790cb7ab5016314018cb606f7bd
David Geo Holmes <geometryzen@gmail.com>
2014-05-13 12:49:38 -0400

    Save file.

--- GA/pseudo.py
+++ GA/pseudo.py
@@ -4,7 +4,7 @@ from random import random
 i = VectorE3(1,0,0)
 j = VectorE3(0,1,0)
 k = VectorE3(0,0,1)
-I3 = i * j * k
+I = i * j * k
 
 a = random() * i + random() * j + random() * k
 b = random() * i + random() * j + random() * k
@@ -12,4 +12,4 @@ c = random() * i + random() * j + random() * k
 
 print a.dot(b.cross(c))
 print a ^ b ^ c
-print a.dot(-I3 * (b^c))
+print a.dot(-I * (b^c))
