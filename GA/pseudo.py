5d7e42b9d0e9dbeb50430b66309cde5ab0a20ce1
David Geo Holmes <geometryzen@gmail.com>
2014-05-13 12:46:19 -0400

    Save file.

--- GA/pseudo.py
+++ GA/pseudo.py
@@ -6,7 +6,7 @@ j = VectorE3(0,1,0)
 k = VectorE3(0,0,1)
 I = i * j * k
 
-a = 5 * i + 3 * j + 2 * k
+a = 5 * i + random() * j + random() * k
 b = 7 * i + 4 * j + 9 * k
 c = random() * i + 5 * j + 16 * k
 
