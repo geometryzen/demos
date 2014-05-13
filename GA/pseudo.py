36f50874a323cbf59862886f090d4c81517b0182
David Geo Holmes <geometryzen@gmail.com>
2014-05-13 12:43:54 -0400

    Save file.

--- GA/pseudo.py
+++ GA/pseudo.py
@@ -5,6 +5,8 @@ j = VectorE3(0,1,0)
 k = VectorE3(0,0,1)
 I = i * j * k
 
-print i.dot(j.cross(k))
-print i ^ j ^ k
-print i.dot(-I * (j^k))
+a = 5 * i + 3 * j + 2 * k
+
+print a.dot(j.cross(k))
+print a ^ j ^ k
+print a.dot(-I * (j^k))
