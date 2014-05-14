21de6a11a07399c09b081e99f3c881b60cd931af
David Geo Holmes <geometryzen@gmail.com>
2014-05-13 20:54:37 -0400

    Save file.

--- Units of Measure/magnetic-field.py
+++ Units of Measure/magnetic-field.py
@@ -7,4 +7,6 @@ print tesla * second * coulomb / kilogram
 print tesla * meter / second
 
 
-print tesla * meter * meter / second
+print tesla * meter * meter * coulomb
+
+print 1/(newton / (joule * second))
