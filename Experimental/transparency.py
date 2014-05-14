9eb1dc6421dd971781a39155e0cf7d5a93be1e9c
David Geo Holmes <geometryzen@gmail.com>
2014-05-14 16:47:17 -0400

    Save file.

--- Experimental/transparency.py
+++ Experimental/transparency.py
@@ -24,7 +24,7 @@ renderer.setClearColor(Color(0x080808), 1.0)
 
 for i in range(0,10):
     mesh = Mesh(CubeGeometry(0.1, 5, 5), MeshLambertMaterial({"color": 0x0000FF,"opacity": 0.25,"transparent": True}))
-    mesh.position = VectorE3(i-5,0,0)
+    mesh.position = Vect orE3(i-5,0,0)
     scene.add(mesh)
     
     mesh = Mesh(CubeGeometry(5, 0.1, 5), MeshLambertMaterial({"color": 0x00FF00,"opacity": 0.25,"transparent": True}))
