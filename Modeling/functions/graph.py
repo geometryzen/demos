783c62d4cbcb52b302846bbab19cc51866c069b9
David Geo Holmes <geometryzen@gmail.com>
2014-05-14 16:17:45 -0400

    Save file.

--- Modeling/functions/graph.py
+++ Modeling/functions/graph.py
@@ -38,11 +38,11 @@ graph = JXG.JSXGraph
 
 win = window.open("","","width=720,height=720")
 
-#link = win.document.createElement("link")
-#link.rel  = "stylesheet"
-#link.href = "http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
-#link.type = "text/css"
-#win.document.head.appendChild(link)
+link = win.document.createElement("link")
+link.rel  = "stylesheet"
+link.href = "http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600"
+link.type = "text/css"
+win.document.head.appendChild(link)
 
 link = win.document.createElement("link")
 link.rel  = "stylesheet"
