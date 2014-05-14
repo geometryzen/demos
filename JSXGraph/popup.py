e2e474b029ec4d81d97111ece5bcbfcf560dceac
David Geo Holmes <geometryzen@gmail.com>
2014-05-14 15:46:35 -0400

    Save file.

--- JSXGraph/popup.py
+++ JSXGraph/popup.py
@@ -14,8 +14,8 @@ win.document.body.innerHTML = '<div id="box" class="jxgbox"></div>'
 
 div = win.document.getElementById("box")
 
-div.style.width  = "600px"
-div.style.height = "600px"
+div.style.width  = "800px"
+div.style.height = "800px"
 win.document = win.document
 
 board = graph.initBoard("box", {"document":win.document,"axis":True,"grid":True,"showNavigation":False,"showCopyright":False})
