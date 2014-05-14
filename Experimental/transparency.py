dddabb431ac779f52b6fa11505a21d54221f6430
David Geo Holmes <geometryzen@gmail.com>
2014-05-14 18:09:44 -0400

    Save file.

--- Experimental/transparency.py
+++ Experimental/transparency.py
@@ -23,7 +23,7 @@ scene.add(directionalLight)
 renderer = WebGLRenderer()
 renderer.setClearColor(Color(0x080808), 1.0)
 
-def material(color):
+def material(color, opacity):
     return MeshLambertMaterial({"color": color,"opacity": 0.25,"transparent": True})
 
 for i in range(0,10):
