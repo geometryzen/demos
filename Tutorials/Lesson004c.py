8a3fd00799184bada8e8d1eacf15d8b1e1a87dad
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 17:00:31 -0400

    Save file.

--- Tutorials/Lesson004c.py
+++ Tutorials/Lesson004c.py
@@ -5,7 +5,7 @@ The sphere performs cirular motion with radius R and periodic time T.
 from browser import WindowAnimationRunner
 from geometry import CartesianSpace, SphereBuilder
 from math import cos, sin, pi
-from workbench import Workbench
+from workbench import Workbench3D
 
 T = 5
 omega = 2 * pi / T
@@ -16,7 +16,7 @@ space = CartesianSpace()
 sphere = SphereBuilder().color(0x0000FF).build()
 space.add(sphere)
 
-workbench = Workbench(space.renderer, space.camera)
+workbench = Workbench3D(space.renderer.domElement, space.renderer, space.camera)
 
 def tick(t):
     sphere.position.set(R * cos(omega*t), R * sin(omega*t), 0.0)
