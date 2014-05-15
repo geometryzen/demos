22801e697b604fa68b38805fc861809486584308
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 15:52:43 -0400

    Save file.

--- Canvas-3D/Lights/PointLight.py
+++ Canvas-3D/Lights/PointLight.py
@@ -21,7 +21,7 @@ scene.add(mesh)
 
 movement = VectorE3(0.02, 0.02, 0.02)
 
-workbench = Workbench3D(renderer, camera)
+workbench = Workbench3D(renderer.domElement, renderer, camera)
 
 def setUp():
     print "Hello!"
