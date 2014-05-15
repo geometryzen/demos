d8d743e72903c61705e7de4aafed797befa01228
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 16:04:13 -0400

    Save file.

--- GA/popup.py
+++ GA/popup.py
@@ -23,7 +23,7 @@ space2D.autoClear = True
 
 font = "20px Helvetica"
 
-output = Text("Hit Esc key to exit.", font, "black")
+output = Text("Hit Esc key to exit.", font, "white")
 output.x = 100
 output.y = 60
 space2D.addChild(output)
