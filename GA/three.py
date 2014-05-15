6fcd994bf2d4db49e1a784f427d7c9d56c64e323
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 11:43:13 -0400

    Save file.

--- GA/three.py
+++ GA/three.py
@@ -69,7 +69,7 @@ def tick(t):
     theta = omega * time
     # The rotor is defined to have a minus sign.
     rotor = exp(-B*theta/2.0)
-    print rotor
+    print rotor.uom
     # Unfortunately, we have to use a minus sign to convert the rotor grade 2 components to the quaternion values.
     arrow.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
     box.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
