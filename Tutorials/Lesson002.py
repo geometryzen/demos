f1c3a60637ee15914850df4fa300bd399518aba0
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 17:06:24 -0400

    Save file.

--- Tutorials/Lesson002.py
+++ Tutorials/Lesson002.py
@@ -5,14 +5,14 @@ The bolder part of the axis lines represents the positive direction.
 '''
 from browser import WindowAnimationRunner
 from geometry import CartesianSpace
-from workbench import Workbench
+from workbench import Workbench3D
 
 # A CartesianSpace is a pre-made scene containing a renderer, camera, and HTML5 canvas element.
 scene = CartesianSpace()
 
 # The Workbench class knows how to incorporate the canvas into the Geometry Zen window document.
 # It also know how to clean up at the end of the animation and how to respond to resize events.
-workbench = Workbench(scene.renderer, scene.camera)
+workbench = Workbench3D(scene.renderer.domElement, scene.renderer, scene.camera)
 
 def tick(t):
     scene.render()
@@ -28,5 +28,7 @@ def setUp():
 def tearDown(e):
     # Clean up by removing the canvas from the Geometry Zen window document. 
     workbench.tearDown()
+    if e:
+        print e
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
