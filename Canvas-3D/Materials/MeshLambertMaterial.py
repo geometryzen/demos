dfba07c32c1428f2f662383ddf185b37c964e08a
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 15:47:51 -0400

    Save file.

--- Canvas-3D/Materials/MeshLambertMaterial.py
+++ Canvas-3D/Materials/MeshLambertMaterial.py
@@ -30,7 +30,7 @@ scene.add(mesh)
 
 movement = VectorE3(0.02, 0.02, 0.02)
 
-workbench = Workbench3D(rebderer.domElement, renderer, camera)
+workbench = Workbench3D(renderer.domElement, renderer, camera)
 
 def setUp():
     workbench.setUp()
