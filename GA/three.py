6cc7adb23074718cb2cf5105e719b571b3782911
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 11:31:56 -0400

    Save file.

--- GA/three.py
+++ GA/three.py
@@ -67,9 +67,9 @@ def tick(t):
     # The rotor is defined to have a minus sign.
     rotor = exp(-B*theta.quantity/2.0)
     arrow.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
-    box.quaternion.set(rotor.yz, rotor.zx, rotor.xy, rotor.w)
-    vortex.quaternion.set(rotor.yz, rotor.zx, rotor.xy, rotor.w)
-    flat.quaternion.set(rotor.yz, rotor.zx, rotor.xy, rotor.w)
+    box.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
+    vortex.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
+    flat.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
     renderer.render(scene, camera)
 
 def tearDown(e):
