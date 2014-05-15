8f099e40ae886a187c18c2436ce4f9a45989469f
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 17:07:01 -0400

    Save file.

--- Tutorials/Lesson003.py
+++ Tutorials/Lesson003.py
@@ -3,7 +3,7 @@ This lesson demonstrates adding a simple object to a scene.
 '''
 from browser import WindowAnimationRunner
 from geometry import CartesianSpace, SphereBuilder
-from workbench import Workbench
+from workbench import Workbench3D
 
 scene = CartesianSpace()
 
@@ -12,7 +12,7 @@ sphere = SphereBuilder().color(0x0000FF).build()
 # Once created, the object must be added to the scene in order to be rendered.
 scene.add(sphere)
 
-workbench = Workbench(scene.renderer, scene.camera)
+workbench = Workbench3D(scene.renderer.domElement, scene.renderer, scene.camera)
 
 def tick(t):
     scene.render()
@@ -24,7 +24,9 @@ def terminate(t):
 def setUp():
     workbench.setUp()
 
-def tearDown():
+def tearDown(e):
     workbench.tearDown()
+    if e:
+        print e
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
