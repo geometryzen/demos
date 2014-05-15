802fd1f4bf955c379531d3d98c2244b52aa438ae
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 11:45:37 -0400

    Save file.

--- GA/three.py
+++ GA/three.py
@@ -60,7 +60,9 @@ tau = 2 * pi
 omega = (tau / 20) / second
 # A unit bivector rotating from k to i
 B = BivectorE3(0.0, 0.0, 5.0)
+# Just make sure that we really do have a unit bivector.
 B = B / magnitude(B)
+print B
 
 def setUp():
     workbench.setUp()
