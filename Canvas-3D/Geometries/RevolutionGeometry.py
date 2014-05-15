9536fd97d9c493a004c7add125c29e9e5b08ea98
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 15:39:26 -0400

    Save file.

--- Canvas-3D/Geometries/RevolutionGeometry.py
+++ Canvas-3D/Geometries/RevolutionGeometry.py
@@ -35,7 +35,9 @@ def terminate(t):
 def setUp():
     workbench.setUp()
 
-def tearDown():
+def tearDown(e):
     workbench.tearDown()
+    if e:
+        print e
 
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
