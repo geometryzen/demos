c7379dabb4b6c96f4d1f7d85b9fa3132bc6f9c33
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 16:55:26 -0400

    Save file.

--- Physics/vectors.py
+++ Physics/vectors.py
@@ -12,7 +12,7 @@ e1 = ArrowBuilder().color(0xFF0000).attitude(exp(-BivectorE3(-1.0, 0.0, 0.0)*pi/
 e2 = ArrowBuilder().color(0x00FF00).attitude(exp(-BivectorE3( 0.0, 0.0, 0.0)*pi/4.0)).build()
 e3 = ArrowBuilder().color(0x0000FF).attitude(exp(-BivectorE3( 0.0, 1.0, 0.0)*pi/4.0)).build()
 
-workbench = Workbench(space.renderer, space.camera)
+workbench = Workbench3D(space.renderer.domElement, space.renderer, space.camera)
 
 def escKey(event, downFlag):
     event.preventDefault()
