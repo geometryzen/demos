c2c753e25b78fa248305ae9683af01d7493eaaab
David Geo Holmes <geometryzen@gmail.com>
2014-05-15 08:32:10 -0400

    Save file.

--- GA/three.py
+++ GA/three.py
@@ -28,7 +28,7 @@ renderer.setClearColor(THREE.Color(0x080808), 1.0)
 def material(color=0x0000FF, opacity=1.0, transparent=False):
     return MeshLambertMaterial({"color": color,"opacity": opacity,"transparent": transparent})
 
-mesh = Mesh(CubeGeometry(5, 0.1, 5), material(0x00FF00, 1.0, False))
+mesh = THREE.Mesh(CubeGeometry(5, 0.1, 5), material(0x00FF00, 1.0, False))
 mesh.position = VectorE3(0, -2, 0)
 scene.add(mesh)
 
