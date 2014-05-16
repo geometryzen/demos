a14389a12fd51e15df6f55581e530acbd0b288fd
David Geo Holmes <geometryzen@gmail.com>
2014-05-16 11:32:26 -0400

    Save file.

--- GA/three.py
+++ GA/three.py
@@ -4,11 +4,33 @@ from math import *
 from units import *
 from e3ga import *
 from geometry import *
+from easel import *
 
 # This works both through the Python DaVinci wrapper as well as using JavaScript dynamic wrapping.
 # Provided you don't try to use the attitude property on the mesh together with native THREE.
 import three as THREE
 #THREE = window.THREE
+
+glwin = window.open("","","width=800,height=600")
+# Changing the background color is a bit of a hack until I figure out the padding.
+glwin.document.body.style.backgroundColor = "080808"
+glwin.document.body.style.overflow = "hidden"
+glwin.document.title = "Visualizing Geometric Algebra with WebGL"
+
+canvas2D = glwin.document.createElement("canvas")
+canvas2D.style.position = "absolute"
+canvas2D.style.top = "0px"
+canvas2D.style.left = "0px"
+workbench2D = Workbench2D(canvas2D, glwin)
+space2D = Stage(canvas2D)
+space2D.autoClear = True
+
+font = "20px Helvetica"
+
+output = Text(glwin.document.title + ". Hit Esc key to exit.", font, "white")
+output.x = 100
+output.y = 60
+space2D.addChild(output)
 
 scene = THREE.Scene()
 
@@ -56,7 +78,7 @@ scene.add(flat)
 
 CartesianSpace(scene, renderer)
 
-workbench = Workbench3D(renderer.domElement, renderer, camera)
+workbench3D = Workbench3D(renderer.domElement, renderer, camera, glwin)
 
 tau = 2 * pi
 omega = (tau / 20) / second
@@ -66,7 +88,8 @@ B = BivectorE3(0.0, 0.0, 1.0)
 B = B / magnitude(B)
 
 def setUp():
-    workbench.setUp()
+    workbench2D.setUp()
+    workbench3D.setUp()
 
 def tick(t):
     time = t * second
@@ -82,10 +105,15 @@ def tick(t):
     vortex.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
     flat.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
     renderer.render(scene, camera)
+    space2D.render()
 
 def tearDown(e):
-    workbench.tearDown()
+    workbench3D.tearDown()
+    workbench2D.tearDown()
+    glwin.close()
     if e:
         print "Error during animation: %s" % (e)
+    else:
+        print "Goodbye!"
 
-WindowAnimationRunner(tick, None, setUp, tearDown).start()
+WindowAnimationRunner(tick, None, setUp, tearDown, glwin).start()
