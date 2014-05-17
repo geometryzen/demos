ab47061923e10e6839e4712b7a6ee6a10997b506
David Geo Holmes <geometryzen@gmail.com>
2014-05-17 14:14:08 -0400

    Save file.

--- Canvas-3D/Geometries/RingGeometry.py
+++ Canvas-3D/Geometries/RingGeometry.py
@@ -8,7 +8,7 @@ THREE = window.THREE
 scene = Scene()
 
 camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
-camera.position.z = 100.0
+camera.position.z = 10.0
 
 renderer = WebGLRenderer()
 renderer.setClearColor(Color(0x080808), 1.0)
