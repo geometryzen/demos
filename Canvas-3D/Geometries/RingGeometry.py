628a574e63ce1ad4eff0ec663ff53f1634f45918
David Geo Holmes <geometryzen@gmail.com>
2014-05-17 14:10:17 -0400

    Save file.

--- Canvas-3D/Geometries/RingGeometry.py
+++ Canvas-3D/Geometries/RingGeometry.py
@@ -1,29 +1,41 @@
 from three import *
 from browser import *
 from workbench import *
+from math import pi
 
-e1 = VectorE3(1.0, 0.0, 0.0)
+scene = Scene()
 
-space = CartesianSpace()
+camera  = PerspectiveCamera(75, 1.0, 0.1, 1000)
+camera.position.z = 100.0
 
-radiusTop = 1
-radiusBottom = 1
-height = 5
-radialSegments = 32
-heightSegments = 5
-openEnded = False
-geom = RingGeometry(1,5,32)
+renderer = WebGLRenderer()
+renderer.setClearColor(Color(0x080808), 1.0)
 
-mesh = Mesh(geom, MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 1}))
-space.add(mesh)
+radius = 50
+segments = 32
+thetaStart = 0
+thetaLength = 2 * pi
+circle = CircleGeometry(radius, segments, thetaStart, thetaLength)
+circle.name = "Foo"
 
-workbench = Workbench3D(space.renderer.canvas, space.renderer, space.camera)
+print repr(circle)
+print "uuid:            " + str(circle.uuid)
+print "name:            " + str(circle.name)
+print circle
+
+mesh = Mesh(circle, MeshBasicMaterial({"wireframe": True, "wireframeLinewidth": 3}))
+scene.add(mesh)
+
+movement = 0.02 * VectorE3(1.0, 1.0, 1.0)
+
+workbench = Workbench3D(renderer.domElement, renderer, camera)
 
 def setUp():
     workbench.setUp()
 
 def tick(t):
-    space.render()
+    mesh.rotation += movement
+    renderer.render(scene, camera)
 
 def terminate(t):
     return t > 3
@@ -32,5 +44,5 @@ def tearDown(e):
     workbench.tearDown()
     if e:
         print e
-
+    
 WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
