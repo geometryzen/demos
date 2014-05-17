8df085815be67abc832621f87118529547f03222
David Geo Holmes <geometryzen@gmail.com>
2014-05-17 14:08:46 -0400

    Save file.

--- Canvas-3D/Geometries/RingGeometry.py
+++ Canvas-3D/Geometries/RingGeometry.py
@@ -1,1 +1,36 @@
-# RingGeometry.py
+from three import *
+from browser import *
+from workbench import *
+
+e1 = VectorE3(1.0, 0.0, 0.0)
+
+space = CartesianSpace()
+
+radiusTop = 1
+radiusBottom = 1
+height = 5
+radialSegments = 32
+heightSegments = 5
+openEnded = False
+cylinder = CylinderGeometry(radiusTop, radiusBottom, height, radialSegments, heightSegments, openEnded, e1)
+
+mesh = Mesh(cylinder, MeshNormalMaterial({"wireframe": True, "wireframeLinewidth": 1}))
+space.add(mesh)
+
+workbench = Workbench3D(space.renderer.canvas, space.renderer, space.camera)
+
+def setUp():
+    workbench.setUp()
+
+def tick(t):
+    space.render()
+
+def terminate(t):
+    return t > 3
+
+def tearDown(e):
+    workbench.tearDown()
+    if e:
+        print e
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown).start()
