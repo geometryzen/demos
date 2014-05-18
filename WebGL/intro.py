9875332fc9695708266f15f9a3eef859dc47838b
David Geo Holmes <geometryzen@gmail.com>
2014-05-17 21:03:58 -0400

    Save file.

--- WebGL/intro.py
+++ WebGL/intro.py
@@ -1,1 +1,16 @@
-# WebGL/intro.py
+from browser import window
+
+canvas = window.document.getElementById( 'canvas' )
+
+# Retrieve the WebGL context from the canvas
+gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
+
+# If the context doesn't exist, we don't have WebGL. Tears.
+if ( !gl ):
+    throw "This is not the WebGL context you're looking for"
+
+# Set the clearColor attribute
+gl.clearColor( 0.82, 0.28, 0.35, 1.0 )
+
+# Clear the color component of the screen buffer
+gl.clear( gl.COLOR_BUFFER_BIT )
