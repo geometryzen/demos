2f785fadcda3de1643d08fdbcb5f45f3234f95f4
David Geo Holmes <geometryzen@gmail.com>
2014-05-19 08:23:47 -0400

    Save file.

--- Experimental/visualization.py
+++ Experimental/visualization.py
@@ -46,9 +46,7 @@ def setUp():
 
 def tick(t):
     r = dwarf.position - giant.position
-    F = giant.mass * dwarf.mass
-    F = F * r
-    F = F / pow(abs(r * r), 3/2)
+    F = giant.mass * dwarf.mass * r / pow(abs(r * r), 3/2)
     giant.momentum = giant.momentum + F * dt
     dwarf.momentum = dwarf.momentum - F * dt
     
