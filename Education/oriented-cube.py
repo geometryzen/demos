d368810713d0aa80372cace734850a8950e6b983
David Geo Holmes <geometryzen@gmail.com>
2014-05-19 22:51:20 -0400

    Save file.

--- Education/oriented-cube.py
+++ Education/oriented-cube.py
@@ -39,6 +39,7 @@ scene = Scene()
 
 geometry = BoxGeometry(200.0, 200.0, 200.0)
 for i in range(0, len(geometry.faces), 2):
+    print i
     hex = int(random() * 0xFFFFFF)
     geometry.faces[i].color.setHex(hex)
     geometry.faces[i+1].color.setHex(hex)
