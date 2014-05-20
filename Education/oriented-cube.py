63f4bf4d2210023418e3d55225a4b6ac1f0f0304
David Geo Holmes <geometryzen@gmail.com>
2014-05-20 10:29:22 -0400

    Save file.

--- Education/oriented-cube.py
+++ Education/oriented-cube.py
@@ -71,7 +71,7 @@ def tick(t):
     B1 = BivectorE3(0,0,1)
     B2 = BivectorE3(0,1,0)
     rotor = exp(-B2*theta/2.0) * exp(-B1*theta/2.0)
-    print rotor
+    print magnitude(rotor)
     # Unfortunately, we have to use a minus sign to convert the rotor grade 2 components to the quaternion values.
     cube.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
     # cube.attitude = rotor
