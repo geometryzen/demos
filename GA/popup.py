006e19a2b6dd729df42d642d9b69c321f990d10d
David Geo Holmes <geometryzen@gmail.com>
2014-05-26 23:20:01 -0400

    Save file.

--- GA/popup.py
+++ GA/popup.py
@@ -95,7 +95,7 @@ def tick(t):
     # Unfortunately, we have to use a minus sign to convert the rotor grade 2 components to the quaternion values.
     arrow.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
     
-    box.useQuaternion = true
+    box.useQuaternion = True
     box.attitude = rotor
 #   box.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
 
