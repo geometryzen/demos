00f475294a6cfe57d765d1ac4be8cf01fc81b771
David Geo Holmes <geometryzen@gmail.com>
2014-05-27 09:31:42 -0400

    Save file.

--- Physics/vectors.py
+++ Physics/vectors.py
@@ -8,7 +8,7 @@ space = CartesianSpace()
 timeOut = 10
 
 # Arrows are assumed to be aligned with the y-axis, so we must adjust their attitudes.
-e1 = ArrowBuilder().color(0xFF0000).attitude(exp(-BivectorE3(-1.0, 0.0, 0.0)*pi/4.0)).build()
+e1 = ArrowBuilder().color(0xFF0000).attitude(exp(-BivectorE3( 0.0, 0.0, 0.0)*pi/4.0)).build()
 e2 = ArrowBuilder().color(0x00FF00).attitude(exp(-BivectorE3( 0.0, 0.0, 0.0)*pi/4.0)).build()
 e3 = ArrowBuilder().color(0x0000FF).attitude(exp(-BivectorE3( 0.0, 1.0, 0.0)*pi/4.0)).build()
 
