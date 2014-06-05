75aaa4da8e55bc811cf17da575ccb5af233855cb
David Geo Holmes <geometryzen@gmail.com>
2014-06-05 10:22:28 -0400

    Save file.

--- Mathematics/e2ga.py
+++ Mathematics/e2ga.py
@@ -5,59 +5,146 @@ def explain(m):
     print str(m) + " is " + repr(m)
     return m
 
-def showValue(name, x):
-    print name + " => " + str(x)
-    return x
+def showValue(name, m):
+    print name + " => " + str(m)
+    return m
 
-zero = explain(Euclidean2(0,0,0,0))
-one  = explain(ScalarE2(1))
-two  = explain(ScalarE2(2))
-three= explain(3)
-i    = explain(VectorE2(1, 0))
-j    = explain(VectorE2(0, 1))
-I    = explain(PseudoscalarE2(1))
+zero  = Euclidean2(0, 0, 0, 0)
+one   = Euclidean2(1, 0, 0, 0)
+two   = Euclidean2(2, 0, 0, 0)
+i     = Euclidean2(0, 1, 0, 0)
+j     = Euclidean2(0, 0, 1, 0)
+I     = Euclidean2(0, 0, 0, 1)
 
-blades = [zero, one, two, three, i, j, I]
+blades = [zero, one, two, i, j, I]
 
-print ""
-print "Sum"
+sum = one + i + j + I
+
+print "----------"
+print "repr"
+print "----------"
+print repr(zero)
+print repr(one)
+print repr(two)
+print repr(i)
+print repr(j)
+print repr(I)
+print "----------"
+print "str"
+print "----------"
+print str(zero)
+print str(one)
+print str(two)
+print str(i)
+print str(j)
+print str(I)
+print "----------"
+print "Addition +"
+print "----------"
 for a in blades:
-    print ""
     for b in blades:
-        showValue(str(a) + " + " + str(b), str(a + b))
-print ""
-print "Difference"
+        showValue(str(a) + " + " + str(b), a + b)
+    print ""
+print "----------"
+print "Addition +="
+print "----------"
 for a in blades:
-    print ""
-    for b in blades:
-        showValue(str(a) + " - " + str(b), str(a - b))
+    x = Euclidean2(0, 0, 0, 0)
+    x += a
+    showValue("0 += " + str(a), x)
 print ""
-print "Geometric Product"
+print "----------"
+print "Subtraction -"
+print "----------"
 for a in blades:
-    print ""
     for b in blades:
-        showValue(str(a) + " * " + str(b), str(a * b))
+        showValue(str(a) + " - " + str(b), a - b)
+    print ""
+print "----------"
+print "Subtraction -="
+print "----------"
+for a in blades:
+    x = Euclidean2(0, 0, 0, 0)
+    x -= a
+    showValue("0 -= " + str(a), x)
 print ""
-print "Exterior Product"
+print "----------"
+print "Multiplication *"
+print "----------"
 for a in blades:
-    print ""
     for b in blades:
-        showValue(str(a) + " ^ " + str(b), str(a ^ b))
+        showValue(str(a) + " * " + str(b), a * b)
+    print ""
+print "----------"
+print "Multiplication *="
+print "----------"
+for a in blades:
+    before = Euclidean2(1, 1, 1, 1)
+    after =  Euclidean2(before.w, before.x, before.y, before.xy)
+    after *= a
+    showValue(str(before) + " *= " + str(a), after)
 print ""
-print "Left Contraction"
+print "----------"
+print "Division /"
+print "----------"
 for a in blades:
-    print ""
     for b in blades:
-        showValue(str(a) + " << " + str(b), str(a << b))
+        showValue(str(a) + " / " + str(b), a / b)
+    print ""
+print "----------"
+print "Division /="
+print "----------"
+for a in blades:
+    before = Euclidean2(1, 1, 1, 1)
+    after =  Euclidean2(before.w, before.x, before.y, before.xy)
+    after /= a
+    showValue(str(before) + " /= " + str(a), after)
 print ""
-print "Right Contraction"
+print "----------"
+print "Extension ^"
+print "----------"
 for a in blades:
-    print ""
     for b in blades:
-        showValue(str(a) + " >> " + str(b), str(a >> b))
+        showValue(str(a) + " ^ " + str(b), a ^ b)
+    print ""
+print "----------"
+print "Extension ^="
+print "----------"
+for a in blades:
+    before = Euclidean2(1, 1, 1, 1)
+    after =  Euclidean2(before.w, before.x, before.y, before.xy)
+    after ^= a
+    showValue(str(before) + " ^= " + str(a), after)
 print ""
-print "Scalar Product"
+print "----------"
+print "Left Contraction <<"
+print "----------"
 for a in blades:
-    print ""
     for b in blades:
-        showValue(str(a) + " % " + str(b), str(a % b))
+        showValue(str(a) + " << " + str(b), a << b)
+    print ""
+print "----------"
+print "Left Contraction <<="
+print "----------"
+for a in blades:
+    before = Euclidean2(1, 1, 1, 1)
+    after =  Euclidean2(before.w, before.x, before.y, before.xy)
+    after <<= a
+    showValue(str(before) + " <<= " + str(a), after)
+print ""
+print "----------"
+print "Right Contraction >>"
+print "----------"
+for a in blades:
+    for b in blades:
+        showValue(str(a) + " >> " + str(b), a >> b)
+    print ""
+print "----------"
+print "Right Contraction >>="
+print "----------"
+for a in blades:
+    before = Euclidean2(1, 1, 1, 1)
+    after =  Euclidean2(before.w, before.x, before.y, before.xy)
+    after >>= a
+    showValue(str(before) + " >>= " + str(a), after)
+print ""
