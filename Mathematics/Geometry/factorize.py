69b0cac44127d5d00a5841b0b7deeed4ec8d6dbd
David Geo Holmes <geometryzen@gmail.com>
2014-06-05 23:36:14 -0400

    Save file.

--- Mathematics/Geometry/factorize.py
+++ Mathematics/Geometry/factorize.py
@@ -7,7 +7,7 @@ e1 = VectorE3(1,0,0)
 e2 = VectorE3(0,1,0)
 e3 = VectorE3(0,0,1)
 
-B = VectorE3(random(),random(),random())# ^ VectorE3(random(),random(),random()) ^ VectorE3(random(),random(),random())
+B = VectorE3(random(),random(),random()) ^ VectorE3(random(),random(),random())# ^ VectorE3(random(),random(),random())
 
 print B
 
