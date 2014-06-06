f08b6ff5a84d15765ac348037a9d42e3ad76453a
David Geo Holmes <geometryzen@gmail.com>
2014-06-05 23:21:47 -0400

    Save file.

--- Mathematics/Geometry/factorize.py
+++ Mathematics/Geometry/factorize.py
@@ -1,4 +1,5 @@
 from e3ga import *
+from math import *
 
 e1 = VectorE3(1,0,0)
 e2 = VectorE3(0,1,0)
@@ -7,3 +8,5 @@ e3 = VectorE3(0,0,1)
 B = e1 ^ e2
 
 print B
+
+x = VectorE3(random(),random(),random())
