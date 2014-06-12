54ef30b75a9a2401c924986406187e30b27ef4ec
David Geo Holmes <geometryzen@gmail.com>
2014-06-12 18:25:49 -0400

    Save file.

--- Eight/baby-steps.py
+++ Eight/baby-steps.py
@@ -68,7 +68,7 @@ def tick(t):
     # Unfortunately, we have to use a minus sign to convert the rotor grade 2 components to the quaternion values.
 #    arrow.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
     
-#   box.attitude = rotor
+    box.attitude = rotor
 
 #    vortex.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
 #    flat.quaternion.set(-rotor.yz, -rotor.zx, -rotor.xy, rotor.w)
