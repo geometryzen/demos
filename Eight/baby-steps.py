7780494f3527bfc4b24af233d103827b6854df6b
David Geo Holmes <geometryzen@gmail.com>
2014-06-12 18:41:09 -0400

    Save file.

--- Eight/baby-steps.py
+++ Eight/baby-steps.py
@@ -9,7 +9,7 @@ from easel import *
 eight = window.EIGHT
 
 glwin = window.open("","","width=800,height=600")
-glwin.document.body.style.backgroundColor = "080808"
+glwin.document.body.style.backgroundColor = "202020"
 glwin.document.body.style.overflow = "hidden"
 glwin.document.title = "Visualizing Geometric Algebra with WebGL"
 
