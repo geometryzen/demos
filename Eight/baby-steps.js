86e1628312fbe4b489eeb7f5e81aeb788c05f4ed
David Geo Holmes <geometryzen@gmail.com>
2014-06-13 11:10:57 -0400

    Save file.

--- Eight/baby-steps.js
+++ Eight/baby-steps.js
@@ -19,12 +19,11 @@ var prism = eight.mesh(eight.prismGeometry());
 scene.add(prism);
 prism.position = eight.vectorE3(0.0,0.0,-5.0);
 
-var workbench3D = eight.workbench3D(renderer.canvas, renderer, camera, glwin)
+var workbench3D = eight.workbench3D(renderer.canvas, renderer, camera, glwin);
 
-function setUp()
-{
-    workbench3D.setUp()
-    monitor.start()
+function setUp() {
+  workbench3D.setUp();
+  monitor.start();
 }
 
 var B = eight.bivectorE3(0,0,1);
@@ -37,8 +36,7 @@ stats.domElement.style.left = '0px';
 stats.domElement.style.top = '0px';
 glwin.document.body.appendChild(stats.domElement);
 
-function tick(t)
-{
+function tick(t) {
   stats.begin();
   // Klunky math until we get the operator overloading and GA carried over to JavaScript!
   var c = eight.scalarE3(Math.cos(angle/2));
@@ -52,38 +50,30 @@ function tick(t)
   stats.end();
 }
 
-function terminate(t)
-{
-    return false;
-}
+function terminate(t) {return false;}
 
-function tearDown(e)
-{
-    monitor.stop();
-    glwin.close();
-    if (e)
-    {
-        console.log("Error during animation: " + e)
-    }
-    else
-    {
-        console.log("Goodbye!")
-        workbench3D.tearDown()
-        scene.tearDown();
-    }
+function tearDown(e) {
+  monitor.stop();
+  glwin.close();
+  if (e) {
+    console.log("Error during animation: " + e);
+  }
+  else {
+    console.log("Goodbye!");
+    workbench3D.tearDown();
+    scene.tearDown();
+  }
 }
 
 var runner = eight.windowAnimationRunner(tick, terminate, setUp, tearDown, glwin);
 
-function onContextLoss()
-{
+function onContextLoss() {
     runner.stop();
     renderer.onContextLoss();
     scene.onContextLoss();
 }
 
-function onContextGain(gl)
-{
+function onContextGain(gl) {
     scene.onContextGain(gl);
     renderer.onContextGain(gl);
     runner.start();
