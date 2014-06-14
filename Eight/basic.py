e8e3a4d91d6668a1674e93edcbcb6f7ea365a82a
David Geo Holmes <geometryzen@gmail.com>
2014-06-13 22:44:51 -0400

    Save file.

--- Eight/basic.py
+++ Eight/basic.py
@@ -47,7 +47,7 @@ workbench3D = workbench3D(renderer.canvas, renderer, camera, glwin)
 tau = 2 * pi
 omega = (tau / 20) / second
 # A unit bivector rotating from k to i
-B = BivectorE3(0.0, 0.0, 1.0)
+B = bivectorE3(0.0, 0.0, 1.0)
 # Just make sure that we really do have a unit bivector.
 B = B / magnitude(B)
 
