9315884783d6099dcb3c79cf5bebb68aa2f50a26
David Geo Holmes <geometryzen@gmail.com>
2014-06-13 22:37:09 -0400

    Save file.

--- Eight/basic.py
+++ Eight/basic.py
@@ -35,10 +35,10 @@ camera = perspectiveCamera(45, 1.0, 0.1, 100)
 
 renderer = webGLRenderer()
 
-box = mesh(eight.boxGeometry())
+box = mesh(boxGeometry())
 scene.add(box)
 box.position = eight.vectorE3(-1.0,-0.5,-5.0)
-prism = mesh(eight.prismGeometry())
+prism = mesh(prismGeometry())
 scene.add(prism)
 prism.position = eight.vectorE3(0.0,0.0,-5.0)
 
