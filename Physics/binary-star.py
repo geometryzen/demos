179c76fc9fccd7824fffad8c569f8b2869a796e2
David Geo Holmes <geometryzen@gmail.com>
2014-06-17 15:53:01 -0400

    Save file.

--- Physics/binary-star.py
+++ Physics/binary-star.py
@@ -42,7 +42,7 @@ def setUp():
 
 def tick(t):
     r = dwarf.position - giant.position
-    F = giant.mass * dwarf.mass * r / pow(abs(r % r), 3/2)
+    F = giant.mass * dwarf.mass * r / pow(abs(r | r), 3/2)
     giant.momentum = giant.momentum + F * dt
     dwarf.momentum = dwarf.momentum - F * dt
     
