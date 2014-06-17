4b10b8a6357bbf026712d23cd95a614b38ecacea
David Geo Holmes <geometryzen@gmail.com>
2014-06-17 15:59:08 -0400

    Save file.

--- Physics/rod-on-frictionless-surface.py
+++ Physics/rod-on-frictionless-surface.py
@@ -8,6 +8,7 @@ def show(name, measure):
 e1 = VectorE3(1,0,0) # Imagine pointing to the right
 e2 = VectorE3(0,1,0) # Imaging going into the screen
 e3 = e1.cross(e2)    # Imaging pointing upwards
+I3 = e1 ^ e2 ^ e3
 
 # Force is 6 N to the right
 F = 6 * newton * e1
@@ -20,7 +21,7 @@ r = 0.5 * meter * e3
 show("moment arm, r", r)
 
 # As a vector, the torque is given by (using Geometric Algebra)
-N = - e1 * e2 * e3 * (r ^ F)
+N = - I3 * (r ^ F)
 
 show("torque, N = - I (r ^ F)", N)
 show("torque, N = r x F", r.cross(F))
