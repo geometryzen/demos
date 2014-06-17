eb25ecaf95c4ab453b983ee38703871352caeba1
David Geo Holmes <geometryzen@gmail.com>
2014-06-17 14:56:23 -0400

    Save file.

--- Physics/angular-velocity-as-bivector.py
+++ Physics/angular-velocity-as-bivector.py
@@ -10,7 +10,7 @@ e3 = VectorE3(0,0,1)
 I_3 = e1 * e2 * e3
 
 # Conventionally, we think of the angular velocity as a vector.
-# Would be nice to have radians in units here!
+# Would be nice to have radians in units here?
 omegaAxial = 5 * e3 / second
 
 print omegaAxial
