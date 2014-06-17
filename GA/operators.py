7118db2d5e70be4b6699aa26a60f273d90126251
David Geo Holmes <geometryzen@gmail.com>
2014-06-17 15:11:52 -0400

    Save file.

--- GA/operators.py
+++ GA/operators.py
@@ -5,10 +5,6 @@ e1 = VectorE3(1,0,0)
 e2 = VectorE3(0,1,0)
 e3 = VectorE3(0,0,1)
 
-print e1
-print e2
-print e3
-
 a = random() * e1 + random() * e2 + random() * e3
 b = random() * e1 + random() * e2 + random() * e3
 
