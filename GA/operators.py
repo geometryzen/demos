4763e8829d8c37e38f83dd0c946c0f6d24e2c325
David Geo Holmes <geometryzen@gmail.com>
2014-06-17 15:47:16 -0400

    Save file.

--- GA/operators.py
+++ GA/operators.py
@@ -1,11 +1,12 @@
 from e3ga import *
 from random import *
+from units import *
 
 e1 = VectorE3(1,0,0)
 e2 = VectorE3(0,1,0)
 e3 = VectorE3(0,0,1)
 
-a = random() * e1 + random() * e2 + random() * e3
+a = (random() * e1 + random() * e2 + random() * e3) * meter
 b = random() * e1 + random() * e2 + random() * e3
 c = random() * e1 + random() * e2 + random() * e3
 
