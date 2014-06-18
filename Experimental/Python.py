292f26f486558d40d34a4c03313c7be9ddbdb097
David Geo Holmes <geometryzen@gmail.com>
2014-06-18 17:17:32 -0400

    Save file.

--- Experimental/Python.py
+++ Experimental/Python.py
@@ -1,18 +1,2 @@
-yORn = int(input("Type 1 to run the program, Type 2 to Exit:  "))
-while yORn == 1:
-   Name = str(input("What is your name?"))
-   print("      1111111111111111111111     ")
-   print("      1                    1     ")
-   print("      1                    1     ")
-   print("      1   Hello...         1     ")
-   print("      1        ", Name,"   1     ")
-   print("      1                    1     ")
-   print("      1                    1     ")
-   print("      1111111111111111111111___  ")
-   print("             11111111          | ")
-   print("     ------------------------- O ")
-   print("    1.............._... ... 1    ")
-   print("   1...................... 1     ")
-   print("  -------------------------      ")
-   yORn = int(input("Type 1 to run the program, Type 2 to Exit:  "))
-print "GoodBye"
+choices=('a
+         'b')
