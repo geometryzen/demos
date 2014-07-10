e6403a01059294dedde60af66d8d5e2eded38476
David Geo Holmes <geometryzen@gmail.com>
2014-07-10 02:34:37 -0400

    Save file.

--- Eight/basic.ts
+++ Eight/basic.ts
@@ -27,15 +27,16 @@ function setUp() {
 var B = eight.bivectorE3(0,0,1);
 var angle: number = 0;
 
-//var stats = new Stats();
-//stats.setMode(0);
-//stats.domElement.style.position = 'absolute';
-//stats.domElement.style.left = '0px';
-//stats.domElement.style.top = '0px';
-//glwin.document.body.appendChild(stats.domElement);
+var x: any = window
+var stats = new x.Stats();
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+popUp.document.body.appendChild(stats.domElement);
 
 function tick(t) {
-//  stats.begin();
+  stats.begin();
   // Klunky math until we get the operator overloading and GA carried over to JavaScript!
   var c = eight.scalarE3(Math.cos(angle/2));
   var s = eight.scalarE3(Math.sin(angle/2));
@@ -45,7 +46,7 @@ function tick(t) {
 
   renderer.render(scene, camera);
   angle += 0.01;
-//  stats.end();
+  stats.end();
 }
 
 function terminate(t) {return false;}
