b25cd8034448f52eb5fdfac0cad504388b5216e3
David Geo Holmes <geometryzen@gmail.com>
2014-08-01 14:13:46 -0400

    Save file.

--- Education/termination.py
+++ Education/termination.py
@@ -1,7 +1,7 @@
 from math import *
 
-def time(D,F,R, alpha):
+def time(D,F,R, alpha, N):
     f = F/R
     return D - f - sqrt(D * (D - 2* alpha) + f * (f - 2 * N))
 
-print time(8, 1700, 1700, 2)
+print time(8, 1700, 1700, 2, 2)
