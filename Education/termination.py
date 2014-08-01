aeddfe0144f11e26a51b44d216e4d47633156133
David Geo Holmes <geometryzen@gmail.com>
2014-08-01 14:52:09 -0400

    Save file.

--- Education/termination.py
+++ Education/termination.py
@@ -18,4 +18,4 @@ for F in range(0, 4500, 500):
     print F, T(D, F, R, alpha, N)
     
 for T in range(0, 80, 5):
-    print T, fee(R, T/10.0, D, alpha, N)
+    print T/10.0, fee(R, T/10.0, D, alpha, N)
