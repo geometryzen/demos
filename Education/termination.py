cdf5b58ec5c4df4a0c5d1294f737cbe7848e0237
David Geo Holmes <geometryzen@gmail.com>
2014-08-01 14:51:22 -0400

    Save file.

--- Education/termination.py
+++ Education/termination.py
@@ -2,8 +2,11 @@ from math import *
 
 def T(D, F, R, alpha, N):
     f = F/R
-    arg = D * (D - 2* alpha) + f * (f - 2 * N)
+    arg = D * (D - 2.0* alpha) + f * (f - 2.0 * N)
     return D - f - sqrt(arg)
+    
+def fee(R,T, D, alpha, N):
+    return ((T * T / 2.0) - T * D + alpha * D) / (D - (N + T))
 
 D = 8
 R = 1700.0
@@ -13,3 +16,6 @@ N = 2
 print "Fee versus Time"
 for F in range(0, 4500, 500):
     print F, T(D, F, R, alpha, N)
+    
+for T in range(0, 8, 0.5):
+    print T, fee(R, T, alpha, N)
