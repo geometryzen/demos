e4133eeb8ea330f53ebdcfa596e91549297dac4d
David Geo Holmes <geometryzen@gmail.com>
2014-08-01 14:37:55 -0400

    Save file.

--- Education/termination.py
+++ Education/termination.py
@@ -6,10 +6,10 @@ def T(D, F, R, alpha, N):
     return D - f - sqrt(D * (D - 2* alpha) + f * (f - 2 * N))
 
 D = 8
-R = 1700
+R = 1700.0
 alpha = 2
 N = 2
 
-print T(D, 1700, R, alpha, N)
-print T(D, 2000, R, alpha, N)
-print T(D, 2500, R, alpha, N)
+print T(D, 1700.0, R, alpha, N)
+print T(D, 2000.0, R, alpha, N)
+print T(D, 2500.0, R, alpha, N)
