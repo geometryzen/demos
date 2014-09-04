e2203ea203c631274741ed7bd812410f3d8643e3
David Geo Holmes <geometryzen@gmail.com>
2014-09-04 14:53:18 -0400

    Save file.

--- Puzzles/September2014.ts
+++ Puzzles/September2014.ts
@@ -8,9 +8,10 @@ How many stickers from the pack are left?
 // The prime factors of 2009 are 7 (twice) and 41.
 // There are only three factors so these must be the side lengths.
 var x = 7
-var z = 41
-var volume = x * x * z
+var height = 41
+var volume = x * x * height
+var area = x * x * 2 + x * height * 4
 console.log("volume: " + volume)
-console.log("area: " + ((x * x * 2) + (x * z * 4)))
+console.log("area: " + area)
 // The area is 1246 units squared so the number of stickers remaining is 2009 - 1246 = 763
 console.log("#stickers remaining: " + (2009-1246))
