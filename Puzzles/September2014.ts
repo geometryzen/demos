9d204982964e60927f107c52357e3ee5d7313b0a
David Geo Holmes <geometryzen@gmail.com>
2014-09-04 14:47:30 -0400

    Save file.

--- Puzzles/September2014.ts
+++ Puzzles/September2014.ts
@@ -1,8 +1,7 @@
 // The prime factors of 2009 are 7 (twice) and 41.
 // There are only three factors so these must be the side lengths.
 var x = 7
-var y = 7
 var z = 41
-var volume = x * y * z
+var volume = x * x * z
 console.log("volume: " + volume)
-console.log("area: " + (x * y * 2) + (y * z * 2) + (z * x * 2))
+console.log("area: " + (x * x * 2) + (x * z * 4))
