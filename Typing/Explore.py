21a09e57aee82c4fef3ccf88b0197cbde20dd1e5
David Geo Holmes <geometryzen@gmail.com>
2014-09-20 14:16:17 -0400

    Save file.

--- Typing/Explore.py
+++ Typing/Explore.py
@@ -1,6 +1,7 @@
 # Typing/Explore.py
 @typechecked
-def greet(name: str, age: int) -> str:
+def greet(name, age):
+#def greet(name: str, age: int) -> str:
     print('Hello {0}, you are {1} years old'.format(name, age))
     
 greet(1,28)
