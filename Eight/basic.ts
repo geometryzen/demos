a1a375cf8974db04d9684f23a6731aec92fbf4e6
David Geo Holmes <geometryzen@gmail.com>
2014-09-25 16:29:51 -0400

    Save file.

--- Eight/basic.ts
+++ Eight/basic.ts
@@ -33,6 +33,7 @@ stats.setMode(0);
 stats.domElement.style.position = 'absolute';
 stats.domElement.style.left = '0px';
 stats.domElement.style.top = '0px';
+
 popUp.document.body.appendChild(stats.domElement);
 
 /**
