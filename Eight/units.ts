fab8608c90cb343ae9059aa0cd8b254e2b6ca56b
David Geo Holmes <geometryzen@gmail.com>
2014-09-25 16:37:54 -0400

    Save file.

--- Eight/units.ts
+++ Eight/units.ts
@@ -1,1 +1,9 @@
 console.log(blade.UNIT_KILOGRAM.toString())
+
+var quantity = new blade.Euclidean3(0,1,0,0,0,0,0,0);
+
+var unit = blade.UNIT_INCH;
+
+var measure = new blade.Measure(quantity, unit);
+
+console.log(measure.toString());
