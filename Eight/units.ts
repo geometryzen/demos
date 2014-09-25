a08e76c37f1e4ac44e044e06d2703d6ba219e75f
David Geo Holmes <geometryzen@gmail.com>
2014-09-25 16:44:49 -0400

    Save file.

--- Eight/units.ts
+++ Eight/units.ts
@@ -4,10 +4,15 @@ var quantity = new blade.Euclidean3(1,0,0,0,0,0,0,0);
 
 var unit = blade.UNIT_INCH;
 
-var measure = new blade.Measure(quantity, unit);
+var measure = new blade.Measure<blade.Euclidean3>(quantity, unit);
 
 console.log(measure.toString());
 
 var sq = measure.mul(measure);
 
+var q = sq.quantity;
+var u = sq.uom;
+
 console.log(sq.toString());
+
+console.log(u.toString());
