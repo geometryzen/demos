47240688f7c98b12f6b4f4ac45447126b84a51e7
David Geo Holmes <geometryzen@gmail.com>
2014-09-28 12:56:31 -0400

    Save file.

--- Drawing/canvas-101.ts
+++ Drawing/canvas-101.ts
@@ -1,8 +1,8 @@
 // Workaround to prevent TS2082 and TS2087.
 // I don't know why this works.
 var unused: Window = window;
-
-var popUp: Window;
+// Global Variables.
+var popUp: Window = window.open("", "", "width=800, height=600", false);
 var context: CanvasRenderingContext2D;
 
 /**
@@ -31,8 +31,6 @@ function terminate(time: number): boolean {
  * Called once at the start of the animation.
  */
 function setUp() {
-  popUp = window.open("", "", "width=800, height=600", false);
-  
   var document = popUp.document;
   
   var canvas = document.createElement("canvas");
