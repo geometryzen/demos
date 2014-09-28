636391c7be36bbf7f085d026f9766d946e0f2bc2
David Geo Holmes <geometryzen@gmail.com>
2014-09-28 15:38:47 -0400

    Save file.

--- Drawing/canvas-101.ts
+++ Drawing/canvas-101.ts
@@ -48,11 +48,11 @@ class Cube {
 
     // back face
     printer.beginPath();
-    printer.moveTo(-100,-100,+100);
-    printer.lineTo(+100,-100,+100);
-    printer.lineTo(+100,+100,+100);
-    printer.lineTo(-100,+100,+100);
-    printer.lineTo(-100,-100,+100);
+    printer.moveTo(this.position.x - 100,-100,+100);
+    printer.lineTo(this.position.x+100,-100,+100);
+    printer.lineTo(this.position.x+100,+100,+100);
+    printer.lineTo(this.position.x-100,+100,+100);
+    printer.lineTo(this.position.x-100,-100,+100);
     printer.stroke();
 
     // RHS face
