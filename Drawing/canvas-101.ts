623c1ec6234e6f0f234520c0bef844de45cfeb98
David Geo Holmes <geometryzen@gmail.com>
2014-09-28 16:21:41 -0400

    Save file.

--- Drawing/canvas-101.ts
+++ Drawing/canvas-101.ts
@@ -63,11 +63,11 @@ class Cube {
 
     // back face
     printer.beginPath();
-    printer.moveTo(this.position.x + this.corners[4].x, this.position.y + this.corners[4].y, this.position.z + this.corners[4].z);
-    printer.lineTo(this.position.x + this.corners[5].x, this.position.y + this.corners[5].y, this.position.z + this.corners[5].z);
-    printer.lineTo(this.position.x + this.corners[6].x, this.position.y + this.corners[6].y, this.position.z + this.corners[6].z);
-    printer.lineTo(this.position.x + this.corners[7].x, this.position.y + this.corners[7].y, this.position.z + this.corners[7].z);
-    printer.lineTo(this.position.x + this.corners[4].x, this.position.y + this.corners[4].y, this.position.z + this.corners[4].z);
+    printer.moveTo(this.position.x + corners[4].x, this.position.y + corners[4].y, this.position.z + corners[4].z);
+    printer.lineTo(this.position.x + corners[5].x, this.position.y + corners[5].y, this.position.z + corners[5].z);
+    printer.lineTo(this.position.x + corners[6].x, this.position.y + corners[6].y, this.position.z + corners[6].z);
+    printer.lineTo(this.position.x + corners[7].x, this.position.y + corners[7].y, this.position.z + corners[7].z);
+    printer.lineTo(this.position.x + corners[4].x, this.position.y + corners[4].y, this.position.z + corners[4].z);
     printer.stroke();
 
     // LHS face
