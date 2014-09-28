6d971be12f37a3b7c3dc913d3be1c18ef0e88c9a
David Geo Holmes <geometryzen@gmail.com>
2014-09-28 15:42:44 -0400

    Save file.

--- Drawing/canvas-101.ts
+++ Drawing/canvas-101.ts
@@ -39,20 +39,20 @@ class Cube {
   {
     // front face
     printer.beginPath();
-    printer.moveTo(this.position.x - 100,-100,this.position.z - 100);
-    printer.lineTo(this.position.x + 100,-100,this.position.z - 100);
-    printer.lineTo(this.position.x + 100,+100,this.position.z - 100);
-    printer.lineTo(this.position.x - 100,+100,this.position.z - 100);
-    printer.lineTo(this.position.x - 100,-100,this.position.z - 100);
+    printer.moveTo(this.position.x - 100,-100, this.position.z - 100);
+    printer.lineTo(this.position.x + 100,-100, this.position.z - 100);
+    printer.lineTo(this.position.x + 100,+100, this.position.z - 100);
+    printer.lineTo(this.position.x - 100,+100, this.position.z - 100);
+    printer.lineTo(this.position.x - 100,-100, this.position.z - 100);
     printer.stroke();
 
     // back face
     printer.beginPath();
-    printer.moveTo(this.position.x - 100,-100,+100);
-    printer.lineTo(this.position.x+100,-100,+100);
-    printer.lineTo(this.position.x+100,+100,+100);
-    printer.lineTo(this.position.x-100,+100,+100);
-    printer.lineTo(this.position.x-100,-100,+100);
+    printer.moveTo(this.position.x - 100, -100, this.position.z + 100);
+    printer.lineTo(this.position.x + 100, -100, this.position.z + 100);
+    printer.lineTo(this.position.x + 100, +100, this.position.z + 100);
+    printer.lineTo(this.position.x - 100, +100, this.position.z + 100);
+    printer.lineTo(this.position.x - 100, -100, this.position.z + 100);
     printer.stroke();
 
     // RHS face
