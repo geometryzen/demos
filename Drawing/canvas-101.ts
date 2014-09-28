ca6adaba2f02956a451fc49501092b3b443d7b07
David Geo Holmes <geometryzen@gmail.com>
2014-09-27 21:39:04 -0400

    Save file.

--- Drawing/canvas-101.ts
+++ Drawing/canvas-101.ts
@@ -12,12 +12,7 @@ document.body.appendChild(canvas);
 
 var context = canvas.getContext("2d");
 
-context.fillStyle = "#0066CC";
-context.fillRect(0, 0, 400, 400);
-context.clearRect(75, 75, 250, 250);
-
-context.strokeStyle = "#FF0000"
-context.strokeRect(150, 150, 100, 100);
+context.beginPath();
 
 
 //popUp.close();
