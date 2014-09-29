500da8f72ad3fb6caac0be8f44302400c49f0484
David Geo Holmes <geometryzen@gmail.com>
2014-09-29 18:43:03 -0400

    Save file.

--- Drawing/vanishingPoints.py
+++ Drawing/vanishingPoints.py
@@ -3,7 +3,7 @@ from browser import window;
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 800
 
-s = "width=%s, height=%s" % (WINDOW_WIDTH, WINDOW_HEIGHT)
+s = "width={0}, height={1}".format(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 print s
 
