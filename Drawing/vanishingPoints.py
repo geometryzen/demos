a1da01c260e44306a5dd328a0609dcc56b5df7b0
David Geo Holmes <geometryzen@gmail.com>
2014-09-29 18:47:46 -0400

    Save file.

--- Drawing/vanishingPoints.py
+++ Drawing/vanishingPoints.py
@@ -6,4 +6,7 @@ WINDOW_HEIGHT = 800
 
 popUp = window.open("","","width=%s, height=%s" % (WINDOW_WIDTH, WINDOW_HEIGHT))
 
+def tick(t):
+    pass
+
 WindowAnimationRunner(tick, terminate, setUp, tearDown)
