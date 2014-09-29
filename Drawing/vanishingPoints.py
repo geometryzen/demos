a5525956159538fc71ab31e2a04ed6088ef85f13
David Geo Holmes <geometryzen@gmail.com>
2014-09-29 18:39:54 -0400

    Save file.

--- Drawing/vanishingPoints.py
+++ Drawing/vanishingPoints.py
@@ -3,4 +3,4 @@ from browser import window;
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 800
 
-popUp = window.open("","","width=%1, height=800" % (WINDOW_WIDTH, WINDOW_HEIGHT))
+popUp = window.open("","","width=%1, height=%2" % (WINDOW_WIDTH, WINDOW_HEIGHT))
