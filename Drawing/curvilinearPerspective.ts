3904d2de11a09c166a10999dc0b63473c504b8ef
David Geo Holmes <geometryzen@gmail.com>
2014-10-15 15:38:36 -0400

    Save file.

--- Drawing/curvilinearPerspective.ts
+++ Drawing/curvilinearPerspective.ts
@@ -250,6 +250,7 @@ function tick(time: number): void {
   
   // Draw the cube at the appropriate attitude.
   for (var i=0;i<cubes.length;i++) {
+    cubes[i].attitude = R
     cubes[i].draw();
   }
 
