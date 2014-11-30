1ed086ca1f544e133b2b884e77950e52886adb55
David Geo Holmes <geometryzen@gmail.com>
2014-11-30 16:56:59 -0500

    Save file.

--- Tutorials/Canvas/Lesson002-The-Canvas.ts
+++ Tutorials/Canvas/Lesson002-The-Canvas.ts
@@ -1,1 +1,68 @@
-# Lesson002-The-Canvas.ts
+/**
+ * Demonstrates the vanishing points from 3D space projected onto a 2D canvas.
+ */
+
+// Workaround to prevent TS2082 and TS2087.
+// I don't know why this works.
+var unused: Window = window;
+
+var WINDOW_HEIGHT = 800;
+var WINDOW_WIDTH  = 800;
+var WINDOW_HALF_HEIGHT = WINDOW_HEIGHT / 2;
+var WINDOW_HALF_WIDTH  = WINDOW_WIDTH / 2;
+var CANVAS_HEIGHT = 800;
+var CANVAS_WIDTH  = 800;
+var CANVAS_HALF_HEIGHT = CANVAS_HEIGHT / 2;
+var CANVAS_HALF_WIDTH  = CANVAS_WIDTH / 2;
+var CANVAS_DISTANCE = 100;
+
+// Global Variables.
+var popUp: Window = window.open("", "", "width=" + WINDOW_WIDTH + ", height=" + WINDOW_HEIGHT, false);
+var context: CanvasRenderingContext2D;
+
+/**
+ * Called for each animation tick.
+ */
+function tick(time: number): void {
+  // Set the background color to gray.
+  context.fillStyle = "#555555";
+  context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+/**
+ * Called to determine whether to end the animation.
+ */
+function terminate(time: number): boolean {
+  return false;
+}
+
+/**
+ * Called once at the start of the animation.
+ */
+function setUp() {
+  var popDoc = popUp.document;
+  
+  var canvas = popDoc.createElement("canvas");
+  
+  canvas.setAttribute("id", "graph");
+  canvas.setAttribute("width",  CANVAS_WIDTH.toString());
+  canvas.setAttribute("height", CANVAS_HEIGHT.toString());
+  
+  popDoc.body.appendChild(canvas);
+  // Remove the margin that pushes the canvas.
+  popDoc.body.style.margin = "0";
+  
+  context = canvas.getContext("2d");
+}
+
+/**
+ * Called once at the end of the animation.
+ */
+function tearDown(e: Error) {
+  popUp.close();
+  if (e) {
+    alert(e.message);
+  }
+}
+
+eight.animationRunner(tick, terminate, setUp, tearDown, popUp).start();
