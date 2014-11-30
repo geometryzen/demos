85dff70148a0bd183a9cf959dd751279a66a04b3
David Geo Holmes <geometryzen@gmail.com>
2014-11-30 17:17:35 -0500

    Save file.

--- Tutorials/Canvas/Lesson001-B.ts
+++ Tutorials/Canvas/Lesson001-B.ts
@@ -6,21 +6,16 @@
 // I don't know why this works.
 var unused: Window = window;
 
-var WINDOW_HEIGHT = 800;
-var WINDOW_WIDTH  = 800;
-var WINDOW_HALF_HEIGHT = WINDOW_HEIGHT / 2;
-var WINDOW_HALF_WIDTH  = WINDOW_WIDTH / 2;
-
 class PopUp {
   private win: Window;
-  constructor(width: number) {
-    this.win = window.open("", "", "width=" + width + ", height=" + WINDOW_HEIGHT, false);
+  constructor(width: number, height) {
+    this.win = window.open("", "", "width=" + width + ", height=" + height, false);
   }
   public close() {
     this.win.close();
   }
 }
 
-var popUp = new PopUp(800)
+var popUp = new PopUp(800, 600)
 
 //popUp.close();
