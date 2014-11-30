fb7ec3410c3c06e4f849f95a60f4f2cab045c3b0
David Geo Holmes <geometryzen@gmail.com>
2014-11-30 17:07:41 -0500

    Save file.

--- Tutorials/Canvas/Lesson001-B.ts
+++ Tutorials/Canvas/Lesson001-B.ts
@@ -1,1 +1,17 @@
-# Lesson001-B.ts
+/**
+ * Demonstrates launching a popup window.
+ */
+
+// Workaround to prevent TS2082 and TS2087.
+// I don't know why this works.
+var unused: Window = window;
+
+var WINDOW_HEIGHT = 800;
+var WINDOW_WIDTH  = 800;
+var WINDOW_HALF_HEIGHT = WINDOW_HEIGHT / 2;
+var WINDOW_HALF_WIDTH  = WINDOW_WIDTH / 2;
+
+// Global Variables.
+var popUp: Window = window.open("", "", "width=" + WINDOW_WIDTH + ", height=" + WINDOW_HEIGHT, false);
+
+//popUp.close()
