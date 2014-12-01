42a2c244d51922cf3c3864613488f19385862cc4
David Geo Holmes <geometryzen@gmail.com>
2014-12-01 12:34:46 -0500

    Save file.

--- Tutorials/Canvas/Lesson006-Euler.ts
+++ Tutorials/Canvas/Lesson006-Euler.ts
@@ -212,11 +212,14 @@ class Complex {
 var R = new Complex(Math.cos(0.01), Math.sin(0.01));
 
 var f = function(z: Complex): Complex {
+  return z;
+  /*
   var z3 = z.multiply(z).multiply(z);
   var z2 = z.multiply(z);
   var sz = new Complex(6,0).multiply(z);
   var tw = new Complex(20,0);
   return z3.add(z2).add(sz).subtract(tw);
+  */
 };
 
 class MinMax {
