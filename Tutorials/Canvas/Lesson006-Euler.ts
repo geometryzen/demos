0f3dce71a1de69163f7c0b03219b50d944ce632d
David Geo Holmes <geometryzen@gmail.com>
2014-12-01 15:36:48 -0500

    Save file.

--- Tutorials/Canvas/Lesson006-Euler.ts
+++ Tutorials/Canvas/Lesson006-Euler.ts
@@ -188,6 +188,7 @@ class ComplexPlane {
         var H = this.f(z).arg();
         var S = 1;
         var L = lightnessFromMagnitude(this.f(z).mod());
+        L = 0.5;
         this._canvas.context.fillStyle = colorFromHSL(H, S, L).asFillStyle();
         this._canvas.context.fillRect(X,Y,1,1);
       }
