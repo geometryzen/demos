b0eb97a29c698fef4fcbd26aad9c8a0b0e1d0ed6
David Geo Holmes <geometryzen@gmail.com>
2014-12-01 10:40:04 -0500

    Save file.

--- Tutorials/Canvas/Lesson005-Complex.ts
+++ Tutorials/Canvas/Lesson005-Complex.ts
@@ -24,7 +24,7 @@ console.log("zero: " + zero);
 console.log("one: " + one);
 console.log("i: " + i);
 
-console.log("arg(one) " + one.arg());
+console.log("arg(1) " + new Complex(1,0).arg());
 console.log("arg(i) " + i.arg());
 console.log("arg(-1) " + new Complex(-1,0).arg());
 console.log("arg(-i) " + new Complex(0,-1).arg());
