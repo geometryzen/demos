adfa37aacb1fdebc6c669ea8c313a01169c753a2
David Geo Holmes <geometryzen@gmail.com>
2014-12-01 20:52:39 -0500

    Save file.

--- Tutorials/Canvas/Lesson006-Euler.py
+++ Tutorials/Canvas/Lesson006-Euler.py
@@ -159,14 +159,6 @@ class ComplexPlane:
         '''
 
 R = 10
-one = Complex(1,0)
-i = Complex(0,1)
-print one
-print i
-print one + i
-print one - i
-print one * i
-print one / i
-#cp = ComplexPlane(MinMax(-R,+R), MinMax(-R,+R),f)
+cp = ComplexPlane(MinMax(-R,+R), MinMax(-R,+R),f)
 #cp.draw()
 
