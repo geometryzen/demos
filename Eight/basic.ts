af602edf9c3355d7b99511d484e4754f45ef6c49
David Geo Holmes <geometryzen@gmail.com>
2015-03-13 16:34:16 -0400

    Save file.

--- Eight/basic.ts
+++ Eight/basic.ts
@@ -3,6 +3,8 @@ var log = function(s: any) {
   w.Sk.output(s+'\n');
 };
 
+log("Hello!");
+
 var popUp: Window = open("", "", "width=800, height=600");
 
 popUp.document.body.style.backgroundColor = "202020";
