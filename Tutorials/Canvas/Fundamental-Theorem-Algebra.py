f7234d7ed747e46adaac43bbf8852e5fa347ac21
David Geo Holmes <geometryzen@gmail.com>
2015-03-15 21:35:27 -0400

    Save file.

--- Tutorials/Canvas/Fundamental-Theorem-Algebra.py
+++ Tutorials/Canvas/Fundamental-Theorem-Algebra.py
@@ -4,22 +4,22 @@ from math import *
 from units import *
 from easel import *
 
-glwin = window.open("","","width=800,height=600")
-glwin.document.body.style.backgroundColor = "202020"
-glwin.document.body.style.overflow = "hidden"
-glwin.document.title = "Visualizing Geometric Algebra with WebGL"
+popup = window.open("","","width=800,height=600")
+popup.document.body.style.backgroundColor = "202020"
+popup.document.body.style.overflow = "hidden"
+popup.document.title = "Visualizing Geometric Algebra with WebGL"
 
 canvas2D = glwin.document.createElement("canvas")
 canvas2D.style.position = "absolute"
 canvas2D.style.top = "0px"
 canvas2D.style.left = "0px"
-workbench2D = Workbench2D(canvas2D, glwin)
+workbench2D = Workbench2D(canvas2D, popup)
 space2D = Stage(canvas2D)
 space2D.autoClear = True
 
 font = "20px Helvetica"
 
-output = Text(glwin.document.title + ". Hit Esc key to exit.", font, "white")
+output = Text(popup.document.title + ". Hit Esc key to exit.", font, "white")
 output.x = 100
 output.y = 60
 space2D.addChild(output)
@@ -29,46 +29,13 @@ stats.setMode(0)
 stats.domElement.style.position = 'absolute'
 stats.domElement.style.left = '0px'
 stats.domElement.style.top = '0px'
-glwin.document.body.appendChild(stats.domElement)
-
-scene = scene()
-
-camera = perspectiveCamera(45, 1.0, 0.1, 100)
-
-renderer = webGLRenderer()
-
-box = mesh(boxGeometry())
-scene.add(box)
-box.position = vectorE3(-1.0,-0.5,-5.0)
-prism = mesh(prismGeometry())
-scene.add(prism)
-prism.position = vectorE3(0.0,0.0,-5.0)
-
-workbench3D = workbench3D(renderer.canvas, renderer, camera, glwin)
-
-tau = 2 * pi
-omega = (tau / 20) / second
-# A unit bivector rotating from k to i
-B = BivectorE3(0.0, 1.0, 1.0)
-# Just make sure that we really do have a unit bivector.
-B = B / magnitude(B)
+popup.document.body.appendChild(stats.domElement)
 
 def setUp():
     workbench2D.setUp()
-    workbench3D.setUp()
-    monitor.start()
 
 def tick(t):
     stats.begin()
-    time = t * second
-    theta = omega * time
-
-    rotor = exp(-B*theta.quantity/2.0)
-
-    box.attitude = rotor
-    prism.attitude = rotor
-
-    renderer.render(scene, camera)
     space2D.render()
     stats.end()
 
@@ -76,28 +43,11 @@ def terminate(t):
     return False
 
 def tearDown(e):
-    monitor.stop()
-    glwin.close()
+    popup.close()
     if e:
         print "Error during animation: %s" % (e)
     else:
         print "Goodbye!"
-    workbench3D.tearDown()
     workbench2D.tearDown()
-    scene.tearDown()
 
-runner = windowAnimationRunner(tick, terminate, setUp, tearDown, glwin)
-
-def onContextLoss():
-    runner.stop()
-    renderer.onContextLoss()
-    scene.onContextLoss()
-
-def onContextGain(gl):
-    scene.onContextGain(gl)
-    renderer.onContextGain(gl)
-    runner.start()
-
-monitor = webGLContextMonitor(renderer.canvas, onContextLoss, onContextGain)
-
-onContextGain(renderer.context)
+runner = windowAnimationRunner(tick, terminate, setUp, tearDown, popup)
