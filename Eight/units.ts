ddd767677283cd5772336cfabc83acf178b6dedb
David Geo Holmes <geometryzen@gmail.com>
2015-03-17 16:30:26 -0400

    Save file.

--- Eight/units.ts
+++ Eight/units.ts
@@ -11,6 +11,7 @@ log(blade.UNIT_METER.toString())
 log(blade.UNIT_SECOND.toString())
 log(blade.UNIT_COULOMB.toString())
 log(blade.UNIT_KELVIN.toString())
+log(blade.UNIT_MOLE.toString())
 
 var quantity = new blade.Euclidean3(1,0,0,0,0,0,0,0);
 
