caaec7eb0f042106efa94ff0a66d10280d149909
David Geo Holmes <geometryzen@gmail.com>
2015-03-19 17:52:12 -0400

    Save file.

--- MathScript/experimental.js
+++ MathScript/experimental.js
@@ -1,6 +1,6 @@
 var x = new blade.Euclidean3(0,4,0,0,0,0,0,0);
 var y = new blade.Euclidean3(0,0,6,0,0,0,0,0);
 var z = x.__add__(y);
-console.log(""+x);
+console.log(x.toString());
 console.log(""+y);
 console.log(""+z);
