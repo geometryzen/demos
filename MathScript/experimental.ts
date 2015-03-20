0073543686b32a56550e797ba6054847c4bdf114
David Geo Holmes <geometryzen@gmail.com>
2015-03-20 14:06:50 -0400

    Save file.

--- MathScript/experimental.ts
+++ MathScript/experimental.ts
@@ -1,6 +1,3 @@
 var x: any = new blade.Euclidean3(0,4,0,0,0,0,0,0);
 var y: any = new blade.Euclidean3(0,0,6,0,0,0,0,0);
-var z = x + y;
-console.log(x.toString());
-console.log(y.toString());
-console.log(z.toString());
+
