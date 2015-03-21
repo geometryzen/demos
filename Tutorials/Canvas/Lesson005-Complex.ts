38a2065719fe66516ddef4161f8cc9a84cd7ddfc
David Geo Holmes <geometryzen@gmail.com>
2015-03-21 11:40:34 -0400

    Save file.

--- Tutorials/Canvas/Lesson005-Complex.ts
+++ Tutorials/Canvas/Lesson005-Complex.ts
@@ -29,7 +29,7 @@ class Complex {
 }
 
 var zero = new blade.Complex(0,0);
-var one = new blade.Complex(1,0);
+var one = new Complex(1,0);
 var i = new Complex(0,1);
 var minusOne = new Complex(-1,0);
 var minusI = new Complex(0,-1);
