62f6e098897628953dad3a52409bc03bdec8def6
David Geo Holmes <geometryzen@gmail.com>
2015-03-25 21:12:47 -0400

    Save file.

--- Easel/localToGlobal.ts
+++ Easel/localToGlobal.ts
@@ -36,5 +36,45 @@ class Workbench2D
   }
 }
 
-var element: any = canvas.node();
-var elemCanvas = element;
+var canvas = document.createElement("canvas")
+
+var stage = new createjs.Stage(canvas, "", {})
+
+var target:any = stage.addChild(new createjs.Shape())
+target.graphics.beginFill("blue").drawCircle(0, 0, 50).endFill()
+target.graphics.beginFill("white").drawCircle(0, 0, 30).endFill()
+target.graphics.beginFill("red").drawCircle(0, 0, 10).endFill()
+target.x = 100
+target.y = 180
+
+var arm: any = stage.addChild(new createjs.Shape())
+arm.graphics.beginFill("black").drawRect(-2, -2, 100, 4).endFill()
+arm.graphics.beginFill("black").drawCircle(100, 0, 8).endFill()
+arm.x = 180
+arm.y = 100
+
+var workbench2D = new Workbench2D(canvas)
+
+function setUp() {
+    workbench2D.setUp()
+}
+
+function tick(t) {
+    arm.rotation = 180 * t
+    target.alpha = 0.2
+    var point = arm.localToLocal(100, 0, target)
+    if (target.hitTest(point.x, point.y)) {
+        target.alpha = 1
+    }
+    stage.update()
+}
+
+function terminate(t) {
+    return t > 10
+}
+
+function tearDown(e) {
+    workbench2D.tearDown()
+}
+
+eight.animationRunner(tick, terminate, setUp, tearDown, window).start()
