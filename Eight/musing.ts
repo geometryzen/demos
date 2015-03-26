10d1119e7bc572da46fd0bb529e3c2fd1edf7aac
David Geo Holmes <geometryzen@gmail.com>
2015-03-25 22:57:02 -0400

    Save file.

--- Eight/musing.ts
+++ Eight/musing.ts
@@ -150,7 +150,7 @@ function setUp() {
     workbench3D.setUp()
 }
 
-function tick(time) {
+function tick(time: number) {
     var theta = omega * time
     // The rotor is defined to have a minus sign.
     var rotor = new blade.Euclidean3(Math.cos(theta/2),0,0,0,0,0,0,0).sub(B.mul(Math.sin(theta/2)));
@@ -166,7 +166,7 @@ function tick(time) {
 //    space2D.draw()
 }
 
-function terminate() {
+function terminate(time: number) {
   return false;
 }
 
