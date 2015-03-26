f489239c3e658fc6d7ea7f7d6e838e60df32342f
David Geo Holmes <geometryzen@gmail.com>
2015-03-25 21:07:11 -0400

    Save file.

--- Easel/localToGlobal.ts
+++ Easel/localToGlobal.ts
@@ -1,1 +1,40 @@
-# localToGlobal.ts
+function removeElementsByTagName(tagName) {
+  var elements = document.getElementsByTagName(tagName);
+  for (var i = elements.length - 1; i >= 0; i--) {
+    var e = elements[i];
+    e.parentNode.removeChild(e);
+  }
+}
+
+class Workbench2D
+{
+  public canvas;
+  private sizer: EventListener;
+  constructor(canvas: HTMLCanvasElement)
+  {
+    this.canvas = canvas;
+    function onWindowResize(event)
+    {
+      var width  = window.innerWidth;
+      var height = window.innerHeight;
+      canvas.width  = width;
+      canvas.height = height;
+    }
+    this.sizer = onWindowResize;
+  }
+  setUp()
+  {
+    document.body.insertBefore(this.canvas, document.body.firstChild);
+    window.addEventListener('resize', this.sizer, false);
+    this.sizer(null);
+
+  }
+  tearDown()
+  {
+    window.removeEventListener('resize', this.sizer, false);
+    removeElementsByTagName("canvas");
+  }
+}
+
+var element: any = canvas.node();
+var elemCanvas = element;
