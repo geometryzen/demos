8ff8aa2b34e7645d7bbaadd37ba3abee9ee04b6f
David Geo Holmes <geometryzen@gmail.com>
2015-03-26 21:21:34 -0400

    Save file.

--- Eight/musing.ts
+++ Eight/musing.ts
@@ -153,9 +153,6 @@ function setUp() {
 
 function tick(time: number) {
     var theta = omega * time;
-    
-    flat.material.opacity = Math.abs(Math.sin(theta/2));
-    
     // The rotor is defined to have a minus sign.
     var s: any = new blade.Euclidean3(Math.cos(theta/2),0,0,0,0,0,0,0);
     var rotor: any = s - B * Math.sin(theta/2);
