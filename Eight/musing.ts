d8402951fa940c903f859107099757117ba9ee97
David Geo Holmes <geometryzen@gmail.com>
2015-03-27 08:39:00 -0400

    Save file.

--- Eight/musing.ts
+++ Eight/musing.ts
@@ -59,7 +59,7 @@ class Box extends Visual<THREE.BoxGeometry>
 
 class Vortex extends Visual<THREE.VortexGeometry>
 {
-  constructor(color: number, opacity: number = 1.0, transparent: boolean = false)
+  constructor(scale: number, color: number, opacity: number = 1.0, transparent: boolean = false)
   {
     super(new THREE.VortexGeometry(4.0, 0.32, 0.04, 0.08, 0.3, 8, 12), color, opacity, transparent);
   }
