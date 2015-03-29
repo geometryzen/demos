5a197779649a468993c462e1bcf064762a3c6034
David Geo Holmes <geometryzen@gmail.com>
2015-03-29 09:05:04 -0400

    Save file.

--- Physics/charged-particle-wire-2.py
+++ Physics/charged-particle-wire-2.py
@@ -1,7 +1,7 @@
 from browser import *
 #rom three import *
 from workbench import *
-from geometry import CartesianSpace, VectorE3, SphereBuilder, ScalarE3
+from geometry import CartesianSpace, VectorE3, SphereBuilder, ScalarE3, ProbeBuilderE3
 
 timeOut = 60
 
