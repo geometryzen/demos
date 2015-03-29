915abc868667c65797a4144562cb8cc86100777b
David Geo Holmes <geometryzen@gmail.com>
2015-03-29 19:31:47 -0400

    Save file.

--- Canvas-3D/Math/Euler.ts
+++ Canvas-3D/Math/Euler.ts
@@ -1,5 +1,10 @@
+var log = function(s: any) {
+  var w: any = window;
+  w.Sk.output(s+'\n');
+};
+
 var a = new THREE.Euler(0, 1, 1.57, 'XYZ');
 var b = new THREE.Vector3(1, 0, 1);
 var c = b.applyEuler(a);
 
-console.log(c.x + "\n")
+log(c.x + "\n")
