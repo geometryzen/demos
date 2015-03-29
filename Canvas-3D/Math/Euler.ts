1711e95f61dc61a1f1f39279cbfc4176dadae0b9
David Geo Holmes <geometryzen@gmail.com>
2015-03-29 19:31:02 -0400

    Save file.

--- Canvas-3D/Math/Euler.ts
+++ Canvas-3D/Math/Euler.ts
@@ -1,5 +1,5 @@
-var a = new THREE.Euler( 0, 1, 1.57, 'XYZ' );
-var b = new THREE.Vector3( 1, 0, 1 );
+var a = new THREE.Euler(0, 1, 1.57, 'XYZ');
+var b = new THREE.Vector3(1, 0, 1);
 var c = b.applyEuler(a);
 
 console.log(c.x + "\n")
