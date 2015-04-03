ed3152295a4d4cdde9ab270f7f3d652d2fb6deed
David Geo Holmes <geometryzen@gmail.com>
2015-04-03 14:25:35 -0400

    Save file.

--- JSXGraph/curve.ts
+++ JSXGraph/curve.ts
@@ -1,1 +1,16 @@
-# curve.ts
+var w: any = window;
+var graph = w.JXG.JSXGraph;
+var sin = Math.sin;
+
+document.getElementById("graph-container").innerHTML = '<div id="box" class="jxgbox"></div>'
+var div = document.getElementById("box")
+
+div.style.width  = "400px"
+div.style.height = "400px"
+
+var board = graph.initBoard("box", {"axis":true,"grid":true})
+
+var A = board.create('point',[1,1],{"name": 'Alice'})
+var B = board.create('point',[2,2],{"name":'Bob'})
+
+var f = board.create('functiongraph',[function(x) {return A.X() * sin(x)}])
