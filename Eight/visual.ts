b57c31238a1ab2b130d74e547e68b80845ace1ce
David Geo Holmes <geometryzen@gmail.com>
2015-04-03 09:32:25 -0400

    Save file.

--- Eight/visual.ts
+++ Eight/visual.ts
@@ -34,7 +34,7 @@ viz.scene.add(vortex)
 var box3 = new visual.Box(10.0,10.0,0.1, 0x0000FF, 0.25, true);
 viz.scene.add(box3);
 
-var sphere = new visual.Sphere();
+var sphere = new visual.Sphere({radius:1});
 viz.scene.add(sphere);
 
 var tau = 2 * Math.PI;
