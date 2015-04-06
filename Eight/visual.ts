749dc9456dfdf36352f6be28a02e132ef42ab670
David Geo Holmes <geometryzen@gmail.com>
2015-04-06 14:19:22 -0400

    Save file.

--- Eight/visual.ts
+++ Eight/visual.ts
@@ -23,6 +23,9 @@ viz.scene.add(box1);
 var arrow = new visual.Arrow(4.0, 0xFFFF00);
 viz.scene.add(arrow);
 
+var arrow2 = new visual.Arrow(4.0, 0xFFFFFF);
+viz.scene.add(arrow2);
+
 var box2 = new visual.Box(1.0, 2.0, 3.0, 0xFF0000, 0.25, false);
 viz.scene.add(box2);
 box2.position.set(3,-3,3);
