f89a34174d39d921b14f0d736921fcf391af175f
David Geo Holmes <geometryzen@gmail.com>
2015-04-07 02:35:45 -0400

    Save file.

--- Eight/visual.ts
+++ Eight/visual.ts
@@ -70,7 +70,7 @@ function setUp() { viz.setUp(); }
 
 function tick(time: number) {
     var theta = omega * time;
-    var x = B * theta / 2;
+    var x = - B * theta / 2;
 //  var rotor: any = Math.cos(theta/2) - B * Math.sin(theta/2);
     var rotor = exp(x);
     var r = e2;
