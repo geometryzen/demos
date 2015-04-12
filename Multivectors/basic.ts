bfb5175fc59b534d426d1b1bdad538a7af573a2c
David Geo Holmes <geometryzen@gmail.com>
2015-04-12 12:46:00 -0400

    Save file.

--- Multivectors/basic.ts
+++ Multivectors/basic.ts
@@ -2,4 +2,5 @@ var e1 = new blade.Euclidean3(0,1,0,0,0,0,0,0);
 var e2 = new blade.Euclidean3(0,0,1,0,0,0,0,0);
 var e3 = new blade.Euclidean3(0,0,0,1,0,0,0,0);
 
-console.log(e1+"")
+// First, we should be able to gracefully send text to the output window.
+Sk.output(e1+"")
