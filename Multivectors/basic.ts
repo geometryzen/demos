494d8385e5d6e897ca9b71ee27ff46e928fa31df
David Geo Holmes <geometryzen@gmail.com>
2015-04-12 19:12:57 -0400

    Save file.

--- Multivectors/basic.ts
+++ Multivectors/basic.ts
@@ -5,4 +5,4 @@ var e3 = new blade.Euclidean3(0,0,0,1,0,0,0,0);
 // First, we should be able to gracefully send text to the output window.
 Sk.output(e1+"")
 
-var times = e1 * e2;
+var times = e1 + e2;
