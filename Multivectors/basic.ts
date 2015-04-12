563e4dcd0b73fc31de9a8f446944e963baada394
David Geo Holmes <geometryzen@gmail.com>
2015-04-12 12:43:10 -0400

    Save file.

--- Multivectors/basic.ts
+++ Multivectors/basic.ts
@@ -2,4 +2,4 @@ var e1 = new blade.Euclidean3(0,1,0,0,0,0,0,0);
 var e2 = new blade.Euclidean3(0,0,1,0,0,0,0,0);
 var e3 = new blade.Euclidean3(0,0,0,1,0,0,0,0);
 
-console.log(e1)
+console.log(e1+"")
