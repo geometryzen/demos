16e9385e7adce343ac6de4b4d9edb73a61a5e670
David Geo Holmes <geometryzen@gmail.com>
2015-04-12 21:59:16 -0400

    Save file.

--- Multivectors/basic.ts
+++ Multivectors/basic.ts
@@ -3,7 +3,8 @@ var e2 = new blade.Euclidean3(0,0,1,0,0,0,0,0);
 var e3 = new blade.Euclidean3(0,0,0,1,0,0,0,0);
 
 // First, we should be able to gracefully send text to the output window.
-Sk.output(e1+"")
+var w: any = window;
+w.Sk.output(e1+"")
 
 var sum = e1 + e2;
 var diff = e1 - e2;
