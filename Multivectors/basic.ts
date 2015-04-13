3155b6d7049bc5dab8da8e1d7cf0c9fca39bf346
David Geo Holmes <geometryzen@gmail.com>
2015-04-12 22:00:23 -0400

    Save file.

--- Multivectors/basic.ts
+++ Multivectors/basic.ts
@@ -4,8 +4,7 @@ var e3 = new blade.Euclidean3(0,0,0,1,0,0,0,0);
 
 // First, we should be able to gracefully send text to the output window.
 var w: any = window;
-w.Sk.output(e1+"")
+w.Sk.output(e1+"\n")
 
-var sum = e1 + e2;
-var diff = e1 - e2;
-var lco = e1 << e2;
+var M = 1 + e1 + e2 + e3 + e1 * e2 + e2 * e3 + e3 * e1 + e1 * e2 * e3
+w.Sk.output(M+"\n");
