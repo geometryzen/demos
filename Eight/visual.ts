33514086236254d5a9a3ecd76553dfbb5381cb5b
David Geo Holmes <geometryzen@gmail.com>
2015-04-12 20:37:29 -0400

    Save file.

--- Eight/visual.ts
+++ Eight/visual.ts
@@ -3,7 +3,7 @@
  * Constructs a vector from the specified Cartesian components.
  */
 var vector: (x: number, y: number, z:  number)=>blade.Euclidean3 = visual.vectorE3;
-var kg = blade.UNIT_KILOGRAM;
+var kg = blade.units.kilogram;
 /**
  * The unit vector in the x-direction.
  */
