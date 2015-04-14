5cb60a6fb82e5ad78932f9a68cdc5e337f1f0852
David Geo Holmes <geometryzen@gmail.com>
2015-04-13 20:29:34 -0400

    Save file.

--- THREE/Introduction.ts
+++ THREE/Introduction.ts
@@ -14,6 +14,10 @@ camera.position.z = 5;
 
 function render() {
 	requestAnimationFrame( render );
+	
+	cube.rotation.x += 0.1;
+  cube.rotation.y += 0.1;
+
 	renderer.render( scene, camera );
 }
 render();
