ba4f22f1ce476636eb37a518f642e35c2f69c461
David Geo Holmes <geometryzen@gmail.com>
2015-04-14 08:23:08 -0400

    Save file.

--- Mathematics/e2ga-constants.py
+++ Mathematics/e2ga-constants.py
@@ -3,7 +3,7 @@ The e2ga module pre-defines the standard basis vectors e1 and e2, as well as the
 '''
 from e2ga import *
 
-e1 = VectorE2(1,0)
+#e1 = VectorE2(1,0)
 e2 = VectorE2(0,1)
 I = e1 * e2
 
