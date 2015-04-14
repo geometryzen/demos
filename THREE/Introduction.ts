6967ff85c33c39f2891671820185c27877cacf53
David Geo Holmes <geometryzen@gmail.com>
2015-04-13 20:34:44 -0400

    Save file.

--- THREE/Introduction.ts
+++ THREE/Introduction.ts
@@ -1,4 +1,4 @@
-// This is the exact introduction code given in the three.js documentation.
+// This is the same as the introductory code given in the three.js documentation.
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
