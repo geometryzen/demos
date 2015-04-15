9617ab0209bc6b16ec34531dd23914c135636e15
David Geo Holmes <geometryzen@gmail.com>
2015-04-14 22:37:39 -0400

    Save file.

--- JSXGraph/curve.ts
+++ JSXGraph/curve.ts
@@ -6,7 +6,7 @@ var popUp: Window = open("", "", "width=800, height=600");
 //popUp.document.body.style.backgroundColor = "202020";
 popUp.document.body.style.overflow = "hidden";
 popUp.document.title = "JXG.Curve";
-//popUp.document.documentElement.innerHTML = '<div id="box" class="jxgbox"></div>'
+popUp.document.documentElement.innerHTML = '<div id="box" class="jxgbox"></div>'
 var div = popUp.document.getElementById("box")
 
 div.style.width  = "400px"
