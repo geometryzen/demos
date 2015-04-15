79152fe5896aeb0d407fc41ba84a5debd45b9431
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 13:08:58 -0400

    Save file.

--- JSXGraph/grid.ts
+++ JSXGraph/grid.ts
@@ -1,1 +1,35 @@
-# grid.ts
+var graph = JXG.JSXGraph;
+
+var popUp: Window = open("", "", "width=800, height=600");
+
+var css = '<link rel="stylesheet" type="text/css" href="http://jsxgraph.uni-bayreuth.de/distrib/jsxgraph.css" />';
+popUp.document.documentElement.innerHTML = css+'<div id="box" class="jxgbox" style="width:800px; height:600px;"></div>'
+popUp.document.title = "JXG.Arrow";
+popUp.document.body.style.backgroundColor = "CCCCCC";
+popUp.document.body.style.overflow = "hidden";
+var div = popUp.document.getElementById("box");
+
+div.style.width  = "760px";
+div.style.height = "560px";
+
+var b = graph.initBoard("box", {axis:true, grid:true, keepaspectratio: true, showCopyright:false, document: popUp.document});
+
+function tick(time: number) {
+    // We can use the variables to drive other windows!
+}
+
+function terminate(time: number) {
+  return false;
+}
+
+function setUp() {
+  
+}
+
+function tearDown() {
+  popUp.close();
+}
+
+// May need to make sure that JSXGraph and animationRunner play well together?
+var war = eight.animationRunner(tick, terminate, setUp, tearDown, popUp.window);
+war.start();
