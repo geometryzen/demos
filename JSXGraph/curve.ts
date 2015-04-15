63dbab4923c9023a6e2f7ec8552a31208a5376d1
David Geo Holmes <geometryzen@gmail.com>
2015-04-14 20:17:39 -0400

    Save file.

--- JSXGraph/curve.ts
+++ JSXGraph/curve.ts
@@ -7,9 +7,9 @@ var div = document.getElementById("box")
 div.style.width  = "400px"
 div.style.height = "400px"
 
-var board = graph.initBoard("box", {"axis":true,"grid":true,showCopyright:false})
+var board = graph.initBoard("box", {axis:true,grid:true, showCopyright:false})
 
-var A = board.create('point',[1,1],{"name": 'Alice'})
-var B = board.create('point',[2,2],{"name":'Bob'})
+var A = board.create('point',[1,1],{name: 'Alice'})
+var B = board.create('point',[2,2],{name:'Bob'})
 
 var f = board.create('functiongraph',[function(x) {return A.X() * sin(x)}])
