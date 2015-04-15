858ed3ae8d8d129f0461982cdee70b4bbd5aaae2
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 09:18:09 -0400

    Save file.

--- JSXGraph/arrow.ts
+++ JSXGraph/arrow.ts
@@ -1,11 +1,12 @@
 var graph = JXG.JSXGraph;
 
+// Experiment with an alternative 'type-safe' API.
 class Board {
     constructor(public board: JXG.Board) {
       
     }
     point(x: number, y: number): JXG.Point {
-        return this.board.create("point",[x, y]);
+        return this.board.create("point", [x, y]);
     }
     arrow(p1: JXG.Point, p2: JXG.Point): JXG.Arrow {
         return this.board.create("arrow",[p1,p2]);
@@ -24,7 +25,7 @@ var div = popUp.document.getElementById("box");
 div.style.width  = "760px";
 div.style.height = "560px";
 
-var b = graph.initBoard("box", {axis:false, grid:false, keepaspectratio: true, showCopyright:false, document: popUp.document});
+var b = graph.initBoard("box", {axis:true, grid:true, keepaspectratio: true, showCopyright:false, document: popUp.document});
 var board = new Board(b);
 
 // Create an arrow providing two points.
