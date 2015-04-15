fae9a883342e9dffe6f15181ffd8fdf2e2a96cf2
David Geo Holmes <geometryzen@gmail.com>
2015-04-14 22:50:13 -0400

    Save file.

--- JSXGraph/line.ts
+++ JSXGraph/line.ts
@@ -23,3 +23,4 @@ var li2 = board.create('line', [q1,q2], {lastArrow:true});
 
 var a1 = board.create('angle', [li1, li2, [5.5, 0], [4, 3]], { radius:1 });
 var a2 = board.create('angle', [li1, li2, 1, -1], { radius:2 });
+
