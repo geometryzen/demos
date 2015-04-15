60ad6389e54a7cc0a63a7377afc4a892c6a3e933
David Geo Holmes <geometryzen@gmail.com>
2015-04-14 22:29:33 -0400

    Save file.

--- JSXGraph/curve.ts
+++ JSXGraph/curve.ts
@@ -12,8 +12,4 @@ var board = graph.initBoard("box", {axis:true,grid:true, showCopyright:false})
 // Parametric curve
 // Create a curve of the form (t-sin(t), 1-cos(t), i.e.
 // the cycloid curve.
-  var curve = board.create('curve',
-                       [function(t){ return t-Math.sin(t);},
-                        function(t){ return 1-Math.cos(t);},
-                        0, 2*Math.PI]
-                    );
+var curve = board.create('curve', [function(t){ return t-Math.sin(t);}, function(t){ return 1-Math.cos(t);}, 0, 2*Math.PI]);
