9e19b75565152b04279214a39774784d4d59d485
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 09:50:34 -0400

    Save file.

--- JSXGraph/arrow.ts
+++ JSXGraph/arrow.ts
@@ -32,6 +32,7 @@ var board = new Board(b);
 var tail = board.point(4.5, 2.0);
 var head = board.point(1.0, 1.0);
 var arrow = board.arrow(tail, head, {dash:3});
+tail.hideElement();
 
 function tick(time: number) {
     // We can use the variables to drive other windows!
