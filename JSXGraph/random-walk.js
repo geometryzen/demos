6c4fc53a4bb51bf9605dc0c98fe3bd838228d018
David Geo Holmes <geometryzen@gmail.com>
2015-04-14 20:59:31 -0400

    Save file.

--- JSXGraph/random-walk.js
+++ JSXGraph/random-walk.js
@@ -3,27 +3,27 @@ var graph = JXG.JSXGraph;
 var board = graph.initBoard("box", 
                         {"boundingbox":[-100,100,100,-100],
                          "showCopyright":false,
-                         "showNavigation":false})
-var t = board.create('turtle')
+                         "showNavigation":false});
+var t = board.create('turtle');
  
 function run() {
     var sumdist=0.0;
     var stepSize = 5;
-    board.suspendUpdate()
-    var walkCount = 20
+    board.suspendUpdate();
+    var walkCount = 20;
     for (var i=0; i < walkCount; i++) {
         t.setPenColor(
                   JXG.hsv2rgb(
                               Math.round(Math.random()*255),
                               Math.random(),
-                              Math.random()))
+                              Math.random()));
         for (var j=0; j < 100; j++) {
-            var angle = Math.floor(360*Math.random()) 
-            t.right(angle) 
-            t.forward(stepSize)
+            var angle = Math.floor(360*Math.random());
+            t.right(angle);
+            t.forward(stepSize);
         }
-        var dist = t.pos[0]*t.pos[0]+t.pos[1]*t.pos[1]
-        sumdist += dist
+        var dist = t.pos[0]*t.pos[0]+t.pos[1]*t.pos[1];
+        sumdist += dist;
         t.home()
     }
     Sk.output(sumdist/walkCount+"\n");
