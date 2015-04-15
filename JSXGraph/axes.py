884965a6d06eb64f4ba3f1f1b5a8fc8fb52db8df
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 08:25:35 -0400

    Save file.

--- JSXGraph/axes.py
+++ JSXGraph/axes.py
@@ -2,6 +2,6 @@ from browser import window
 
 graph = window.JXG.JSXGraph
 
-board = graph.initBoard("box",{"grid":True,"boundingbox":[-10,10,10,-10]})
+board = graph.initBoard("box",{"grid":True,"boundingbox":[-2,2,2,-2]})
 
 board.create('axis', [[0.0,1.0], [1.0,1.3]], {"withLabel": True, "name": "x-axis", "label": {"offset": [280,130]}})
