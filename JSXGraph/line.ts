3a18a13efb10d6e9f44e5ab52368b1878c6a2cb6
David Geo Holmes <geometryzen@gmail.com>
2015-04-14 22:42:20 -0400

    Save file.

--- JSXGraph/line.ts
+++ JSXGraph/line.ts
@@ -13,8 +13,13 @@ div.style.width  = "400px"
 div.style.height = "400px"
 
 var board = graph.initBoard("box", {axis:true, grid:true, showCopyright:false, document:popUp.document})
+var p1 = board.create('point', [-1, 4]);
+var p2 = board.create('point', [4, 1]);
+var q1 = board.create('point', [-2, -3]);
+var q2 = board.create('point', [4,3]);
 
-// Parametric curve
-// Create a curve of the form (t-sin(t), 1-cos(t), i.e.
-// the cycloid curve.
-var curve = board.create('curve', [function(t){ return t-Math.sin(t);}, function(t){ return 1-Math.cos(t);}, 0, 2*Math.PI]);
+var li1 = board.create('line', [p1,p2], {strokeColor:'black', lastArrow:true});
+var li2 = board.create('line', [q1,q2], {lastArrow:true});
+
+var a1 = board.create('angle', [li1, li2, [5.5, 0], [4, 3]], { radius:1 });
+var a2 = board.create('angle', [li1, li2, 1, -1], { radius:2 });
