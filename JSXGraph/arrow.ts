e3a1d561ec77f7ca4615963332e8ef207947aa81
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 08:46:24 -0400

    Save file.

--- JSXGraph/arrow.ts
+++ JSXGraph/arrow.ts
@@ -1,1 +1,37 @@
-# arrow.ts
+var graph = JXG.JSXGraph;
+
+var popUp: Window = open("", "", "width=800, height=600");
+
+var css = '<link rel="stylesheet" type="text/css" href="http://jsxgraph.uni-bayreuth.de/distrib/jsxgraph.css" />';
+popUp.document.documentElement.innerHTML = css+'<div id="box" class="jxgbox" style="width:800px; height:600px;"></div>'
+popUp.document.title = "JXG.Axis";
+popUp.document.body.style.backgroundColor = "CCCCCC";
+popUp.document.body.style.overflow = "hidden";
+var div = popUp.document.getElementById("box");
+
+div.style.width  = "760px";
+div.style.height = "560px";
+
+var board = graph.initBoard("box", {axis:false, grid:false, keepaspectratio: true, showCopyright:false, document: popUp.document});
+
+var axis = board.create('axis', [[0.0,1.0], [1.0,1.0]], {"withLabel": true, "name": "x-axis", "label": {"offset": [280,130]}})
+
+function tick(time: number) {
+    // We can use the variables to drive other windows!
+}
+
+function terminate(time: number) {
+  return false;
+}
+
+function setUp() {
+  
+}
+
+function tearDown() {
+  popUp.close();
+}
+
+// May need to make sure that JSXGraph and animationRunner play well together?
+var war = eight.animationRunner(tick, terminate, setUp, tearDown, popUp.window);
+war.start();
