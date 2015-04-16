5954469f0093cd2376c31661cca460fe2fd9732a
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 22:05:37 -0400

    Save file.

--- JSXGraph/point.ts
+++ JSXGraph/point.ts
@@ -8,7 +8,7 @@ var popUp: Window = open("", "", "width=800, height=600");
 
 var css = '<link rel="stylesheet" type="text/css" href="http://jsxgraph.uni-bayreuth.de/distrib/jsxgraph.css" />';
 popUp.document.documentElement.innerHTML = css+'<div id="box" class="jxgbox" style="width:800px; height:600px;"></div>'
-popUp.document.title = "JXG.Ellipse";
+popUp.document.title = "JXG.Point";
 popUp.document.body.style.backgroundColor = "CCCCCC";
 popUp.document.body.style.overflow = "hidden";
 var div = popUp.document.getElementById("box");
