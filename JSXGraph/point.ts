fddb8d0dc32b24af6a9baf1001489dbfc9a64577
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 22:04:03 -0400

    Save file.

--- JSXGraph/point.ts
+++ JSXGraph/point.ts
@@ -25,7 +25,7 @@ var board = graph.initBoard("box", {axis:true,
 
 var E: JXG.Point = board.create('point', [0,0]);
 
-E.strokeColor = "rgb(0,0,255)"
+E.strokeColor = "0x00FF00"
 
 E.setName("m");
 
