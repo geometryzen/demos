122d4c295aca2e288037bf510eb7b20d28ac49bf
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 21:40:49 -0400

    Save file.

--- JSXGraph/point.ts
+++ JSXGraph/point.ts
@@ -16,7 +16,7 @@ var board = graph.initBoard("box", {axis:true, grid:true, keepaspectratio: true,
 
 var point: JXG.Point = board.create('point', [0,0]);
 
-point.getName();
+Sk.output(point.getName()+"\n");
 
 function tick(time: number) {
     // We can use the variables to drive other windows!
