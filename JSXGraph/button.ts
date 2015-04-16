d8df579f3cb2ddcb0e5d8dee6357166d099ba2e2
David Geo Holmes <geometryzen@gmail.com>
2015-04-15 20:50:08 -0400

    Save file.

--- JSXGraph/button.ts
+++ JSXGraph/button.ts
@@ -4,7 +4,7 @@ var popUp: Window = open("", "", "width=800, height=600");
 
 var css = '<link rel="stylesheet" type="text/css" href="http://jsxgraph.uni-bayreuth.de/distrib/jsxgraph.css" />';
 popUp.document.documentElement.innerHTML = css+'<div id="box" class="jxgbox" style="width:800px; height:600px;"></div>'
-popUp.document.title = "JXG.Slider";
+popUp.document.title = "JXG.Button";
 popUp.document.body.style.backgroundColor = "CCCCCC";
 popUp.document.body.style.overflow = "hidden";
 var div = popUp.document.getElementById("box");
