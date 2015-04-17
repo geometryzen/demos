4087863234c0aa93e5fa71beaa80d162b2e7939f
David Geo Holmes <geometryzen@gmail.com>
2015-04-17 18:39:17 -0400

    Save file.

--- GA/operators.ts
+++ GA/operators.ts
@@ -12,7 +12,7 @@ var random = Math.random;
 var meter = blade.units.meter;
 
 var a = (random() * e1 + random() * e2 + random() * e3);
-var b = (random() * e1 + random() * e2 + random() * e3) * meter
+var b = (random() * e1 + random() * e2 + random() * e3);
 var c = (random() * e1 + random() * e2 + random() * e3) * meter
 
 log(a * b);
