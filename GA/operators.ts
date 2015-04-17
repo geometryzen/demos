9c139c47dc61aa3f7f8ae3c83ae347a035b6a0fd
David Geo Holmes <geometryzen@gmail.com>
2015-04-17 18:31:45 -0400

    Save file.

--- GA/operators.ts
+++ GA/operators.ts
@@ -1,1 +1,16 @@
-# operators.ts
+
+var e1 = eight.vectorE3(1,0,0);
+e2 = VectorE3(0,1,0)
+e3 = VectorE3(0,0,1)
+
+a = (random() * e1 + random() * e2 + random() * e3) * meter
+b = (random() * e1 + random() * e2 + random() * e3) * meter
+c = (random() * e1 + random() * e2 + random() * e3) * meter
+
+print a * b
+print a | b + a ^ b
+print a << b + a ^ b
+print a >> b + a ^ b
+print
+print a * b | c
+print a * (b | c)
