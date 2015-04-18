088135a92b8b9006acd28e1a515ac05ccb25e540
David Geo Holmes <geometryzen@gmail.com>
2015-04-17 22:47:36 -0400

    Save file.

--- Eight/visual.ts
+++ Eight/visual.ts
@@ -59,7 +59,7 @@ var box1 = new visual.Box({height:0.02, color:0x00FF00});
 box1.pos = -2 * e2 / 5;
 viz.scene.add(box1);
 
-var arrow = new visual.Arrow({scale: 2.0, color: 0xFFFF00});
+var arrow = new visual.Arrow({scale: 0.4, color: 0xFFFF00});
 viz.scene.add(arrow);
 
 var box2 = new visual.Box({width:1, height:2, depth:3, color:0xFF0000, opacity:0.25});
