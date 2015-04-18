3ed84eb184beee42631102245ac75e691e824538
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 17:45:20 -0400

    Save file.

--- GA/e2ga.ts
+++ GA/e2ga.ts
@@ -1,2 +1,11 @@
 var e1 = new blade.Euclidean2(0,1,0,0);
 var e2 = new blade.Euclidean2(0,0,1,0);
+
+var log = function(s: any) {
+  var w: any = window;
+  w.Sk.output(s+'\n');
+};
+
+function show(label: string, x) {
+  log(label + ": " + x);
+}
