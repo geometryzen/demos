670261acac55a1d9651c61a8e2916ff7cdf06b0a
David Geo Holmes <geometryzen@gmail.com>
2015-04-17 22:38:31 -0400

    Save file.

--- Eight/visual.ts
+++ Eight/visual.ts
@@ -69,7 +69,7 @@ box2.position.set(3,-3,3);
 var vortex = new visual.Vortex();
 viz.scene.add(vortex)
 
-var box3 = new visual.Box({depth:0.1, color:0x0000FF, opacity:0.25, transparent:true});
+var box3 = new visual.Box({width:10, height:10, depth:0.1, color:0x0000FF, opacity:0.25, transparent:true});
 viz.scene.add(box3);
 
 var sphere = new visual.Sphere({radius:0.4});
