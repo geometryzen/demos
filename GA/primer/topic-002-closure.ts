58253703e531149c61e916af1bf0de838074dd37
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 10:51:53 -0400

    Save file.

--- GA/primer/topic-002-closure.ts
+++ GA/primer/topic-002-closure.ts
@@ -17,7 +17,7 @@ var div = popUp.document.getElementById("box");
 div.style.width  = "760px";
 div.style.height = "560px";
 
-var board = graph.initBoard("box", {axis:false, grid:false, keepaspectratio: true, showCopyright:false, showNavigation:false, document: popUp.document});
+var board = graph.initBoard("box", {axis:false, grid:false, keepaspectratio: true, showCopyright:false, showNavigation:true, document: popUp.document});
 
 // Create an arrow providing two points.
 var B = board.create('point', [0,0], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
