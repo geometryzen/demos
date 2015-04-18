7cbefa3fdf202d84361f910a9efcf5d01bcc8b03
David Geo Holmes <geometryzen@gmail.com>
2015-04-17 21:29:23 -0400

    Save file.

--- GA/operators.ts
+++ GA/operators.ts
@@ -6,7 +6,7 @@ var log = function(s: any) {
 };
 
 // We should support shortcut construction in the BLADE library.
-var e1 = blade.vectorE3(0,1,0,0,0,0,0,0);
+var e1 = blade.vectorE3(1,0,0);
 var e2 = new blade.Euclidean3(0,0,1,0,0,0,0,0);
 var e3 = new blade.Euclidean3(0,0,0,1,0,0,0,0);
 
