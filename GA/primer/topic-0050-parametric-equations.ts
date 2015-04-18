da4fe47546ad96545a108a4cb4e7256abb153055
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 15:44:51 -0400

    Save file.

--- GA/primer/topic-0050-parametric-equations.ts
+++ GA/primer/topic-0050-parametric-equations.ts
@@ -1,13 +1,3 @@
-// In the previous commutative rule demonstration, we did not really show that a+b=c+a.
-// We now use the JXG visual elements to update a model (the underlying mathematical structures)
-// which is then used to update the view.
-var log = function(s: any) {
-  var w: any = window;
-  w.Sk.output(s+'\n');
-};
-
-var graph = JXG.JSXGraph;
-
 var popUp: Window = open("", "", "width=800, height=600");
 
 var css = '<link rel="stylesheet" type="text/css" href="http://jsxgraph.uni-bayreuth.de/distrib/jsxgraph.css" />';
@@ -34,17 +24,14 @@ var b = e2;
 var c = 2 * e1 + e2;
 
 // View
-// There will be two free points that act as input controlling the vectors a and b.
-var board = graph.initBoard("box", {boundingbox:[-2, 6, 14, -3], axis:true, grid:true, keepaspectratio: true, showCopyright:false, showNavigation:true, document: popUp.document});
+var board = JXG.JSXGraph.initBoard("box", {boundingbox:[-2, 6, 14, -3], axis:true, grid:true, keepaspectratio: true, showCopyright:false, showNavigation:true, document: popUp.document});
 
 var alpha = board.create('slider', [[10, -1], [12, -1], [0, 1, 4]]);
 alpha.setAttribute({strokeColor: colorA});
 var beta = board.create('slider', [[10, -2], [12, -2], [0, 1, 4]]);
 beta.setAttribute({strokeColor: colorB});
 
-// TODO: These functions could be generalized to visualize multivectors.
 function createInputArrow(mv: blade.Euclidean3, pos: blade.Euclidean3, color: string, handler: (tail:JXG.Point, head:JXG.Point)=>void) {
-  // This construction could be turned into a function...
   var head = board.create('point', [pos.x + mv.x/2, pos.y + mv.y/2], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
   var tail = board.create('point', [function(){return pos.x - mv.x/2;}, function(){return pos.y-mv.y/2;}], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
   tail.hideElement();
@@ -53,7 +40,6 @@ function createInputArrow(mv: blade.Euclidean3, pos: blade.Euclidean3, color: st
 }
 
 function createOutputArrow(mv: ()=>blade.Euclidean3, pos: ()=>blade.Euclidean3, color: string) {
-
     var head = board.create('point', [function(){return mv().x+pos().x;},function(){return mv().y+pos().y;}], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
     var tail = board.create('point', [function(){return pos().x;},function(){return pos().y;}], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
     head.hideElement();
