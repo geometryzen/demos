d35bd9ba7d9ec83fdab457d87500c1c113ecee47
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 11:07:02 -0400

    Save file.

--- GA/primer/topic-0030-commutative-rule.ts
+++ GA/primer/topic-0030-commutative-rule.ts
@@ -1,1 +1,50 @@
-# topic-0030-commutative-rule.ts
+var log = function(s: any) {
+  var w: any = window;
+  w.Sk.output(s+'\n');
+};
+
+var graph = JXG.JSXGraph;
+
+var popUp: Window = open("", "", "width=800, height=600");
+
+var css = '<link rel="stylesheet" type="text/css" href="http://jsxgraph.uni-bayreuth.de/distrib/jsxgraph.css" />';
+popUp.document.documentElement.innerHTML = css+'<div id="box" class="jxgbox" style="width:800px; height:600px;"></div>'
+popUp.document.title = "Closure";
+popUp.document.body.style.backgroundColor = "CCCCCC";
+popUp.document.body.style.overflow = "hidden";
+var div = popUp.document.getElementById("box");
+
+div.style.width  = "760px";
+div.style.height = "560px";
+
+var board = graph.initBoard("box", {boundingbox:[-1,2,3,-1], axis:false, grid:false, keepaspectratio: true, showCopyright:false, showNavigation:true, document: popUp.document});
+
+// Create an arrow providing two points.
+var B = board.create('point', [0,0], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
+var C = board.create('point', [1,0], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
+var A = board.create('point', [2,1], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
+
+var a = board.create('arrow', [B, C]);
+a.setAttribute({strokeColor: '#FF0000'});
+var b = board.create('arrow', [C, A]);
+b.setAttribute({strokeColor: '#0000FF'});
+var c = board.create('arrow', [B, A]);
+c.setAttribute({strokeColor: '#00FF00'});
+board.create('text',[function(){return (B.X()+C.X())/2},function(){return (B.Y()+C.Y())/2},"A"]);
+board.create('text',[function(){return (C.X()+A.X())/2},function(){return (C.Y()+A.Y())/2},"B"]);
+board.create('text',[function(){return (B.X()+A.X())/2},function(){return (B.Y()+A.Y())/2},"C=A+B"]);
+
+var omega = 2 * Math.PI * 1/10;
+
+function tick(time: number) {
+  board.update();
+}
+
+function terminate(time: number) {return false;}
+
+function setUp() {}
+
+function tearDown() {popUp.close();}
+
+var runner = eight.animationRunner(tick, terminate, setUp, tearDown, popUp.window);
+runner.start();
