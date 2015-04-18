2fe09cc5ae6ec1c3771ea522b8ce1b850b5e33c6
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 10:49:39 -0400

    Save file.

--- GA/primer/topic-002-closure.ts
+++ GA/primer/topic-002-closure.ts
@@ -32,7 +32,8 @@ b.setAttribute({strokeColor: '#0000FF', withLabel:true});
 b.setLabelText("B");
 var c = board.create('arrow', [B, A]);
 c.setAttribute({strokeColor: '#00FF00'});
-var cLabel = board.create('text',[function(){return (B.X()+A.X())/2},function(){return (B.Y()+A.Y())/2},"C=A+B"]);
+board.create('text',[function(){return (B.X()+C.X())/2},function(){return (B.Y()+C.Y())/2},"A"]);
+board.create('text',[function(){return (B.X()+A.X())/2},function(){return (B.Y()+A.Y())/2},"C=A+B"]);
 
 var omega = 2 * Math.PI * 1/10;
 
