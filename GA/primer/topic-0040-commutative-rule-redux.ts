76e48ff7918544a78a08d6fdecc8eaf47eef615e
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 12:52:19 -0400

    Save file.

--- GA/primer/topic-0040-commutative-rule-redux.ts
+++ GA/primer/topic-0040-commutative-rule-redux.ts
@@ -40,13 +40,13 @@ function createInputArrow(initial: blade.Euclidean3, color: string): JXG.Point {
 }
 
 var A = createInputArrow(a, '#FF0000');
-var B = createInputArrow(b, '#0000FF');
+var B = createInputArrow(b, '#00FF00');
 
 var CHead = board.create('point', [function(){return (a+b).x/2;},function(){return (a+b).y/2;}], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
 var CTail = board.create('point', [function(){return -CHead.X();},function(){return -CHead.Y();}], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
 
-//var a2 = board.create('arrow', [H, A]);
-//a2.setAttribute({strokeColor: '#0000FF'});
+var C = board.create('arrow', [CTail, CHead]);
+C.setAttribute({strokeColor: '#0000FF'});
 
 function tick(time: number) {
   a.x = A.X() * 2;
