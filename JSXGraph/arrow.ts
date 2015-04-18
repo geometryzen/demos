515fb713ca8febe7b5c9d5d22fc8309291d023a5
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 09:35:03 -0400

    Save file.

--- JSXGraph/arrow.ts
+++ JSXGraph/arrow.ts
@@ -23,7 +23,7 @@ var board = graph.initBoard("box", {axis:false, grid:false, keepaspectratio: tru
 
 // Create an arrow providing two points.
 var tail = board.create('point', [0.0, 0.0], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0, highlightStrokeColor:'#00FF00'});
-var head = board.create('point', [1.0, 0.0], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0});
+var head = board.create('point', [1.0, 0.0], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0, highlightStrokeColor:'#00FF00'});
 var arrow = board.create('arrow', [tail, head]);
 //tail.hideElement();
 //head.hideElement();
