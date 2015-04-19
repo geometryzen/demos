da6a26311d6124a0c119403a83222553821c5fc4
David Geo Holmes <geometryzen@gmail.com>
2015-04-19 16:33:48 -0400

    Save file.

--- gamingjs/basic.ts
+++ gamingjs/basic.ts
@@ -1,22 +1,17 @@
-  // This is where stuff in our game will happen:
-  var scene = new THREE.Scene();
+var scene = new THREE.Scene();
 
-  // This is what sees the stuff:
-  var aspect_ratio = window.innerWidth / window.innerHeight;
-  var camera = new THREE.PerspectiveCamera(75, aspect_ratio, 1, 10000);
-  camera.position.z = 500;
-  scene.add(camera);
+var aspect_ratio = window.innerWidth / window.innerHeight;
+var camera = new THREE.PerspectiveCamera(75, aspect_ratio, 1, 10000);
+camera.position.z = 500;
+scene.add(camera);
 
-  // This will draw what the camera sees onto the screen:
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-  // ******** START CODING ON THE NEXT LINE ********
-  var shape = new THREE.SphereGeometry(100);
-  var cover = new THREE.MeshNormalMaterial();
-  var ball = new THREE.Mesh(shape, cover);
-  scene.add(ball);
+var shape = new THREE.SphereGeometry(100);
+var cover = new THREE.MeshNormalMaterial();
+var ball = new THREE.Mesh(shape, cover);
+scene.add(ball);
 
-  // Now, show what the camera sees on the screen:
-  renderer.render(scene, camera);
+renderer.render(scene, camera);
