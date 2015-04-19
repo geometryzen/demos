04be93c6d55fe71c57517db548f28b9fc00a835a
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 20:26:59 -0400

    Save file.

--- GA/e2ga.ts
+++ GA/e2ga.ts
@@ -1,3 +1,4 @@
+var one = new blade.Euclidean2(1,0,0,0);
 var e1 = new blade.Euclidean2(0,1,0,0);
 var e2 = new blade.Euclidean2(0,0,1,0);
 var I = e1 * e2
@@ -31,4 +32,8 @@ show('e1 / 2', e1 / 2);
 
 show('e1 ^ e2', e1 ^ e2);
 
-show('e1 << I', e1 << I);
+show('e1 << 1',   e1 << 1);
+show('e1 << one', e1 << one);
+show('e1 << e1',  e1 << e1);
+show('e1 << e2',  e1 << e2);
+show('e1 << I',   e1 << I);
