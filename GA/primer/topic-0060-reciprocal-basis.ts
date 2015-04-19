1141dd38b79c31633512b859616e98213056b24e
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 21:05:38 -0400

    Save file.

--- GA/primer/topic-0060-reciprocal-basis.ts
+++ GA/primer/topic-0060-reciprocal-basis.ts
@@ -10,17 +10,16 @@ var div = popUp.document.getElementById("box");
 div.style.width  = "760px";
 div.style.height = "560px";
 
-var colorA = '#0000FF';
-var colorB = '#FF0000';
-var colorC = '#00FF00';
-var colorX = '#000000';
+var colorA  = '#0000FF';
+var colorB  = '#FF0000';
+var colorAR = '#FFFF00';
+var colorBR = '#00FFFF';
 
 var o  = new blade.Euclidean2(0,0,0,0);
 var e1 = new blade.Euclidean2(0,1,0,0);
 var e2 = new blade.Euclidean2(0,0,1,0);
 var a = 2 * e1;
 var b = e2;
-var c = 2 * e1 + e2;
 
 // View
 var board = JXG.JSXGraph.initBoard("box", {boundingbox:[-2, 6, 14, -3], axis:true, grid:true, keepaspectratio: true, showCopyright:false, showNavigation:true, document: popUp.document});
@@ -43,8 +42,7 @@ function createOutputArrow(mv: ()=>blade.Euclidean2, pos: ()=>blade.Euclidean2, 
 
 createInputArrow(a, 2 * e1 - 2 * e2, colorA, function(tail, head) {a.x=head.X()-tail.X();a.y=head.Y()-tail.Y()});
 createInputArrow(b, 5 * e1 - 2 * e2, colorB, function(tail, head) {b.x=head.X()-tail.X();b.y=head.Y()-tail.Y()});
-createInputArrow(c, 8 * e1 - 2 * e2, colorC, function(tail, head) {c.x=head.X()-tail.X();c.y=head.Y()-tail.Y()});
 
-createOutputArrow(function(){return a;}, function(){return c;}, colorA);
-createOutputArrow(function(){return b;}, function(){return c;}, colorB);
-createOutputArrow(function(){return c;}, function(){return o;}, colorC);
+createOutputArrow(function(){return a;}, function(){return o;}, colorA);
+createOutputArrow(function(){return b;}, function(){return o;}, colorB);
+createOutputArrow(function(){return a;}, function(){return o;}, colorAR);
