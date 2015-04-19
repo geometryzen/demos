59515370c5f063ae9156004b71a5aaf3acec3602
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 21:02:36 -0400

    Save file.

--- GA/primer/topic-0060-reciprocal-basis.ts
+++ GA/primer/topic-0060-reciprocal-basis.ts
@@ -25,11 +25,6 @@ var c = 2 * e1 + e2;
 // View
 var board = JXG.JSXGraph.initBoard("box", {boundingbox:[-2, 6, 14, -3], axis:true, grid:true, keepaspectratio: true, showCopyright:false, showNavigation:true, document: popUp.document});
 
-var alpha = board.create('slider', [[10, -1], [12, -1], [0, 1, 4]]);
-alpha.setAttribute({strokeColor: colorA});
-var beta = board.create('slider', [[10, -2], [12, -2], [0, 1, 4]]);
-beta.setAttribute({strokeColor: colorB});
-
 function createInputArrow(mv: blade.Euclidean2, pos: blade.Euclidean2, color: string, handler: (tail:JXG.Point, head:JXG.Point)=>void) {
   var head = board.create('point', [pos.x + mv.x/2, pos.y + mv.y/2], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
   var tail = board.create('point', [function(){return pos.x - mv.x/2;}, function(){return pos.y-mv.y/2;}], {withLabel:false, strokeColor:'#CCCCCC', fillOpacity: 0, highlightFillOpacity: 0});
@@ -53,12 +48,3 @@ createInputArrow(c, 8 * e1 - 2 * e2, colorC, function(tail, head) {c.x=head.X()-
 createOutputArrow(function(){return a;}, function(){return c;}, colorA);
 createOutputArrow(function(){return b;}, function(){return c;}, colorB);
 createOutputArrow(function(){return c;}, function(){return o;}, colorC);
-createOutputArrow(
-  function(){
-      return c + alpha.Value() * a + beta.Value() * b;
-  },
-  function(){
-    return o;
-  },
-  colorX
-);
