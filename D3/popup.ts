84651a25ba95fd22dc314133a060dfd821ea73fb
David Geo Holmes <geometryzen@gmail.com>
2015-04-19 04:24:20 -0400

    Save file.

--- D3/popup.ts
+++ D3/popup.ts
@@ -63,7 +63,7 @@ function tick(t) {
         return circle
     }
     // The following is correct. Maybe an error in the TypeLibrary definitions?
-    d3.select({}).transition().duration(2000).ease(Math.sqrt).tween("circle", tweeny)
+    d3.select(popUp.document.body).transition().duration(2000).ease(Math.sqrt).tween("circle", tweeny)
 }
 
 function terminate(t) {
