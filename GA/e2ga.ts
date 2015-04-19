2f7f3c5b1bcd234a37427ee718d15b6196ddaf64
David Geo Holmes <geometryzen@gmail.com>
2015-04-18 20:04:53 -0400

    Save file.

--- GA/e2ga.ts
+++ GA/e2ga.ts
@@ -11,9 +11,15 @@ function show(label: string, x) {
 }
 
 show('e1', e1);
+
 show('e1 + e2', e1 + e2);
 show('1 + e2', 1 + e2);
 show('e1 + 1', e1 + 1);
+
 show('e1 - e2', e1 - e2);
 show('1 - e2', 1 - e2);
 show('e1 - 1', e1 - 1);
+
+show('e1 * e2', e1 * e2);
+show('2 * e2', 2 * e2);
+show('e1 * 2', e1 * 2);
