814a9b559ab6e8d262c512145c561260dcc1a21f
David Geo Holmes <geometryzen@gmail.com>
2015-04-19 16:29:45 -0400

    Save file.

--- gamingjs/basic.ts
+++ gamingjs/basic.ts
@@ -1,1 +1,22 @@
-# gamingjs/basic.ts
+  // This is where stuff in our game will happen:
+  var scene = new THREE.Scene();
+
+  // This is what sees the stuff:
+  var aspect_ratio = window.innerWidth / window.innerHeight;
+  var camera = new THREE.PerspectiveCamera(75, aspect_ratio, 1, 10000);
+  camera.position.z = 500;
+  scene.add(camera);
+
+  // This will draw what the camera sees onto the screen:
+  var renderer = new THREE.CanvasRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // ******** START CODING ON THE NEXT LINE ********
+  var shape = new THREE.SphereGeometry(100);
+  var cover = new THREE.MeshNormalMaterial();
+  var ball = new THREE.Mesh(shape, cover);
+  scene.add(ball);
+
+  // Now, show what the camera sees on the screen:
+  renderer.render(scene, camera);
